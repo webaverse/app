255e5744f9a3300420a2a68e6a7d4a2f25327776
Shubham Jain <shubham@sjain.dev>
2020-06-16 11:02:28 +0100

    Add local express server

---
+++ index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+app.use((req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
+});
+app.use(express.static(path.join(__dirname, 'src')));
+
+http.createServer(app)
+  .listen(3000);
+
+console.log(`http://127.0.0.1:3000`);
