5595d5ddffb4a6fc01db8a460fef2d1e67453533
Avaer Kazmer <a@modules.io>
2020-06-21 04:48:10 -0400

    Small screenshot bugfixing

--- screenshot-object.js
+++ screenshot-object.js
@@ -1,4 +1,4 @@
-import THREE from './xrpackage/three.module.js';
+import THREE from 'https://static.xrpackage.org/xrpackage/three.module.js';
 import {GLTFExporter} from './GLTFExporter.js';
 import './gif.js';
 import screenshot from './screenshot.js';
