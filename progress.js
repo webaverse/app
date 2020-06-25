d660978f3e44eb4dbb381330b720e2bf651dd274
Avaer Kazmer <a@modules.io>
2020-06-25 14:43:46 -0400

    Beautify progress.js

--- progress.js
+++ progress.js
@@ -1,42 +1,42 @@
 import './nanobar.js';
 
 const nanobar = new Nanobar({
-  target: document.getElementById('progress-bar'),
+    target: document.getElementById('progress-bar'),
 });
 /* nanobar.go(30);
 nanobar.go(76); */
 
 class Progress {
-  constructor() {
-    this.numerator = 0;
-    this.denominator = 1;
-  }
-  setNumerator(n) {
-  	this.numerator = n;
-  	this.update();
-  }
-  setDemoninator(d) {
-  	this.denominator = d;
-  	this.update();
-  }
-  setNumeratorDenominator(n, d) {
-  	this.numerator = n;
-  	this.denominator = d;
-  	this.update();
-  }
-  addNumerator() {
-  	this.setNumerator(this.numerator + 1);
-  }
-  update() {
-  	if (this.numerator < this.denominator) {
-      nanobar.go((this.numerator + 1) / (this.denominator + 1) * 100);
-  	} else {
-  	  nanobar.go(100);
-  	}
-  }
+    constructor() {
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+    setNumerator(n) {
+        this.numerator = n;
+        this.update();
+    }
+    setDemoninator(d) {
+        this.denominator = d;
+        this.update();
+    }
+    setNumeratorDenominator(n, d) {
+        this.numerator = n;
+        this.denominator = d;
+        this.update();
+    }
+    addNumerator() {
+        this.setNumerator(this.numerator + 1);
+    }
+    update() {
+        if (this.numerator < this.denominator) {
+            nanobar.go((this.numerator + 1) / (this.denominator + 1) * 100);
+        } else {
+            nanobar.go(100);
+        }
+    }
 }
 const progress = new Progress();
 
 export {
-  progress,
+    progress,
 };
