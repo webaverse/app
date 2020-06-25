a1d10d4ae7b7f52c1b567bd194709da34ff0b5f8
Avaer Kazmer <a@modules.io>
2020-06-25 12:01:28 -0400

    Major browse page work

--- progress.js
+++ progress.js
@@ -5,3 +5,38 @@ const nanobar = new Nanobar({
 });
 nanobar.go(30);
 nanobar.go(76);
+
+class Progress {
+  constructor() {
+    this.numerator = 0;
+    this.denominator = 1;
+  }
+  setNumerator(n) {
+  	this.numerator = n;
+  	this.update();
+  }
+  setDemoninator(d) {
+  	this.denominator = d;
+  	this.update();
+  }
+  setNumeratorDenominator(n, d) {
+  	this.numerator = n;
+  	this.denominator = d;
+  	this.update();
+  }
+  addNumerator() {
+  	this.setNumerator(this.numerator + 1);
+  }
+  update() {
+  	if (this.numerator < this.denominator) {
+  	  nanobar.go((this.numerator + 1) / (this.denominator + 1));
+  	} else {
+  	  nanobar.go(100);
+  	}
+  }
+}
+const progress = new Progress();
+
+export {
+  progress,
+};
