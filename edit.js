d9d3453498d19354d57f70663116a3cf75e4dee1
Avaer Kazmer <a@modules.io>
2020-06-25 12:36:33 -0400

    Support more query strings on edit page

--- edit.js
+++ edit.js
@@ -1868,11 +1868,45 @@ const _renderObjects = () => {
   }
 };
 _renderObjects();
-const _handleUrl = u => {
+const _handleUrl = async u => {
   const {search} = new URL(u);
   const q = parseQuery(search);
-  const w = q.w || null;
-  _enterWorld(w);
+
+  if (q.p) { // package
+    const metadata = await fetch(packagesEndpoint + '/' + q.p)
+      .then(res => res.json())
+    const {dataHash} = metadata;
+
+    const arrayBuffer = await fetch(`${apiHost}/${dataHash}.wbn`)
+      .then(res => res.arrayBuffer());
+
+    const p = new XRPackage(new Uint8Array(arrayBuffer));
+    await p.waitForLoad();
+    await pe.add(p);
+  } else if (q.i) { // index
+    const metadataHash = await contract.methods.getMetadata(parseInt(q.i, 10), 'hash').call();
+    const metadata = await fetch(`${apiHost}/${metadataHash}`)
+      .then(res => res.json());
+    const {dataHash} = metadata;
+
+    const arrayBuffer = await fetch(`${apiHost}/${dataHash}.wbn`)
+      .then(res => res.arrayBuffer());
+
+    const p = new XRPackage(new Uint8Array(arrayBuffer));
+    await pe.add(p);
+  } else if (q.u) { // url
+    const arrayBuffer = await fetch(q.u)
+      .then(res => res.arrayBuffer());
+
+    const p = new XRPackage(new Uint8Array(arrayBuffer));
+    await pe.add(p);
+  } else if (q.h) { // hash
+    const p = await XRPackage.download(q.h);
+    await pe.add(p);
+  } else {
+    const w = q.w || null;
+    _enterWorld(w);
+  }
 };
 window.addEventListener('popstate', e => {
   _handleUrl(window.location.href);
