a82453db479077a66dc8159ec41394922ff2ac5e
Avaer Kazmer <a@modules.io>
2020-07-04 15:35:17 -0400

    Add objects menu

--- edit.js
+++ edit.js
@@ -162,74 +162,14 @@ const _makeTextMesh = (text, fontSize) => {
 const wristMenu = (() => {
   const object = new THREE.Object3D();
 
-  const _makePackageSide = name => {
+  const size = 1;
+  const packageWidth = size*0.9;
+  const packageHeight = size*0.1;
+  const packageMargin = size*0.2;
+  const sidebarSize = size*0.1;
+
+  const _makeSide = name => {
     const object = new THREE.Object3D();
-
-    const size = 1;
-    const packageWidth = size*0.9;
-    const packageHeight = size*0.1;
-    const packageMargin = size*0.2;
-    const sidebarSize = size*0.1;
-    const _makePackageMesh = pJ => {
-      const {name, dataHash, icons} = pJ;
-      const iconHash = icons && icons.find(i => i.type === 'image/gif').hash;
-
-      const object = new THREE.Object3D();
-      object.position.x = -size/2 + packageWidth/2;
-      object.dataHash = dataHash;
-
-      const backgroundMesh = new THREE.Mesh(
-        new THREE.PlaneBufferGeometry(1, 1),
-        new THREE.MeshBasicMaterial({
-          color: 0xb0bec5,
-          side: THREE.DoubleSide,
-        })
-      );
-      backgroundMesh.scale.set(packageWidth, packageHeight, 0.01);
-      object.add(backgroundMesh);
-      object.backgroundMesh = backgroundMesh;
-
-      /* (async () => {
-        const u = await p.getScreenshotImageUrl();
-        const res = await fetch(u);
-        const ab = await res.arrayBuffer();
-        const uint8Array = new Uint8Array(ab);
-        const gif = parseGIF(uint8Array);
-        const frames = decompressFrames(gif, true);
-      })(); */
-
-      const img = new Image();
-      const texture = new THREE.Texture(img);
-      (async () => {
-        img.crossOrigin = 'Anonymous';
-        img.src = `${apiHost}/${iconHash}.gif`;
-        await new Promise((accept, reject) => {
-          img.onload = () => {
-            texture.needsUpdate = true;
-          };
-          img.onerror = reject;
-        });
-      })();
-
-      const imgMesh = new THREE.Mesh(
-        new THREE.PlaneBufferGeometry(packageHeight, packageHeight),
-        new THREE.MeshBasicMaterial({
-          map: texture,
-          side: THREE.DoubleSide,
-        })
-      );
-      imgMesh.position.x = -packageWidth/2 + packageHeight/2;
-      imgMesh.position.z = 0.001;
-      object.add(imgMesh);
-
-      const textMesh = _makeTextMesh(name, size*0.05);
-      textMesh.position.x = -packageWidth/2 + packageHeight;
-      textMesh.position.y = packageHeight/2;
-      textMesh.position.z = 0.001;
-      object.add(textMesh);
-
-      return object;
-    };
     
     const background = new THREE.Mesh(
       new THREE.PlaneBufferGeometry(size, size),
@@ -304,6 +244,72 @@ const wristMenu = (() => {
       object.add(chevronDown);
       object.chevronDown = chevronDown;
     }
+    
+    return object;
+  };
+  const _makePackageSide = name => {
+    const object = _makeSide(name);
+    
+    const _makePackageMesh = pJ => {
+      const {name, dataHash, icons} = pJ;
+      const iconHash = icons && icons.find(i => i.type === 'image/gif').hash;
+
+      const object = new THREE.Object3D();
+      object.position.x = -size/2 + packageWidth/2;
+      object.dataHash = dataHash;
+
+      const backgroundMesh = new THREE.Mesh(
+        new THREE.PlaneBufferGeometry(1, 1),
+        new THREE.MeshBasicMaterial({
+          color: 0xb0bec5,
+          side: THREE.DoubleSide,
+        })
+      );
+      backgroundMesh.scale.set(packageWidth, packageHeight, 0.01);
+      object.add(backgroundMesh);
+      object.backgroundMesh = backgroundMesh;
+
+      /* (async () => {
+        const u = await p.getScreenshotImageUrl();
+        const res = await fetch(u);
+        const ab = await res.arrayBuffer();
+        const uint8Array = new Uint8Array(ab);
+        const gif = parseGIF(uint8Array);
+        const frames = decompressFrames(gif, true);
+      })(); */
+
+      const img = new Image();
+      const texture = new THREE.Texture(img);
+      (async () => {
+        img.crossOrigin = 'Anonymous';
+        img.src = `${apiHost}/${iconHash}.gif`;
+        await new Promise((accept, reject) => {
+          img.onload = () => {
+            texture.needsUpdate = true;
+          };
+          img.onerror = reject;
+        });
+      })();
+
+      const imgMesh = new THREE.Mesh(
+        new THREE.PlaneBufferGeometry(packageHeight, packageHeight),
+        new THREE.MeshBasicMaterial({
+          map: texture,
+          side: THREE.DoubleSide,
+        })
+      );
+      imgMesh.position.x = -packageWidth/2 + packageHeight/2;
+      imgMesh.position.z = 0.001;
+      object.add(imgMesh);
+
+      const textMesh = _makeTextMesh(name, size*0.05);
+      textMesh.position.x = -packageWidth/2 + packageHeight;
+      textMesh.position.y = packageHeight/2;
+      textMesh.position.z = 0.001;
+      object.add(textMesh);
+
+      return object;
+    };
     
     const packages = new THREE.Object3D();
     packages.position.z = 0.001;
@@ -391,6 +397,156 @@ const wristMenu = (() => {
     };
     return object;
   };
+  const _makeObjectsSide = name => {
+    const object = _makeSide(name);
+    
+    const objects = new THREE.Object3D();
+    objects.position.z = 0.001;
+    object.add(objects);
+    
+    const _makeObjectMesh = oJ => {
+      const {name, dataHash, icons} = oJ;
+      const iconHash = icons && icons.find(i => i.type === 'image/gif').hash;
+
+      const object = new THREE.Object3D();
+      object.position.x = -size/2 + packageWidth/2;
+      object.dataHash = dataHash;
+
+      const backgroundMesh = new THREE.Mesh(
+        new THREE.PlaneBufferGeometry(1, 1),
+        new THREE.MeshBasicMaterial({
+          color: 0xb0bec5,
+          side: THREE.DoubleSide,
+        })
+      );
+      backgroundMesh.scale.set(packageWidth, packageHeight, 0.01);
+      object.add(backgroundMesh);
+      object.backgroundMesh = backgroundMesh;
+
+      /* (async () => {
+        const u = await p.getScreenshotImageUrl();
+        const res = await fetch(u);
+        const ab = await res.arrayBuffer();
+        const uint8Array = new Uint8Array(ab);
+        const gif = parseGIF(uint8Array);
+        const frames = decompressFrames(gif, true);
+      })(); */
+
+      const img = new Image();
+      const texture = new THREE.Texture(img);
+      (async () => {
+        const u = await oJ.getScreenshotImageUrl();
+        img.crossOrigin = 'Anonymous';
+        img.src = u;
+        await new Promise((accept, reject) => {
+          img.onload = () => {
+            texture.needsUpdate = true;
+          };
+          img.onerror = reject;
+        });
+      })();
+
+      const imgMesh = new THREE.Mesh(
+        new THREE.PlaneBufferGeometry(packageHeight, packageHeight),
+        new THREE.MeshBasicMaterial({
+          map: texture,
+          side: THREE.DoubleSide,
+        })
+      );
+      imgMesh.position.x = -packageWidth/2 + packageHeight/2;
+      imgMesh.position.z = 0.001;
+      object.add(imgMesh);
+
+      const textMesh = _makeTextMesh(name, size*0.05);
+      textMesh.position.x = -packageWidth/2 + packageHeight;
+      textMesh.position.y = packageHeight/2;
+      textMesh.position.z = 0.001;
+      object.add(textMesh);
+
+      return object;
+    };
+
+    let currentPage = 0;
+    const objectsPerPage = 8;
+    let os = [];
+    object.setObjects = newOs => {
+      os = newOs;
+      object.renderObjects();
+    };
+    object.goPage = n => {
+      currentPage += n;
+      currentPage = Math.min(Math.max(currentPage, 0), Math.floor(os.length / objectsPerPage));
+      object.renderObjects();
+    };
+    object.renderObjects = () => {
+      objects.children.length = 0;
+      os.slice(currentPage * objectsPerPage, (currentPage + 1) * objectsPerPage).forEach((o, i) => {
+        const packageMesh = _makeObjectMesh(o);
+        packageMesh.offset = i*packageHeight;
+        packageMesh.position.y = size/2 - packageMargin - packageHeight/2 - packageMesh.offset;
+        objects.add(packageMesh);
+      });
+    };
+    object.updateIntersect = () => {
+      if (!highlightMesh.onmousedown) {
+        highlightMesh.visible = false;
+        
+        raycaster.ray.origin.copy(ray.position);
+        raycaster.ray.direction.set(0, 0, -1).applyQuaternion(ray.quaternion);
+        const intersects = raycaster.intersectObjects(
+          [object.chevronUp, object.chevronDown].concat(
+            objects.children.map(p => p.backgroundMesh)
+          )
+        );
+        if (intersects.length > 0) {
+          const [intersect] = intersects;
+          const {object: intersectObject} = intersect;
+          if (intersectObject === object.chevronUp) {
+            highlightMesh.onmousedown = () => {
+              object.goPage(-1);
+            };
+
+            return true;
+          } else if (intersectObject === object.chevronDown) {
+            highlightMesh.onmousedown = () => {
+              object.goPage(1);
+            };
+
+            return true;
+          } else {
+            intersectObject.getWorldPosition(highlightMesh.position);
+            intersectObject.getWorldQuaternion(highlightMesh.quaternion);
+            intersectObject.getWorldScale(highlightMesh.scale);
+            highlightMesh.visible = true;
+
+            const packageMesh = intersectObject.parent;
+            highlightMesh.onmousedown = () => {
+              dragMesh = packageMesh.clone(true);
+              dragMesh.dataHash = packageMesh.dataHash;
+              dragMesh.startMatrix = packageMesh.matrixWorld.clone();
+              dragMesh.startRayMatrix = ray.matrixWorld.clone();
+              scene.add(dragMesh);
+            };
+            highlightMesh.onmouseup = () => {
+              (async () => {
+                const {dataHash, matrix} = dragMesh;
+                const p = await XRPackage.download(dataHash);
+                await _addPackage(p, matrix);
+              })();
+              scene.remove(dragMesh);
+              dragMesh = null;
+            };
+
+            return true;
+          }
+        }
+      }
+      return false;
+    };
+    
+    return object;
+  };
+  
   const packageSide = _makePackageSide('Packages');
   object.add(packageSide);
   object.packageSide = packageSide;
@@ -399,9 +555,14 @@ const wristMenu = (() => {
   inventorySide.position.x = 1;
   object.add(inventorySide);
   object.inventorySide = inventorySide;
+  
+  const objectsSide = _makeObjectsSide('Objects');
+  objectsSide.position.x = 2;
+  object.add(objectsSide);
+  object.objectsSide = objectsSide;
 
   object.update = () => {
-    packageSide.updateIntersect() || inventorySide.updateIntersect();
+    packageSide.updateIntersect() || inventorySide.updateIntersect() || objectsSide.updateIntersect();
   };
   
   return object;
@@ -2461,6 +2622,8 @@ const _renderObjects = () => {
         });
       };
       _renderChildren(objectsEl, pe.children, 0);
+      
+      wristMenu.objectsSide.setObjects(pe.children);
     } else {
       objectsEl.innerHTML = `<h1 class=placeholder>No objects in scene</h1>`;
     }
