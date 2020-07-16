7b39f37666a72419cfa56595a21b2b3568df4ee5
Avaer Kazmer <a@modules.io>
2020-07-16 11:40:58 -0400

    Clean up global constants

--- constants.js
+++ constants.js
@@ -1,29 +1,11 @@
-(() => {
-
-const PARCEL_SIZE = 300;
-const SUBPARCEL_SIZE = 10;
-const SUBPARCEL_SIZE_P1 = SUBPARCEL_SIZE + 1;
-const NUM_PARCELS = PARCEL_SIZE/SUBPARCEL_SIZE;
-const slabTotalSize = 8 * 1024 * 1024;
-const slabNumAttributes = 4;
-const slabAttributeSize = slabTotalSize/slabNumAttributes;
-const numSlices = 42;
-const slabSliceTris = Math.floor(slabAttributeSize/numSlices/9/Float32Array.BYTES_PER_ELEMENT);
-const slabSliceVertices = slabSliceTris * 3;
-const BUILD_SNAP = 2;
-
-globalThis.constants = {
-  PARCEL_SIZE,
-  SUBPARCEL_SIZE,
-  SUBPARCEL_SIZE_P1,
-  NUM_PARCELS,
-  slabTotalSize,
-  slabNumAttributes,
-  slabAttributeSize,
-  numSlices,
-  slabSliceTris,
-  slabSliceVertices,
-  BUILD_SNAP,
-};
-
-})();
+export const PARCEL_SIZE = 300;
+export const SUBPARCEL_SIZE = 10;
+export const SUBPARCEL_SIZE_P1 = SUBPARCEL_SIZE + 1;
+export const NUM_PARCELS = PARCEL_SIZE/SUBPARCEL_SIZE;
+export const slabTotalSize = 8 * 1024 * 1024;
+export const slabNumAttributes = 4;
+export const slabAttributeSize = slabTotalSize/slabNumAttributes;
+export const numSlices = 42;
+export const slabSliceTris = Math.floor(slabAttributeSize/numSlices/9/Float32Array.BYTES_PER_ELEMENT);
+export const slabSliceVertices = slabSliceTris * 3;
+export const BUILD_SNAP = 2;
