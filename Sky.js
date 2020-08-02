d47f7df0978e09962fd8e60563f76ef317251993
Avaer Kazmer <a@modules.io>
2020-08-02 03:45:04 -0400

    Add stars/moon to sky

--- Sky.js
+++ Sky.js
@@ -166,6 +166,31 @@ Sky.SkyShader = {
 		'	return ONE_OVER_FOURPI * ( ( 1.0 - g2 ) * inverse );',
 		'}',
 
+		`float mod289(float x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+		vec4 mod289(vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+		vec4 perm(vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
+        float noise(vec3 p){
+		    vec3 a = floor(p);
+		    vec3 d = p - a;
+		    d = d * d * (3.0 - 2.0 * d);
+
+		    vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
+		    vec4 k1 = perm(b.xyxy);
+		    vec4 k2 = perm(k1.xyxy + b.zzww);
+
+		    vec4 c = k2 + a.zzzz;
+		    vec4 k3 = perm(c);
+		    vec4 k4 = perm(c + 1.0);
+
+		    vec4 o1 = fract(k3 * (1.0 / 41.0));
+		    vec4 o2 = fract(k4 * (1.0 / 41.0));
+
+		    vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
+		    vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+
+		    return o4.y * d.y + o4.x * (1.0 - d.y);
+		}`,
+
 		'void main() {',
 
 		'	vec3 direction = normalize( vWorldPosition - cameraPos );',
@@ -202,7 +227,18 @@ Sky.SkyShader = {
 		'	float sundisk = smoothstep( sunAngularDiameterCos, sunAngularDiameterCos + 0.00002, cosTheta );',
 		'	L0 += ( vSunE * 19000.0 * Fex ) * sundisk;',
 
-		'	vec3 texColor = ( Lin + L0 ) * 0.04 + vec3( 0.0, 0.0003, 0.00075 );',
+		"float cosTheta2 = dot(normalize(vWorldPosition - cameraPos), vec3(-vSunDirection.x, -vSunDirection.y, vSunDirection.z));",
+        "float moonDisk = smoothstep(sunAngularDiameterCos,sunAngularDiameterCos+0.00002,cosTheta2);",
+        "L0 += vec3(50.0)*moonDisk;",
+
+        `vec3 starsColor;
+        if (noise(floor(vWorldPosition / 0.1) * 0.1) < 0.00001) {
+		  starsColor = vec3((0.1 / 0.04) * max(1.0 - (vSunE / 300.0), 0.0));
+		} else {
+		  starsColor = vec3(0.);
+		}`,
+
+		'	vec3 texColor = ( Lin + L0 + starsColor ) * 0.04 + vec3( 0.0, 0.0003, 0.00075 );',
 
 		'	vec3 retColor = pow( texColor, vec3( 1.0 / ( 1.2 + ( 1.2 * vSunfade ) ) ) );',
 
