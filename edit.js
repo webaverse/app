f0d0b37c20eeae2b19e340e822649db520c06f72
Avaer Kazmer <a@modules.io>
2020-08-09 03:04:39 -0400

    Hook in animals updating

--- edit.js
+++ edit.js
@@ -14,6 +14,7 @@ import {downloadFile, readFile, bindUploadFileButton} from 'https://static.xrpac
 import './gif.js';
 import {makeUiFullMesh, makeTextMesh} from './vr-ui.js';
 import {makeLineMesh, makeTeleportMesh} from './teleport.js';
+import {makeAnimalFactory} from './animal.js';
 import {
   PARCEL_SIZE,
   SUBPARCEL_SIZE,
@@ -276,7 +277,7 @@ function mod(a, b) {
 }
 const _getPotentialIndex = (x, y, z, subparcelSize) => x + y*subparcelSize*subparcelSize + z*subparcelSize;
 
-const animals = [];
+let animals = [];
 const itemMeshes = [];
 const npcMeshes = [];
 /* const _decorateMeshForRaycast = mesh => {
@@ -319,6 +320,7 @@ let chunkWorker = null;
 let physxWorker = null;
 let physicsWorker = null;
 let geometryWorker = null;
+let makeAnimal = null;
 let chunkMeshes = [];
 let chunkMesh = null;
 const worldContainer = new THREE.Object3D();
@@ -755,6 +757,7 @@ const [
       };
       return w;
     })();
+    makeAnimal = makeAnimalFactory(geometryWorker);
 
     const vegetationMaterialOpaque = new THREE.ShaderMaterial({
       uniforms: {
@@ -946,139 +949,6 @@ const [
             animation && animation.update();
           };
           scene.add(crosshairMesh);
-        }
-        {
-          const animalMaterial = new THREE.ShaderMaterial({
-            uniforms: {
-              headRotation: {
-                type: 'v4',
-                value: new THREE.Quaternion(),
-                needsUpdate: true,
-              },
-              walkFactor: {
-                type: 'f',
-                value: 1,
-                needsUpdate: true,
-              },
-              walkCycle: {
-                type: 'f',
-                value: 0,
-                needsUpdate: true,
-              },
-            },
-            vertexShader: `\
-              precision highp float;
-              precision highp int;
-
-              #define PI 3.1415926535897932384626433832795
-
-              attribute vec3 color;
-              attribute vec3 head;
-              attribute vec4 leg;
-
-              uniform vec4 headRotation;
-              uniform float walkFactor;
-              uniform float walkCycle;
-              varying vec3 vColor;
-
-              vec4 quat_from_axis_angle(vec3 axis, float angle)
-              {
-                vec4 qr;
-                float half_angle = angle * 0.5;
-                float s = sin(half_angle);
-                qr.x = axis.x * s;
-                qr.y = axis.y * s;
-                qr.z = axis.z * s;
-                qr.w = cos(half_angle);
-                return qr;
-              }
-              vec3 multiply_vq(vec3 v, vec4 q) {
-                return v + 2.0 * cross(q.xyz, cross(q.xyz, v) + q.w * v);
-              }
-              vec3 rotate_vertex_position(vec3 position, vec3 axis, float angle)
-              {
-                vec4 q = quat_from_axis_angle(axis, angle);
-                return multiply_vq(position, q);
-              }
-
-              void main() {
-                vec3 p = position;
-                if (head.y != 0.) {
-                  // p = vec3(0.);
-                  p -= head.xyz;
-                  p += multiply_vq(head, headRotation);
-                }
-                if (leg.y != 0.) {
-                  p -= leg.xyz;
-                  p += rotate_vertex_position(leg.xyz, vec3(leg.w, 0., 0.), sin(walkCycle*PI*2.)*PI/2.*walkFactor);
-                }
-                gl_Position = projectionMatrix * modelViewMatrix * vec4(p, 1.0);
-                vColor = color;
-              }
-            `,
-            fragmentShader: `\
-              precision highp float;
-              precision highp int;
-
-              varying vec3 vColor;
-
-              void main() {
-                gl_FragColor = vec4(vColor, 1.0);
-              }
-            `,
-          });
-
-          const rng = alea('lol');
-          const animalSpec = await geometryWorker.requestAnimalGeometry(Math.floor(rng()*0xFFFFFF));
-          const geometry = new THREE.BufferGeometry();
-          geometry.setAttribute('position', new THREE.BufferAttribute(animalSpec.positions, 3));
-          geometry.setAttribute('color', new THREE.BufferAttribute(animalSpec.colors, 3, true));
-          geometry.setIndex(new THREE.BufferAttribute(animalSpec.indices, 1));
-          geometry.setAttribute('head', new THREE.BufferAttribute(animalSpec.heads, 3));
-          geometry.setAttribute('leg', new THREE.BufferAttribute(animalSpec.legs, 4));
-          const animal = new THREE.Mesh(geometry, animalMaterial);
-          const headPivot = new THREE.Vector3().fromArray(animalSpec.headPivot);
-          const aabb = new THREE.Box3().setFromCenterAndSize(new THREE.Vector3().fromArray(animalSpec.aabb.center), new THREE.Vector3().fromArray(animalSpec.aabb.size));
-
-          let animation = null;
-          animal.lookAt = p => {
-            const startTime = Date.now();
-            const endTime = startTime + 300;
-            const startQuaternion = animal.material.uniforms.headRotation.value.clone();
-            const endQuaternion = new THREE.Quaternion().setFromRotationMatrix(
-              new THREE.Matrix4().lookAt(
-                headPivot,
-                p,
-                new THREE.Vector3(0, 1, 0)
-              )
-            );
-            animation = {
-              update() {
-                const now = Date.now();
-                const factor = (now - startTime) / (endTime - startTime);
-                if (factor < 1) {
-                  animal.material.uniforms.headRotation.value.copy(startQuaternion).slerp(endQuaternion, factor);
-                } else {
-                  animal.material.uniforms.headRotation.value.copy(endQuaternion);
-                  animation = null;
-                }
-                animal.material.uniforms.headRotation.needsUpdate = true;
-              },
-            };
-          };
-          animal.update = () => {
-            animation && animation.update();
-
-            // animal.material.uniforms.headRotation.value.setFromEuler(localEuler.set(-0.2, 0.2, 0, 'YXZ'));
-            // animal.material.uniforms.headRotation.needsUpdate = true;
-            // animal.material.uniforms.walkFactor.value = 1;
-            // animal.material.uniforms.walkFactor.needsUpdate = true;
-            animal.material.uniforms.walkCycle.value = (Date.now()%2000)/2000;
-            animal.material.uniforms.walkCycle.needsUpdate = true;
-          };
-          animal.isAnimating = () => !!animation;
-          scene.add(animal);
-          animals.push(animal);
         }
       })(),
       (async () => {
@@ -2280,6 +2150,33 @@ const _makeChunkMesh = (seedString, parcelSize, subparcelSize) => {
       }
     }
   };
+  const _updateAnimalsRemove = () => {
+    if (removedCoords.length > 0) {
+      animals = animals.filter(animal => {
+        if (removedCoords.some(removedCoord => removedCoord.index === animal.index)) {
+          animal.parent.remove(animal);
+          return false;
+        } else {
+          return true;
+        }
+      })
+    }
+  };
+  const _updateAnimalsAdd = () => {
+    for (const addedCoord of addedCoords) {
+      if (addedCoord.y === 30 && Math.random() < 0.1) {
+        const animal = makeAnimal(Math.floor(Math.random()*0xFFFFFF));
+        animal.position.set(addedCoord.x + 0.5, addedCoord.y - 0.7, addedCoord.z + 0.5).multiplyScalar(SUBPARCEL_SIZE);
+        animal.index = addedCoord.index;
+        currentChunkMesh.add(animal);
+        animals.push(animal);
+      }
+    }
+  };
+  const _updateAnimals = () => {
+    _updateAnimalsRemove();
+    _updateAnimalsAdd();
+  };
   mesh.update = position => {
     _updateCurrentCoord(position);
     _updateNeededCoords();
@@ -2287,6 +2184,7 @@ const _makeChunkMesh = (seedString, parcelSize, subparcelSize) => {
     // _updateBuilds();
     _updateVegetations();
     _updatePackages();
+    _updateAnimals();
     _updateLastNeededCoords();
   };
   return mesh;
