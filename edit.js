da7c63002adb6e96c663e735593a37005e79d4c0
Avaer Kazmer <a@modules.io>
2020-08-20 18:18:30 -0400

    Reduce chunk prealloc size

--- edit.js
+++ edit.js
@@ -2113,7 +2113,7 @@ const _makeChunkMesh = async (seedString, parcelSize, subparcelSize) => {
     waterMaterial.uniforms.tex.needsUpdate = true;
   })();
 
-  const numPositions = 4 * 1024 * 1024;
+  const numPositions = 3 * 1024 * 1024;
   const allocators = {
     positions: geometryWorker.makeArenaAllocator(numPositions * 3*Float32Array.BYTES_PER_ELEMENT),
     normals: geometryWorker.makeArenaAllocator(numPositions * 3*Float32Array.BYTES_PER_ELEMENT),
