8b6952f899fbd14ce139f6617e113b16c0ae5729
Avaer Kazmer <a@modules.io>
2020-09-06 18:39:53 -0400

    Hook in https server

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const fs = require('fs');
 const http = require('http');
+const https = require('https');
 const express = require('express');
+
+const CERT = fs.readFileSync('./certs/cert.pem');
+const PRIVKEY = fs.readFileSync('./certs/key.pem');
 
 const app = express();
 app.use((req, res, next) => {
@@ -10,5 +15,10 @@ app.use(express.static(__dirname));
 
 http.createServer(app)
   .listen(3000);
+https.createServer({
+  cert: CERT,
+  key: PRIVKEY,
+}, app)
+  .listen(4443);
 
 console.log('http://127.0.0.1:3000');
