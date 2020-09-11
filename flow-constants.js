47065b8cd53772d1f3a4469f0f4f0af0599e5f9f
Avaer Kazmer <a@modules.io>
2020-09-11 07:24:55 -0400

    Add flow-constants.js

---
+++ flow-constants.js
@@ -0,0 +1,13 @@
+export default {
+  host: 'http://access-001.devnet12.nodes.onflow.org:8000',
+  FungibleToken: '0x9a0766d93b6608b7',
+  FlowToken: '0x7e60df042a9c0868',
+  NonFungibleToken: '0x631e88ae7f1d7c20',
+
+  // host: 'http://127.0.0.1:8080',
+
+  address: '',
+  publicKey: '',
+  privateKey: '',
+  flowKey: '',
+};
