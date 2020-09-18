087e93d94bb24321a9951bfc9a6f166f7166b202
Avaer Kazmer <a@modules.io>
2020-09-18 02:13:59 -0400

    Use new thing shader for shapes

--- constants.js
+++ constants.js
@@ -27,5 +27,28 @@ export const PLANET_OBJECT_SIZE = (
   Float32Array.BYTES_PER_ELEMENT * 4 // build.quaternion
 );
 
+export const colors = [
+  'ef5350',
+  'ec407a',
+  'ab47bc',
+  '7e57c2',
+  '5c6bc0',
+  '42a5f5',
+  '29b6f6',
+  '26c6da',
+  '26a69a',
+  '66bb6a',
+  '9ccc65',
+  'd4e157',
+  'ffee58',
+  'ffca28',
+  'ffa726',
+  'ff7043',
+  '8d6e63',
+  'bdbdbd',
+  '78909c',
+  '333333',
+];
+
 export const storageHost = 'https://storage.exokit.org';
 // export const storageHost = 'https://127.0.0.1:443/storage';
