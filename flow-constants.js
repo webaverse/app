f5267577a849197a17548fc5ae9d0e15aef4f0f7
Avaer Kazmer <a@modules.io>
2020-09-20 20:02:25 -0400

    Update testnet endpoint

--- flow-constants.js
+++ flow-constants.js
@@ -1,5 +1,5 @@
 export default {
-  host: 'http://access-001.devnet13.nodes.onflow.org:8000',
+  host: 'https://access-testnet.onflow.org',
   FungibleToken: '0x9a0766d93b6608b7',
   FlowToken: '0x7e60df042a9c0868',
   NonFungibleToken: '0x631e88ae7f1d7c20',
