74e473c8911a5cb2f472b23c803c4a5ef2302147
Avaer Kazmer <a@modules.io>
2020-09-23 10:41:15 -0400

    Remove host from flow-constants.js

--- flow-constants.js
+++ flow-constants.js
@@ -1,5 +1,4 @@
 export default {
-  host: 'https://testnet.webaverse.workers.dev',
   FungibleToken: '0x9a0766d93b6608b7',
   FlowToken: '0x7e60df042a9c0868',
   NonFungibleToken: '0x631e88ae7f1d7c20',
