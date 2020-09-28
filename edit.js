6b11781512f677406be87e119638d5a41ec405bc
Avaer Kazmer <a@modules.io>
2020-09-28 14:46:25 -0400

    Remove ugc comet mesh

--- edit.js
+++ edit.js
@@ -4633,97 +4633,6 @@ const _tickPlanetAnimation = factor => {
   }
 }; */
 
-const cometFireMesh = (() => {
-  const radius = 1;
-  const opacity = 0.5;
-  const _makeSphereGeometry = (radius, color, position, scale) => {
-    const geometry = new THREE.SphereBufferGeometry(radius, 8, 5);
-    for (let i = 0; i < geometry.attributes.position.array.length; i += 3) {
-      if (geometry.attributes.position.array[i + 1] > 0) {
-        geometry.attributes.position.array[i] = Math.sign(geometry.attributes.position.array[i]);
-        geometry.attributes.position.array[i + 2] = Math.sign(geometry.attributes.position.array[i + 2]);
-      }
-    }
-
-    geometry
-      .applyMatrix4(new THREE.Matrix4().makeTranslation(position.x, position.y, position.z))
-      .applyMatrix4(new THREE.Matrix4().makeScale(scale.x, scale.y, scale.z));
-
-    const c = new THREE.Color(color);
-    const colors = new Float32Array(geometry.attributes.position.array.length);
-    for (let i = 0; i < colors.length; i += 3) {
-      c.toArray(colors, i);
-    }
-    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
-
-    return geometry;
-  };
-  const cometFireGeometry = BufferGeometryUtils.mergeBufferGeometries([
-    _makeSphereGeometry(radius, 0x5c6bc0, new THREE.Vector3(0, 0.9, 0), new THREE.Vector3(0.8, 10, 0.8)),
-    _makeSphereGeometry(radius, 0xef5350, new THREE.Vector3(0, 0.7, 0), new THREE.Vector3(2, 5, 2)),
-    _makeSphereGeometry(radius, 0xffa726, new THREE.Vector3(0, 0, 0), new THREE.Vector3(1, 1, 1)),
-  ]);
-  const cometFireMaterial = new THREE.ShaderMaterial({
-    uniforms: {
-      uAnimation: {
-        type: 'f',
-        value: 0,
-      },
-    },
-    vertexShader: `\
-      #define PI 3.1415926535897932384626433832795
-
-      uniform float uAnimation;
-      attribute vec3 color;
-      attribute float y;
-      attribute vec3 barycentric;
-      // varying float vY;
-      varying vec2 vUv;
-      // varying float vOpacity;
-      varying vec3 vColor;
-      void main() {
-        // vY = y * ${opacity.toFixed(8)};
-        // vUv = uv.x + uAnimation;
-        vUv = uv;
-        // vOpacity = 0.8 + 0.2 * (sin(uAnimation*5.0*PI*2.0)+1.0)/2.0;
-        // vOpacity *= 1.0-(uv.y/0.5);
-        // vOpacity = (0.5 + 0.5 * (sin(uv.x*PI*2.0/0.05) + 1.0)/2.0) * (0.3 + 1.0-uv.y/0.5);
-        /* vec3 p = position;
-        if (p.y > 0.0) {
-          p.x = sign(p.x) * 1.0;
-          p.z = sign(p.z) * 1.0;
-        } */
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-        vColor = color;
-      }
-    `,
-    fragmentShader: `\
-      #define PI 3.1415926535897932384626433832795
-
-      uniform float uAnimation;
-      uniform sampler2D uCameraTex;
-      // varying float vY;
-      varying vec2 vUv;
-      // varying float vOpacity;
-      varying vec3 vColor;
-
-      void main() {
-        vec3 c2 = vColor * (2.0-vUv.y/0.5);
-        float a = 0.2 + (0.5 + 0.5 * pow((sin((vUv.x + uAnimation) *PI*2.0/0.1) + 1.0)/2.0, 2.0)) * (1.0-vUv.y/0.5);
-        gl_FragColor = vec4(c2, a);
-      }
-    `,
-    side: THREE.DoubleSide,
-    transparent: true,
-    depthWrite: false,
-  });
-  const cometFireMesh = new THREE.Mesh(cometFireGeometry, cometFireMaterial);
-  cometFireMesh.position.y = 1;
-  cometFireMesh.frustumCulled = false;
-  return cometFireMesh;
-})();
-scene.add(cometFireMesh);
-
 const hpMesh = (() => {
   const mesh = new THREE.Object3D();
 
@@ -5509,7 +5418,6 @@ function animate(timestamp, frame) {
   for (const animal of animals) {
     animal.update();
   }
-  cometFireMesh.material.uniforms.uAnimation.value = (Date.now() % 2000) / 2000;
   hpMesh.update();
   if (skybox) {
     skybox.material.uniforms.iTime.value = ((Date.now() - startTime) % 3000) / 3000;
