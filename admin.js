e55c8e3524639fb20009679b1aa9e6a0536a9972
Avaer Kazmer <a@modules.io>
2020-09-29 09:17:04 -0400

    Clean up admin.js spacing

--- admin.js
+++ admin.js
@@ -6,11 +6,11 @@ const _jsonParse = s => {
   try {
     return JSON.parse(s);
   } catch (err) {
-  	return null;
+    return null;
   }
 };
 const _runArray = async (userKeys, array) => {
-	const result = Array(array.length);
+  const result = Array(array.length);
   for (let i = 0; i < array.length; i++) {
     result[i] = await _runSpec(userKeys, array[i]);
   }
@@ -89,7 +89,7 @@ const _runScript = async script => {
   return response2;
 };
 const _runSpec = async (userKeys, spec) => {
-	const {transaction, script, args} = spec;
+  const {transaction, script, args} = spec;
   const res = await fetch(`https://accounts.exokit.org/sendTransaction`, {
     method: 'POST',
     body: JSON.stringify({
@@ -112,58 +112,58 @@ const _runSpec = async (userKeys, spec) => {
 };
 
 {
-	const createAccountForm = document.getElementById('create-account-form');
+  const createAccountForm = document.getElementById('create-account-form');
   const createAccountFormBakedCheckbox = document.getElementById('create-account-form-baked-checkbox');
-	const createAccountFormOutput = document.getElementById('create-account-form-output');
-	createAccountForm.addEventListener('submit', async e => {
-	  e.preventDefault();
+  const createAccountFormOutput = document.getElementById('create-account-form-output');
+  createAccountForm.addEventListener('submit', async e => {
+    e.preventDefault();
 
-	  createAccountFormOutput.setAttribute('disabled', '');
+    createAccountFormOutput.setAttribute('disabled', '');
 
-	  const userKeys = await _createAccount({
+    const userKeys = await _createAccount({
       bake: createAccountFormBakedCheckbox.checked,
     });
-	  createAccountFormOutput.value = JSON.stringify(userKeys, null, 2);
-	  createAccountFormOutput.removeAttribute('disabled');
-	});
+    createAccountFormOutput.value = JSON.stringify(userKeys, null, 2);
+    createAccountFormOutput.removeAttribute('disabled');
+  });
 }
 {
-	const contractForm = document.getElementById('contract-form');
-	const contractFormKeys = document.getElementById('contract-form-keys');
-	const contractFormSource = document.getElementById('contract-form-source');
-	contractForm.addEventListener('submit', async e => {
-	  e.preventDefault();
+  const contractForm = document.getElementById('contract-form');
+  const contractFormKeys = document.getElementById('contract-form-keys');
+  const contractFormSource = document.getElementById('contract-form-source');
+  contractForm.addEventListener('submit', async e => {
+    e.preventDefault();
 
-	  const contractSource = await resolveContractSource(contractFormSource.value);
-	  contractFormSource.setAttribute('disabled', '');
+    const contractSource = await resolveContractSource(contractFormSource.value);
+    contractFormSource.setAttribute('disabled', '');
 
     const contractKeys = _jsonParse(contractFormKeys.value);
 
     try {
-	    let result, o;
-	    if (contractSource.charAt(0) === '[') {
-	     	console.log('run array');
-	      result = await _runArray(contractKeys, eval(contractSource));
-	    } else if (contractSource.charAt(0) === '{') {
-	     	console.log('run array');
-	      result = await _runSpec(contractKeys, eval(contractSource));
-	    } else if (/pub contract /.test(contractSource)) {
-	    	console.log('run contract');
-			  result = await _bakeContract(contractKeys, contractSource);
+      let result, o;
+      if (contractSource.charAt(0) === '[') {
+        console.log('run array');
+        result = await _runArray(contractKeys, eval(contractSource));
+      } else if (contractSource.charAt(0) === '{') {
+        console.log('run array');
+        result = await _runSpec(contractKeys, eval(contractSource));
+      } else if (/pub contract /.test(contractSource)) {
+        console.log('run contract');
+        result = await _bakeContract(contractKeys, contractSource);
       } else if (/pub fun main\(\)/.test(contractSource)) {
-				console.log('run script');
-				result = await _runScript(contractSource);
-			} else if (/transaction /.test(contractSource)) {
-				console.log('run transaction');
-				result = await _runTransaction(contractKeys, contractSource);
-			} else {
-				console.warn('do not know how to run source', contractSource);
-			}
+        console.log('run script');
+        result = await _runScript(contractSource);
+      } else if (/transaction /.test(contractSource)) {
+        console.log('run transaction');
+        result = await _runTransaction(contractKeys, contractSource);
+      } else {
+        console.warn('do not know how to run source', contractSource);
+      }
 
-			contractFormSource.value = JSON.stringify(result, null, 2);
-		} catch(err) {
-			contractFormSource.value = err.stack;
-		}
-		contractFormSource.removeAttribute('disabled');
-	});
+      contractFormSource.value = JSON.stringify(result, null, 2);
+    } catch(err) {
+      contractFormSource.value = err.stack;
+    }
+    contractFormSource.removeAttribute('disabled');
+  });
 }
