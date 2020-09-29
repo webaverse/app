2847545b9696e666cc7375b6ae694671c94a7425
Avaer Kazmer <a@modules.io>
2020-09-29 09:59:29 -0400

    Clean up spacing in app-object.js

--- app-object.js
+++ app-object.js
@@ -28,59 +28,59 @@ camera.rotation.order = 'YXZ';
 // camera.quaternion.set(0, 0, 0, 1);
 
 class AppManager {
-	constructor() {
-		this.apps = [];
-		this.animationLoops = [];
-	}
-	createApp(appId) {
-		const app = new App(appId);
-		this.apps.push(app);
-		return app;
-	}
-	destroyApp(appId) {
-		const appIndex = this.apps.findIndex(app => app.appId === appId);
-		if (appIndex !== -1) {
-			const app = this.apps[appIndex];
-			app.dispatchEvent(new MessageEvent('terminate'));
-			this.apps.splice(appIndex, 1);
-		}
-		this.removeAnimationLoop(appId);
-	}
-	getApp(appId) {
+  constructor() {
+    this.apps = [];
+    this.animationLoops = [];
+  }
+  createApp(appId) {
+    const app = new App(appId);
+    this.apps.push(app);
+    return app;
+  }
+  destroyApp(appId) {
+    const appIndex = this.apps.findIndex(app => app.appId === appId);
+    if (appIndex !== -1) {
+      const app = this.apps[appIndex];
+      app.dispatchEvent(new MessageEvent('terminate'));
+      this.apps.splice(appIndex, 1);
+    }
+    this.removeAnimationLoop(appId);
+  }
+  getApp(appId) {
     return this.apps.find(app => app.appId === appId);
-	}
-	setAnimationLoop(appId, fn) {
-		let animationLoop = this.animationLoops.find(al => al.appId === appId);
-		if (!animationLoop) {
-			animationLoop = {
+  }
+  setAnimationLoop(appId, fn) {
+    let animationLoop = this.animationLoops.find(al => al.appId === appId);
+    if (!animationLoop) {
+      animationLoop = {
         appId,
         fn: null,
-			};
-			this.animationLoops.push(animationLoop);
-		}
-		animationLoop.fn = fn;
-	}
-	removeAnimationLoop(appId) {
-		const index = this.animationLoops.findIndex(al => al.appId === appId);
-		if (inded !== -1) {
-			this.animationLoops.splice(index, 1);
-		}
-	}
-	tick() {
-		for (const al of this.animationLoops) {
-			al.fn.apply(null, arguments);
-		}
-	}
+      };
+      this.animationLoops.push(animationLoop);
+    }
+    animationLoop.fn = fn;
+  }
+  removeAnimationLoop(appId) {
+    const index = this.animationLoops.findIndex(al => al.appId === appId);
+    if (inded !== -1) {
+      this.animationLoops.splice(index, 1);
+    }
+  }
+  tick() {
+    for (const al of this.animationLoops) {
+      al.fn.apply(null, arguments);
+    }
+  }
 }
 const appManager = new AppManager();
 
 class App extends EventTarget {
-	constructor(appId) {
-		super();
+  constructor(appId) {
+    super();
 
     this.appId = appId;
     this.files = {};
-	}
+  }
 }
 
 export {renderer, scene, camera, appManager};
