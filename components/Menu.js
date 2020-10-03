ff3d20831990a220443e5960cc90474070b52258
Chris <chris.eddy.dev@gmail.com>
2020-10-02 23:09:15 -0700

    checkpoint new state, Menu, WeaponWheel setup, iomanager stateset

---
+++ components/Menu.js
@@ -0,0 +1,12 @@
+import Inventory from './Inventory.js';
+
+const Menu = (props = {}) => {
+    return `
+        <div class="twoD-menu">
+            <div class="twoD-menuNav">
+                hello
+            </div>
+        </div>
+    `;
+}
+export default Menu;
