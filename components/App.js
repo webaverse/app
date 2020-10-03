7ba6474d6f32aeb56458a13d7da4156862ebd0a9
Chris <chris.eddy.dev@gmail.com>
2020-10-03 00:19:26 -0700

    big state changes

--- components/App.js
+++ components/App.js
@@ -1,39 +1,36 @@
 import Menu from './Menu.js';
 import WeaponWheel from './WeaponWheel.js';
 import inventory from '../inventory.js';
-import {loginManager} from '../login.js';
-import {getState, setState, getSpecificState} from '../state.js';
-import {setBindings} from './bindings.js';
+import { loginManager } from '../login.js';
+import { state, getState, setState, getSpecificState } from '../state.js';
+import { setBindings } from './bindings.js';
 
-let appState = getState();
-
-let appProps = {
-    inventoryItems: appState.inventory.items,
-    selectedWeapon: appState.selectedWeapon,
-    isXR: appState.isXR,
-};
+let appState = state;
 
 const onclickBindings = {
-  'inventory-wear': e => {
-    const id = parseInt(e.target.getAttribute('inventoryid'), 10);
-    loginManager.setAvatar(id);
-  },
-  'inventory-upload': e => {
-    const file = document.getElementById("twoD-inventoryUploadBtn").files[0];
-    inventory.uploadFile(file);
-  },
+    'inventory-wear': (e) => {
+        const id = parseInt(e.target.getAttribute('inventoryid'), 10);
+        loginManager.setAvatar(id);
+    },
+    'inventory-upload': (e) => {
+        setState({ invento })
+        const file = document.getElementById("twoD-inventoryUploadBtn").files[0];
+        inventory.uploadFile(file);
+    },
 };
+
 inventory.addEventListener('filesupdate', (e) => {
-  updateProps({ inventoryItems: appProps.inventoryItems.concat(e.data)})
+    state.menu.inventory.items = state.menu.inventory.items.concat(e.data)
+    updateProps({ inventoryItems: state.menu.inventory.items })
 });
 
 export const toggleMenus = (props) => {
-    switch (appProps.selectedWeapon) {
+    switch (appState.selectedWeapon) {
         case 'menu':
             return Menu(props);
         case 'weaponWheel':
             return WeaponWheel(props);
-        default: 
+        default:
             return;
     }
 }
@@ -49,17 +46,17 @@ export const App = (props) => {
 export const updateProps = (newProps) => {
     const appContainer = document.getElementById('appContainer');
     for (let k in newProps) {
-        if (appProps[k] !== newProps[k]) {
-            appProps[k] = newProps[k];
+        if (appState[k] !== newProps[k]) {
+            appState[k] = newProps[k];
         }
     }
-    if (appProps.pointerLock || appProps.isXR || !appProps.selectedWeapon && !appProps.weaponWheel) {
+    if (appState.pointerLock || appState.isXR) {
         appContainer.style.display = 'none';
         appContainer.innerHTML = '';
         setBindings(null, onclickBindings);
     } else {
         appContainer.style.display = 'block';
-        appContainer.innerHTML = App(appProps);
+        appContainer.innerHTML = App(appState);
         setBindings(appContainer, onclickBindings);
     }
 }
