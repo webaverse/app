6d76858c50f3e52f37804359d225187ab787a459
Chris <chris.eddy.dev@gmail.com>
2020-10-04 03:48:24 -0700

    major threeD menu styling

--- threeD-components/Inventory.js
+++ threeD-components/Inventory.js
@@ -1,23 +1,23 @@
 const InventoryCard = (props = {}) => {
     return `
-        <a class="twoD-inventoryCard" draggable dragid="inventory-${props.id}">
-            <img class="twoD-inventoryCardPreview" src="${props.preview}"></img>
-            <h4 class="twoD-inventoryCardName">${props.name}</h4>
-            <div class="twoD-inventoryCardActions">
-                <a class="twoD-inventoryCardWearBtn" onclick=inventory-wear inventoryid="${props.id}">
+        <div class="threeD-inventoryCard" draggable dragid="inventory-${props.id}">
+            <img class="threeD-inventoryCardPreview" src="${props.preview}"></img>
+            <h4 class="threeD-inventoryCardName">${props.name}</h4>
+            <div class="threeD-inventoryCardActions">
+                <a id="threeD-inventoryCardWearBtn" onclick=inventory-wear inventoryid="${props.id}">
                     <i class="fal fa-hand-sparkles" style="margin-right: 5px;"></i>
                     Wear
                 </a>
-                <a class="twoD-inventoryCardDiscardBtn" onclick=inventory-discard inventoryid="${props.id}">
+                <a id="threeD-inventoryCardDiscardBtn" onclick=inventory-discard inventoryid="${props.id}">
                     <i class="fal fa-trash" style="margin-right: 5px;"></i>
                     Discard
                 </a>
-                <a class="twoD-inventoryCardInspectBtn">
+                <a id="threeD-inventoryCardInspectBtn">
                     <i class="fal fa-search-plus" style="margin-right: 5px;"></i>
                     Inspect
                 </a>
             </div>
-        </a>
+        </div>
     `;
 }
 
@@ -25,43 +25,44 @@ const Inventory = (props = {}) => {
     let inventoryItems = props.inventoryItems || [];
     return `
         <style>
-        .twoD-inventory {
+        .threeD-inventory {
 
         }
         
-        .twoD-inventory .twoD-inventoryHeader {
-            padding: 25px 35px 20px 35px;
+        .threeD-inventory .threeD-inventoryHeader {
+            padding: 70px 35px 70px 35px;
         }
         
-        .twoD-inventory .twoD-inventoryHeader .twoD-inventoryIcon {
+        .threeD-inventory .threeD-inventoryHeader .threeD-inventoryIcon {
             margin-right: 20px;
             font-size: 30px;
         }
         
-        .twoD-inventory .twoD-inventoryHeader .twoD-inventoryHeaderTitle {
+        .threeD-inventory .threeD-inventoryHeader .threeD-inventoryHeaderTitle {
             display: inline;
             font-size: 30px;
         }
         
-        .twoD-inventory .twoD-inventoryHeader .twoD-inventoryUploadBtn {
-            padding: 10px 20px;
+        .threeD-inventory .threeD-inventoryHeader #threeD-inventoryUploadBtn {
+            padding: 20px 20px;
             background-color: #42a5f5;
             border: 0;
             color: #FFF;
-            border-radius: 8px;
-            font-size: 16px;
+            border-radius: 30px;
+            font-size: 60px;
             white-space: nowrap;
             text-decoration: none;
             cursor: pointer;
             user-select: none;
+            margin: 20px;
         }
         
-        .twoD-inventory .twoD-inventoryDivider {
+        .threeD-inventory .threeD-inventoryDivider {
             border-color: white;
             margin: 0px 30px 10px 30px;
         }
         
-        .twoD-inventory .twoD-inventoryList {
+        .threeD-inventory .threeD-inventoryList {
             max-height: calc(80vh - 155px);
             display: flex;
             flex-wrap: wrap;
@@ -70,9 +71,9 @@ const Inventory = (props = {}) => {
             overflow-y: auto;
         }
         
-        .twoD-inventory .twoD-inventoryList .twoD-inventoryCard {
-            height: 240px;
-            width: 250px;
+        .threeD-inventory .threeD-inventoryList .threeD-inventoryCard {
+            height: 500px;
+            width: 600px;
             background-color: #e6e8ef;
             margin: 10px;
             box-shadow: 0 3px 3px rgba(0, 0, 0, 0.12), 0 1px 4px rgba(0, 0, 0, 0.24);
@@ -83,37 +84,28 @@ const Inventory = (props = {}) => {
             cursor: pointer;
         }
         
-        .twoD-inventory .twoD-inventoryList .twoD-inventoryCard:hover {
-            height: 320px;
-            transition: height 0.5s;
-        }
-        
-        .twoD-inventory .twoD-inventoryList .twoD-inventoryCard:hover {
-            border: 1px #eeeef5 solid;
-        }
-        
-        .twoD-inventory .twoD-inventoryList .twoD-inventoryCard .twoD-inventoryCardName {
+        .threeD-inventory .threeD-inventoryList .threeD-inventoryCard .threeD-inventoryCardName {
             margin-bottom: 9px;
             margin-top: 5px;
-            font-size: 15px;
+            font-size: 60px;
         }
         
-        .twoD-inventory .twoD-inventoryList .twoD-inventoryCard .twoD-inventoryCardPreview {
+        .threeD-inventory .threeD-inventoryList .threeD-inventoryCard .threeD-inventoryCardPreview {
             height: 200px;
             width: 100%;
         }
         
-        .twoD-inventory .twoD-inventoryList .twoD-inventoryCard .twoD-inventoryCardActions {
-        
+        .threeD-inventory .threeD-inventoryList .threeD-inventoryCard .threeD-inventoryCardActions {
+            margin-top: 40px;
         }
         
-        .twoD-inventory .twoD-inventoryList .twoD-inventoryCard .twoD-inventoryCardActions .twoD-inventoryCardWearBtn {
+        .threeD-inventory .threeD-inventoryList .threeD-inventoryCard .threeD-inventoryCardActions #threeD-inventoryCardWearBtn {
             padding: 6px 10px;
             background-color: #2ec19c;
             border: 0;
             color: #FFF;
             border-radius: 8px;
-            font-size: 14px;
+            font-size: 50px;
             white-space: nowrap;
             text-decoration: none;
             cursor: pointer;
@@ -121,13 +113,13 @@ const Inventory = (props = {}) => {
             margin: 5px;
         }
         
-        .twoD-inventory .twoD-inventoryList .twoD-inventoryCard .twoD-inventoryCardActions .twoD-inventoryCardInspectBtn {
+        .threeD-inventory .threeD-inventoryList .threeD-inventoryCard .threeD-inventoryCardActions #threeD-inventoryCardInspectBtn {
             padding: 6px 10px;
             background-color: #a049e8;
             border: 0;
             color: #FFF;
             border-radius: 8px;
-            font-size: 14px;
+            font-size: 50px;
             white-space: nowrap;
             text-decoration: none;
             cursor: pointer;
@@ -135,13 +127,13 @@ const Inventory = (props = {}) => {
             margin: 5px;
         }
         
-        .twoD-inventory .twoD-inventoryList .twoD-inventoryCard .twoD-inventoryCardActions .twoD-inventoryCardDiscardBtn {
+        .threeD-inventory .threeD-inventoryList .threeD-inventoryCard .threeD-inventoryCardActions #threeD-inventoryCardDiscardBtn {
             padding: 6px 10px;
             background-color:#c12e5a;
             border: 0;
             color: #FFF;
             border-radius: 8px;
-            font-size: 14px;
+            font-size: 50px;
             white-space: nowrap;
             text-decoration: none;
             cursor: pointer;
@@ -149,15 +141,15 @@ const Inventory = (props = {}) => {
             margin: 5px;
         }
         </style>
-        <div class="twoD-inventory">
-            <div class="twoD-inventoryHeader">
-                <a class="twoD-inventoryUploadBtn">
+        <div class="threeD-inventory">
+            <div class="threeD-inventoryHeader">
+                <a id="threeD-inventoryUploadBtn">
                     <i class="fal fa-arrow-alt-from-top" style="margin-right: 5px;"></i>
                     Upload
-                    <input id="twoD-inventoryUploadBtn" type="file" onchange="inventory-upload">
+                    <input id="threeD-inventoryUploadBtn" type="file" onchange="inventory-upload">
                 </a>
             </div>
-            <div class="twoD-inventoryList">
+            <div class="threeD-inventoryList">
                 ${
                     inventoryItems.map((value, index) => {
                         return InventoryCard({
