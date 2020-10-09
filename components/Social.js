e4103c5233b57b21943f5574859ee80ef71abe6a
Chris <chris.eddy.dev@gmail.com>
2020-10-09 00:04:42 -0700

    cleanup some inline styles

--- components/Social.js
+++ components/Social.js
@@ -2,14 +2,14 @@ const PeerCard = (props) => {
   console.log(props)
   return `
     <div class="twoD-social-peerCard">
-      <div style="width: 220px;">
+      <div class="twoD-social-peerCard-imgWrap">
         <img class="twoD-social-peerCard-avatar" src="https://media.istockphoto.com/vectors/default-avatar-profile-icon-gray-placeholder-photo-vector-id844061224?b=1&k=6&m=844061224&s=612x612&w=0&h=adm3xmgWwC3YTVC2swWz02NDmtZ6vFaIw8a_XBGsOLk=">
       </div>
-      <div style="width: 220px;">
+      <div class="twoD-social-peerCard-peerName">
         <h2>Peer ID</h2>
         <h1>${props.peerName}</h1>
       </div>
-      <div style="width: 220px; text-align: center; padding-top: 20px;">
+      <div class="twoD-social-peerCard-actions">
         <button class="twoD-social-peerCard-teleport">Teleport</button>
         <button class="twoD-social-peerCard-trade">Trade</button>
       </div>
@@ -31,8 +31,8 @@ const Social = (props) => {
   } else {
     return `
       <div class="twoD-social">
-        <div style="text-align: center; width: 100%;">
-          <h1 style="margin-top: 15vh; font-size: 45px;">World is Empty</h1>
+        <div class="twoD-social-noPeers">
+          <h1 class="twoD-social-noPeers-header">World is Empty</h1>
           <button class="twoD-social-peerCard-shareWorld" onclick="twoD-social-peerCard-shareWorld" value="Copy / Paste">
             <p style="margin: 0;">
               <i class="fal fa-copy" style="padding: 15px;"></i>
