627b3addde74714f4fcca88dbf0a17a35c63ee3c
Chris <chris.eddy.dev@gmail.com>
2020-10-13 22:19:18 -0700

    style file upload button

--- components/Inventory.js
+++ components/Inventory.js
@@ -3,29 +3,27 @@ const InventoryCard = (props = {}) => {
         <div class="twoD-inventoryCard" draggable dragid="inventory-${props.id}">
             <img class="twoD-inventoryCardPreview" src="${props.preview}"></img>
             <h4 class="twoD-inventoryCardName">${props.name}</h4>
+            <div class="twoD-inventoryCardActions">
+                <button class="twoD-inventoryCardSpawnBtn" onclick=inventory-spawn name="${props.id}">
+                    <i class="fal fa-magic" style="margin-right: 5px;"></i>
+                    Spawn
+                </button>
+                <button class="twoD-inventoryCardWearBtn" onclick=inventory-wear name="${props.id}">
+                    <i class="fal fa-tshirt" style="margin-right: 5px;"></i>
+                    Wear
+                </button>
+                <button class="twoD-inventoryCardDiscardBtn" onclick=inventory-discard name="${props.id}">
+                    <i class="fal fa-trash" style="margin-right: 5px;"></i>
+                    Discard
+                </button>
+                <button class="twoD-inventoryCardTradeBtn" onclick="twoD-inventoryCardTradeBtn" name="${props.id}">
+                    <i class="fal fa-gift" style="margin-right: 5px;"></i>
+                    Trade
+                </button>
+            </div>
         </div>
     `;
 }
-
-const lol = (props) => `
-<div class="twoD-inventoryCardActions">
-<button class="twoD-inventoryCardWearBtn" onclick=inventory-spawn name="${props.id}">
-    <i class="fal fa-magic" style="margin-right: 5px;"></i>
-    Spawn
-</button>
-<button class="twoD-inventoryCardWearBtn" onclick=inventory-wear name="${props.id}">
-    <i class="fal fa-tshirt" style="margin-right: 5px;"></i>
-    Wear
-</button>
-<button class="twoD-inventoryCardDiscardBtn" onclick=inventory-discard name="${props.id}">
-    <i class="fal fa-trash" style="margin-right: 5px;"></i>
-    Discard
-</button>
-<button class="twoD-inventoryCardTradeBtn" onclick="twoD-inventoryCardTradeBtn" name="${props.id}">
-    <i class="fal fa-gift" style="margin-right: 5px;"></i>
-    Trade
-</button>
-</div>`
 
 const Inventory = (props = {}) => {
     let inventoryItems = props.inventoryItems || [];
@@ -35,8 +33,8 @@ const Inventory = (props = {}) => {
                 <button class="twoD-inventoryUploadBtn">
                     <i class="fal fa-arrow-alt-from-top" style="margin-right: 5px;"></i>
                     Upload
-                    <input id="twoD-inventoryUploadBtn" type="file" onchange="inventory-upload">
                 </button>
+                <input id="twoD-inventoryUploadBtn" type="file" onchange="inventory-upload">
             </div>
             <div class="twoD-inventoryList">
                 ${
