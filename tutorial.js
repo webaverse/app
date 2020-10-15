96e68b1d419b814dfd0554db11f36135573043a1
Avaer Kazmer <a@modules.io>
2020-10-15 16:10:21 -0400

    More FTU work

--- tutorial.js
+++ tutorial.js
@@ -1,10 +1,11 @@
-import storage from './storage.js';
+import {loginManager} from './login.js';
 
 const ftu = document.getElementById('ftu');
-// const ftuPhases = Array.from(document.querySelectorAll('.ftu-phase'));
+const ftuUsername = document.getElementById('ftu-username');
+
 let ftuPhase;
 export async function tryTutorial() {
-  const ftuDone = await storage.get('ftuDone');
+  const ftuDone = loginManager.getFtu();
   ftuPhase = ftuDone ? 4 : 1;
   ftu.classList.add('phase-' + ftuPhase);
 }
@@ -16,7 +17,7 @@ Array.from(document.querySelectorAll('.next-phase-button')).forEach(nextPhaseBut
     if (ftuPhase <= 3) {
       ftu.classList.add('phase-' + ftuPhase);
     } else {
-      await storage.set('ftuDone', true);
+      await loginManager.setFtu(ftuUsername.value, '');
     }
   };
 });
