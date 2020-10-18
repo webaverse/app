b44d974935aa6fe40c42cbbb47fee53fe72028df
Avaer Kazmer <a@modules.io>
2020-10-18 18:50:35 -0400

    Dead code cleanup

--- ethereum.js
+++ ethereum.js
@@ -2,12 +2,6 @@ import Web3 from './web3.min.js';
 import contractAddress from 'https://contracts.webaverse.com/ethereum/address.js';
 import contractAbi from 'https://contracts.webaverse.com/ethereum/abi.js';
 
-/* const _fetchJson = async u => {
-  const res = await fetch(u);
-  const j = await res.json();
-  return j;
-}; */
-
 (async () => {
   const web3 = new Web3(window.ethereum);
   window.ethereum.enable();
