53e1dee5cff29a6059cf3ce8cb3122735db529f9
Avaer Kazmer <a@modules.io>
2020-10-18 20:51:37 -0400

    Update ethereum.js

--- ethereum.js
+++ ethereum.js
@@ -8,6 +8,8 @@ import contractAbi from 'https://contracts.webaverse.com/ethereum/abi.js';
 
   const contract = new web3.eth.Contract(contractAbi, contractAddress);
   const address = web3.currentProvider.selectedAddress;
+  
+  // contract.methods.mint('0x08E242bB06D85073e69222aF8273af419d19E4f6', '0x1', 1).send({from: address})
 
   window.Web3 = Web3;
   window.web3 = web3;
