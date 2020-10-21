228373e1798ff799470ab0782ff0ad7c97ea5624
Avaer Kazmer <a@modules.io>
2020-10-20 20:27:40 -0400

    Update ethereum.js

--- ethereum.js
+++ ethereum.js
@@ -1,14 +1,20 @@
 import Web3 from './web3.min.js';
 import bip32 from './bip32.js';
 import bip39 from './bip39.js';
-import contractAddress from 'https://contracts.webaverse.com/ethereum/address.js';
-import contractAbi from 'https://contracts.webaverse.com/ethereum/abi.js';
+import addresses from 'https://contracts.webaverse.com/ethereum/address.js';
+import abis from 'https://contracts.webaverse.com/ethereum/abi.js';
+let {sidechain: {FT: FTAddress, FTProxy: FTProxyAddress}} = addresses;
+let {FT: FTAbi, FTProxy: FTProxyAbi} = abis;
+
+// FTAddress = `0x2e5eE6508D8c5DCd7403b0B4286500dc2Db80744`;
+// FTProxyAddress = `0x9d0aA50A44B7C1CC9D668cfa09998C60e61AEB97`;
 
 (async () => {
   const web3 = new Web3(window.ethereum);
   window.ethereum.enable();
 
-  const contract = new web3.eth.Contract(contractAbi, contractAddress);
+  const contract = new web3.eth.Contract(FTAbi, FTAddress);
+  const proxyContract = new web3.eth.Contract(FTProxyAbi, FTProxyAddress);
   const address = web3.currentProvider.selectedAddress;
   
   // contract.methods.mint('0x08E242bB06D85073e69222aF8273af419d19E4f6', '0x1', 1).send({from: address})
@@ -20,20 +26,49 @@ import contractAbi from 'https://contracts.webaverse.com/ethereum/abi.js';
   window.contract = contract;
   window.address = address;
   window.test = async () => {
-    const to = '0x08E242bB06D85073e69222aF8273af419d19E4f6';
-    const amount = {t: 'uint256', v: new web3.utils.BN(1).toString(10) };
-    const timestamp = {t: 'uint256', v: new web3.utils.BN(10).toString(10) };
-    const chainId = {t: 'uint256', v: new web3.utils.BN(1).toString(10) };
-    const message = web3.utils.encodePacked(to, amount, timestamp, chainId);
+    const address = web3.currentProvider.selectedAddress;
+    const amount = {
+      t: 'uint256',
+      v: new web3.utils.BN(1),
+    };
+    
+    const receipt = await contract.methods.transfer(FTProxyAddress, amount.v).send({
+      from: address,
+    });
+    console.log('got receipt', receipt);
+    const {transactionHash} = receipt;
+    
+    const timestamp = {
+      t: 'uint256',
+      // v: new web3.utils.BN(Date.now()),
+      v: transactionHash,
+    };
+    const chainId = {
+      t: 'uint256',
+      v: new web3.utils.BN(1),
+    };
+
+    const message = web3.utils.encodePacked(address, amount, timestamp, chainId);
     const hashedMessage = web3.utils.sha3(message);
-    console.log('got message', {to, amount, timestamp, message, hashedMessage});
     const sgn = await web3.eth.personal.sign(hashedMessage, address);
     const r = sgn.slice(0, 66);
     const s = '0x' + sgn.slice(66, 130);
     const v = '0x' + sgn.slice(130, 132);
     console.log('got', JSON.stringify({r, s, v}, null, 2));
-    
-    const res = await fetch();
+
+    await proxyContract.methods.withdraw(address, amount.v, timestamp.v, r, s, v).send({
+      from: address,
+    });
+  };
+  window.testEvents = async () => {
+    const events = await contract.getPastEvents('Transfer', {fromBlock: 0, toBlock: 'latest',})
+    for (const event of events) {
+      const {returnValues} = event;
+      const {from, to, value} = returnValues;
+      if (to === FTProxyAddress) {
+        console.log('got event', {from, to, value});
+      }
+    }
   };
   /* function verify() internal pure returns(bool) {
     bytes memory prefix = "\x19Ethereum Signed Message:\n32";
@@ -47,11 +82,4 @@ import contractAbi from 'https://contracts.webaverse.com/ethereum/abi.js';
     
     return ecrecover(prefixedHash, v, r, s) == a;
   } */
-  /*
-    const events = await contract.getPastEvents('Transfer', {fromBlock: 0, toBlock: 'latest',})
-    for (const event of events) {
-      const {returnValues} = event;
-      const {from, to, amount} = returnValues;
-    }
-  */
 })();
