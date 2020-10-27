e7805b41217a3bb27d9e5422f5b01135b0875bec
Avaer Kazmer <a@modules.io>
2020-10-26 23:15:36 -0400

    Update opensea link

--- ethereum.js
+++ ethereum.js
@@ -911,7 +911,7 @@ const discordOauthUrl = `https://discord.com/api/oauth2/authorize?client_id=6841
   ftContractAddressLink.href = `https://${networkType === 'main' ? '' : networkType + '.'}etherscan.io/address/${FTAddress}`;
   nftContractAddressLink.innerText = NFTAddress;
   nftContractAddressLink.href = `https://${networkType === 'main' ? '' : networkType + '.'}etherscan.io/address/${NFTAddress}`;
-  nftContractOpenSeaLink.href = `https://${networkType === 'main' ? '' : networkType + '.'}opensea.io/assets/m3-v3`;
+  nftContractOpenSeaLink.href = `https://${networkType === 'main' ? '' : networkType + '.'}opensea.io/assets/m3-v7`;
   _connectMetamask().catch(console.warn);
   _absorbSidechain().catch(console.warn);
 
