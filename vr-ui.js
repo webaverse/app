4504681ecc6b8e23dc7726233e6bfe32ce4c501a
Avaer Kazmer <a@modules.io>
2020-11-09 05:26:25 -0500

    Add initial enter handling

--- vr-ui.js
+++ vr-ui.js
@@ -2693,7 +2693,7 @@ const makeTabs = (tabs, selectedTab, size = 0.08, width = 1) => {
 
   return object;
 };
-const makeItem = (previewUrl, text, size = 0.1, width = 1) => {
+const makeItem = (item, previewUrl, text, size = 0.1, width = 1) => {
   const object = new THREE.Object3D();
 
   const cornersMesh = makeCornersMesh();
@@ -2766,6 +2766,9 @@ const makeItem = (previewUrl, text, size = 0.1, width = 1) => {
   };
   object.selectOffset = offset => {
     object.select(Math.min(Math.max(horizontalIndex + offset, 0), 1));
+  };
+  object.enter = () => {
+    console.log('enter object', item, horizontalIndex);
   };
 
   return object;
