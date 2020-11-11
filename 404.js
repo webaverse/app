782c9d5ca1f42a8d3f52441fe61ac88e3ba5bc9b
Avaer Kazmer <a@modules.io>
2020-11-11 07:12:29 -0500

    Major home page debugging

--- 404.js
+++ 404.js
@@ -8,8 +8,33 @@ if (match) {
 
   const div = document.createElement('div');
   div.classList.add('store');
+  div.innerHTML = `\
+    <section>
+      <div class=tabs>
+        <div class=tab>Me</div>
+        <div class=tab>Creators</div>
+        <div class=tab>Items</div>
+      </div>
+    </section>
+    <section>
+      <ul>
+        <li>
+          <img src="https://preview.exokit.org/[https://raw.githubusercontent.com/avaer/vrm-samples/master/vroid/male.vrm]/preview.png" class="preview">
+          <div class="wrap">
+            <img src="https://preview.exokit.org/[https://raw.githubusercontent.com/avaer/vrm-samples/master/vroid/male.vrm]/preview.png" class="avatar">
+            <div class=detail-1>avaer</div>
+            <div class=detail-2>0xdeadbeef</div>
+          </div>
+        </li>
+      </ul>
+    </section>
+    <section id=iframe-container></section>
+  `;
   document.body.appendChild(div);
+
   const iframe = document.createElement('iframe');
-  iframe.src = '/edit.html?o=';
-  document.body.appendChild(iframe);
+  iframe.classList.add('preview');
+  iframe.src = '/edit.html?o=' + `https://raw.githubusercontent.com/avaer/vrm-samples/master/vroid/male.vrm`;
+  iframe.setAttribute('frameBorder', 0)
+  document.getElementById('iframe-container').appendChild(iframe);
 }
