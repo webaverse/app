f03daeec854934c7e244ecab819c516803014514
Avaer Kazmer <a@modules.io>
2020-12-02 16:10:03 -0500

    Spacing cleanup

--- io-manager.js
+++ io-manager.js
@@ -4,7 +4,7 @@ import cameraManager from './camera-manager.js';
 import uiManager from './ui-manager.js';
 import weaponsManager from './weapons-manager.js';
 import physicsManager from './physics-manager.js';
-import { setState } from './state.js';
+import {setState} from './state.js';
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
