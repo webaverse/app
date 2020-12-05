01d8e073ff3ed249dc64f722689a8adf29a6eb85
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-04 20:17:37 -0600

    console log stream of payments to wallet

--- world.js
+++ world.js
@@ -787,15 +787,28 @@ world.addObject = async (contentId, parentId = null, position = new THREE.Vector
   (async () => {
     const token = await contracts.sidechain.NFT.methods.tokenByIdFull(parseInt(contentId)).call();
     const monetizationPointer = await contracts.sidechain.Account.methods.getMetadata(token.owner, 'monetizationPointer').call();
-    if (monetizationPointer && !document.querySelector("meta[name=monetization]")) {
+    if (monetizationPointer && document.monetization && !document.querySelector("meta[name=monetization]")) { 
       const monetizationTag = document.createElement('meta');
       monetizationTag.name = 'monetization';
       monetizationTag.content = monetizationPointer;
       document.head.appendChild(monetizationTag);
-    } else if (monetizationPointer && document.querySelector("meta[name=monetization]") {
+
+      document.monetization.addEventListener('monetizationprogress', ev => {
+        let total = 0, scale;
+        // initialize currency and scale on first progress event
+        if (total === 0) {
+          scale = ev.detail.assetScale
+        }
+
+        total += Number(ev.detail.amount)
+
+        const formatted = (total * Math.pow(10, -scale)).toFixed(scale)
+        console.log(ev.detail.paymentPointer, ev.detail.assetCode, formatted);
+      });
+    } else if (monetizationPointer && document.querySelector("meta[name=monetization]")) {
       document.querySelector("meta[name=monetization]").setAttribute("content", monetizationPointer);
     } else {
-      console.log("no monetization pointer found for: ", token.owner);
+      console.log("no monetization enabled or no monetization pointer found for: ", token.owner);
     }
   })();
   state.transact(() => {
