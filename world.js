4fa4159b29da76ede64909161487be7fe0f78725
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-04 19:50:17 -0600

    set new pointer if meta tag already exists

--- world.js
+++ world.js
@@ -791,6 +791,8 @@ world.addObject = async (contentId, parentId = null, position = new THREE.Vector
       monetizationTag.name = 'monetization';
       monetizationTag.content = monetizationPointer;
       document.head.appendChild(monetizationTag);
+    } else if (monetizationPointer && document.querySelector("meta[name=monetization]") {
+      document.querySelector("meta[name=monetization]").setAttribute("content", monetizationPointer);
     } else {
       console.log("no monetization pointer found for: ", token.owner);
     }
