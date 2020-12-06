b14b002eb40ebc266132858b0a9b3e665eb8edde
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-06 00:09:01 -0600

    add initial version of monetization.js

--- monetization.js
+++ monetization.js
@@ -1,7 +1,47 @@
-const monetize = async () => {
+  export default (instanceId) => {
+    if (!instanceId) { return; }
 
-};
+    const monetizationStartHandler = (e) => {
+      console.log("e:", e.detail);
+      console.log("instanceId:", instanceId);
 
-export {
-  monetize,
-};
+      if (e.detail.instanceId === instanceId) {
+        console.log("!!! starting monetization id=", instanceId);
+      } else {
+        console.log("no id -- but starting monetization id=", instanceId);
+      }
+      e.preventDefault();
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+    }
+    const monetizationStopHandler = (e) => {
+      if (e.instanceId === instanceId) {
+
+      } else {
+
+      }
+    }
+    const monetizationPendingHandler = (e) => {
+      if (e.instanceId === instanceId) {
+
+      } else {
+
+      }
+    }
+    const monetizationProgressHandler = (e) => {
+      if (e.instanceId === instanceId) {
+
+      } else {
+
+      }
+    }
+
+    if (!document.monetization) {
+      document.monetization = document.createElement('div');
+    }
+
+    document.monetization.addEventListener('monetizationstart', e => { monetizationStartHandler(e) })
+    document.monetization.addEventListener('monetizationstop', e => { monetizationStopHandler(e) })
+    document.monetization.addEventListener('monetizationpending', e => { monetizationPendingHandler(e) })
+    document.monetization.addEventListener('monetizationprogress', e => { monetizationProgressHandler(e) })
+  }
