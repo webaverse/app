d7009c8281bdda1b7fdcf17f3876ff3a95682920
Avaer Kazmer <a@modules.io>
2020-12-06 15:19:21 -0500

    More world load debugging

--- universe.js
+++ universe.js
@@ -2,6 +2,7 @@ import * as THREE from './three.module.js';
 import {rigManager} from './rig.js';
 import {scene} from './app-object.js';
 import {Sky} from './Sky.js';
+import {world} from './world.js';
 import {GuardianMesh} from './land.js';
 import minimap from './minimap.js';
 
@@ -49,6 +50,7 @@ let skybox;
   scene.add(skybox);
 }
 const universeSpecs = [{
+	name: 'Erithor',
   extents: [
     0, 0, -10 - 4,
     10, 3, -4,
@@ -56,12 +58,14 @@ const universeSpecs = [{
 }];
 const worldObjects = universeSpecs.map(spec => {
   const guardianMesh = GuardianMesh(spec.extents, blueColor);
+  guardianMesh.name = spec.name;
   const worldObject = minimap.addWorld(spec.extents);
   guardianMesh.worldObject = worldObject;
   scene.add(guardianMesh);
   return guardianMesh;
 });
 
+let currentWorld = null;
 const lastCoord = new THREE.Vector3(0, 0, 0);
 const locationIcon = document.getElementById('location-icon');
 const locationLabel = document.getElementById('location-label');
@@ -74,29 +78,42 @@ const update = () => {
   skybox.position.copy(rigManager.localRig.inputs.hmd.position);
   skybox.update();
 
+  const _parseUniverseSpec = spec => localBox.set(localVector.fromArray(spec.extents, 0), localVector2.fromArray(spec.extents, 3));
+  const intersectionIndex = universeSpecs.findIndex(spec =>
+  	_parseUniverseSpec(spec)
+  	  .containsPoint(rigManager.localRig.inputs.hmd.position)
+  );
+  const intersection = universeSpecs[intersectionIndex];
+  if (intersection) {
+    const newWorld = worldObjects[intersectionIndex];
+    if (newWorld !== currentWorld) {
+    	currentWorld = newWorld;
+
+	    const objects = world.getObjects();
+	    for (const object of objects) {
+	      world.removeObject(object.instanceId);
+	    }
+
+	    const u = `https://avaer.github.io/physicscube/index.js`;
+	    const center = _parseUniverseSpec(intersection).getCenter(localVector);
+	    world.addObject(u, null, center, new THREE.Quaternion());
+	  }
+  } else {
+  	currentWorld = null;
+  }
+
   for (const worldObject of worldObjects) {
     worldObject.material.uniforms.uColor.value.setHex(blueColor);
   }
-
-  const intersectionIndex = universeSpecs.findIndex(spec =>
-  	localBox.set(localVector.fromArray(spec.extents, 0), localVector2.fromArray(spec.extents, 3))
-  	  .containsPoint(rigManager.localRig.inputs.hmd.position)
-  );
-  let worldName;
-  const intersected = intersectionIndex !== -1;
-  if (intersected) {
-    const worldObject = worldObjects[intersectionIndex];
-    worldObject.material.uniforms.uColor.value.setHex(greenColor);
-    worldName = 'Erithor';
-  } else {
-  	worldName = 'The Void';
+  if (currentWorld) {
+  	currentWorld.material.uniforms.uColor.value.setHex(greenColor);
   }
 
   _getCurrentCoord(rigManager.localRig.inputs.hmd.position, localVector);
   if (!localVector.equals(lastCoord)) {
-  	locationIcon.classList.toggle('highlight', intersected);
-  	locationLabel.classList.toggle('highlight', intersected);
-    locationLabel.innerText = worldName + ` @${localVector.x},${localVector.z}`;
+  	locationIcon.classList.toggle('highlight', !!intersection);
+  	locationLabel.classList.toggle('highlight', !!intersection);
+    locationLabel.innerText = (currentWorld ? currentWorld.name : 'The Void') + ` @${localVector.x},${localVector.z}`;
     lastCoord.copy(localVector);
   }
 };
