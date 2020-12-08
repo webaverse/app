bd5373c56274f986d5543c85b3e35fe0ead73640
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-08 03:45:49 -0600

    make sure to close the icon too

--- weapons-manager.js
+++ weapons-manager.js
@@ -2188,6 +2188,7 @@ const _updateMenu = () => {
   locationIcon.classList.toggle('open', false);
   profileIcon.classList.toggle('open', false);
   itemIcon.classList.toggle('open', false);
+  itemMonetizationIcon.classList.toggle('open', false);
 
   deployMesh.visible = false;
 
