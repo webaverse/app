c8c373eb869266dcd1eb70fffde289cbfbbda7db
Avaer Kazmer <a@modules.io>
2020-12-25 19:20:47 -0500

    Add no-ext support to util.js getExt method

--- util.js
+++ util.js
@@ -31,7 +31,7 @@ export function uint8Array2hex(uint8Array) {
 }
 export function getExt(fileName) {
   const match = fileName.match(/\.([^\.]+)$/);
-  return match && match[1].toLowerCase();
+  return match ? match[1].toLowerCase() : '';
 }
 export function downloadFile(file, filename) {
   const blobURL = URL.createObjectURL(file);
