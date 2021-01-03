378808ffd50cbec8587c41fc476d371bdd09886a
Avaer Kazmer <a@modules.io>
2021-01-03 08:07:38 -0500

    Hook in new runtime portal code

--- runtime.js
+++ runtime.js
@@ -3,6 +3,7 @@ import {GLTFLoader} from './GLTFLoader.js';
 // import {KTX2Loader} from './KTX2Loader.js';
 import {VOXLoader} from './VOXLoader.js';
 import {CSS3DObject} from './CSS3DRenderer.js';
+import {BufferGeometryUtils} from './BufferGeometryUtils.js';
 import {MeshoptDecoder} from './meshopt_decoder.module.js';
 import {BasisTextureLoader} from './BasisTextureLoader.js';
 // import {GLTFExporter} from './GLTFExporter.js';
@@ -16,7 +17,8 @@ import {loginManager} from './login.js';
 import {makeTextMesh} from './vr-ui.js';
 import {renderer, scene2, appManager} from './app-object.js';
 import wbn from './wbn.js';
-// import {storageHost} from './constants.js';
+import {portalMaterial} from './shaders.js';
+import {baseUnit} from './constants.js';
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
@@ -882,7 +884,7 @@ const _loadPortal = async file => {
   }
   href = href.replace(/^([\S]*)/, '$1');
 
-  const geometry = new THREE.CircleBufferGeometry(1, 32)
+  /* const geometry = new THREE.CircleBufferGeometry(1, 32)
     .applyMatrix4(new THREE.Matrix4().makeScale(0.5, 1, 1))
     .applyMatrix4(new THREE.Matrix4().makeTranslation(0, 1, 0));
   const material = new THREE.ShaderMaterial({
@@ -963,6 +965,111 @@ const _loadPortal = async file => {
     } else {
       inRangeStart = null;
     }
+  }); */
+
+  const geometries = [];
+
+  const w = baseUnit;
+  const planeGeometry = new THREE.PlaneBufferGeometry(w, w, w, w)
+    .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI/2)));
+  for (let i = 0; i < planeGeometry.attributes.position.array.length; i += 3) {
+    planeGeometry.attributes.position.array[i+1] = Math.random() * 0.2;
+  }
+  planeGeometry.setAttribute('particle', new THREE.BufferAttribute(new Float32Array(planeGeometry.attributes.position.array.length/3), 1));
+  planeGeometry.setAttribute('bar', new THREE.BufferAttribute(new Float32Array(planeGeometry.attributes.position.array.length/3), 1));
+  geometries.push(planeGeometry);
+
+  const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
+  const numBars = 10;
+  // xz
+  for (let dx = 1; dx < numBars; dx++) {
+    for (let dz = 1; dz < numBars; dz++) {
+      const g = boxGeometry.clone()
+        .applyMatrix4(new THREE.Matrix4().makeScale(0.01, w, 0.01))
+        .applyMatrix4(new THREE.Matrix4().makeTranslation(-w/2 + dx/numBars * w, w/2, -w/2 + dz/numBars * w));
+      g.setAttribute('particle', new THREE.BufferAttribute(new Float32Array(boxGeometry.attributes.position.array.length/3), 1));
+      g.setAttribute('bar', new THREE.BufferAttribute(new Float32Array(boxGeometry.attributes.position.array.length/3).fill(1), 1));
+      geometries.push(g);
+    }
+  }
+  // xy
+  for (let dx = 1; dx < numBars; dx++) {
+    for (let dy = 1; dy < numBars; dy++) {
+      const g = boxGeometry.clone()
+        .applyMatrix4(new THREE.Matrix4().makeScale(0.01, 0.01, w))
+        .applyMatrix4(new THREE.Matrix4().makeTranslation(-w/2 + dx/numBars * w, dy/numBars * w, 0));
+      g.setAttribute('particle', new THREE.BufferAttribute(new Float32Array(boxGeometry.attributes.position.array.length/3), 1));
+      g.setAttribute('bar', new THREE.BufferAttribute(new Float32Array(boxGeometry.attributes.position.array.length/3).fill(1), 1));
+      geometries.push(g);
+    }
+  }
+  // yz
+  for (let dy = 1; dy < numBars; dy++) {
+    for (let dz = 1; dz < numBars; dz++) {
+      const g = boxGeometry.clone()
+        .applyMatrix4(new THREE.Matrix4().makeScale(w, 0.01, 0.01))
+        .applyMatrix4(new THREE.Matrix4().makeTranslation(0, dy/numBars * w, -w/2 + dz/numBars * w));
+      g.setAttribute('particle', new THREE.BufferAttribute(new Float32Array(boxGeometry.attributes.position.array.length/3), 1));
+      g.setAttribute('bar', new THREE.BufferAttribute(new Float32Array(boxGeometry.attributes.position.array.length/3).fill(1), 1));
+      geometries.push(g);
+    }
+  }
+
+  for (let i = 0; i < 20; i++) {
+    const width = 0.02;
+    const height = 0.2;
+    const g = boxGeometry.clone()
+      .applyMatrix4(new THREE.Matrix4().makeScale(width, height, width))
+      .applyMatrix4(new THREE.Matrix4().makeTranslation(width/2 + (-1/2 + Math.random()) * w * (1-width/2), 0.3/2 + Math.random() * (1-width/2), height/2 + (-1/2 + Math.random()) * w * (1-width/2)));
+    g.setAttribute('particle', new THREE.BufferAttribute(new Float32Array(g.attributes.position.array.length/3).fill(1), 1));
+    g.setAttribute('bar', new THREE.BufferAttribute(new Float32Array(g.attributes.position.array.length/3), 1));
+    geometries.push(g);
+  }
+
+  const geometry = BufferGeometryUtils.mergeBufferGeometries(geometries);
+  const material = portalMaterial.clone();
+  const portalMesh = new THREE.Mesh(geometry, material);
+  portalMesh.boundingBox = new THREE.Box3(
+    new THREE.Vector3(-w/2, 0, -w/2),
+    new THREE.Vector3(w/2, w, w/2),
+  );
+  portalMesh.frustumCulled = false;
+  portalMesh.update = () => {
+    const transforms = rigManager.getRigTransforms();
+    const {position} = transforms[0];
+
+    const now = Date.now();
+    portalMesh.material.uniforms.uTime.value = (now%500)/500;
+    portalMesh.material.uniforms.uDistance.value = portalMesh.boundingBox.distanceToPoint(position);
+    portalMesh.material.uniforms.uUserPosition.value.copy(position);
+  };
+  portalMesh.destroy = () => {
+    appManager.destroyApp(appId);
+  };
+
+  const appId = ++appIds;
+  const app = appManager.createApp(appId);
+  appManager.setAnimationLoop(appId, () => {
+    portalMesh.update();
+
+    /* const distance = rigManager.localRig.inputs.hmd.position.distanceTo(
+      localVector.copy(portalMesh.position)
+        .add(localVector2.set(0, 1, 0).applyQuaternion(portalMesh.quaternion))
+    );
+    if (distance < 1) {
+      const now = Date.now();
+      if (inRangeStart !== null) {
+        const timeDiff = now - inRangeStart;
+        if (timeDiff >= 2000) {
+          renderer.setAnimationLoop(null);
+          window.location.href = href;
+        }
+      } else {
+        inRangeStart = now;
+      }
+    } else {
+      inRangeStart = null;
+    } */
   });
 
   return portalMesh;
