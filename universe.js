0c7f12f8544e1c2178b6099ee6c6def47f33da9f
Avaer Kazmer <a@modules.io>
2021-01-04 16:32:27 -0500

    Support dynamic flag it universe load default world

--- universe.js
+++ universe.js
@@ -201,8 +201,8 @@ const loadDefaultWorld = async () => {
     const position = objectSpec.position ? new THREE.Vector3().fromArray(objectSpec.position) : new THREE.Vector3();
     const quaternion = objectSpec.quaternion ? new THREE.Quaternion().fromArray(objectSpec.quaternion) : new THREE.Quaternion();
     // const scale = objectSpec.scale ? new THREE.Vector3().fromArray(objectSpec.scale) : new THREE.Vector3();
-    const {physics, physics_url} = objectSpec;
-    await world.addStaticObject(objectSpec.start_url, null, position, quaternion, {
+    const {physics, physics_url, dynamic} = objectSpec;
+    await world[dynamic ? 'addObject' : 'addStaticObject'](objectSpec.start_url, null, position, quaternion, {
       physics,
       physics_url,
     });
