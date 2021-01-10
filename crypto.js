8e823b763e44dc6401529fffce5ba67e903907fa
Avaer Kazmer <a@modules.io>
2021-01-09 23:10:49 -0500

    Add getParcel method in crypto.js

--- crypto.js
+++ crypto.js
@@ -1,7 +1,22 @@
+/* import Web3 from './web3.min.js';
+import bip39 from './bip39.js';
+import hdkeySpec from './hdkey.js';
+const hdkey = hdkeySpec.default;
+import ethereumJsTx from './ethereumjs-tx.js';
+import {makePromise} from './util.js';
+import {storageHost, web3SidechainEndpoint} from './constants.js';
+const {Transaction, Common} = ethereumJsTx; */
+
 import {loginManager} from './login.js';
+import {web3, contracts} from './blockchain.js';
 
 const mintToken = (file, opts) => loginManager.uploadFile(file, opts);
+const getParcel = id => {
+  id = new web3['sidechain'].utils.BN(id);
+  return contracts['sidechain'].LAND.methods.tokenByIdFull(id).call();
+};
 
 export {
   mintToken,
+  getParcel,
 };
