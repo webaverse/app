04aebd27bbe558f0716dc03fabc83b939d16434b
Avaer Kazmer <a@modules.io>
2021-01-11 04:12:32 -0500

    Io cleanup

--- physics-manager.js
+++ physics-manager.js
@@ -406,11 +406,4 @@ const _updatePhysics = timeDiff => {
 };
 physicsManager.update = _updatePhysics;
 
-document.getElementById('key-f').addEventListener('click', e => {
-  e.preventDefault();
-  e.stopPropagation();
-
-  physicsManager.setFlyState(!physicsManager.getFlyState());
-});
-
 export default physicsManager;
