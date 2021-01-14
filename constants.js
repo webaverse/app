5fb7d2ff3521e73a25dea30cca6486453a8d5e7a
Avaer Kazmer <a@modules.io>
2021-01-14 00:30:31 -0500

    Move rarity colors to constants.js

--- constants.js
+++ constants.js
@@ -1,6 +1,14 @@
 export const baseUnit = 4;
 export const previewExt = 'jpg';
 
+export const rarityColors = {
+  common: [0xCACACA, 0x7B7B7B],
+  uncommon: [0x80cf3f, 0x3a7913],
+  rare: [0x2fd5e8, 0x1258a2],
+  epic: [0xbd3ffa, 0x460d7f],
+  legendary: [0xfdae53, 0xff7605],
+};
+
 export const storageHost = 'https://ipfs.exokit.org';
 export const previewHost = 'https://preview.exokit.org'
 export const worldsHost = 'https://worlds.exokit.org';
