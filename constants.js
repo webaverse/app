44b0b298b7e32775278d94f58dec88d5ccb706a7
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-17 14:34:54 -0600

    update rarity constants

--- constants.js
+++ constants.js
@@ -2,11 +2,11 @@ export const baseUnit = 4;
 export const previewExt = 'jpg';
 
 export const rarityColors = {
-  common: [0xCACACA, 0x7B7B7B],
-  uncommon: [0x80cf3f, 0x3a7913],
-  rare: [0x2fd5e8, 0x1258a2],
-  epic: [0xbd3ffa, 0x460d7f],
-  legendary: [0xfdae53, 0xff7605],
+  common: [0xDCDCDC, 0x373737],
+  uncommon: [0xff8400, 0x875806],
+  rare: [0x00CE21, 0x00560E],
+  epic: [0x00B3DB, 0x003743],
+  legendary: [0xAD00EA, 0x32002D],
 };
 
 export const storageHost = 'https://ipfs.exokit.org';
