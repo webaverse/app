11625294a621be9a7634c81232056440beeecea4
Avaer Kazmer <a@modules.io>
2021-01-20 01:34:32 -0500

    Break out endpoints in constants.js

--- constants.js
+++ constants.js
@@ -18,5 +18,6 @@ export const localstorageHost = 'https://localstorage.webaverse.com';
 export const loginEndpoint = 'https://login.exokit.org';
 export const tokensHost = 'https://tokens.webaverse.com';
 export const landHost = 'https://land.webaverse.com';
-export const web3SidechainEndpoint = 'https://ethereums.exokit.org';
+export const web3MainnetSidechainEndpoint = 'https://ethereums.exokit.org:8545';
+export const web3RinkebySidechainEndpoint = 'https://ethereums.exokit.org:8546';
 export const homeScnUrl = `https://webaverse.github.io/street/street.scn`;
