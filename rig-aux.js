01fb710b4ab28455c35c6c4826a866242553c2b7
Avaer Kazmer <a@modules.io>
2021-01-26 17:22:51 -0500

    Add edge case detection in rig aux

--- rig-aux.js
+++ rig-aux.js
@@ -33,27 +33,9 @@ class RigAux {
       gltfLoader.load(srcUrl, accept, function onprogress() {}, reject);
     }); */
     const root = o;
-    const animations = o.getAnimations();
     /* o = o.scene;
     // o.scale.multiplyScalar(0.2);
     scene.add(o); */
-    
-    
-    const sitComponent = o.components.find(c => c.type === 'sit');
-    // console.log('got sit component', sitComponent);
-  	const {sitBone = 'Spine', walkAnimation = 'walk'} = sitComponent;
-
-    const animation = animations.find(a => a.name === walkAnimation);
-
-    // hacks
-    {
-      root.position.y = 0;
-    }
-    
-    const mixer = new THREE.AnimationMixer(root);
-    const clip = animation;
-    const action = mixer.clipAction(clip);
-    action.play();
     
     let mesh = null;
     root.traverse(o => {
@@ -62,48 +44,75 @@ class RigAux {
       }
     });
     if (mesh) {
-      const {skeleton} = mesh;
-      const spineBoneIndex = skeleton.bones.findIndex(b => b.name === sitBone);
-      const spineBone = root.getObjectByName(sitBone);
-      // const spine = skeleton.bones[spineBoneIndex];
-      // const spineBoneMatrix = skeleton.boneMatrices[spineBoneIndex];
-      // const spineBoneMatrixInverse = skeleton.boneInverses[spineBoneIndex];
-      // console.log('got spine', mesh, skeleton);
-      // window.THREE = THREE;
+      const animations = o.getAnimations();
       
-      physicsManager.setSitState(true);
-      // const sitTarget = physicsManager.getSitTarget();
-      physicsManager.setSitController(root);
-      physicsManager.setSitTarget(spineBone);
-      
-      rigManager.localRig.sitState = true;
+      const sitComponent = o.components.find(c => c.type === 'sit');
+      // console.log('got sit component', sitComponent);
+      const {sitBone = 'Spine', walkAnimation = 'walk'} = sitComponent;
+      const animation = animations.find(a => a.name === walkAnimation);
 
-      let lastTimestamp = Date.now();
-      const smoothVelocity = new THREE.Vector3();
-      const update = now => {
-        // const speed = 0.003;
-        const timeDiff = now - lastTimestamp;
+      if (animation) {
+        // hacks
+        {
+          root.position.y = 0;
+        }
         
-        // console.log('velocity', physicsManager.velocity.length());
-        
-        action.weight = physicsManager.velocity.length() * 10;
+        const mixer = new THREE.AnimationMixer(root);
+        const clip = animation;
+        const action = mixer.clipAction(clip);
+        action.play();
 
-        const deltaSeconds = timeDiff / 1000;
-        mixer.update(deltaSeconds);
-        lastTimestamp = now;
-        
-        // spineBone.updateMatrixWorld();
-        // const bonePosition = spineBone.getWorldPosition(new THREE.Vector3());
-        
-        
-        // rigManager.localRig.sitTarget.matrixWorld.decompose(spine.position, spine.quaternion, localVector);
-        
-        // rigManager.localRigMatrix.decompose(localVector, localQuaternion, localVector2);
-        // rigManager.setLocalRigMatrix(rigManager.localRigMatrix.compose(localVector, cubeMesh.quaternion, localVector2));
-      };
-      this.sittables.push({
-        update,
-      });
+        const {skeleton} = mesh;
+        const spineBoneIndex = skeleton.bones.findIndex(b => b.name === sitBone);
+        const spineBone = root.getObjectByName(sitBone);
+        if (spineBoneIndex !== -1 && spineBone) {
+          // const spine = skeleton.bones[spineBoneIndex];
+          // const spineBoneMatrix = skeleton.boneMatrices[spineBoneIndex];
+          // const spineBoneMatrixInverse = skeleton.boneInverses[spineBoneIndex];
+          // console.log('got spine', mesh, skeleton);
+          // window.THREE = THREE;
+          
+          physicsManager.setSitState(true);
+          // const sitTarget = physicsManager.getSitTarget();
+          physicsManager.setSitController(root);
+          physicsManager.setSitTarget(spineBone);
+          
+          rigManager.localRig.sitState = true;
+
+          let lastTimestamp = Date.now();
+          const smoothVelocity = new THREE.Vector3();
+          const update = now => {
+            // const speed = 0.003;
+            const timeDiff = now - lastTimestamp;
+            
+            // console.log('velocity', physicsManager.velocity.length());
+            
+            action.weight = physicsManager.velocity.length() * 10;
+
+            const deltaSeconds = timeDiff / 1000;
+            mixer.update(deltaSeconds);
+            lastTimestamp = now;
+            
+            // spineBone.updateMatrixWorld();
+            // const bonePosition = spineBone.getWorldPosition(new THREE.Vector3());
+            
+            
+            // rigManager.localRig.sitTarget.matrixWorld.decompose(spine.position, spine.quaternion, localVector);
+            
+            // rigManager.localRigMatrix.decompose(localVector, localQuaternion, localVector2);
+            // rigManager.setLocalRigMatrix(rigManager.localRigMatrix.compose(localVector, cubeMesh.quaternion, localVector2));
+          };
+          this.sittables.push({
+            update,
+          });
+        } else {
+          console.warn('could not find sit bone in model: ' + sitBone + '; bones available: ' + JSON.stringify(skeleton.bones.map(b => b.name)));
+        }
+      } else {
+        console.warn('could not find walk animation in model: ' + walkAnimation + '; animation available: ' + JSON.stringify(animations.map(a => a.name)));
+      }
+    } else {
+      console.warn('no skinned mesh in model');
     }
   }
   update(now) {
