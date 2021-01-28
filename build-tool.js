93e0e3f686c6c5c3760978c6c52cdd9952108ba8
Avaer Kazmer <a@modules.io>
2021-01-28 01:10:41 -0500

    Remove physics placement collision from self-consideration

--- build-tool.js
+++ build-tool.js
@@ -525,6 +525,7 @@ const makeShapeMesh = () => {
     updateGrabbedObject(modeMesh, appManager.grabbedObjectMatrices[0], localMatrix, {
       collisionEnabled: true,
       handSnapEnabled: false,
+      appManager,
       geometryManager,
       gridSnap,
     });
