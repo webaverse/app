94ea6e70c2a2956493b3f0ec11b97239da4ed036
Avaer Kazmer <a@modules.io>
2021-01-29 05:41:05 -0500

    Update runtime.js

--- runtime.js
+++ runtime.js
@@ -317,7 +317,6 @@ const _loadGltf = async (file, {optimize = false, physics = false, physics_url =
       const physicsId = physicsManager.addGeometry(physicsMesh);
       physicsIds.push(physicsId);
       staticPhysicsIds.push(physicsId);
-      debugger;
     }
     if (physicsBuffer) {
       const physicsId = physicsManager.addCookedGeometry(physicsBuffer, mesh.position, mesh.quaternion);
