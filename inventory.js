80731f42cf5ca8849a7c6351e4171e156647dd7d
Avaer Kazmer <a@modules.io>
2021-01-31 22:40:49 -0500

    Load real inventory slots

--- inventory.js
+++ inventory.js
@@ -3,6 +3,58 @@ import Avatar from './avatars/avatars.js';
 import {RigAux} from './rig-aux.js';
 import runtime from './runtime.js';
 
+const inventorySpecs = [
+  {
+    "start_url": "https://ipfs.exokit.org/QmWgij93j5oU9SHGtGcKATJVsQ5ZEarqjg3bZDZMxXLdjF.vrm",
+    "preview_url": "https://preview.exokit.org/QmWgij93j5oU9SHGtGcKATJVsQ5ZEarqjg3bZDZMxXLdjF.vrm/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/cat-in-hat/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/cat-in-hat/cat-in-hat.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/sword/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/sword/sword.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-pet/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-pet/dragon.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-mount/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-mount/dragon.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-fly/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-fly/dragon-fly.glb]/preview.png",
+  },
+];
+
+{
+  let inventorySpecIndex = 0;
+  const inventorySlotsEl = document.getElementById('inventory-slots');
+  for (const rowEl of inventorySlotsEl.children) {
+    if (inventorySpecIndex < inventorySpecs.length) {
+      for (const slotEl of rowEl.children) {
+        if (inventorySpecIndex < inventorySpecs.length) {
+          const inventorySpec = inventorySpecs[inventorySpecIndex++];
+          
+          const img = document.createElement('img');
+          img.src = inventorySpec.preview_url;
+          img.classList.add('item');
+          img.setAttribute('draggable', '');
+          
+          slotEl.appendChild(img);
+        } else {
+          break;
+        }
+      }
+    } else {
+      break;
+    }
+  }
+}
+
 const inventoryAvatarScene = new THREE.Scene();
 const inventoryAvatarCamera = new THREE.PerspectiveCamera();
 const inventoryAvatarRenderer = (() => {
