c3a4cbd5a6847d8ece7c44887d4380a7886e5e85
Avaer Kazmer <a@modules.io>
2021-02-02 06:26:02 -0500

    Update universe.js

--- universe.js
+++ universe.js
@@ -234,8 +234,8 @@ const enterWorld = async worldSpec => {
           physics_url,
           autoScale,
         });
-        if (autoRun) {
-          o.use();
+        if (autoRun && o.useAux) {
+          o.useAux();
         }
       });
       promises.push.apply(promises, ps);
