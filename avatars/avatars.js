951f1d708b91943a215b995ce5ddedbbc9e05a18
Avaer Kazmer <a@modules.io>
2021-02-05 16:03:45 -0500

    Bake cleanup

--- avatars/avatars.js
+++ avatars/avatars.js
@@ -10,6 +10,9 @@ import skeletonString from './skeleton.js';
 import easing from '../easing.js';
 import CBOR from '../cbor.js';
 
+import {FBXLoader} from '../FBXLoader.js';
+import {downloadFile} from '../util.js';
+
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
 const localQuaternion = new THREE.Quaternion();
@@ -227,8 +230,6 @@ const loadPromise = (async () => {
   hitAnimation = animations.find(a => a.isHit);
   
   /* // bake animations
-  import {FBXLoader} from '../FBXLoader.js';
-  import {downloadFile} from '../util.js';
   (async () => {
     animations = [];
     const fbxLoader = new FBXLoader();
