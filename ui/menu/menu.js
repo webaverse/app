b31a97927643e3a4ad7749da9b1f398eb47f9de0
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-02-10 22:48:04 -0800

    MenuUI progress

--- ui/menu/menu.js
+++ ui/menu/menu.js
@@ -5,6 +5,85 @@ export const MenuUI = {};
 MenuUI.isOpen = false; // boolean
 MenuUI.parentRef = null; // HTML element
 
+const inventorySpecs = [
+  {
+    "start_url": "https://ipfs.exokit.org/QmWgij93j5oU9SHGtGcKATJVsQ5ZEarqjg3bZDZMxXLdjF/model.vrm",
+    "preview_url": "https://preview.exokit.org/QmWgij93j5oU9SHGtGcKATJVsQ5ZEarqjg3bZDZMxXLdjF.vrm/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/cat-in-hat/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/cat-in-hat/cat-in-hat.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/sword/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/sword/sword.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-pet/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-pet/dragon.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-mount/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-mount/dragon.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-fly/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-fly/dragon-fly.glb]/preview.png",
+  },
+  {
+    "start_url": "https://ipfs.exokit.org/QmWgij93j5oU9SHGtGcKATJVsQ5ZEarqjg3bZDZMxXLdjF/model.vrm",
+    "preview_url": "https://preview.exokit.org/QmWgij93j5oU9SHGtGcKATJVsQ5ZEarqjg3bZDZMxXLdjF.vrm/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/cat-in-hat/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/cat-in-hat/cat-in-hat.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/sword/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/sword/sword.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-pet/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-pet/dragon.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-mount/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-mount/dragon.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-fly/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-fly/dragon-fly.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-fly/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-fly/dragon-fly.glb]/preview.png",
+  },
+  {
+    "start_url": "https://ipfs.exokit.org/QmWgij93j5oU9SHGtGcKATJVsQ5ZEarqjg3bZDZMxXLdjF/model.vrm",
+    "preview_url": "https://preview.exokit.org/QmWgij93j5oU9SHGtGcKATJVsQ5ZEarqjg3bZDZMxXLdjF.vrm/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/cat-in-hat/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/cat-in-hat/cat-in-hat.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/sword/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/sword/sword.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-pet/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-pet/dragon.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-mount/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-mount/dragon.glb]/preview.png",
+  },
+  {
+    "start_url": "https://avaer.github.io/dragon-fly/manifest.json",
+    "preview_url": "https://preview.exokit.org/[https://avaer.github.io/dragon-fly/dragon-fly.glb]/preview.png",
+  },
+];
+
 MenuUI.bind = async () => {
   const response = await fetch("/ui/menu/menu.html");
   const html = await response.text();
@@ -12,7 +91,19 @@ MenuUI.bind = async () => {
   parent.id = "MenuUI";
   parent.innerHTML = html;
   document.body.appendChild(parent);
-  MenuUI.parentRef = document.getElementById(parent.id)
+  MenuUI.parentRef = document.getElementById(parent.id);
+};
+
+MenuUI.loadInventory = () => {
+  const listContainer = document.getElementById("MenuUI-items-list");
+  const cardsHTML = inventorySpecs.map(item => {
+    return `
+      <div class="MenuUI-items-list-card">
+        <img class="MenuUI-items-list-card-image" src="${item.preview_url}">
+      </div>
+    `;
+  }).join(" ");
+  listContainer.innerHTML = cardsHTML;
 }
 
 MenuUI.toggle = () => {
@@ -20,8 +111,9 @@ MenuUI.toggle = () => {
   if (MenuUI.isOpen) {
     MenuUI.parentRef.style.display = "flex";
     document.exitPointerLock();
+    MenuUI.loadInventory();
   } else {
     MenuUI.parentRef.style.display = "none";
     cameraManager.requestPointerLock();
   }
-}
+};
