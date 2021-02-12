5acc03b0b36b37bc2d6b59366ab313c1b4103cc0
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-02-11 23:49:53 -0800

    menu styles and navigation tabs

--- mithril-ui/menu/menu.js
+++ mithril-ui/menu/menu.js
@@ -1,13 +1,11 @@
-import {menuActions} from '../store/actions.js';
-import {menuState} from '../store/state.js';
+import {MenuNav} from './MenuNav.js';
 
 export const Menu = (initialVnode) => {
   return {
     view: (vnode) => {
       return m("div", { class: "Menu-container" }, [
         m("div", { class: "Menu-content" }, [
-          m("h1", "Menu"),
-          m("button", { onclick: () => menuActions.setIsOpen(false) }, "Close Window")
+          m(MenuNav),
         ]),
       ]);
     }
--- mithril-ui/store/actions.js
+++ mithril-ui/store/actions.js
@@ -3,12 +3,20 @@ import cameraManager from '../../camera-manager.js';
 
 export const appActions = {
 
-}
+};
 
 export const menuActions = {
   setIsOpen: (isOpen) => {
     isOpen ? document.exitPointerLock() : cameraManager.requestPointerLock();
     menuState.isOpen = isOpen;
-    m.redraw()
+    m.redraw();
   },
+  setActiveTab: (tab) => {
+    menuState.activeTab = tab;
+    m.redraw();
+  },
+  setInventory: (items) => {
+    menuState.inventory = items;
+    m.redraw();
+  }
 };
