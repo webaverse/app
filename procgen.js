768428a77ffa7e8294b19d7b67fde80d70a2c085
Avaer Kazmer <a@modules.io>
2021-02-11 23:37:01 -0500

    Update procgen.js

--- procgen.js
+++ procgen.js
@@ -7,7 +7,7 @@ const rarities = [
   'epic',
   'legendary',
 ];
-const rarityFactors = [80, 15, 4, 0.99, 0.01].map(n => n / 100);
+const rarityFactors = [80, 18, 1.8, 0.18, 0.02].map(n => n / 100);
 
 function makeRandom(rng, n) {
   const raw = new Float32Array(n);
