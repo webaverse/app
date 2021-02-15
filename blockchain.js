cd7fe93ce6f1a20a56b1ea93928dd3eb82960252
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-02-14 20:20:46 -0600

    set main chain true

--- blockchain.js
+++ blockchain.js
@@ -37,7 +37,7 @@ function _setMainChain(isMainChain) {
   }
 }
 // _setMainChain(!/^test\./.test(location.hostname));
-_setMainChain(/^main\./.test(location.hostname));
+_setMainChain(true);
 
 const contracts = {
   front: {
