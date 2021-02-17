a7f5a64083a07b6a1fab82b30123233ddd2e5987
Avaer Kazmer <a@modules.io>
2021-02-16 22:11:01 -0500

    Hook in activate manager

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ import minimap from './minimap.js';
 import weaponsManager from './weapons-manager.js';
 import cameraManager from './camera-manager.js';
 import hpManager from './hp-manager.js';
+import activateManager from './activate-manager.js';
 import {bindInterface as inventoryBindInterface} from './inventory.js';
 import fx from './fx.js';
 import {parseCoord} from './util.js';
@@ -346,6 +347,7 @@ export default class App {
 
       weaponsManager.update();
       hpManager.update();
+      activateManager.update();
       fx.update();
 
       appManager.tick(timestamp, frame);
