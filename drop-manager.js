a3d2dd9992404ff9a56b202f37cdae7c2899a767
Avaer Kazmer <a@modules.io>
2021-02-17 00:18:05 -0500

    Test drops on drop activation

--- drop-manager.js
+++ drop-manager.js
@@ -82,68 +82,66 @@ const loadPromise = (async () => {
     const o = sphere;
 
     let animation = null;
-    const ticker = {
-      update() {
-        const now = Date.now(); 
-        if (!animation) {
-          rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector);
-          localVector.y = 0;
-          const distance = localVector.distanceTo(o.position);
-          if (distance < 1) {
-            animation = {
-              startPosition: o.position.clone(),
-              startTime: now,
-              endTime: now + 1000,
-            };
-          }
+    o.update = () => {
+      const now = Date.now(); 
+      if (!animation) {
+        rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector);
+        localVector.y = 0;
+        const distance = localVector.distanceTo(o.position);
+        if (distance < 1) {
+          animation = {
+            startPosition: o.position.clone(),
+            startTime: now,
+            endTime: now + 1000,
+          };
         }
-        if (animation) {
-            const headOffset = 0.5;
-            const bodyOffset = -0.3;
-            const tailTimeFactorCutoff = 0.8;
-            const timeDiff = now - animation.startTime;
-            const timeFactor = Math.min(Math.max(timeDiff / (animation.endTime - animation.startTime), 0), 1);
-            if (timeFactor < 1) {
-              if (timeFactor < tailTimeFactorCutoff) {
-                const f = cubicBezier(timeFactor);
-                rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector)
-                  .add(localVector2.set(0, headOffset, 0));
-                o.position.copy(animation.startPosition).lerp(localVector, f);
-              } else {
-                {
-                  const f = cubicBezier(tailTimeFactorCutoff);
-                  rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector)
-                    .add(localVector2.set(0, headOffset, 0));
-                  o.position.copy(animation.startPosition).lerp(localVector, f);
-                }
-                {
-                  const tailTimeFactor = (timeFactor - tailTimeFactorCutoff) / (1 - tailTimeFactorCutoff);
-                  const f = cubicBezier2(tailTimeFactor);
-                  rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector)
-                    .add(localVector2.set(0, bodyOffset, 0));
-                  o.position.lerp(localVector, f);
-                  o.scale.copy(defaultScale).multiplyScalar(1 - tailTimeFactor);
-                }
-              }
-            } else {
-              scene.remove(o);
-              tickers.splice(tickers.indexOf(ticker), 1);
+      }
+      if (animation) {
+        const headOffset = 0.5;
+        const bodyOffset = -0.3;
+        const tailTimeFactorCutoff = 0.8;
+        const timeDiff = now - animation.startTime;
+        const timeFactor = Math.min(Math.max(timeDiff / (animation.endTime - animation.startTime), 0), 1);
+        if (timeFactor < 1) {
+          if (timeFactor < tailTimeFactorCutoff) {
+            const f = cubicBezier(timeFactor);
+            rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector)
+              .add(localVector2.set(0, headOffset, 0));
+            o.position.copy(animation.startPosition).lerp(localVector, f);
+          } else {
+            {
+              const f = cubicBezier(tailTimeFactorCutoff);
+              rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector)
+                .add(localVector2.set(0, headOffset, 0));
+              o.position.copy(animation.startPosition).lerp(localVector, f);
+            }
+            {
+              const tailTimeFactor = (timeFactor - tailTimeFactorCutoff) / (1 - tailTimeFactorCutoff);
+              const f = cubicBezier2(tailTimeFactor);
+              rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector)
+                .add(localVector2.set(0, bodyOffset, 0));
+              o.position.lerp(localVector, f);
+              o.scale.copy(defaultScale).multiplyScalar(1 - tailTimeFactor);
             }
           }
-
-        const time = performance.now() * 0.002;
-        const k = 1;
-        for (var i = 0; i < sphere.geometry.vertices.length; i++) {
-          const p = sphere.geometry.vertices[i];
-          const f = 0.5 + 0.2 * simplex.noise3D(p.x * k + time, p.y * k, p.z * k);
-          p.normalize().multiplyScalar(f);
+        } else {
+          scene.remove(o);
+          tickers.splice(tickers.indexOf(o), 1);
         }
-        sphere.geometry.computeVertexNormals();
-        sphere.geometry.normalsNeedUpdate = true;
-        sphere.geometry.verticesNeedUpdate = true;
-      },
+      }
+
+      const time = performance.now() * 0.002;
+      const k = 1;
+      for (var i = 0; i < sphere.geometry.vertices.length; i++) {
+        const p = sphere.geometry.vertices[i];
+        const f = 0.5 + 0.2 * simplex.noise3D(p.x * k + time, p.y * k, p.z * k);
+        p.normalize().multiplyScalar(f);
+      }
+      sphere.geometry.computeVertexNormals();
+      sphere.geometry.normalsNeedUpdate = true;
+      sphere.geometry.verticesNeedUpdate = true;
     };
-    tickers.push(ticker);
+    tickers.push(o);
   };
   
   const addDrop = p => {
@@ -163,63 +161,61 @@ const loadPromise = (async () => {
     o.add(glowMesh);
     
     let animation = null;
-    const ticker = {
-      update() {
-        const now = Date.now();
-        if (!animation) {
-          rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector);
-          localVector.y = 0;
-          const distance = localVector.distanceTo(o.position);
-          if (distance < 1) {
-            animation = {
-              startPosition: o.position.clone(),
-              startTime: now,
-              endTime: now + 1200,
-            };
-          }
+    o.update = () => {
+      const now = Date.now();
+      if (!animation) {
+        rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector);
+        localVector.y = 0;
+        const distance = localVector.distanceTo(o.position);
+        if (distance < 1) {
+          animation = {
+            startPosition: o.position.clone(),
+            startTime: now,
+            endTime: now + 1200,
+          };
         }
-        if (animation) {
-          const headOffset = 0.5;
-          const bodyOffset = -0.3;
-          const tailTimeFactorCutoff = 0.8;
-          const timeDiff = now - animation.startTime;
-          const timeFactor = Math.min(Math.max(timeDiff / (animation.endTime - animation.startTime), 0), 1);
-          if (timeFactor < 1) {
-            if (timeFactor < tailTimeFactorCutoff) {
-              const f = cubicBezier(timeFactor);
+      }
+      if (animation) {
+        const headOffset = 0.5;
+        const bodyOffset = -0.3;
+        const tailTimeFactorCutoff = 0.8;
+        const timeDiff = now - animation.startTime;
+        const timeFactor = Math.min(Math.max(timeDiff / (animation.endTime - animation.startTime), 0), 1);
+        if (timeFactor < 1) {
+          if (timeFactor < tailTimeFactorCutoff) {
+            const f = cubicBezier(timeFactor);
+            rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector)
+              .add(localVector2.set(0, headOffset, 0));
+            o.position.copy(animation.startPosition).lerp(localVector, f);
+            
+            const f2 = cubicBezier(timeFactor / tailTimeFactorCutoff);
+            glowMesh.scale.setScalar(1 - f2);
+          } else {
+            {
+              const f = cubicBezier(tailTimeFactorCutoff);
               rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector)
                 .add(localVector2.set(0, headOffset, 0));
               o.position.copy(animation.startPosition).lerp(localVector, f);
-              
-              const f2 = cubicBezier(timeFactor / tailTimeFactorCutoff);
-              glowMesh.scale.setScalar(1 - f2);
-            } else {
-              {
-                const f = cubicBezier(tailTimeFactorCutoff);
-                rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector)
-                  .add(localVector2.set(0, headOffset, 0));
-                o.position.copy(animation.startPosition).lerp(localVector, f);
-              }
-              {
-                const tailTimeFactor = (timeFactor - tailTimeFactorCutoff) / (1 - tailTimeFactorCutoff);
-                const f = cubicBezier2(tailTimeFactor);
-                rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector)
-                  .add(localVector2.set(0, bodyOffset, 0));
-                o.position.lerp(localVector, f);
-                o.scale.copy(defaultScale).multiplyScalar(1 - tailTimeFactor);
-              }
             }
-          } else {
-            scene.remove(o);
-            tickers.splice(tickers.indexOf(ticker), 1);
+            {
+              const tailTimeFactor = (timeFactor - tailTimeFactorCutoff) / (1 - tailTimeFactorCutoff);
+              const f = cubicBezier2(tailTimeFactor);
+              rigManager.localRig.modelBoneOutputs.Head.getWorldPosition(localVector)
+                .add(localVector2.set(0, bodyOffset, 0));
+              o.position.lerp(localVector, f);
+              o.scale.copy(defaultScale).multiplyScalar(1 - tailTimeFactor);
+            }
           }
+        } else {
+          scene.remove(o);
+          tickers.splice(tickers.indexOf(o), 1);
         }
+      }
 
-        const time = performance.now() * 0.0005;
-        o.rotation.y = time * Math.PI*2;
-      },
+      const time = performance.now() * 0.0005;
+      o.rotation.y = time * Math.PI*2;
     };
-    tickers.push(ticker);
+    tickers.push(o);
   };
   
   return {
@@ -235,7 +231,8 @@ const drop = async o => {
   addSilk(new THREE.Vector3(1.5, 0.4, 0.5));
 };
 const update = () => {
-  for (const ticker of tickers) {
+  const localTickers = tickers.slice();
+  for (const ticker of localTickers) {
     ticker.update();
   }
 };
