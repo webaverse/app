143d58c64716d1b97437cc4d3097c160357007b0
Avaer Kazmer <a@modules.io>
2021-03-11 10:19:00 -0500

    Increase camera distance

--- app-object.js
+++ app-object.js
@@ -38,7 +38,7 @@ const scene = new THREE.Scene();
 const orthographicScene = new THREE.Scene();
 const avatarScene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 10000);
 camera.position.set(0, 1.6, 0);
 camera.rotation.order = 'YXZ';
 
