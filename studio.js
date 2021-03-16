1bb8cf6c4ab4522f8088476a9003c287fe14b9a0
Avaer Kazmer <a@modules.io>
2021-03-16 19:41:11 -0400

    More studio work

--- studio.js
+++ studio.js
@@ -1,9 +1,6 @@
 import m from './mithril.js';
 
-function init() {
-}
-
-function feed(a, src) {
+/* function feed(a, src) {
   return {
     view() {
       return m(a, {
@@ -11,30 +8,49 @@ function feed(a, src) {
       });
     },
   };
-}
+} */
 
 const Root = {
-  oninit: init,
+  oninit() {
+    this.playing = false;
+    this.needleOffset = 0;
+
+    window.addEventListener('keydown', e => {
+      switch (e.which) {
+        case 32: { // space
+          this.playing = !this.playing;
+          _render();
+          break;
+        }
+      }
+    });
+  },
   // To ensure the tag gets properly diffed on route change.
-  onbeforeupdate: init,
-  view: () => {
+  // onbeforeupdate: init,
+  view() {
     return m(".studio", [
       // m("p", "My ramblings about everything"),
 
       m("footer", [
         m(".toolbar", [
           m(".buttons", [
-            m(".button", [
+            m(".button", {
+              class: this.playing ? 'disabled' : '',
+            }, [
               m("i.fa.fa-play", {
-                onclick(e) {
-                  console.log('play');
+                onclick: e => {
+                  this.playing = true;
+                  _render();
                 },
               }),
             ]),
-            m(".button.disabled", [
+            m(".button", {
+              class: this.playing ? '' : 'disabled',
+            }, [
               m("i.fa.fa-stop", {
-                onclick(e) {
-                  console.log('stop');
+                onclick: e => {
+                  this.playing = false;
+                  _render();
                 },
               }),
             ]),
@@ -45,8 +61,22 @@ const Root = {
           }),
         ]),
         m(".core", [
-          m(".playlist", [
-            m(".needle"),
+          m(".playlist", {
+            onclick: e => {
+              e.preventDefault();
+              e.stopPropagation();
+              
+              const playListEl = mithrilRoot.querySelector('.playlist');
+              const box = playListEl.getBoundingClientRect();
+              this.needleOffset = e.clientX - box.left;
+              _render();
+            },
+          }, [
+            m(".needle", {
+              style: {
+                left: `${this.needleOffset}px`,
+              },
+            }),
             m(".tracks", [
               m(".track"),
               m(".track"),
@@ -63,10 +93,14 @@ const Root = {
     ]);
   },
 };
+const mithrilRoot = document.getElementById('mithril-root');
+const _render = () => {
+  m.render(mithrilRoot, m(Root));
+};
 
 const studio = {
   init() {
-    m.render(document.getElementById('mithril-root'), m(Root));
+    _render();
   }
 };
 export default studio;
