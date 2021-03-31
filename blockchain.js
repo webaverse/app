7e68cb90781f31b8f4daa6ce13cd168ad4ab9a35
Avaer Kazmer <a@modules.io>
2021-03-31 01:11:01 -0400

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -110,14 +110,14 @@ Object.keys(Networks).forEach(network => {
 
 const getNetworkName = () => chainName;
 
-  const getMainnetAddress = async () => {
-    if (typeof window !== "undefined" && window.ethereum) {
-      const [address] = await window.ethereum.enable();
-      return address || null;
-    } else {
-      return null;
-    }
-  };
+const getMainnetAddress = async () => {
+  if (typeof window !== "undefined" && window.ethereum) {
+    const [address] = await window.ethereum.enable();
+    return address || null;
+  } else {
+    return null;
+  }
+};
 
 const transactionQueue = {
   running: false,
