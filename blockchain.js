f8c83283f4b7ba2b90a666fcc5b606bfa6bc0b6f
Avaer Kazmer <a@modules.io>
2021-04-06 11:48:14 -0400

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -44,16 +44,12 @@ const web3 = {
   testnetsidechain: new Web3(new Web3.providers.HttpProvider(web3TestnetSidechainEndpoint)),
   polygon: new Web3(new Web3.providers.HttpProvider(`https://rpc-mainnet.maticvigil.com/v1/${polygonVigilKey}`)),
   testnetpolygon: new Web3(new Web3.providers.HttpProvider(`https://rpc-mumbai.maticvigil.com/v1/${polygonVigilKey}`)),
-  front: null,
-  back: null,
 };
 let common = null;
 let addressFront;
 let addressBack;
 let networkName = '';
 function _setChain(nn) {
-  web3.front = web3[nn];
-  web3.back = web3[nn + 'sidechain'];
   addressFront = addresses[nn];
   addressBack = addresses[nn + 'sidechain'];
   networkName = nn;
@@ -138,33 +134,32 @@ const transactionQueue = {
 const runSidechainTransaction = mnemonic => async (contractName, method, ...args) => {
   const wallet = hdkey.fromMasterSeed(bip39.mnemonicToSeedSync(mnemonic)).derivePath(`m/44'/60'/0'/0/0`).getWallet();
   const address = wallet.getAddressString();
-  // console.log('got mnem', mnemonic, address);
   const privateKey = wallet.getPrivateKeyString();
-  const privateKeyBytes = Uint8Array.from(web3.back.utils.hexToBytes(privateKey));
+  const privateKeyBytes = Uint8Array.from(web3['mainnetsidechain'].utils.hexToBytes(privateKey));
 
-  const txData = contracts.back[contractName].methods[method](...args);
+  const txData = contracts['mainnetsidechain'][contractName].methods[method](...args);
   const data = txData.encodeABI();
   const gas = await txData.estimateGas({
     from: address,
   });
-  let gasPrice = await web3.back.eth.getGasPrice();
+  let gasPrice = await web3['mainnetsidechain'].eth.getGasPrice();
   gasPrice = parseInt(gasPrice, 10);
 
   await transactionQueue.lock();
-  const nonce = await web3.back.eth.getTransactionCount(address);
+  const nonce = await web3['mainnetsidechain'].eth.getTransactionCount(address);
   let tx = Transaction.fromTxData({
-    to: contracts.back[contractName]._address,
-    nonce: '0x' + new web3.back.utils.BN(nonce).toString(16),
-    gas: '0x' + new web3.back.utils.BN(gas).toString(16),
-    gasPrice: '0x' + new web3.back.utils.BN(gasPrice).toString(16),
-    gasLimit: '0x' + new web3.back.utils.BN(8000000).toString(16),
+    to: contracts['mainnetsidechain'][contractName]._address,
+    nonce: '0x' + new web3['mainnetsidechain'].utils.BN(nonce).toString(16),
+    gas: '0x' + new web3['mainnetsidechain'].utils.BN(gas).toString(16),
+    gasPrice: '0x' + new web3['mainnetsidechain'].utils.BN(gasPrice).toString(16),
+    gasLimit: '0x' + new web3['mainnetsidechain'].utils.BN(8000000).toString(16),
     data,
   }, {
     common,
   }).sign(privateKeyBytes);
   const rawTx = '0x' + tx.serialize().toString('hex');
   // console.log('signed tx', tx, rawTx);
-  const receipt = await web3.back.eth.sendSignedTransaction(rawTx);
+  const receipt = await web3['mainnetsidechain'].eth.sendSignedTransaction(rawTx);
   transactionQueue.unlock();
   return receipt;
 };
