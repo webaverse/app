67067839a5408fd3d364eef8ff304795e15be7c1
Avaer Kazmer <a@modules.io>
2021-04-18 03:16:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -239,6 +239,11 @@ export default class App extends EventTarget {
         }
         localMatrix
           .decompose(localVector, localQuaternion, localVector2);
+        if (appManager.aimed) {
+          camera.matrixWorld
+            .decompose(localVector3, localQuaternion2, localVector4);
+          localQuaternion.copy(localQuaternion2);
+        }
 
         if (session) {
           let inputSources = Array.from(session.inputSources);
