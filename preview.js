3fe3ba6e38917f452091855557444f228d50eee5
Avaer Kazmer <a@modules.io>
2021-04-24 19:34:36 -0400

    Update preview.js

--- preview.js
+++ preview.js
@@ -1,9 +1,49 @@
 import WaveSurfer from './wavesurfer.js';
 import {parseQuery} from './util.js';
+import {storageHost} from './constants.js';
 
-const q = parseQuery(window.location.search);
+(async () => {
+  const container = document.getElementById('container');
 
-const container = document.getElementById('container');
-container.innerHTML = 'Hello, preview!\n' + JSON.stringify(q, null, 2);
+  const _setContainerContent = el => {
+    container.innerHTML = '';
+    container.appendChild(el);
+  };
+  const handlers = {
+    'png': async ({
+      hash,
+    }) => {
+      const img = new Image();
+      img.classList.add('img');
+      _setContainerContent(img);
+      await new Promise((accept, reject) => {
+        img.onload = () => {
+          accept();
+        };
+        img.onerror = reject;
+        img.src = `${storageHost}/ipfs/${hash}`;
+      });
+    },
+    'html': async ({
+      hash,
+    }) => {
+      const iframe = document.createElement('iframe');
+      iframe.classList.add('iframe');
+      _setContainerContent(iframe);
+    },
+  };
 
-console.log('preview');
+  const q = parseQuery(window.location.search);
+  const {hash, ext} = q;
+  
+  container.innerHTML = 'Loading preview:<br>' + JSON.stringify(q, null, 2);
+  
+  const handler = handlers[ext];
+  if (handler) {
+    await handler({
+      hash,
+    });
+  } else {
+    throw new Error('unknown extension: ' + ext);
+  }
+})();
