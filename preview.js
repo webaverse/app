c85eae7051092314b60c52644e8fe50b2deddb20
Avaer Kazmer <a@modules.io>
2021-04-24 16:31:22 -0400

    Update preview.html

---
+++ preview.js
@@ -0,0 +1,8 @@
+import {parseQuery} from './util.js';
+
+const q = parseQuery(window.location.search);
+
+const container = document.getElementById('container');
+container.innerHTML = 'Hello, preview!\n' + JSON.stringify(q, null, 2);
+
+console.log('preview');
