41e857b0b1d0893a54eba8a6c207e480fd2d7938
Avaer Kazmer <a@modules.io>
2021-05-05 22:43:37 -0400

    Update util.js

--- util.js
+++ util.js
@@ -41,7 +41,9 @@ export function uint8Array2hex(uint8Array) {
   return Array.prototype.map.call(uint8Array, x => ('00' + x.toString(16)).slice(-2)).join('');
 }
 export function getExt(fileName) {
-  const match = fileName.match(/\.([^\.]+)(?:\?.*)?$/);
+  const match = fileName
+    .replace(/^[a-z]+:\/\/[^\/]+\//, '')
+    .match(/\.([^\.]+)(?:\?.*)?$/);
   return match ? match[1].toLowerCase() : '';
 }
 export function downloadFile(file, filename) {
