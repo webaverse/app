64013da0444f703c20b00d758501fb098da6a6c1
Avaer Kazmer <a@modules.io>
2021-05-10 10:32:26 -0400

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -4,17 +4,20 @@ const addNotification = (html, {timeout = 5000} = {}) => {
   const div = document.createElement('div');
   div.classList.add('notification');
   div.innerHTML = html;
-  notificationsEl.appendChild(div);
-  if (timeout < Infinity) {
-    div.timeout = setTimeout(() => {
-      removeNotification(div);
-    }, timeout);
+  if (notificationsEl) {
+    notificationsEl.appendChild(div);
+    if (timeout < Infinity) {
+      div.timeout = setTimeout(() => {
+        removeNotification(div);
+      }, timeout);
+    }
   }
   return div;
 };
 const removeNotification = div => {
-  if (Array.from(notificationsEl.childNodes).includes(div)) {
+  if (notificationsEl && Array.from(notificationsEl.childNodes).includes(div)) {
     notificationsEl.removeChild(div);
+    clearTimeout(div.timeout);
   }
 };
 
