9d72c7cbd9c04f4708264fcd34d63121f57aa8bc
Avaer Kazmer <a@modules.io>
2021-05-12 15:09:15 -0400

    Checkpoint

--- api.js
+++ api.js
@@ -1,1 +1,19 @@
-export {default as physics} from './physics-manager.js';
+import physicsManager from './physics-manager.js';
+
+function Box(props) {
+  useEffect(() => {
+    physicsManager.addBoxGeometry();
+    return () => {
+    };
+  }, []);
+  return (<div />);
+}
+
+const physics = {
+  addBoxGeometry: physicsManager.addBoxGeometry.bind(physicsManager),
+  removeGeometry: physicsManager.removeGeometry.bind(physicsManager),
+  Box,
+};
+export {
+  physics,
+};
