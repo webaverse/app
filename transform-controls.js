69110edfae2af75762853dfed041fafc184193ae
Avaer Kazmer <a@modules.io>
2021-05-14 14:12:31 -0400

    Create transform-controls.js

---
+++ transform-controls.js
@@ -0,0 +1,22 @@
+import {scene} from './app-object.js';
+import TransformGizmo from './TransformGizmo.js';
+import {capitalize} from './util.js';
+
+let transformGizmo = null;
+const loadPromise = (async () => {
+  await TransformGizmo.load();
+
+  transformGizmo = new TransformGizmo();
+  // transformGizmo.setTransformMode('Translate');
+  scene.add(transformGizmo);
+})();
+
+const transformControls = {
+  setTransformMode(transformMode) {
+    transformGizmo.setTransformMode(capitalize(transformMode));
+  },
+  waitForLoad() {
+    return loadPromise;
+  },
+};
+export default transformControls;
