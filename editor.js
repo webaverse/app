d2290737d0583ace04eb91c52772c6133015f077
Avaer Kazmer <a@modules.io>
2021-05-13 22:51:19 -0400

    Update editor.js

--- editor.js
+++ editor.js
@@ -496,6 +496,28 @@ const bindTextarea = codeEl => {
     (() => {
       const width = 50;
       
+      const FileInput = ({
+        file,
+        setFile,
+      }) => {
+        return (file ?
+          <div className="file">
+            <div className="text">{file.name}</div>
+            <img className="cancel-button" src="/cancel.svg" onClick={e => {
+              setFile(null);
+            }} />
+          </div>
+        :
+          <input type="file" placeholder="File" onChange={e => {
+            const files = Array.from(e.target.files);
+            if (files.length > 0) {
+              setFile(files[0]);
+            } else {
+              setFile(null);
+            }
+          }} />
+        );
+      };
       const Search = React.memo(({
         q,
         setQ,
