577bb00d41e9fff507549d7db07960df9afe8fd3
Avaer Kazmer <a@modules.io>
2021-05-15 05:23:52 -0400

    Update CapsuleGeometry.js

--- CapsuleGeometry.js
+++ CapsuleGeometry.js
@@ -1,4 +1,58 @@
 import * as THREE from 'three';
+import {Vector3, Color} from 'three';
+
+class Face3 {
+
+	constructor( a, b, c, normal, color, materialIndex = 0 ) {
+
+		this.a = a;
+		this.b = b;
+		this.c = c;
+
+		this.normal = ( normal && normal.isVector3 ) ? normal : new Vector3();
+		this.vertexNormals = Array.isArray( normal ) ? normal : [];
+
+		this.color = ( color && color.isColor ) ? color : new Color();
+		this.vertexColors = Array.isArray( color ) ? color : [];
+
+		this.materialIndex = materialIndex;
+
+	}
+
+	clone() {
+
+		return new this.constructor().copy( this );
+
+	}
+
+	copy( source ) {
+
+		this.a = source.a;
+		this.b = source.b;
+		this.c = source.c;
+
+		this.normal.copy( source.normal );
+		this.color.copy( source.color );
+
+		this.materialIndex = source.materialIndex;
+
+		for ( let i = 0, il = source.vertexNormals.length; i < il; i ++ ) {
+
+			this.vertexNormals[ i ] = source.vertexNormals[ i ].clone();
+
+		}
+
+		for ( let i = 0, il = source.vertexColors.length; i < il; i ++ ) {
+
+			this.vertexColors[ i ] = source.vertexColors[ i ].clone();
+
+		}
+
+		return this;
+
+	}
+
+}
 
 function updateFace(
   tri,
@@ -340,7 +394,7 @@ function CapsuleGeometry(
     // The first three parameters are the coordinate indices. The next
     // parameter either accepts one normal for flat, per-face shading, or an
     // array of normals for smooth, per-vertex shading.
-    const tri = new THREE.Face3(0, 0, 0, vertNormals, faceColor, materialIndex);
+    const tri = new Face3(0, 0, 0, vertNormals, faceColor, materialIndex);
 
     // Since an array of vertex normals was supplied to the constructor, the
     // per face normal needs to be created.
