0787dcb7be90650c6ef4ef609f4c2456b6595d8c
Avaer Kazmer <a@modules.io>
2021-05-16 16:25:46 -0400

    Update editor.js

--- editor.js
+++ editor.js
@@ -15,10 +15,12 @@ import {storageHost} from './constants.js';
 // import TransformGizmo from './TransformGizmo.js';
 // import transformControls from './transform-controls.js';
 import easing from './easing.js';
+import Simplex from './simplex-noise.js';
 import ghDownloadDirectory from './gh-download-directory.js';
 
 const cubicBezier = easing(0, 1, 0, 1);
 // const cubicBezier2 = easing(0, 0.7, 0, 0.7);
+const simplex = new Simplex('lol');
 const ghDownload = ghDownloadDirectory.default;
 // window.ghDownload = ghDownload;
 
@@ -1793,6 +1795,48 @@ app.waitForLoad()
     scene.add(inventoryMesh);
     app.addEventListener('frame', () => {
       inventoryMesh.update();
+    });
+    
+    const _makeSilkMesh = () => {
+      // const velocity = v.clone();
+      // let grounded = false;
+      
+      const sphere = new THREE.Mesh(new THREE.BoxBufferGeometry(0.1, 0.05, 0.1, 10, 10, 10), new THREE.MeshNormalMaterial());
+      // sphere.position.copy(p);
+      const defaultScale = new THREE.Vector3(1, 0.3, 1).multiplyScalar(0.5);
+      sphere.scale.copy(defaultScale);
+      scene.add(sphere);
+      const o = sphere;
+
+      const startTime = Date.now();
+      let lastTimestamp = startTime;
+      let animation = null;
+      const timeOffset = Math.random() * 10;
+      o.update = () => {
+        const now = Date.now();
+        const timeDiff = (now - lastTimestamp) / 1000;
+        lastTimestamp = now;
+
+        const time = timeOffset + performance.now() * 0.002;
+        const k = 1;
+        for (var i = 0; i < sphere.geometry.attributes.position.array.length; i += 3) {
+          const p = localVector.fromArray(sphere.geometry.attributes.position.array, i);
+          const f = 0.5 + 0.2 * simplex.noise3D(p.x * k + time, p.y * k, p.z * k);
+          p.normalize().multiplyScalar(f);
+          p.toArray(sphere.geometry.attributes.position.array, i);
+        }
+        sphere.geometry.attributes.position.needsUpdate = true;
+        sphere.geometry.computeVertexNormals();
+        sphere.geometry.normalsNeedUpdate = true;
+        sphere.geometry.verticesNeedUpdate = true;
+      };
+      return o;
+    };
+    const silkMesh = _makeSilkMesh();
+    silkMesh.position.set(0, 1.2, -2);
+    scene.add(silkMesh);
+    app.addEventListener('frame', () => {
+      silkMesh.update();
     });
     
     const defaultScene = [
