f771c2ccb77368e44d111cd9db590ac1db497f6c
Avaer Kazmer <a@modules.io>
2021-05-22 05:54:53 -0400

    Small debugging

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ import npcManager from './npc-manager.js';
 import {bindInterface as inventoryBindInterface} from './inventory.js';
 import fx from './fx.js';
 import {parseCoord, getExt} from './util.js';
+import {storageHost, tokensHost} from './constants.js';
 // import './procgen.js';
 import {getRenderer, scene, orthographicScene, avatarScene, camera, orthographicCamera, avatarCamera, dolly, /*orbitControls, renderer2,*/ sceneHighPriority, sceneLowPriority, appManager, bindCanvas} from './app-object.js';
 // import {mithrilInit} from './mithril-ui/index.js'
