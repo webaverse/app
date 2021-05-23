513a6e4b82b0ea6836a3905d9f6d3b56c4c5f0dd
Avaer Kazmer <a@modules.io>
2021-05-22 22:26:24 -0400

    Update shaders.js

--- shaders.js
+++ shaders.js
@@ -1777,6 +1777,54 @@ const arrowMaterial = new THREE.ShaderMaterial({
   // transparent: true,
 });
 
+const copyScenePlaneGeometry = new THREE.PlaneGeometry(2, 2);
+const copySceneVertexShader = `#version 300 es
+  precision highp float;
+
+  in vec3 position;
+  in vec2 uv;
+  uniform mat4 modelViewMatrix;
+  uniform mat4 projectionMatrix;
+  out vec2 vUv;
+
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+`;
+const copyScene = (() => {
+  const mesh = new THREE.Mesh(
+    copyScenePlaneGeometry,
+    new THREE.RawShaderMaterial({
+      uniforms: {
+        tex: {
+          value: null,
+          // needsUpdate: false,
+        },
+      },
+      vertexShader: copySceneVertexShader,
+      fragmentShader: `#version 300 es
+        precision highp float;
+
+        uniform sampler2D tex;
+        in vec2 vUv;
+        out vec4 fragColor;
+        
+        void main() {
+          fragColor = texture(tex, vUv);
+        }
+      `,
+      depthWrite: false,
+      depthTest: false,
+    })
+  );
+  const scene = new THREE.Scene();
+  scene.add(mesh);
+  scene.mesh = mesh;
+  return scene;
+})();
+const copySceneCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+
 export {
   /* LAND_SHADER,
   WATER_SHADER,
@@ -1791,4 +1839,8 @@ export {
   portalMaterial,
   arrowGeometry,
   arrowMaterial,
+  copyScenePlaneGeometry,
+  copySceneVertexShader,
+  copyScene,
+  copySceneCamera,
 };
