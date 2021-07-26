299050fb2aa5cdf3a7d40dab8e0cdb1f4588e663
Avaer Kazmer <a@modules.io>
2021-07-26 01:03:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,10 @@ app.use((req, res, next) => {
   next();
 });
 const appStatic = express.static(__dirname);
+app.get('config.json', (req, res, next) => {
+  res.status(404);
+  res.end();
+});
 app.get('*', (req, res, next) => {
   const ext = getExt(req.url);
   if (['tjs', 'rtfjs'].includes(ext)) {
