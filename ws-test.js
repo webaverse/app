61b8fdecbd987e6660596cc973d9e16b177fe714
Avaer Kazmer <a@modules.io>
2021-08-23 19:49:15 -0400

    Create ws-test.js

---
+++ ws-test.js
@@ -0,0 +1,45 @@
+window.addEventListener('click', async e => {
+  const mediaStream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+  });
+  const audioTracks = mediaStream.getAudioTracks();
+  const audioTrack = audioTracks[0];
+  const audioTrackSettings = audioTrack.getSettings();
+  const audio = (new MediaStreamTrackProcessor(audioTrack)).readable;
+  console.log('got media', audioTrack, audioTrack.getSettings(), audio);
+
+  function muxAndSend(encodedChunk) {
+    console.log('got chunk', encodedChunk);
+  }
+  function onEncoderError(err) {
+    console.warn(err);
+  }
+  function readAndEncode(reader, encoder) {
+    reader.read().then((result) => {
+      // App handling for stream closure.
+      if (result.done) {
+        return;
+      }
+
+      // Encode!
+      encoder.encode(result.value);
+
+      // Keep reading until the stream closes.
+      readAndEncode(reader, encoder);
+    });
+  }
+
+  const audioEncoder = new AudioEncoder({
+    output: muxAndSend,
+    error: onEncoderError,
+  });
+  await audioEncoder.configure({
+    codec: 'opus',
+    numberOfChannels: audioTrackSettings.channelCount,
+    sampleRate: audioTrackSettings.sampleRate,
+    tuning: {
+      bitrate: 60_000,
+    },
+  });
+  readAndEncode(audio.getReader(), audioEncoder);
+});
