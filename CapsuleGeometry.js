b48eb79c806ea94b62d0c0bc54ded57fb4643415
Avaer Kazmer <a@modules.io>
2021-08-23 17:33:05 -0400

    Update CapsuleGeometry.js

--- CapsuleGeometry.js
+++ CapsuleGeometry.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import {Vector3, Color} from 'three';
+import * as THREE from 'https://lib.webaverse.com/three.js';
+import {Vector3, Color} from 'https://lib.webaverse.com/three.js';
 
 class Face3 {
 
