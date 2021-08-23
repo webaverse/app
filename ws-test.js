662c8b8e832040b678a8d30cb78524b85940520d
Avaer Kazmer <a@modules.io>
2021-08-23 19:57:11 -0400

    Update ws-test.js

--- ws-test.js
+++ ws-test.js
@@ -8,27 +8,6 @@ window.addEventListener('click', async e => {
   const audio = (new MediaStreamTrackProcessor(audioTrack)).readable;
   console.log('got media', audioTrack, audioTrack.getSettings(), audio);
 
-  function muxAndSend(encodedChunk) {
-    audioDecoder.decode(encodedChunk);
-  }
-  function onEncoderError(err) {
-    console.warn(err);
-  }
-  function readAndEncode(reader, encoder) {
-    reader.read().then((result) => {
-      // App handling for stream closure.
-      if (result.done) {
-        return;
-      }
-
-      // Encode!
-      encoder.encode(result.value);
-
-      // Keep reading until the stream closes.
-      readAndEncode(reader, encoder);
-    });
-  }
-
   const audioEncoder = new AudioEncoder({
     output: muxAndSend,
     error: onEncoderError,
@@ -42,13 +21,6 @@ window.addEventListener('click', async e => {
     }, */
   });
 
-  function demuxAndPlay(encodedChunk) {
-    console.log('demux encodedChunk', encodedChunk);
-  }
-  function onDecoderError(err) {
-    console.warn(err);
-  }
-
   const audioDecoder = new AudioDecoder({
     output: demuxAndPlay,
     error: onDecoderError,
@@ -61,6 +33,26 @@ window.addEventListener('click', async e => {
       bitrate: 60_000,
     }, */
   });  
+  
+  function muxAndSend(encodedChunk) {
+    audioDecoder.decode(encodedChunk);
+  }
+  function onEncoderError(err) {
+    console.warn(err);
+  }
+  async function readAndEncode(reader, encoder) {
+    const result = await reader.read();
+    if (!result.done) {
+      encoder.encode(result.value);
+      readAndEncode(reader, encoder);
+    }
+  }
+  function demuxAndPlay(encodedChunk) {
+    console.log('demux encodedChunk', encodedChunk);
+  }
+  function onDecoderError(err) {
+    console.warn(err);
+  }
   
   readAndEncode(audio.getReader(), audioEncoder);
 });
