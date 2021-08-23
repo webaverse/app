34609b5b29e3dbcd1deb9a3013cbc75a2ed73b94
Avaer Kazmer <a@modules.io>
2021-08-23 17:37:17 -0400

    Update teleport.js

--- teleport.js
+++ teleport.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import {BufferGeometryUtils} from 'three';
+import * as THREE from 'https://lib.webaverse.com/three.js';
+import {BufferGeometryUtils} from 'https://lib.webaverse.com/BufferGeometryUtils.js';
 import {getRenderer, scene} from './app-object.js';
 
 const localVector = new THREE.Vector3();
