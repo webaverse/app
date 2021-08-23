30673825325365fee0ae7234b108e4fb846abc18
Avaer Kazmer <a@modules.io>
2021-08-23 17:31:32 -0400

    Update transform-controls.js

--- transform-controls.js
+++ transform-controls.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://lib.webaverse.com/three.js';
 import {scene, sceneLowPriority} from './app-object.js';
 import weaponsManager from './weapons-manager.js';
 import TransformGizmo from './TransformGizmo.js';
