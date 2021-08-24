b283df745ae48c319d0e4ad6155dbbef1b8c3e15
Avaer Kazmer <a@modules.io>
2021-08-24 00:44:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -66,11 +66,50 @@ app.use(appStatic);
 const wss = new ws.WebSocketServer({
   noServer: true,
 });
+const users = [];
 wss.on('connection', (ws, req) => {
+  const id = Math.floor(Math.random() * 0xFFFFFF);
+  const localUser = {
+    id,
+    ws,
+  };
+  users.push(localUser);
+  ws.addEventListener('close', () => {
+    for (const user of users) {
+      if (user !== localUser) {
+        user.ws.send(JSON.stringify({
+          method: 'leave',
+          id,
+        }));
+      }
+    }
+    
+    users.splice(users.indexOf(localUser), 1);
+  });
+  ws.send(JSON.stringify({
+    method: 'init',
+    args: {
+      id,
+      users: users.map(u => u.id),
+    },
+  }));
+  for (const user of users) {
+    if (user !== localUser) {
+      user.ws.send(JSON.stringify({
+        method: 'join',
+        id,
+      }));
+    }
+  }
+  
   // console.log('got ws', req.url);
   ws.addEventListener('message', e => {
     // console.log('got message', e.data);
-    ws.send(e.data);
+    for (const user of users) {
+      if (user !== localUser) {
+        user.ws.send(e.data);
+      }
+    }
   });
 });
 
@@ -89,9 +128,9 @@ if (CERT && PRIVKEY) {
   console.log('https://localhost:'+httpsPort);
 }
 for (const server of servers) {
-  server.on('upgrade', (request, socket, head) => {
-    wss.handleUpgrade(request, socket, head, ws => {
-      wss.emit('connection', ws, request);
+  server.on('upgrade', (req, socket, head) => {
+    wss.handleUpgrade(req, socket, head, ws => {
+      wss.emit('connection', ws, req);
     });
   });
 }
