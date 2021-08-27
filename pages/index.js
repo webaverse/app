fd2efa75fe87435a8b3f0392b26d53849388e200
Avaer Kazmer <a@modules.io>
2021-08-27 08:19:53 -0400

    Small cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Image from 'next/image';
+// import Image from 'next/image';
 // import styles from '../styles/Home.module.css';
 // <div className={styles.container}>
 
