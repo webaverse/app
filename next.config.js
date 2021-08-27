3ea175e8fd4a98ee4b044f5489c9ab7a9a3d0572
Avaer Kazmer <a@modules.io>
2021-08-27 18:22:49 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    // if (isServer) {
+      return {
+        ...config,
+        entry() {
+          return config.entry().then((entry) => ({
+            ...entry,
+            // adding custom entry points
+            wsrtc: path.resolve(process.cwd(), 'node_modules/wsrtc/wsrtc.js'),
+            // run: path.resolve(process.cwd(), 'src/run.js'),
+          }));
+        },
+        /* output: {
+          filename: '[name].js',
+          path: __dirname + '/dist',
+        }, */
+      };
+    // }
+  },
+};
