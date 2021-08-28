60542d747fa30a838333be6a9160eb316167059a
Avaer Kazmer <a@modules.io>
2021-08-28 06:14:16 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,8 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import webaversePlugin from './rollup-webaverse-plugin.js'
 
-/* const pathAliasMap = {
-  'webaverse': '/app-object.js',
-}; */
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  /* resolvers: [
-    {
-      alias(path) {
-        for (const [slug, res] of Object.entries(pathAliasMap)) {
-          if (path.startsWith(slug)) {
-            return path.replace(slug, res)
-          }
-        }
-      },
-    },
-  ], */
   plugins: [
     webaversePlugin(),
     reactRefresh(),
