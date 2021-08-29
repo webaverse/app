2996839f40124a5c2dca1cd5482e9c332c45faf9
Avaer Kazmer <a@modules.io>
2021-08-29 03:47:39 -0400

    Entry refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,53 @@
-import React, { useState } from 'react'
+import React, {useState, useEffect} from 'react'
 import logo from './logo.svg'
 import './App.css'
+import App from '/app.js';
 
-function App() {
-  const [count, setCount] = useState(0)
+const _startApp = () => {
+  const app = new App();
+
+  app.bootstrapFromUrl(location);
+
+  app.bindLogin();
+  app.bindInput();
+  app.bindInterface();
+  const uploadFileInput = document.getElementById('upload-file-input');
+  app.bindUploadFileInput(uploadFileInput);
+  const canvas = document.getElementById('canvas');
+  app.bindCanvas(canvas);
+
+  const mapCanvas = document.getElementById('map-canvas')
+  app.bindMinimap(mapCanvas);
+
+  app.setPossessed(true);
+
+  const enterXrButton = document.getElementById('enter-xr-button');
+  const noXrButton = document.getElementById('no-xr-button');
+  app.bindXrButton({
+    enterXrButton,
+    // noXrButton,
+    onSupported(ok) {
+      if (ok) {
+        enterXrButton.style.display = null;
+        noXrButton.style.display = 'none';
+      }
+    },
+  });
+
+  app.waitForLoad()
+    .then(() => {
+      app.startLoop();
+    });
+};
+
+function RootNode() {
+  // const [count, setCount] = useState(0)
+  useEffect(() => {
+    _startApp();
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <div></div>
+  );
 }
-
-export default App
+export default RootNode;
