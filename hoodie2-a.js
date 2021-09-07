9b9250142311dfc3a0112a4d93bf6588eea38532
Avaer Kazmer <a@modules.io>
2021-09-07 15:53:00 -0400

    Add new a-pose handling

---
+++ hoodie2-a.js
@@ -0,0 +1,49 @@
+import * as THREE from './node_modules/three/build/three.module.js';
+import {GLTFLoader} from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import {GLTFExporter} from './node_modules/three/examples/jsm/exporters/GLTFExporter.js';
+
+const gltfLoader = new GLTFLoader();
+const gltfExporter = new GLTFExporter();
+
+window.renderHoodieA = async () => {
+  const shirtObject = await new Promise((accept, reject) => {
+    gltfLoader.load('./hoodie-a.glb', accept, function onprogress() {}, reject);
+  });
+  const {scene} = shirtObject;
+  const hoodieMesh = scene.children[0];
+  // const {material} = hoodieMesh;
+  console.log('got', hoodieMesh);
+
+  for (let i = 1; i <= 8000; i++) {
+    const src = `./hoodie2\/${i}\.png`;
+    const img = await (async () => {
+      const img = new Image();
+      img.src = src;
+      await new Promise((accept, reject) => {
+        img.onload = accept;
+        img.onerror = reject;
+      });
+      return img;
+    })();
+    hoodieMesh.material.map.image = img;
+    
+    const gltf = await new Promise((accept, reject) => {
+      gltfExporter.parse(scene, accept, {
+        binary: true,
+      });
+    });
+    // console.log('got blob', gltf);
+    const blob = new Blob([gltf], { type: 'application/octet-stream' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `hoodie-a${i}.glb`;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    
+    setTimeout(() => {
+      URL.revokeObjectURL(url);
+    }, 1000);
+  }
+};
---
+++ shirt2-a.js
@@ -0,0 +1,49 @@
+import * as THREE from './node_modules/three/build/three.module.js';
+import {GLTFLoader} from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import {GLTFExporter} from './node_modules/three/examples/jsm/exporters/GLTFExporter.js';
+
+const gltfLoader = new GLTFLoader();
+const gltfExporter = new GLTFExporter();
+
+window.renderShirtA = async () => {
+  const shirtObject = await new Promise((accept, reject) => {
+    gltfLoader.load('./shirt-a.glb', accept, function onprogress() {}, reject);
+  });
+  const {scene} = shirtObject;
+  const shirtMesh = scene.children[0];
+  // const {material} = shirtMesh;
+  console.log('got', shirtMesh);
+
+  for (let i = 1; i <= 8000; i++) {
+    const src = `./shirt2\/${i}\.png`;
+    const img = await (async () => {
+      const img = new Image();
+      img.src = src;
+      await new Promise((accept, reject) => {
+        img.onload = accept;
+        img.onerror = reject;
+      });
+      return img;
+    })();
+    shirtMesh.material.map.image = img;
+    
+    const gltf = await new Promise((accept, reject) => {
+      gltfExporter.parse(scene, accept, {
+        binary: true,
+      });
+    });
+    // console.log('got blob', gltf);
+    const blob = new Blob([gltf], { type: 'application/octet-stream' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `shirt-a${i}.glb`;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    
+    setTimeout(() => {
+      URL.revokeObjectURL(url);
+    }, 1000);
+  }
+};
