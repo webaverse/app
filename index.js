93c9c8e26db5f6273c268849c1f577d8b417cfbb
Avaer Kazmer <a@modules.io>
2021-09-12 09:49:50 -0400

    Add AI backend

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const path = require('path');
 const http = require('http');
+const https = require('https');
 const url = require('url');
 const fs = require('fs');
 const util = require('util');
 const express = require('express');
 const vite = require('vite');
 const wsrtc = require('wsrtc/wsrtc-server.js');
+const OpenAI = require('openai-api');
+const GPT3Encoder = require('gpt-3-encoder');
+let openAiKey = null;
+try {
+  const config = require('./config.json');
+  openAiKey = config.openAiKey;
+} catch(err) {
+  console.warn(err);
+}
+if (!openAiKey) {
+  throw new Error('fail');
+}
+const aiPrefix = fs.readFileSync('ai-prefix.js');
 // const htmlRenderIframeString = fs.readFileSync('./html_render_iframe.html', 'utf8');
 
 Error.stackTraceLimit = 300;
@@ -14,10 +28,66 @@ const _isMediaType = p => /\.(?:png|jpe?g|gif|glb|mp3)$/.test(p);
 /* const fillTemplate = function(templateString, templateVars) {
   return new Function("return `"+templateString +"`;").call(templateVars);
 }; */
+OpenAI.prototype._send_request = (sendRequest => async function(url, method, opts = {}) {
+  let camelToUnderscore = (key) => {
+    let result = key.replace(/([A-Z])/g, " $1");
+    return result.split(' ').join('_').toLowerCase();
+  }
+  
+  // console.log('got req', url, method, opts);
+
+  const data = {};
+  for (const key in opts) {
+    data[camelToUnderscore(key)] = opts[key];
+  }
+
+  const rs = await new Promise((accept, reject) => {
+    const req = https.request(url, {
+      method,
+      headers: {
+        'Authorization': `Bearer ${this._api_key}`,
+        'Content-Type': 'application/json'
+      }
+    }, res => {
+      res.setEncoding('utf8');
+      accept(res);
+    });
+    req.end(Object.keys(data).length ? JSON.stringify(data) : '');
+    req.on('error', reject);
+  });
+  return rs;
+})(OpenAI.prototype._send_request);
+const openai = openAiKey && new OpenAI(openAiKey);
+const _openAiCodex = async (prompt, stop) => {
+  const maxTokens = 4096;
+  const max_tokens = maxTokens - GPT3Encoder.encode(prompt).length;
+  console.log('max tokens: ' + max_tokens);
+  const gptRes = await openai.complete({
+    engine: 'davinci-codex',
+    prompt,
+    stop,
+    temperature: 0,
+    topP: 1,
+    max_tokens,
+    stream: true,
+
+    /* stream: false,
+    prompt: o.prompt, // 'this is a test',
+    maxTokens: o.maxTokens, // 5,
+    temperature: o.temperature, // 0.9,
+    topP: o.topP, // 1,
+    presencePenalty: o.presencePenalty, // 0,
+    frequencyPenalty: o.frequencyPenalty, // 0,
+    bestOf: o.bestOf, // 1,
+    n: o.n, // 1,
+    stop: o.stop, // ['\n'] */
+  });
+  return gptRes;
+};
 
 (async () => {
   const app = express();
-  app.use('*', (req, res, next) => {
+  app.use('*', async (req, res, next) => {
     const o = url.parse(req.originalUrl, true);
     if (/^\/(?:@proxy|public)\//.test(o.pathname) && o.search !== '?import') {
       const u = o.pathname
@@ -37,6 +107,27 @@ const _isMediaType = p => /\.(?:png|jpe?g|gif|glb|mp3)$/.test(p);
       });
       res.set('Content-Type', 'text/html');
       res.end(s); */
+    } else if (!!openai && o.pathname === '/ai' && o.query.p) {
+      const p = decodeURIComponent(o.query.p);
+      const proxyRes = await _openAiCodex(aiPrefix + p + ' */\n', '\n/* Command: ');
+      if (proxyRes.statusCode >= 200 && proxyRes.statusCode < 300) {
+        for (const key in proxyRes.headers) {
+          const value = proxyRes.headers[key];
+          res.setHeader(key, value);
+        }
+        // console.log('render');
+        proxyRes.pipe(res);
+        /* proxyRes.on('data', d => {
+          console.log('got data', d.toString('utf8'));
+        }); */
+      } else {
+        proxyRes.setEncoding('utf8');
+        proxyRes.on('data', s => {
+          console.log(s);
+        });
+        res.setHeader('Content-Type', 'text/event-stream');
+        res.end('data: [DONE]');
+      }
     } else {
       next();
     }
