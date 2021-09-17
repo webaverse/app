d3fe9d07d82b20e45e801a64f3e7054dbb950d5e
Avaer Kazmer <a@modules.io>
2021-09-17 18:35:46 -0400

    Major design work

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,18 +1,20 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, {useState, useEffect, useRef} from 'react';
+import classnames from 'classnames';
+import styles from './Footer.module.css';
 
 export default () => {
   return (
     <footer>
-      <div className="helper left">
-        <div className="loadout" id="loadout">
+      <div className={classnames(styles.helper, styles.left)}>
+        <div className={styles.loadout}>
           {(() => {
             const numItems = 8;
             const items = Array(numItems);
             for (let i = 0; i < numItems; i++) {
               items[i] = (
-                <div className="item" key={i}>
-                  <div className="box"></div>
-                  <div className="label">{i}</div>
+                <div className={styles.item} key={i}>
+                  <div className={styles.box}></div>
+                  <div className={styles.label}>{i}</div>
                 </div>
               );
             }
@@ -20,61 +22,61 @@ export default () => {
           })()}
         </div>
       </div>
-      <div className="helper right">
-        <div className="chat" id="chat">
-          <div className="messages" id="chat-messages"></div>
-          <input type="text" className="input" id="chat-input" />
+      <div className={classnames(styles.helper, styles.right)}>
+        <div className={styles.chat} id="chat">
+          <div className={styles.messages} id="chat-messages"></div>
+          <input type="text" className={styles.input} />
         </div>
-        <div className="keys">
-          <div className="label">Move</div>
-          <div className="row">
-            <div className="key"></div>
-            <div className="key">W</div>
-            <div className="key"></div>
+        <div className={styles.keys}>
+          <div className={styles.label}>Move</div>
+          <div className={styles.row}>
+            <div className={styles.key}></div>
+            <div className={styles.key}>W</div>
+            <div className={styles.key}></div>
           </div>
-          <div className="row">
-            <div className="key">A</div>
-            <div className="key">S</div>
-            <div className="key">D</div>
-          </div>
-        </div>
-        <div className="keys">
-          <div className="row"></div>
-          <div className="row">
-            <div className="label center">Fly</div>
-            <div className="key">F</div>
+          <div className={styles.row}>
+            <div className={styles.key}>A</div>
+            <div className={styles.key}>S</div>
+            <div className={styles.key}>D</div>
           </div>
         </div>
-        <div className="keys">
-          <div className="row"></div>
-          <div className="row">
-            <div className="label center">Camera</div>
-            <div className="key"><i className="far fa-mouse-alt"></i></div>
+        <div className={styles.keys}>
+          <div className={styles.row}></div>
+          <div className={styles.row}>
+            <div className={classnames(styles.label, styles.center)}>Fly</div>
+            <div className={styles.key}>F</div>
           </div>
         </div>
-        <div className="keys">
-          <div className="row"></div>
-          <div className="row">
-            <div className="label center">Talk</div>
-            <div className="key" id="key-t">T</div>
+        <div className={styles.keys}>
+          <div className={styles.row}></div>
+          <div className={styles.row}>
+            <div className={classnames(styles.label, styles.center)}>Camera</div>
+            <div className={styles.key}><i className="far fa-mouse-alt"></i></div>
           </div>
         </div>
-        <div className="keys">
-          <div className="row"></div>
-          <div className="row">
-            <div className="label center">Upload</div>
-            <div className="key" id="key-t">U</div>
+        <div className={styles.keys}>
+          <div className={styles.row}></div>
+          <div className={styles.row}>
+            <div className={classnames(styles.label, styles.center)}>Talk</div>
+            <div className={styles.key}>T</div>
           </div>
         </div>
-        <div className="keys">
-          <div className="row"></div>
-          <div className="row">
-            <div className="label center">Text chat</div>
-            <div className="key" id="key-enter">Enter</div>
+        <div className={styles.keys}>
+          <div className={styles.row}></div>
+          <div className={styles.row}>
+            <div className={classnames(styles.label, styles.center)}>Upload</div>
+            <div className={styles.key}>U</div>
           </div>
         </div>
-        <nav className="enter-xr-button" id="enter-xr-button" style={{display: 'none'}}>Enter XR</nav>
-        <nav className="enter-xr-button disabled" id="no-xr-button">No XR</nav>
+        <div className={styles.keys}>
+          <div className={styles.row}></div>
+          <div className={styles.row}>
+            <div className={classnames(styles.label, styles.center)}>Text chat</div>
+            <div className={styles.key}>Enter</div>
+          </div>
+        </div>
+        <nav className={styles['enter-xr-button']} id="enter-xr-button" style={{display: 'none'}}>Enter XR</nav>
+        <nav className={styles['no-xr-button']} id="no-xr-button">No XR</nav>
       </div>
     </footer>
   );
