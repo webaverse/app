9ef3ee2c1d376c9d038f26376354b97d6a090645
Avaer Kazmer <a@modules.io>
2021-09-21 15:29:52 -0400

    Add metaverse-modules.js

---
+++ metaverse-modules.js
@@ -0,0 +1,25 @@
+import metaversefile from 'metaversefile';
+
+const moduleUrls = {
+  button: `./metaverse_modules/button/`,
+  silk: `./metaverse_modules/silk/`,
+  card: `./metaverse_modules/card/`,
+};
+const modules = {};
+const loadPromise = (async () => {
+  const promises = [];
+  for (const moduleName in moduleUrls) {
+    const moduleUrl = moduleUrls[moduleName];
+    const p = metaversefile.import(moduleUrl)
+      .then(m => {
+        modules[moduleName] = m;
+      });
+    promises.push(p);
+  }
+  await Promise.all(promises);
+})();
+const waitForLoad = () => loadPromise;
+export {
+  modules,
+  waitForLoad,
+};
