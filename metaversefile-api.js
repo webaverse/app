0f62c44ce9255462eb740cfdcdd5fa993237b05e
Avaer Kazmer <a@modules.io>
2021-10-22 19:41:21 -0400

    Add missing metaversefile import

--- metaversefile-api.js
+++ metaversefile-api.js
@@ -27,6 +27,7 @@ import {web3} from './blockchain.js';
 import {moduleUrls, modules} from './metaverse-modules.js';
 import easing from './easing.js';
 import {LocalPlayer, RemotePlayer} from './player.js';
+import {getRandomString} from './util.js';
 import {rarityColors} from './constants.js';
 
 const localVector = new THREE.Vector3();
