3b8842c9a63bc0b7f276681d3a6874f631b34ac6
Muhammad Abeer <abeersaqib@gmail.com>
2021-10-26 16:01:32 +0500

    Updated Error Message

--- src/Error.jsx
+++ src/Error.jsx
@@ -6,7 +6,7 @@ export default () => {
       <h1>
         Sorry ES-6 Module loading as Web-Workers is not supported in this browser.
         <br/>
-        Please use some other browser.
+        Please use some other browser. Supported browsers are Chrome, Safari or Opera.
       </h1>
     </div>
   );
