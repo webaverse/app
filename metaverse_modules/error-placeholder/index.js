3cd65415d5486ed0f17afe051ac80612c9164c2a
Avaer Kazmer <a@modules.io>
2021-10-26 21:09:40 -0400

    Rewrite error placeholder app

--- metaverse_modules/error-placeholder/index.js
+++ metaverse_modules/error-placeholder/index.js
@@ -1,69 +1,57 @@
-import {Vector3,TextureLoader,Mesh,BoxGeometry,MeshBasicMaterial} from 'three';
+import {Mesh, PlaneGeometry, DoubleSide, MeshBasicMaterial, NearestFilter, Texture} from 'three';
 // import {scene, renderer, camera, runtime, world, physics, ui, app, appManager} from 'app';
 import metaversefile from 'metaversefile';
-const {useApp, usePhysics, useCleanup, useActivate, removeApp} = metaversefile;
+const {useApp, useActivate, removeApp} = metaversefile;
 
-const baseUrl = import.meta.url.replace(/(\/)[^\/\/]*$/, '$1');	
+const baseUrl = import.meta.url.replace(/(\/)[^\/\/]*$/, '$1');
+const imagePath = baseUrl + 'textures/x.png';
 
+const geometry = new PlaneGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({
+  side: DoubleSide,
+  transparent: true,
+  alphaTest: 0.5,
+});
+(async () => {
+  try {
+    const img = await new Promise((accept, reject) => {
+      const img = new Image();
+      img.onload = () => {
+        accept(img);
+      };
+      img.onerror = reject;
+      img.crossOrigin = 'Anonymous';
+      img.src = imagePath;
+    });
+    const texture = new Texture(img);
+    texture.minFilter = NearestFilter;
+    texture.magFilter = NearestFilter;
+    texture.needsUpdate = true;
+    material.map = texture;
+    material.needsUpdate = true;
+  } catch(err) {
+    console.warn(err);
+    material.color = 0xFF0000;
+    material.needsUpdate = true;
+  }
+})();
 
 export default () => {
+  const app = useApp();
+  // const physics = usePhysics();
 	
-    const app = useApp();
-	const physics = usePhysics();
+	// let physicsIds = [];
+
+  const mesh = new Mesh(geometry, material);
+  app.add(mesh);
+  
+  /* const physicsId = physics.addGeometry(mesh);
+  physicsIds.push(physicsId); */
 	
-	let physicsIds = [];
-
-    const imagePath = baseUrl+"textures/x.png";
-
-
-    function addTexuredBox(texture)
-    {
-        const geometry = new BoxGeometry( 1, 1, 1 );
-        let material = null;
-        if (texture)
-        {
-            material = new MeshBasicMaterial( { map: texture } );
-        }
-        else
-        {
-            material = new MeshBasicMaterial( {color: 0xff0000} );
-        }
-    
-        const mesh = new Mesh( geometry, material );
-        app.add(mesh);
-    
-        const physicsId = physics.addGeometry(mesh);
-        physicsIds.push(physicsId);
-    }
-    
-
-    const textureX = new TextureLoader().load( imagePath,
-        function ( texture ) {
-            addTexuredBox(texture);
-        },
-    
-        undefined,
-    
-        function ( err ) {
-            addTexuredBox(null);
-        });
-
-	
-	let activateCb = () => {
-        //console.log("no-fill activate action");
-        //app.destroy();
-        removeApp(app);
-	};
 	useActivate(() => {
-		activateCb && activateCb();
+		removeApp(app);
+    app.destroy();
 	});
-	
-	useCleanup(() => {
-		for (const physicsId of physicsIds) {
-			physics.removeGeometry(physicsId);
-		}
-	});
-
 
   return app;
 };
