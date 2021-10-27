7a3e95b7e3f6d66d0e0cbc2916e90f0cc9604cb8
Avaer Kazmer <a@modules.io>
2021-10-27 07:07:36 -0400

    Clean up fov constants

--- constants.js
+++ constants.js
@@ -48,3 +48,5 @@ export const metaverseProfileDefinition = `kjzl6cwe1jw145wm7u2sy1wpa33hglvmuy6th
 
 export const crouchMaxTime = 200;
 export const activateMaxTime = 750;
+export const minFov = 60;
+export const maxFov = 120;
