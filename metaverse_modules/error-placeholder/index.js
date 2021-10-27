56aa85fa5745bde2b733ca08f97643a3ea38e3a0
Avaer Kazmer <a@modules.io>
2021-10-26 21:51:49 -0400

    Update error placeholder

--- metaverse_modules/error-placeholder/index.js
+++ metaverse_modules/error-placeholder/index.js
@@ -1,16 +1,36 @@
-import {Mesh, PlaneGeometry, DoubleSide, MeshBasicMaterial, NearestFilter, Texture} from 'three';
+import {Vector3, Matrix4, Mesh, BoxGeometry, PlaneGeometry, DoubleSide, sRGBEncoding, MeshBasicMaterial, NearestFilter, Texture} from 'three';
+import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 // import {scene, renderer, camera, runtime, world, physics, ui, app, appManager} from 'app';
 import metaversefile from 'metaversefile';
 const {useApp, useActivate, removeApp} = metaversefile;
 
 const baseUrl = import.meta.url.replace(/(\/)[^\/\/]*$/, '$1');
-const imagePath = baseUrl + 'textures/x.png';
+const imagePath = baseUrl + 'textures/remove.svg';
+const redColor = 0xef5350;
 
-const geometry = new PlaneGeometry(1, 1, 1);
+const geometry = BufferGeometryUtils.mergeBufferGeometries([
+  new BoxGeometry(1, 0.1, 0.1 + 0.01)
+    .applyMatrix4(
+      new Matrix4().lookAt(
+        new Vector3(0, 0, 0),
+        new Vector3(0, 0, -1),
+        new Vector3(1, 1, 0).normalize(),
+      ),
+    ),
+  new BoxGeometry(1, 0.1, 0.1)
+    .applyMatrix4(
+      new Matrix4().lookAt(
+        new Vector3(0, 0, 0),
+        new Vector3(0, 0, -1),
+        new Vector3(-1, 1, 0).normalize(),
+      ),
+    ),
+]);
 const material = new MeshBasicMaterial({
   side: DoubleSide,
-  transparent: true,
-  alphaTest: 0.5,
+  // transparent: true,
+  // alphaTest: 0.1,
+  anisotropy: 16,
 });
 (async () => {
   try {
@@ -24,14 +44,15 @@ const material = new MeshBasicMaterial({
       img.src = imagePath;
     });
     const texture = new Texture(img);
-    texture.minFilter = NearestFilter;
-    texture.magFilter = NearestFilter;
+    texture.encoding = sRGBEncoding;
+    // texture.minFilter = NearestFilter;
+    // texture.magFilter = NearestFilter;
     texture.needsUpdate = true;
     material.map = texture;
     material.needsUpdate = true;
   } catch(err) {
     console.warn(err);
-    material.color = 0xFF0000;
+    material.color = redColor;
     material.needsUpdate = true;
   }
 })();
