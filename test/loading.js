47a530ced69df74e7d3c49f273f05b5e5193056c
Muhammad Abeer <abeersaqib@gmail.com>
2021-10-31 06:03:15 +0500

    Fixed capture of errors on Localhost

--- test/loading.js
+++ test/loading.js
@@ -1,29 +1,51 @@
 var assert = require('assert');
 const LoadTester = require('./loading/index');
-const mlog = require('mocha-logger');
+const ChildProcess = require('child_process');
+const path = require('path');
+//const mlog = require('mocha-logger');
+
+
+
 
 describe('Running Pupeeteer', function() {
   describe('Loading Test Suite', function() {
     it('Checking Scenes', async (done) => {
-      let error = false;
-      const appTester = new LoadTester({
-        slowMo: 0,
-        host: 'https://app.webaverse.com',
+
+      const server = ChildProcess.spawn('node',[path.resolve(__dirname,'../index.js')]);
+      server.on('error',(payload)=>{
+        error = true;
+        console.log(payload);
+        // mlog.error(payload)
+      });
+
+      server.on('message',(payload)=>{
+        console.log(payload);
+        // mlog.error(payload)
+      });
+
+      server.on('spawn',async ()=>{
+        let error = false;
+        const appTester = new LoadTester({
+          slowMo: 0,
+          host: 'http://localhost:3000',
+        })
+  
+        appTester.MochaIntercept = ()=>{
+          error = true;
+        }
+  
+        await appTester.run();
+        // if(error){
+        //   mlog.error(JSON.stringify(appTester.scenes,null, 5))
+        // }else{
+        //   mlog.success(JSON.stringify(appTester.scenes,null, 5))
+        // }
+  
+        assert.equal(error,false);
+        //done();
       })
 
-      appTester.MochaIntercept = ()=>{
-        error = true;
-      }
 
-      await appTester.run();
-      if(error){
-        mlog.error(JSON.stringify(appTester.scenes,null, 5))
-      }else{
-        mlog.success(JSON.stringify(appTester.scenes,null, 5))
-      }
-
-      assert.equal(error,false);
-      //done();
     });
   });
 });
