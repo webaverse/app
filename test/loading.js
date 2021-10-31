98d7e7f3dee52acf0944f78dc7b7a9b0586da550
Muhammad Abeer <abeersaqib@gmail.com>
2021-10-31 06:07:28 +0500

    ll

--- test/loading.js
+++ test/loading.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 const LoadTester = require('./loading/index');
 const ChildProcess = require('child_process');
 const path = require('path');
-//const mlog = require('mocha-logger');
+const mlog = require('mocha-logger');
 
 
 
@@ -15,7 +15,7 @@ describe('Running Pupeeteer', function() {
       server.on('error',(payload)=>{
         error = true;
         console.log(payload);
-        // mlog.error(payload)
+        mlog.error(payload)
       });
 
       server.on('message',(payload)=>{
@@ -35,12 +35,25 @@ describe('Running Pupeeteer', function() {
         }
   
         await appTester.run();
-        // if(error){
-        //   mlog.error(JSON.stringify(appTester.scenes,null, 5))
-        // }else{
-        //   mlog.success(JSON.stringify(appTester.scenes,null, 5))
-        // }
+        if(error){
+          mlog.error(JSON.stringify(appTester.scenes,null, 5))
+        }else{
+          mlog.success(JSON.stringify(appTester.scenes,null, 5))
+        }
+
+        process.on('uncaughtException',(e)=>{
+          error = true;
+          mlog.error(e);
+          assert.equal(error,false);
+        })
   
+        process.on('unhandledRejection',(e)=>{
+          error = true;
+          mlog.error(e);
+          assert.equal(error,false);
+        })
+  
+
         assert.equal(error,false);
         //done();
       })
