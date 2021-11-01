ccf6e42b2e8c92f3c8cda6d298ee5c2545d1fc63
Muhammad Abeer <abeersaqib@gmail.com>
2021-11-01 23:12:47 +0500

    custom network wait for load added

--- test/loading.js
+++ test/loading.js
@@ -9,63 +9,30 @@ describe('Running Pupeeteer', function() {
   describe('Loading Test Suite', function() {
     it('Checking Scenes', async (done) => {
 
-      mlog.log(path.resolve(__dirname,'../index.js'));
-
-      const server = ChildProcess.exec('cd .. && npm run dev');
-      server.on('error',(payload)=>{
-        error = true;
-        mlog.log(payload)
-      });
-
-      server.stdout.on('data',(_d)=>{
-        mlog.log(Buffer(_d).toString());
+      const appTester = new LoadTester({
+        slowMo: 0,
+        host: 'https://app.webaverse.com',
       })
 
-      server.stdout.on('error',(e)=>{
+      appTester.MochaIntercept = ()=>{
         error = true;
-        mlog.error(Buffer(e).toString())
-      });
+      }
 
-      server.on('message',(payload)=>{
-        
-      });
+      try{
+        await appTester.run();
+      }catch(e){
+        //digest pupeteer crash error that comes up very rare.
+      }
 
-      server.on('disconnect',(payload)=>{
-        mlog.log(payload);
-      });
 
-      server.on('spawn',async ()=>{
-        let error = false;
-        const appTester = new LoadTester({
-          slowMo: 0,
-          host: 'http://localhost:3000',
-        })
-  
-        appTester.MochaIntercept = ()=>{
-          error = true;
-        }
-  
-        try{
-          await appTester.run();
-        }catch(e){
-          //digest pupeteer crash error that comes up very rare.
-        }
+      if(error){
+        mlog.error(JSON.stringify(appTester.scenes,null, 5))
+      }else{
+        mlog.success(JSON.stringify(appTester.scenes,null, 5))
+      }
 
-  
-        if(error){
-          mlog.error(JSON.stringify(appTester.scenes,null, 5))
-        }else{
-          mlog.success(JSON.stringify(appTester.scenes,null, 5))
-        }
-  
-        server.kill('SIGINT');
-        server.on('exit',()=>{
-          assert.equal(error,false);
-          done();
-        });
-
-      })
-
+      assert.equal(error,false);
+      done();
 
     });
   });
