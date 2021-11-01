86fc2de0e7b41ec6e1477ad299f42e40e9034021
rob-webaverse <92062447+rob-webaverse@users.noreply.github.com>
2021-11-01 11:32:35 +0000

    Update loading.js

--- test/loading.js
+++ test/loading.js
@@ -59,30 +59,3 @@ describe('Running Pupeeteer', function() {
     });
   });
 });
-const mlog = require('mocha-logger');
-
-describe('Running Pupeeteer', function() {
-  describe('Loading Test Suite', function() {
-    it('Checking Scenes', async (done) => {
-      let error = false;
-      const appTester = new LoadTester({
-        slowMo: 0,
-        host: 'http://localhost:3000',
-      })
-
-      appTester.MochaIntercept = ()=>{
-        error = true;
-      }
-
-      await appTester.run();
-      if(error){
-        mlog.error(JSON.stringify(appTester.scenes,null, 5))
-      }else{
-        mlog.success(JSON.stringify(appTester.scenes,null, 5))
-      }
-
-      assert.equal(error,false);
-      //done();
-    });
-  });
-});
