a77f3a4a082752904b6970bfc37c394a8f98ba44
Avaer Kazmer <a@modules.io>
2021-11-02 05:17:59 -0400

    Dead code removal in rig.js

--- rig.js
+++ rig.js
@@ -5,17 +5,17 @@ this file binds logical characters (local player, remote players, npcs) to metav
 import * as THREE from 'three';
 // import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 
-import {makeRigCapsule} from './vr-ui.js';
+// import {makeRigCapsule} from './vr-ui.js';
 import {unFrustumCull} from './util.js';
 import {getRenderer, scene, camera, dolly} from './renderer.js';
 import Avatar from './avatars/avatars.js';
-import {chatManager} from './chat-manager.js';
+// import {chatManager} from './chat-manager.js';
 import metaversefile from 'metaversefile';
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
-const localQuaternion = new THREE.Quaternion();
-const localQuaternion2 = new THREE.Quaternion();
+// const localQuaternion = new THREE.Quaternion();
+// const localQuaternion2 = new THREE.Quaternion();
 const localEuler = new THREE.Euler();
 const localEuler2 = new THREE.Euler();
 
@@ -101,48 +101,6 @@ class RigManager {
   async setLocalAvatar(app) {
     this.localRig = await this._switchAvatar(this.localRig, app);
   }
-
-  async addPeerRig(peerId, meta) {
-    const contentId = meta.avatarUrl;
-    const app = await metaversefile.load(contentId);
-    app.contentId = contentId;
-    
-    const peerRig = await this._switchAvatar(null, app);
-    scene.add(peerRig.model);
-    
-    peerRig.rigCapsule = makeRigCapsule();
-    peerRig.rigCapsule.visible = false;
-    this.scene.add(peerRig.rigCapsule);
-
-    this.peerRigs.set(peerId, peerRig);
-  }
-
-  async removePeerRig(peerId) {
-    const peerRig = this.peerRigs.get(peerId);
-    if (peerRig) {
-      peerRig.model.parent.remove(peerRig.model);
-      peerRig.rigCapsule.parent.remove(peerRig.rigCapsule);
-      // peerRig.textMesh.parent.remove(peerRig.textMesh);
-      // peerRig.aux.destroy();
-      this.peerRigs.delete(peerId);
-    }
-  }
-  
-  setPeerAvatarUrl(peerId, avatarUrl) {
-    const peerRig = this.peerRigs.get(peerId);
-    if (peerRig) {
-      if (peerRig.app.contentId !== avatarUrl) {
-        throw new Error('do not know how to set change avatar url yet');
-        // debugger;
-      }
-    } else {
-      console.warn('set peer avatar url for unknown peer:', peerRig);
-    }
-  }
-
-  async setPeerAvatar(peerId, app) {
-    this.peerRigs.set(peerId, _makeRig(app));
-  }
   
   setLocalMicMediaStream(mediaStream, options) {
     this.localRig.setMicrophoneMediaStream(mediaStream, options);
@@ -152,198 +110,6 @@ class RigManager {
     const peerRig = this.peerRigs.get(peerId);
     peerRig.setMicrophoneMediaStream(mediaStream);
     this.peerRigs.set(peerId, peerRig);
-  }
-
-  getLocalAvatarPose() {
-    if (this.localRig) {
-      const hmdPosition = this.localRig.inputs.hmd.position.toArray();
-      const hmdQuaternion = this.localRig.inputs.hmd.quaternion.toArray();
-
-      const leftGamepadPosition = this.localRig.inputs.leftGamepad.position.toArray();
-      const leftGamepadQuaternion = this.localRig.inputs.leftGamepad.quaternion.toArray();
-      const leftGamepadPointer = this.localRig.inputs.leftGamepad.pointer;
-      const leftGamepadGrip = this.localRig.inputs.leftGamepad.grip;
-      const leftGamepadEnabled = this.localRig.inputs.leftGamepad.enabled;
-
-      const rightGamepadPosition = this.localRig.inputs.rightGamepad.position.toArray();
-      const rightGamepadQuaternion = this.localRig.inputs.rightGamepad.quaternion.toArray();
-      const rightGamepadPointer = this.localRig.inputs.rightGamepad.pointer;
-      const rightGamepadGrip = this.localRig.inputs.rightGamepad.grip;
-      const rightGamepadEnabled = this.localRig.inputs.rightGamepad.enabled;
-
-      const floorHeight = this.localRig.getFloorHeight();
-      const handsEnabled = [this.localRig.getHandEnabled(0), this.localRig.getHandEnabled(1)];
-      const topEnabled = this.localRig.getTopEnabled();
-      const bottomEnabled = this.localRig.getBottomEnabled();
-      const direction = this.localRig.direction.toArray();
-      const velocity = this.localRig.velocity.toArray();
-      const {
-        jumpState,
-        jumpTime,
-        flyState,
-        flyTime,
-        useTime,
-        useAnimation,
-        sitState,
-        sitAnimation,
-        danceState,
-        danceTime,
-        danceAnimation,
-        throwState,
-        throwTime,
-        crouchState,
-        crouchTime,
-      } = this.localRig;
-
-      return [
-        [hmdPosition, hmdQuaternion],
-        [leftGamepadPosition, leftGamepadQuaternion, leftGamepadPointer, leftGamepadGrip, leftGamepadEnabled],
-        [rightGamepadPosition, rightGamepadQuaternion, rightGamepadPointer, rightGamepadGrip, rightGamepadEnabled],
-        floorHeight,
-        handsEnabled,
-        topEnabled,
-        bottomEnabled,
-        direction,
-        velocity,
-        jumpState,
-        jumpTime,
-        flyState,
-        flyTime,
-        useTime,
-        useAnimation,
-        sitState,
-        sitAnimation,
-        danceState,
-        danceTime,
-        danceAnimation,
-        throwState,
-        throwTime,
-        crouchState,
-        crouchTime,
-      ];
-    } else {
-      return null;
-    }
-  }
-
-  setLocalAvatarPose(poseArray) {
-    if (this.localRig) {
-      const [
-        [hmdPosition, hmdQuaternion],
-        [leftGamepadPosition, leftGamepadQuaternion, leftGamepadPointer, leftGamepadGrip, leftGamepadEnabled],
-        [rightGamepadPosition, rightGamepadQuaternion, rightGamepadPointer, rightGamepadGrip, rightGamepadEnabled],
-      ] = poseArray;
-
-      this.localRig.inputs.hmd.position.fromArray(hmdPosition);
-      this.localRig.inputs.hmd.quaternion.fromArray(hmdQuaternion);
-
-      this.localRig.inputs.leftGamepad.position.fromArray(leftGamepadPosition);
-      this.localRig.inputs.leftGamepad.quaternion.fromArray(leftGamepadQuaternion);
-      this.localRig.inputs.leftGamepad.pointer = leftGamepadPointer;
-      this.localRig.inputs.leftGamepad.grip = leftGamepadGrip;
-      this.localRig.inputs.leftGamepad.enabled = leftGamepadEnabled;
-
-      this.localRig.inputs.rightGamepad.position.fromArray(rightGamepadPosition);
-      this.localRig.inputs.rightGamepad.quaternion.fromArray(rightGamepadQuaternion);
-      this.localRig.inputs.rightGamepad.pointer = rightGamepadPointer;
-      this.localRig.inputs.rightGamepad.grip = rightGamepadGrip;
-      this.localRig.inputs.rightGamepad.enabled = rightGamepadEnabled;
-
-      /* this.localRig.textMesh.position.copy(this.localRig.inputs.hmd.position);
-      this.localRig.textMesh.position.y += 0.5;
-      this.localRig.textMesh.quaternion.copy(this.localRig.inputs.hmd.quaternion);
-      localEuler.setFromQuaternion(camera.quaternion, 'YXZ');
-      localEuler.z = 0;
-      this.localRig.textMesh.quaternion.setFromEuler(localEuler); */
-    }
-  }
-
-  setPeerAvatarPose(player) {
-    const peerRig = this.peerRigs.get(player.id);
-    if (peerRig) { 
-      const pose = player.pose;
-      const {hmd, leftGamepad, rightGamepad} = peerRig.inputs;
-
-      hmd.position.fromArray(pose.position);
-      hmd.quaternion.fromArray(pose.quaternion);
-
-      if (pose.extra.length > 0) {
-        leftGamepad.position.fromArray(pose.extra[0]);
-        leftGamepad.quaternion.fromArray(pose.extra[1]);
-        leftGamepad.pointer = pose.extra[2][0];
-        leftGamepad.grip = pose.extra[2][1];
-        peerRig.setHandEnabled(0, pose.extra[2][2]);
-
-        rightGamepad.position.fromArray(pose.extra[3]);
-        rightGamepad.quaternion.fromArray(pose.extra[4]);
-        rightGamepad.pointer = pose.extra[5][0];
-        rightGamepad.grip = pose.extra[5][1];
-        peerRig.setHandEnabled(0, pose.extra[5][2]);
-
-        peerRig.setFloorHeight(pose.extra[6][0]);
-
-        peerRig.setTopEnabled(pose.extra[6][1]);
-        peerRig.setBottomEnabled(pose.extra[6][2]);
-
-        peerRig.direction.fromArray(pose.extra[7]);
-        peerRig.velocity.fromArray(pose.extra[8]);
-
-        peerRig.jumpState = pose.extra[9][0];
-        peerRig.jumpTime = pose.extra[9][1];
-        peerRig.flyState = pose.extra[9][2];
-        peerRig.flyTime = pose.extra[9][3];
-        peerRig.useTime = pose.extra[9][4];
-        peerRig.useAnimation = pose.extra[9][5];
-        peerRig.sitState = pose.extra[9][6];
-        peerRig.sitAnimation = pose.extra[9][7];
-        peerRig.danceState = pose.extra[9][8];
-        peerRig.danceTime = pose.extra[9][9];
-        peerRig.danceAnimation = pose.extra[9][10];
-        peerRig.throwState = pose.extra[9][11];
-        peerRig.throwTime = pose.extra[9][12];
-        peerRig.crouchState = pose.extra[9][13];
-        peerRig.crouchTime = pose.extra[9][14];
-      }
-
-      /* peerRig.textMesh.position.copy(peerRig.inputs.hmd.position);
-      peerRig.textMesh.position.y += 0.5;
-      peerRig.textMesh.quaternion.copy(peerRig.inputs.hmd.quaternion);
-      localEuler.setFromQuaternion(peerRig.textMesh.quaternion, 'YXZ');
-      localEuler.x = 0;
-      localEuler.y += Math.PI;
-      localEuler.z = 0;
-      peerRig.textMesh.quaternion.setFromEuler(localEuler); */
-
-      peerRig.rigCapsule.position.copy(peerRig.inputs.hmd.position);
-      
-      peerRig.volume = player.volume;
-    }
-  }
-  
-  getRigTransforms() {
-    if (this.localRig) {
-      return [
-        {
-          position: this.localRig.inputs.leftGamepad.position,
-          quaternion: this.localRig.inputs.leftGamepad.quaternion,
-        },
-        {
-          position: this.localRig.inputs.rightGamepad.position,
-          quaternion: this.localRig.inputs.rightGamepad.quaternion,
-        },
-      ];
-    } else {
-      return [
-        {
-          position: localVector.set(0, 0, 0),
-          quaternion: localQuaternion.set(0, 0, 0, 1),
-        },
-        {
-          position: localVector2.set(0, 0, 0),
-          quaternion: localQuaternion2.set(0, 0, 0, 1),
-        },
-      ];
-    }
   }
 
   update(timestamp, timeDiff) {
