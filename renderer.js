e839e504947805a2de379f2314af838e02c86d6d
yaser <yaser@superblacksoftware.net>
2021-11-08 01:39:53 +0000

    enable log depth buf

--- renderer.js
+++ renderer.js
@@ -24,7 +24,7 @@ function bindCanvas(c) {
     antialias: true,
     alpha: true,
     // preserveDrawingBuffer: false,
-    // logarithmicDepthBuffer: true,
+    logarithmicDepthBuffer: true,
   });
 
   const rect = renderer.domElement.getBoundingClientRect();
@@ -96,7 +96,6 @@ const camera = new THREE.PerspectiveCamera(minFov, window.innerWidth / window.in
 camera.position.set(0, 1.6, 0);
 camera.rotation.order = 'YXZ';
 camera.name = 'sceneCamera';
-
 /* const avatarCamera = camera.clone();
 avatarCamera.near = 0.2;
 avatarCamera.updateProjectionMatrix(); */
