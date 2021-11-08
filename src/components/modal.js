ce7c260849b99c044371082cef7a206590b0945f
mujabmuneeb <mujabmuneeb@gmail.com>
2021-11-08 12:22:19 +0500

    modal added

---
+++ src/components/modal.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./modal.css";
+import PropTypes from "prop-types";
+
+export default class Modal extends React.Component {
+  onClose = e => {
+    this.props.onClose && this.props.onClose(e);
+  };
+  render() {
+    if (!this.props.show) {
+      return null;
+    }
+    return (
+      <div className="modal" id="modal">
+        {/* <h2>Modal Window</h2> */}
+        <div className="content">{this.props.children}
+        
+        <div className="specialBtnDiv2" style={{marginTop: '50px'}}>
+            <div className="specialBtnDiv">
+            <button className="specialBtn" onClick={this.onClose}>Close</button>
+            </div>              
+        </div>
+
+        {/* <div className="actions">
+          <button className="toggle-button" onClick={this.onClose}>
+            close
+          </button>
+        </div> */}
+        </div>
+      </div>
+    );
+  }
+}
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  show: PropTypes.bool.isRequired
+};
