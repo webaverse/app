fae9fbfdb44ae7e0268e230fb059d2b66e13e28a
Avaer Kazmer <a@modules.io>
2021-11-11 11:44:54 -0500

    Physics manager dead code cleanup

--- physics-manager.js
+++ physics-manager.js
@@ -4,7 +4,6 @@ it contains code for character capsules and world simulation.
 */
 
 import * as THREE from 'three';
-// import uiManager from './ui-manager.js';
 import {getRenderer, camera, dolly} from './renderer.js';
 import physx from './physx.js';
 import cameraManager from './camera-manager.js';
@@ -14,8 +13,6 @@ import metaversefileApi from 'metaversefile';
 import {getNextPhysicsId, convertMeshToPhysicsMesh} from './util.js';
 import {getVelocityDampingFactor} from './util.js';
 import {groundFriction} from './constants.js';
-
-// const leftQuaternion = new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 0, -1), new THREE.Vector3(-1, 0, 0));
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
@@ -42,65 +39,6 @@ physicsManager.direction = direction;
 const offset = new THREE.Vector3();
 physicsManager.offset = offset;
 
-/* let jumpState = false;
-let jumpTime = -1;
-const getJumpState = () => jumpState;
-physicsManager.getJumpState = getJumpState;
-const getJumpTime = () => jumpTime;
-physicsManager.getJumpTime = getJumpTime;
-const jump = () => {
-  jumpState = true;
-  jumpTime = 0;
-  sitState = false;
-  physicsManager.velocity.y += 5;
-};
-physicsManager.jump = jump;
-
-let flyState = false;
-let flyTime = -1;
-const getFlyState = () => flyState;
-physicsManager.getFlyState = getFlyState;
-const setFlyState = newFlyState => {
-  const now = Date.now();
-  flyState = newFlyState;
-  if (newFlyState) {
-    flyTime = 0;
-    if (!jumpState) {
-      jump();
-    }
-  } else {
-    flyTime = -1;
-    jumpTime = 0;
-  }
-  glideState = false;
-};
-physicsManager.setFlyState = setFlyState;
-const getFlyTime = () => flyTime;
-physicsManager.getFlyTime = getFlyTime;
-
-let glideState = false;
-const getGlideState = () => glideState;
-physicsManager.getGlideState = getGlideState;
-const setGlide = newGlideState => {
-  glideState = newGlideState;
-};
-physicsManager.setGlide = setGlide;
-
-let sitState = false;
-const getSitState = () => sitState;
-physicsManager.getSitState = getSitState;
-const setSitState = newSitState => {
-  sitState = newSitState;
-};
-physicsManager.setSitState = setSitState; */
-
-/* let damping;
-const setDamping = (newDamping = 0.7) => {
-  damping = newDamping;
-};
-setDamping();
-physicsManager.setDamping = setDamping; */
-
 let sitTarget = null;
 const getSitTarget = () => sitTarget;
 physicsManager.getSitTarget = getSitTarget;
@@ -122,68 +60,11 @@ const setSitController = newSitController => {
 };
 physicsManager.setSitController = setSitController;
 
-/* let useTime = -1;
-const getUseTime = () => useTime;
-physicsManager.getUseTime = getUseTime;
-const startUse = () => {
-  useTime = 0;
-};
-physicsManager.startUse = startUse;
-const stopUse = () => {
-  useTime = -1;
-};
-physicsManager.stopUse = stopUse;
-
-let danceState = null;
-let danceTime = 0;
-const getDanceState = () => danceState;
-physicsManager.getDanceState = getDanceState;
-const getDanceTime = () => danceTime;
-physicsManager.getDanceTime = getDanceTime;
-const setDanceState = newDanceState => {
-  danceState = newDanceState;
-  if (danceState) {
-    danceTime = 0;
-  }
-};
-physicsManager.setDanceState = setDanceState;
-
-let throwState = null;
-let throwTime = 0;
-const getThrowState = () => throwState;
-physicsManager.getThrowState = getThrowState;
-const getThrowTime = () => throwTime;
-physicsManager.getThrowTime = getThrowTime;
-const setThrowState = newThrowState => {
-  throwState = newThrowState;
-  if (throwState) {
-    throwTime = 0;
-  }
-};
-physicsManager.setThrowState = setThrowState;
-
-let crouchState = false;
-let crouchTime = 1000;
-const getCrouchState = () => crouchState;
-physicsManager.getCrouchState = getCrouchState;
-const getCrouchTime = () => crouchTime;
-physicsManager.getCrouchTime = getCrouchTime;
-physicsManager.setCrouchState = newCrouchState => {
-  crouchState = newCrouchState;
-  crouchTime = 0;
-}; */
-
-// const physicsObjects = {};
 const physicsUpdates = [];
 const _makePhysicsObject = (physicsId/*, position, quaternion, scale*/) => {
   const physicsObject = new THREE.Object3D();
-  // physicsObject.position.copy(position);
-  // physicsObject.quaternion.copy(quaternion);
-  // physicsObject.scale.copy(scale);
-  // physicsObject.updateMatrixWorld();
   physicsObject.physicsId = physicsId;
   physicsObject.error = new Error().stack;
-  // physicsObject.needsUpdate = false;
   return physicsObject;
 };
 const _extractPhysicsGeometryForId = physicsId => {
@@ -205,9 +86,6 @@ physicsManager.addCapsuleGeometry = (position, quaternion, radius, halfHeight, c
     new THREE.CapsuleGeometry(radius, radius, halfHeight*2)
   );
   physicsMesh.visible = false;
-  // physicsMesh.position.copy(position);
-  // physicsMesh.quaternion.copy(quaternion);
-  // physicsMesh.scale.copy(size);
   physicsObject.add(physicsMesh);
   physicsObject.physicsMesh = physicsMesh;
   return physicsObject;
@@ -222,15 +100,11 @@ physicsManager.addBoxGeometry = (position, quaternion, size, dynamic) => {
     new THREE.BoxGeometry(2, 2, 2)
   );
   physicsMesh.visible = false;
-  // physicsMesh.position.copy(position);
-  // physicsMesh.quaternion.copy(quaternion);
-  // physicsMesh.scale.copy(size);
   physicsObject.add(physicsMesh);
   physicsObject.physicsMesh = physicsMesh;
   return physicsObject;
 };
 physicsManager.addGeometry = mesh => {
-  // mesh.updateMatrixWorld();
   const physicsMesh = convertMeshToPhysicsMesh(mesh);
   if (mesh.parent) {
     mesh.parent.matrixWorld.decompose(
@@ -240,13 +114,6 @@ physicsManager.addGeometry = mesh => {
     );
     physicsMesh.updateMatrixWorld();
   }
-  /* physicsMesh.position.copy(mesh.position);
-  physicsMesh.quaternion.copy(mesh.quaternion);
-  physicsMesh.scale.copy(mesh.scale);
-  physicsMesh.updateMatrixWorld(); */
-  
-  // mesh.updateMatrixWorld();
-  // mesh.matrixWorld.decompose(localVector, localQuaternion, localVector2);
   
   const physicsId = getNextPhysicsId();
   physx.physxWorker.addGeometryPhysics(physx.physics, physicsMesh, physicsId);
@@ -329,18 +196,6 @@ physicsManager.removeGeometry = physicsObject => {
 };
 
 physicsManager.raycast = (position, quaternion) => physx.physxWorker.raycastPhysics(physx.physics, position, quaternion);
-// physicsManager.getPhysicsObject = physicsId => physicsObjects[physicsId];
-/* physicsManager.pushPhysicsUpdate = physicsId => {
-  const physicsObject = physicsObjects[physicsId];
-  // physicsObject.updateMatrixWorld();
-  physicsObject.matrixWorld.decompose(localVector, localQuaternion, localVector2);
-  physicsUpdates.push({
-    id: physicsId,
-    position: localVector.clone(),
-    quaternion: localQuaternion.clone(),
-    scale: localVector2.clone(),
-  });
-}; */
 physicsManager.simulatePhysics = timeDiff => {
   const t = timeDiff/1000;
   // console.log('simulate', timeDiff, t);
@@ -370,10 +225,6 @@ physicsManager.pushUpdate = physicsObject => {
 
 physicsManager.getRigTransforms = () => rigManager.getRigTransforms();
 
-/* const makeAnimal = null;
-const animals = [];
-physicsManager.animals = animals; */
-
 const gravity = new THREE.Vector3(0, -9.8, 0);
 const _applyGravity = timeDiff => {
   const localPlayer = metaversefileApi.useLocalPlayer();
@@ -397,19 +248,12 @@ const _applyDamping = timeDiffS => {
       physicsManager.velocity.x *= factor;
       physicsManager.velocity.z *= factor;
     }
-
-    /* const terminalVelocity = 50;
-    const _clampToTerminalVelocity = v => Math.min(Math.max(v, -terminalVelocity), terminalVelocity);
-    physicsManager.velocity.x = _clampToTerminalVelocity(physicsManager.velocity.x);
-    physicsManager.velocity.z = _clampToTerminalVelocity(physicsManager.velocity.z);
-    physicsManager.velocity.y = _clampToTerminalVelocity(physicsManager.velocity.y); */
   }
 };
 const _getAvatarWorldObject = o => {
   const renderer = getRenderer();
   const xrCamera = renderer.xr.getSession() ? renderer.xr.getCamera(camera) : camera;
   o.matrix.copy(camera.matrixWorld)
-    // .premultiply(dolly.matrix)
     .decompose(o.position, o.quaternion, o.scale);
   return o;
 };
@@ -587,53 +431,8 @@ const applyVelocity = (() => {
   };
 })();
 physicsManager.applyVelocity = applyVelocity;
-/* const _collideItems = matrix => {
-  matrix.decompose(localVector3, localQuaternion2, localVector4);
-
-  if (uiManager.popupMesh.visible) {
-    uiManager.popupMesh.position.lerp(localVector4.copy(localVector3).add(localVector5.set(0, -0.25, -1).applyQuaternion(localQuaternion2)), 0.1);
-    uiManager.popupMesh.quaternion.slerp(localQuaternion2, 0.1);
-  }
-
-  physx.updatePhysics(localVector3);
-}; */
-/* const _collideChunk = matrix => {
-  matrix.decompose(localVector3, localQuaternion2, localVector4);
-  physx.currentChunkMesh.update(localVector3);
-}; */
 
 physicsManager.getGravity = () => gravity;
-/* physicsManager.setGravity = g => {
-  if (g === true) {
-    gravity.set(0, -9.8, 0);
-  } else if (g === false) {
-    gravity.setScalar(0);
-  } else {
-    gravity.copy(g);
-  }
-}; */
-
-// physicsManager.convertMeshToPhysicsMesh = convertMeshToPhysicsMesh;
-
-
-// const unlocked = true;
-/* physicsManager.unlockControls = () => {
-  debugger;
-  unlocked = true;
-}; */
-
-/* const _shiftPhysicsObjects = (physicsIds, oldMatrixWorld, newMatrixWorld) => {
-  const oldMatrixWorldInverse = localMatrix.copy(oldMatrixWorld)
-    .invert();
-  for (const physicsId of physicsIds) {
-    const physicsObject = physicsObject[physicsId];
-    physicsObject.matrix
-      .premultiply(newMatrixWorld)
-      .premultiply(oldMatrixWorldInverse)
-      .decompose(physicsObject.position, physicsObject.quaternion, physicsObject.scale);
-  }
-};
-physicsManager.shiftPhysicsObjects = _shiftPhysicsObjects; */
 
 const _copyPQS = (dst, src) => {
   dst.position.copy(src.position);
