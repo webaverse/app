ff9acf0079a88581856b06bad9591108002a93df
Avaer Kazmer <a@modules.io>
2021-11-21 22:41:50 -0500

    Import zjs in world.js

--- world.js
+++ world.js
@@ -4,6 +4,7 @@ this file contains the singleplayer code.
 
 import * as THREE from 'three';
 import * as Y from 'yjs';
+import * as Z from 'zjs/z.mjs';
 import WSRTC from 'wsrtc/wsrtc.js';
 
 import hpManager from './hp-manager.js';
