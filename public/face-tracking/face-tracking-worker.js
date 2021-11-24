65e4da20293bb87265442804043de09b2d3dc7ef
Avaer Kazmer <a@modules.io>
2021-11-24 09:21:13 -0500

    Major worker rewrite

--- public/face-tracking/face-tracking-worker.js
+++ public/face-tracking/face-tracking-worker.js
@@ -2,7 +2,15 @@ import '/face-tracking/holistic/holistic.js';
 // window.Holistic = Holistic;
 console.log('worker listening for message 0');
 
-document.body.style.backgroundColor = '#0F0';
+// document.body.style.backgroundColor = '#0F0';
+
+/* setTimeout(() => {
+  for(;;) {}
+}, 1000); */
+
+/* setInterval(() => {
+  console.log('still alive');
+}, 2000); */
 
 const locateFile = file => {
   // console.log('find file', file, `/face-tracking/holistic/${file}`);
@@ -15,12 +23,13 @@ const holistic = new Holistic({
 holistic.setOptions({
   // staticImageMode: true,
   modelComplexity: 2,
+  // modelComplexity: 1,
   smoothLandmarks: true,
   enableSegmentation: true,
   smoothSegmentation: true,
   refineFaceLandmarks: true,
-  // minDetectionConfidence: 0.9,
-  // minTrackingConfidence: 0.9,
+  // minDetectionConfidence: 0.5,
+  // minTrackingConfidence: 0.5,
 });
 holistic.onResults(results => {
   const {
@@ -31,6 +40,8 @@ holistic.onResults(results => {
     leftHandLandmarks,
   } = results;
 
+  // console.timeEnd('lol');
+  
   // console.log('got results', results);
   messagePort.postMessage({
     error: null,
@@ -41,26 +52,30 @@ holistic.onResults(results => {
       rightHandLandmarks,
       leftHandLandmarks,
     },
-  })
+  });
 });
 /* holistic.g.D = {
   canvas: null,
   lol: 'zol',
 }; */
 
-// console.log('worker listening for message 1');
+console.log('worker listening for message 1');
 let messagePort;
 window.addEventListener('message', e => {
+  // console.log('worker got message', e);
   if (e.data?._webaverse) {
     messagePort = e.data.messagePort;
     // console.log('worker got message', messagePort, e);
     messagePort.onmessage = async e => {
+      // console.time('frame');
       // console.log('message port got message', e);
       const image = e.data.image;
       if (image) {
-        // console.log('send image for processing', image);
-        await holistic.send({
-          image,
+        // console.time('lol');
+        requestAnimationFrame(async () => {
+          await holistic.send({
+            image,
+          });
         });
       } else {
         postMessage({
@@ -68,7 +83,14 @@ window.addEventListener('message', e => {
           result: null,
         });
       }
+      // console.timeEnd('frame');
     };
   }
 });
 console.log('worker listening for message 2');
+
+// this hack is needed to make the browser think the iframe is active
+const _fakeRaf = () => {
+  requestAnimationFrame(_fakeRaf);
+};
+_fakeRaf();
