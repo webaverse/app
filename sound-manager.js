2a923b84b8305be50d595cceb0c232aadb079a38
skaljac <skaljac@gmail.com>
2021-11-24 19:10:02 +0100

    Sound manager implementation

---
+++ sound-manager.js
@@ -0,0 +1,142 @@
+/* 
+this is the sound manager implementation.
+*/
+
+import * as THREE from 'three';
+import ioManager from './io-manager.js';
+import gameManager from './game.js';
+import physx from './physx.js';
+import {camera} from './renderer.js';
+import metaversefileApi from 'metaversefile';
+
+const localVector = new THREE.Vector3();
+const localVector2 = new THREE.Vector3();
+const localVector3 = new THREE.Vector3();
+const localVector4 = new THREE.Vector3();
+const localVector5 = new THREE.Vector3();
+const localQuaternion = new THREE.Quaternion();
+const localQuaternion2 = new THREE.Quaternion();
+const localMatrix = new THREE.Matrix4();
+
+const zeroVector = new THREE.Vector3();
+const upVector = new THREE.Vector3(0, 1, 0);
+
+class SoundManager {
+  constructor() {
+
+    this.lastIsJumping = false;
+
+    this.timeNextStep = 0;
+    this.sounds = new Map();
+
+    this.listener = new THREE.AudioListener();
+    camera.add( this.listener );
+
+
+    this.audioLoader = new THREE.AudioLoader();
+
+    this.loadBasicSounds();
+  }
+  update(timeDiffS) {
+
+    this.localPlayer = metaversefileApi.useLocalPlayer();
+    
+    let timeForNextStep = 450.0;
+    const playerSpeed = gameManager.getSpeed();
+
+    if (playerSpeed > 0.15) {
+      timeForNextStep = 225.0;
+    }
+    //console.log(playerSpeed);
+    //console.log(timeDiffS);
+
+    /*this.timeNextStep -= timeDiffS;
+    if (this.timeNextStep <= 0){
+      this.timeNextStep = 10.0;
+      this.play('step1');
+    }*/
+
+    //console.log(this.localPlayer.characterPhysics.velocity.length());
+
+    const isJumping = this.localPlayer.hasAction('jump');
+    const isFlying = gameManager.isFlying();
+    
+    if (this.lastIsJumping && !isJumping) {
+      this.play('jumpEnd');
+    }
+
+    const vel = this.localPlayer.characterPhysics.velocity.length();
+
+    //console.log(this.localPlayer.characterPhysics.velocity);
+
+    if (vel > 0.1 && !isJumping && !isFlying) {
+      this.timeNextStep -= timeDiffS;
+      if (this.timeNextStep <= 0){
+        this.timeNextStep = timeForNextStep;
+        if (Math.random() < 0.85) {
+          this.play('step1');
+        }
+        else {
+          this.play('step2');
+        }
+      }    
+    }
+
+    this.lastIsJumping = isJumping;
+  }
+  setMasterVolume(volume){
+    this.listener.setMasterVolume(volume);
+  }
+  getMasterVolume(){
+    this.listener.getMasterVolume();
+  }
+  setSoundLoop(name,loop) {
+    if (this.sounds.get(name)) {
+      this.sounds.get(name).setLoop(loop);
+    }
+  }
+  setSoundVolume(name,volume) {
+    if (this.sounds.get(name)) {
+      this.sounds.get(name).setVolume(volume);
+    }
+  } 
+  play(name) {
+    if (this.sounds.get(name)) {
+      if (this.sounds.get(name).isPlaying) {
+        this.sounds.get(name).stop();
+      }
+      this.sounds.get(name).play();
+    }
+  }
+  loadBasicSounds() {
+
+    this.loadSound('jump','sounds/Jump1.wav');
+    this.loadSound('jumpEnd','sounds/JumpEnd.wav');
+    this.loadSound('step1','sounds/FootstepA.wav',0.15);
+    this.loadSound('step2','sounds/FootstepB.wav',0.15);
+  
+  }
+  loadSound(name,path,volume=1.0,shouldLoop=false,shouldReload=false) {
+
+    if (!shouldReload && this.sounds[name]) {
+      return;
+    }
+
+    const sound = new THREE.Audio( this.listener );
+    sound.setLoop( shouldLoop );
+    sound.setVolume( volume );
+
+    this.sounds.set(name,sound);
+
+    this.audioLoader.load( path, function( buffer ) {
+
+      sound.setBuffer( buffer );
+
+    });
+
+  }
+}
+
+const soundManager = new SoundManager();
+
+export default soundManager;
