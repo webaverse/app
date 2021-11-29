c8eeadc634750e4ac3914e4b245748a3ee4f3940
skaljac <skaljac@gmail.com>
2021-11-29 16:45:52 +0100

    Step sound disabled for sit,jump,fly player state

--- avatars/avatars.js
+++ avatars/avatars.js
@@ -1776,35 +1776,35 @@ class Avatar {
       if (currentSpeed > 0.1) {
         if (key == 'walk') {
           if (currAniTime > 0.26 && currAniTime < 0.4)
-            soundManager.playWithId('step1',1);
+            soundManager.playStepSound(1);
           if (currAniTime > 0.76 && currAniTime < 0.9)
-            soundManager.playWithId('step1',2);
+            soundManager.playStepSound(2);
           if (currAniTime > 1.26 && currAniTime < 1.4)
-            soundManager.playWithId('step1',3);
+            soundManager.playStepSound(3);
           if (currAniTime > 1.76 && currAniTime < 1.9)
-            soundManager.playWithId('step1',4);
+            soundManager.playStepSound(4);
           if (currAniTime > 2.26 && currAniTime < 2.5)
-            soundManager.playWithId('step1',5);
+            soundManager.playStepSound(5);
         }
         if (key == 'run') {
           if (currAniTime > 0.16 && currAniTime < 0.3)
-            soundManager.playWithId('step1',1);
+            soundManager.playStepSound(1);
           if (currAniTime > 0.43 && currAniTime < 0.45)
-            soundManager.playWithId('step1',2);
+            soundManager.playStepSound(2);
           if (currAniTime > 0.693 && currAniTime < 0.8)
-            soundManager.playWithId('step1',3);
+            soundManager.playStepSound(3);
           if (currAniTime > 0.963 && currAniTime < 1.1)
-            soundManager.playWithId('step1',4);
+            soundManager.playStepSound(4);
           if (currAniTime > 1.226 && currAniTime < 1.3)
-            soundManager.playWithId('step1',5);
+            soundManager.playStepSound(5);
           if (currAniTime > 1.496 && currAniTime < 1.6)
-            soundManager.playWithId('step1',6);
+            soundManager.playStepSound(6);
           if (currAniTime > 1.759 && currAniTime < 1.9)
-            soundManager.playWithId('step1',7);
+            soundManager.playStepSound(7);
           if (currAniTime > 2.029 && currAniTime < 2.1)
-            soundManager.playWithId('step1',8);
+            soundManager.playStepSound(8);
           if (currAniTime > 2.292 && currAniTime < 2.4)
-            soundManager.playWithId('step1',9);
+            soundManager.playStepSound(9);
         }
       }
       
