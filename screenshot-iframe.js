77a5b60cd05bdba9842646b6338676cf1ae576be
mujabmuneeb <mujabmuneeb@gmail.com>
2021-12-01 23:48:37 +0500

    regex check removed

--- screenshot-iframe.js
+++ screenshot-iframe.js
@@ -1,19 +1,4 @@
 import {storageHost} from './constants';
-import url from 'url';
-
-function parseQuery(queryString) {
-    const query = {};
-    const pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
-    for (let i = 0; i < pairs.length; i++) {
-      const pair = pairs[i].split('=');
-      const k = decodeURIComponent(pair[0]);
-      if (k) {
-        const v = decodeURIComponent(pair[1] || '');
-        query[k] = v;
-      }
-    }
-    return query;
-}
 
 export const getScreenshotBlob = async (reqUrl, hash, ext, type, width, height) => {
 
@@ -28,40 +13,9 @@ export const getScreenshotBlob = async (reqUrl, hash, ext, type, width, height) 
     }
 
     if(hash) {
-        var tempUrl = `https://app.webaverse.com/${hash}.${ext}/preview.${type}`
+        reqUrl = `${storageHost}/ipfs/${hash}`;
     }
-    else {
-        var tempUrl = `https://app.webaverse.com/screenshot.html?url=${reqUrl}&hash=${hash}&ext=${ext}&type=${type}&width=${width}&height=${height}`;
-    }
-
-    const u = url.parse(tempUrl, true);
-
-    const spec = (() => {
-        if(!u.search){
-          const match = u.pathname.match(/^\/([^\.]+)\.([^\/]+)\/([^\.]+)\.(.+)$/);
-          if (match) {
-            let hash = match[1];
-            let ext = match[2].toLowerCase();
-            let type = match[4].toLowerCase();
-            let url = `${storageHost}/ipfs/${hash}`;
-            return {
-              url,
-              hash,
-              ext,
-              type
-            }
-          }
-        }
-        else if(u.search){
-            return parseQuery(u.search);
-        }
-        spec = null;
-    })();
-
-    if (spec) {
-        const {url, hash, ext, type, height, width} = spec;
-        var ssUrl = `https://app.webaverse.com/screenshot.html?url=${url}&ext=${ext}&type=${type}&width=${width}&height=${height}`;
-    }
+    var ssUrl = `https://app.webaverse.com/screenshot.html?url=${reqUrl}&ext=${ext}&type=${type}&width=${width}&height=${height}`;
 
     window.open(ssUrl, 'screenshotIframe')
 
