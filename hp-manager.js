097224762c3f93e100cf6f86235a2c07c17a22a6
yaser <yaser@superblacksoftware.net>
2021-12-04 22:55:09 +0000

    fix damage material and jitter

--- hp-manager.js
+++ hp-manager.js
@@ -32,6 +32,7 @@ const update = (timestamp, timeDiff) => {
       const f = (timestamp - damageAnimation.startTime) / animationDuration;
       damagePhysicsMesh.material.uniforms.uTime.value = 1-f;
       damagePhysicsMesh.material.uniforms.uTime.needsUpdate = true;
+      damagePhysicsMesh.updateMatrixWorld();
     } else {
       damageAnimation = null;
     }
@@ -47,7 +48,7 @@ const triggerDamageAnimation = collisionId => {
   damagePhysicsMesh.matrix.copy(physicsMesh.matrixWorld);
   damagePhysicsMesh.matrixWorld.copy(physicsMesh.matrixWorld)
     .decompose(damagePhysicsMesh.position, damagePhysicsMesh.quaternion, damagePhysicsMesh.scale);
-      
+  damagePhysicsMesh.updateMatrixWorld();
   damageAnimation = {
     startTime: timestamp,
     endTime: timestamp + hitAnimationLength,
@@ -98,7 +99,7 @@ const makeHitTracker = ({
       
       const scale = (1-hitTime/hitAnimationLength) * 0.1;
       jitterObject.position.set((-1+Math.random()*2)*scale, (-1+Math.random()*2)*scale, (-1+Math.random()*2)*scale);
-
+      jitterObject.updateMatrixWorld();
       if (hitTime > hitAnimationLength) {
         hitTime = -1;
       }
