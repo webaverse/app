160da37ef0efef67cd0a57960444efe56ba7f050
Avaer Kazmer <a@modules.io>
2021-12-08 05:19:45 -0500

    Offset cleanup

--- character-physics.js
+++ character-physics.js
@@ -181,7 +181,7 @@ class CharacterPhysics {
         localVector.y += this.player.avatar.height;
         localQuaternion.premultiply(localQuaternion2.setFromAxisAngle(localVector3.set(0, 1, 0), Math.PI));
       }
-      localOffset2.set(0, 0.05, 0); // Feet offset: Or feet will be in ground, only cosmetical, works for all avatars
+      // localOffset2.set(0, 0.05, 0); // Feet offset: Or feet will be in ground, only cosmetical, works for all avatars
       localVector.add(localOffset2);
       localMatrix.compose(localVector, localQuaternion, localVector2);
 
