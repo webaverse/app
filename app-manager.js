568e3696b7cc17395ca6450f65a7359d8b2aa3a4
Avaer Kazmer <a@modules.io>
2021-12-08 05:14:20 -0500

    Do not special case vrm

--- app-manager.js
+++ app-manager.js
@@ -555,9 +555,9 @@ class AppManager extends EventTarget {
                 physicsObject.quaternion.copy(app.quaternion);
                 physicsObject.scale.copy(app.scale);
 
-                if(app.appType === "vrm") {
+                /* if (app.appType === "vrm") {
                   physicsObject.position.add(new THREE.Vector3(0, 1, 0));
-                }
+                } */
 
                 physicsObject.updateMatrixWorld();
                 
