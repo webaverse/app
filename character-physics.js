c6cb9ab875c86e28d0c7aa432566d954847c1cd3
Avaer Kazmer <a@modules.io>
2021-12-09 20:33:29 -0500

    Increase jump drop sensitivity

--- character-physics.js
+++ character-physics.js
@@ -160,7 +160,7 @@ class CharacterPhysics {
           } else if (!jumpAction) {
             //_ensureJumpAction();
           }
-        } else if (!jumpAction && localVector4.y < -4) {
+        } else if (!jumpAction && localVector4.y < -1) {
           _ensureJumpAction();
         }
       } else {
