1f9ab3c48de8b3a4cc71b06029edbe0dfce680bb
Avaer Kazmer <a@modules.io>
2021-12-09 20:34:03 -0500

    Decrease walk speed

--- game.js
+++ game.js
@@ -1671,7 +1671,7 @@ const gameManager = {
   getSpeed() {
     let speed = 0;
     
-    const walkSpeed = 0.1;
+    const walkSpeed = 0.075;
     const flySpeed = walkSpeed * 2;
     const defaultCrouchSpeed = walkSpeed * 0.7;
     const isCrouched = gameManager.isCrouched();
