89b09561cb4bc7493c22051da1b1566337ed704b
Avaer Kazmer <a@modules.io>
2021-12-14 19:37:09 -0500

    Add more reset animations

--- avatars/avatars.js
+++ avatars/avatars.js
@@ -2258,7 +2258,7 @@ class Avatar {
                 const src2 = useAnimation.interpolants[k];
                 const v2 = src2.evaluate(t2);
 
-                const idleAnimation = _getIdleAnimation('walk');
+                const idleAnimation = _getIdleAnimation('reset');
                 const t3 = 0;
                 const src3 = idleAnimation.interpolants[k];
                 const v3 = src3.evaluate(t3);
@@ -2315,7 +2315,7 @@ class Avatar {
               const src2 = aimAnimation.interpolants[k];
               const v2 = src2.evaluate(t2);
 
-              const idleAnimation = _getIdleAnimation('walk');
+              const idleAnimation = _getIdleAnimation('reset');
               const t3 = 0;
               const src3 = idleAnimation.interpolants[k];
               const v3 = src3.evaluate(t3);
