ea02977326c13c2b79140b34d5c968fc709bc1cf
Avaer Kazmer <a@modules.io>
2021-12-15 01:36:14 -0500

    Logging cleanup

--- avatars/microphone-worklet.js
+++ avatars/microphone-worklet.js
@@ -4,39 +4,71 @@ let tick = 0;
 let sampleSum = 0;
 let numSamples = 0;
 let muted = true;
+let emitVolume = false;
+let emitBuffer = false;
+
+// console.log('load worklet');
 
 class VolumeProcessor extends AudioWorkletProcessor {
   constructor() {
     super();
 
+    // console.log('cons volume processor');
+
     this.port.addEventListener('message', e => {
       const data = JSON.parse(e.data);
       const {method} = data;
-      if (method === 'muted') {
-        muted = data.muted;
+      if (method === 'options') {
+        const {args} = data;
+        // console.log('worklet got message', {method, args});
+        if (args.muted !== undefined) {
+          muted = args.muted;
+        }
+        if (args.emitVolume !== undefined) {
+          emitVolume = args.emitVolume;
+        }
+        if (args.emitBuffer !== undefined) {
+          emitBuffer = args.emitBuffer;
+        }
       }
     });
     this.port.start();
   }
   process(inputs, outputs) {
+    // console.log('got process', inputs, outputs, emitBuffer);
+    
     const channels = inputs[0];
     // const output = outputs[0];
 
-    // for (let i = 0; i < channels.length; i++) {
+    if (emitVolume) {
+      {
+        const i = 0;
+        const samples = channels[i];
+        for (let j = 0; j < samples.length; j++) {
+          sampleSum += Math.abs(samples[j]);
+        }
+        numSamples += samples.length;
+      }
+
+      if (++tick >= numTicks) {
+        const value = sampleSum / numSamples;
+        this.port.postMessage({
+          method: 'volume',
+          data: value,
+        });
+
+        tick = 0;
+        sampleSum = 0;
+        numSamples = 0;
+      }
+    }
+    if (emitBuffer) {
       const i = 0;
       const samples = channels[i];
-      for (let j = 0; j < samples.length; j++) {
-        sampleSum += Math.abs(samples[j]);
-      }
-      numSamples += samples.length;
-    // }
-
-    if (++tick >= numTicks) {
-      this.port.postMessage(sampleSum / numSamples);
-
-      tick = 0;
-      sampleSum = 0;
-      numSamples = 0;
+      this.port.postMessage({
+        method: 'buffer',
+        data: samples,
+      });
     }
 
     if (!muted) {
