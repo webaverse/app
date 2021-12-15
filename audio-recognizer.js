96ae2b904001b6614be53368bc3e3669d9cff144
Avaer Kazmer <a@modules.io>
2021-12-15 07:25:07 -0500

    Break out audio-recognizer.js

--- audio-recognizer.js
+++ audio-recognizer.js
@@ -1,148 +1,128 @@
-const workerUrl = './public/pocketsphinx.js/webapp/js/recognizer.js';
-const u = '/sounds/pissbaby.mp3';
+import {makePromise} from './util.js';
 
-console.log('initialize');
-
-let recognizer;
-function spawnWorker(workerurl, onReady) {
-  recognizer = new Worker(workerurl, {
-    // type: 'module',
-  });
-  recognizer.onmessage = function(event) {
-      recognizer.onmessage = null;
-      // onReady will be called when there is a message
-      // back
-      onReady(recognizer);
-  };
-  recognizer.postMessage({
-    'pocketsphinx.js': 'pocketsphinx.js',
-    'pocketsphinx.wasm': 'pocketsphinx.wasm',
-  });
+class CallbackManager {
+  constructor() {
+    this.currentId = 0;
+    this.callbackPool = {};
+  }
+  add(clb) {
+    var id = this.currentId;
+    this.callbackPool[id] = clb;
+    this.currentId++;
+    return id;
+  }
+  get(id) {
+    if (this.callbackPool.hasOwnProperty(id)) {
+      var clb = this.callbackPool[id];
+      delete this.callbackPool[id];
+      return clb;
+    }
+    return null;
+  }
 }
-spawnWorker(workerUrl, recognizer => {
-  console.log('got recognizer', recognizer);
+class AudioRecognizer extends EventTarget {
+  constructor({sampleRate}) {
+    super();
 
+    this.callbackManager = new CallbackManager();
+    this.loadPromise = makePromise();
 
-  class CallbackManager {
-    constructor() {
-      this.callbackIds = 0;
-      this.callbacks = {};
-    }
-    add(callback) {
-      const id = ++this.callbackIds;
-      this.callbacks[id] = callback;
-      return id;
-    }
-    get(callbackId) {
-      const result = this.callbacks[callbackId];
-      delete this.callbacks[callbackId];
-      return result;
-    }
-  }
-  const callbackManager = new CallbackManager();
-  function updateCount(newCount) {
-    console.log('update count', newCount);
-  }
-  function updateStatus(newStatus) {
-    console.log('update status', newStatus);
-  }
-  recognizer.onmessage = function(e) {
-    console.log('got message', e.data);
+    const workerURL = '/pocketsphinx.js/webapp/js/recognizer.js';
+    this.worker = new Worker(workerURL);
+    this.worker.onmessage = event => {
+      this.worker.onmessage = e => {
+        // This is the case when we have a callback id to be called
+        if (e.data.hasOwnProperty('id')) {
+          var clb = this.callbackManager.get(e.data['id']);
+          var data = {};
+          if ( e.data.hasOwnProperty('data')) data = e.data.data;
+          if(clb) clb(data);
+        }
+        /* // This is a case when the recognizer has a new hypothesis
+        if (e.data.hasOwnProperty('hyp')) {
+          var newHyp = e.data.hyp;
+          if (e.data.hasOwnProperty('final') &&  e.data.final) newHyp = "Final: " + newHyp;
+          updateHyp(newHyp);
+        } */
+        // This is a case when the recognizer has a new result
+        if (e.data.hasOwnProperty('result')) {
+          this.dispatchEvent(new MessageEvent('result', {
+            data: e.data.result,
+          }));
+        }
+        // This is the case when we have an error
+        if (e.data.hasOwnProperty('status') && (e.data.status == "error")) {
+          // updateStatus("Error in " + e.data.command + " with code " + e.data.code);
+          this.dispatchEvent(new MessageEvent('error', {
+            data: e.data,
+          }));
+        }
+      };
 
-    // This is the case when we have a callback id to be called
-    if (e.data.hasOwnProperty('id')) {
-      var clb = callbackManager.get(e.data['id']);
-      var data = {};
-      if ( e.data.hasOwnProperty('data')) data = e.data.data;
-      if(clb) clb(data);
-    }
-    // This is a case when the recognizer has a new count number
-    if (e.data.hasOwnProperty('hyp')) {
-      var newCount = e.data.hyp;
-      if (e.data.hasOwnProperty('final') &&  e.data.final) newCount = "Final: " + newCount;
-      updateCount(newCount);
-    }
-    // This is the case when we have an error
-    if (e.data.hasOwnProperty('status') && (e.data.status == "error")) {
-      updateStatus("Error in " + e.data.command + " with code " + e.data.code);
-    }
-};
-  function postRecognizerJob(message, callback) {
-    var msg = message || {};
-    msg.callbackId = callbackManager.add(callback);
-    recognizer.postMessage(msg);
-  }
-  function initRecognizer() {
-    postRecognizerJob({
-      command: 'lazyLoad',
-      data: {
-        // pocketsphinx/model/en-us/en-us-phone.lm.bin
-        // folders: [["/", "zh_broadcastnews_ptm256_8000"]],
-        folders: [],
-        files: [
-          ["/", "en-us-phone.lm.bin", "../model/en-us/en-us-phone.lm.bin"],
-          /* ["/zh_broadcastnews_ptm256_8000", "means", "../zh_broadcastnews_ptm256_8000/means"],
-          ["/zh_broadcastnews_ptm256_8000", "variances", "../zh_broadcastnews_ptm256_8000/variances"],
-          ["/zh_broadcastnews_ptm256_8000", "transition_matrices", "../zh_broadcastnews_ptm256_8000/transition_matrices"],
-          ["/zh_broadcastnews_ptm256_8000", "sendump", "../zh_broadcastnews_ptm256_8000/sendump"],
-          ["/zh_broadcastnews_ptm256_8000", "mdef", "../zh_broadcastnews_ptm256_8000/mdef"],
-          ["/zh_broadcastnews_ptm256_8000", "feat.params", "../zh_broadcastnews_ptm256_8000/feat.params"],
-          ["/zh_broadcastnews_ptm256_8000", "mixture_weights", "../zh_broadcastnews_ptm256_8000/mixture_weights"],
-          ["/zh_broadcastnews_ptm256_8000", "noisedict", "../zh_broadcastnews_ptm256_8000/noisedict"] */
-        ],
-      },
-    }, () => {
-      console.log('lazy loaded');
-
-      // You can pass parameters to the recognizer, such as : {command: 'initialize', data: [["-hmm", "my_model"], ["-fwdflat", "no"]]}
-      postRecognizerJob({
-        command: 'initialize',
-        data: [["-kws_threshold", "1e-25", '-allphone', 'en-us-phone.lm.bin']],
-
-        // pocketsphinx/model/en-us/en-us-phone.lm.bin
-
-        // data: [['-allophone']],
-      },
-        function() {
-          console.log('audio recognizer loaded');
-
-          window.addEventListener('keydown', async e => {
-            // console.log('got which', e.which);
-            if (e.which === 80) {
-              const context = new AudioContext();
-
-              const response = await fetch(u)
-              const arrayBuffer = await response.arrayBuffer();
-              const audioBuffer = await context.decodeAudioData(arrayBuffer);
-              console.log('got audio buffer', audioBuffer);
-
-              recognizer.postMessage({
-                command: 'start',
-                data: 1,
-                /* data: {
-                  lol: 'zol',
-                }, */
-              });
-              const data = new Uint16Array(4096);
-              recognizer.postMessage({
-                command: 'process',
-                data,
-              }, [data.buffer]);
-              recognizer.postMessage({
-                command: 'stop',
-              });
-              /* playButton.disabled = false;
-              yodelBuffer = audioBuffer; */
-
-              /* if (recorder) {
-                recorder.consumers = [recognizer];
-              }
-              feedWords(wordList); */
-            }
-          });
+      (async () => {
+        this.worker.postMessage({ command: 'configure', data: {
+          sampleRate,
+        } });
+        await this.postRecognizerJob({
+          command: 'lazyLoad',
+          data: {
+            // pocketsphinx/model/en-us/en-us-phone.lm.bin
+            // folders: [["/", "zh_broadcastnews_ptm256_8000"]],
+            folders: [],
+            files: [
+              ["/", "en-us-phone.lm.bin", "../../model/en-us/en-us-phone.lm.bin"],
+              /* ["/zh_broadcastnews_ptm256_8000", "means", "../zh_broadcastnews_ptm256_8000/means"],
+              ["/zh_broadcastnews_ptm256_8000", "variances", "../zh_broadcastnews_ptm256_8000/variances"],
+              ["/zh_broadcastnews_ptm256_8000", "transition_matrices", "../zh_broadcastnews_ptm256_8000/transition_matrices"],
+              ["/zh_broadcastnews_ptm256_8000", "sendump", "../zh_broadcastnews_ptm256_8000/sendump"],
+              ["/zh_broadcastnews_ptm256_8000", "mdef", "../zh_broadcastnews_ptm256_8000/mdef"],
+              ["/zh_broadcastnews_ptm256_8000", "feat.params", "../zh_broadcastnews_ptm256_8000/feat.params"],
+              ["/zh_broadcastnews_ptm256_8000", "mixture_weights", "../zh_broadcastnews_ptm256_8000/mixture_weights"],
+              ["/zh_broadcastnews_ptm256_8000", "noisedict", "../zh_broadcastnews_ptm256_8000/noisedict"] */
+              // ["/", "kws.txt", "../kws.txt"],
+              // ["/", "kws.dict", "../kws.dict"],
+            ],
+          },
         });
-      }
-    );
+        await this.postRecognizerJob(
+          {
+            command: 'initialize',
+            data: [/*["-kws", "kws.txt"], ["-dict","kws.dict"], */ ['-allphone', 'en-us-phone.lm.bin'], ['-logfn', '/dev/null']],
+          }
+        );
+
+        this.worker.postMessage({ command: 'start', data: '' });
+
+        this.loadPromise.accept();
+      })();
+    };
+
+    this.worker.postMessage({
+      'pocketsphinx.js': 'pocketsphinx.js',
+      'pocketsphinx.wasm': 'pocketsphinx.wasm',
+    });
   }
-  initRecognizer();
-});
+  waitForLoad() {
+    return this.loadPromise;
+  }
+  postRecognizerJob(message) {
+    return new Promise((accept, reject) => {
+      var msg = message || {};
+      msg.callbackId = this.callbackManager.add(accept);
+      this.worker.postMessage(msg);
+    });
+  }
+  send(result) {
+    this.worker.postMessage({
+      command: 'process',
+      data: result,
+    }, [result.buffer]);
+  }
+  destroy() {
+    this.worker.postMessage({ command: 'stop' });
+  }
+}
+
+export {
+  AudioRecognizer,
+};
