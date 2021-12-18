e0780ba5c395e13fe1112f366d31d5142be3606b
Avaer Kazmer <a@modules.io>
2021-12-18 17:33:44 -0500

    Remove old three-vrm source

--- packages/three-vrm/types/VRMUtils/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { extractThumbnailBlob } from './extractThumbnailBlob';
-import { removeUnnecessaryJoints } from './removeUnnecessaryJoints';
-export declare class VRMUtils {
-    private constructor();
-    static extractThumbnailBlob: typeof extractThumbnailBlob;
-    static removeUnnecessaryJoints: typeof removeUnnecessaryJoints;
-}
