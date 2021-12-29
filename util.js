a5caa342fbd7ab1e2a6e965fe342afa05a22eb90
Avaer Kazmer <a@modules.io>
2021-12-29 16:27:37 -0500

    Remove shadows editing from frustum culling

--- util.js
+++ util.js
@@ -543,8 +543,8 @@ export const unFrustumCull = o => {
   o.traverse(o => {
     if (o.isMesh) {
       o.frustumCulled = false;
-      o.castShadow = true;
-      o.receiveShadow = true;
+      // o.castShadow = true;
+      // o.receiveShadow = true;
     }
   });
 };
