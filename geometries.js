e4a844d8f0e7efd986a93462e4e09ad069368f27
Avaer Kazmer <a@modules.io>
2022-01-18 05:13:31 -0500

    Export DoubleSidedPlaneGeometry

--- geometries.js
+++ geometries.js
@@ -1,5 +1,7 @@
 import {CapsuleGeometry} from './CapsuleGeometry.js';
+import {DoubleSidedPlaneGeometry} from './DoubleSidedPlaneGeometry.js';
 
 export {
   CapsuleGeometry,
+  DoubleSidedPlaneGeometry,
 };
