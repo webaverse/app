63a77fddfed7407b533522ef1ef94d6ddc1bd140
Mujab Muneeb <mujabmuneeb@gmail.com>
2022-01-19 18:57:28 +0500

    Further Fixes on test cases

---
+++ test/drivers/slenium.js
@@ -0,0 +1,26 @@
+const {Builder, ITimeouts} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+
+let driver;
+
+const args = [
+  '--ignore-certificate-errors',
+  '--no-sandbox',
+  '--disable-setuid-sandbox',
+  '--disable-accelerated-2d-canvas',
+  '--disable-gpu',
+];
+
+module.exports = class SeleniumDriver {
+  constructor() {
+    if (driver) { return driver; }
+  }
+
+  async init(url) {
+    driver = await new Builder().forBrowser('chrome')
+      .setChromeOptions(new chrome.Options().addArguments(args).headless()).build();
+    await driver.get(process.env.URL);
+  }
+};
