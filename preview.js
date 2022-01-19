d247ac0ee293a2d1640d2ddc46ae90e7567b42d6
Avaer Kazmer <a@modules.io>
2022-01-19 05:55:01 -0500

    Rewrite preview.js

--- preview.js
+++ preview.js
@@ -3,19 +3,23 @@ import {storageHost, inappPreviewHost} from './constants';
 
 const queue = [];
 let running = false;
-let f;
+let resolve, reject;
+let rejectionTimeout = null;
 
 const next = () => {
   /** Unshift the queue to generate the next preview */
-  const {url, ext, type, width, height, resolve, reject} = queue.shift();
-  generatePreview(url, ext, type, width, height, resolve, reject);
+  const {url, type, width, height, resolve, reject} = queue.shift();
+  generatePreview(url, type, width, height, resolve, reject);
 };
 
-export const generatePreview = async (url, ext, type, width, height, resolve, reject) => {
+export const generatePreview = async (url, type, width, height, _resolve, _reject) => {
+  resolve = _resolve;
+  reject = _reject;
+
   const previewHost = inappPreviewHost;
   running = true;
   // check for existing iframe
-  var iframe = document.querySelector(`iframe[src^="${previewHost}/screenshot.html"]`);
+  let iframe = document.querySelector(`iframe[src^="${previewHost}/screenshot.html"]`);
 
   // else create new iframe
   if (!iframe) {
@@ -26,53 +30,51 @@ export const generatePreview = async (url, ext, type, width, height, resolve, re
   }
 
   // check either first param is url or hash
-  if (!isValidURL(url)) {
+  /* if (!isValidURL(url)) {
+    throw new Error('Invalid URL: ' + url);
     url = `${storageHost}/${url}/preview.${ext}`;
-  }
+  } */
 
   // create URL
-  var ssUrl = `${previewHost}/screenshot.html?url=${url}&ext=${ext}&type=${type}&width=${width}&height=${height}`;
+  const ssUrl = `${previewHost}/screenshot.html?url=${url}&type=${type}&width=${width}&height=${height}`;
 
   // set src attr for iframe
   iframe.src = ssUrl;
   console.log('Preview generation in progress for ', ssUrl);
+
   // event listener for postMessage from screenshot.js
-  const rejection = setTimeout(() => {
+  rejectionTimeout = setTimeout(() => {
     reject('Preview Server Timed Out');
     running = false;
-    /** discard old function */
-    window.removeEventListener('message', f, false);
     if (queue.length > 0) {
       next();
     }
   }, 30 * 1000);
-
-  f = event => {
-    if (event.data.method === 'result') {
-      window.removeEventListener('message', f, false);
-      let blob;
-      if (type === 'webm') {
-        blob = new Blob([event.data.result], {
-          type: `video/${type}`,
-        });
-      } else {
-        blob = new Blob([event.data.result], {
-          type: `image/${type}`,
-        });
-      }
-      clearTimeout(rejection);
-      resolve({
-        blob: blob,
-        url: URL.createObjectURL(blob),
-      });
-      running = false;
-      if (queue.length > 0) {
-        next();
-      }
-    }
-  };
-  window.addEventListener('message', f);
 };
+
+window.addEventListener('message', event => {
+  if (event.data.method === 'result') {
+    let blob;
+    if (type === 'webm') {
+      blob = new Blob([event.data.result], {
+        type: `video/${type}`,
+      });
+    } else {
+      blob = new Blob([event.data.result], {
+        type: `image/${type}`,
+      });
+    }
+    clearTimeout(rejection);
+    resolve({
+      blob: blob,
+      url: URL.createObjectURL(blob),
+    });
+    running = false;
+    if (queue.length > 0) {
+      next();
+    }
+  }
+});
 
 // URL validate function
 function isValidURL(string) {
@@ -86,9 +88,9 @@ export const preview = async (url, ext, type, width, height, priority=10) => {
       return reject('Undefined Extension');
     }
     if (!running) {
-      generatePreview(url, ext, type, width, height, resolve, reject);
+      generatePreview(url, type, width, height, resolve, reject);
     } else {
-      queue.push({url, ext, type, width, height, resolve, reject, priority});
+      queue.push({url, type, width, height, resolve, reject, priority});
       queue.sort((a, b) => a.priority - b.priority)
     }
   });
