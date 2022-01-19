abc315b388d6cc2365732a69310ccbc709255e47
Avaer Kazmer <a@modules.io>
2022-01-19 05:52:25 -0500

    Remove type argument passing in app-manager.js

--- app-manager.js
+++ app-manager.js
@@ -237,14 +237,14 @@ class AppManager extends EventTarget {
         if (!live) return _bailout(null);
         const app = metaversefile.createApp({
           name: contentId,
-          type: (() => {
+          /* type: (() => {
             const match = contentId.match(/\.([a-z0-9]+)$/i);
             if (match) {
               return match[1];
             } else {
               return '';
             }
-          })(),
+          })(), */
         });
         
         app.position.fromArray(position);
