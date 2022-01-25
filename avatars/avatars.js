d9c09eae2b4f7b3f0d034c2ce5e91da68290c3ef
Avaer Kazmer <a@modules.io>
2022-01-24 20:02:19 -0500

    Break out character sfx implementation to character-sfx.js

--- avatars/avatars.js
+++ avatars/avatars.js
@@ -11,7 +11,14 @@ import {angleDifference, getVelocityDampingFactor} from '../util.js';
 import easing from '../easing.js';
 import {zbdecode} from 'zjs/encoding.mjs';
 import Simplex from '../simplex-noise.js';
-import {crouchMaxTime, useMaxTime, aimMaxTime, avatarInterpolationFrameRate, avatarInterpolationTimeDelay, avatarInterpolationNumFrames} from '../constants.js';
+import {
+  crouchMaxTime,
+  useMaxTime,
+  aimMaxTime,
+  avatarInterpolationFrameRate,
+  avatarInterpolationTimeDelay,
+  avatarInterpolationNumFrames,
+} from '../constants.js';
 import {FixedTimeStep} from '../interpolants.js';
 import * as avatarCruncher from '../avatar-cruncher.js';
 import * as avatarSpriter from '../avatar-spriter.js';
@@ -29,9 +36,6 @@ import {
   // retargetAnimation,
   // animationBoneToModelBone,
 } from './util.mjs';
-import {
-  mod,
-} from '../util.js';
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
@@ -46,54 +50,6 @@ const localEuler = new THREE.Euler();
 const localEuler2 = new THREE.Euler();
 const localMatrix = new THREE.Matrix4();
 const localMatrix2 = new THREE.Matrix4();
-
-const walkSoundFileNames = `\
-walk1.wav
-walk2.wav
-walk3.wav
-walk4.wav
-walk5.wav
-walk6.wav
-walk7.wav
-walk8.wav
-walk9.wav
-walk10.wav
-walk11.wav
-walk12.wav`.split('\n');
-const runSoundFileNames = `\
-run1.wav
-run2.wav
-run3.wav
-run4.wav
-run5.wav
-run6.wav
-run7.wav
-run8.wav
-run9.wav
-run10.wav
-run11.wav
-run12.wav`.split('\n');
-const narutoRunSoundFileNames = `\
-narutoRun1.wav
-narutoRun2.wav
-narutoRun3.wav
-narutoRun4.wav
-narutoRun5.wav
-narutoRun6.wav
-narutoRun7.wav
-narutoRun8.wav
-narutoRun9.wav
-narutoRun10.wav
-narutoRun11.wav
-narutoRun12.wav`.split('\n');
-const jumpSoundFileNames = `\
-jump1.wav
-jump2.wav
-jump3.wav`.split('\n');
-const landSoundFileNames = `\
-land1.wav
-land2.wav
-land3.wav`.split('\n');
 
 // const y180Quaternion = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI);
 const maxIdleVelocity = 0.01;
@@ -136,6 +92,17 @@ const _makeSimplexes = numSimplexes => {
 };
 const simplexes = _makeSimplexes(5);
 
+const getClosest2AnimationAngles = (key, angle) => {
+  const animationAngleArray = animationsAngleArrays[key];
+  animationAngleArray.sort((a, b) => {
+    const aDistance = Math.abs(angleDifference(angle, a.angle));
+    const bDistance = Math.abs(angleDifference(angle, b.angle));
+    return aDistance - bDistance;
+  });
+  const closest2AnimationAngles = animationAngleArray.slice(0, 2);
+  return closest2AnimationAngles;
+};
+
 const upVector = new THREE.Vector3(0, 1, 0);
 const upRotation = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI*0.5);
 const downRotation = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI*0.5);
@@ -151,7 +118,6 @@ const defaultActivateAnimation = 'activate';
 const defaultNarutoRunAnimation = 'narutoRun';
 const defaultchargeJumpAnimation = 'chargeJump';
 const defaultStandChargeAnimation = 'standCharge';
-
 
 const infinityUpVector = new THREE.Vector3(0, Infinity, 0);
 // const crouchMagnitude = 0.2;
@@ -284,11 +250,6 @@ let narutoRunAnimations;
 let fallLoop;
 // let swordSideSlash;
 // let swordTopDownSlash;
-let walkSoundFiles;
-let runSoundFiles;
-let jumpSoundFiles;
-let landSoundFiles;
-let narutoRunSoundFiles;
 const loadPromise = (async () => {
   await Promise.resolve(); // wait for metaversefile to be defined
   
@@ -335,51 +296,6 @@ const loadPromise = (async () => {
       if (o) {
         animationsBaseModel = o;
       }
-    })(),
-    (async () => {
-      const audioTimeoutTime = 5 * 1000;
-      const _loadSoundFiles = (fileNames, soundType) => Promise.all(fileNames.map(async fileName => {
-        const audio = new Audio();
-        const p = new Promise((accept, reject) => {
-          const timeout = setTimeout(() => {
-            reject(new Error('audio load timed out'));
-          }, audioTimeoutTime);
-          const _cleanup = () => {
-            clearTimeout(timeout);
-          };
-          audio.oncanplaythrough = () => {
-            _cleanup();
-            accept();
-          };
-          audio.onerror = err => {
-            _cleanup();
-            reject(err);
-          };
-        });
-        // console.log('got src', `../sounds/${soundType}/${fileName}`);
-        audio.src = `/sounds/${soundType}/${fileName}`;
-        audio.load();
-        await p;
-        // document.body.appendChild(audio);
-        return audio;
-      }));
-      await Promise.all([
-        _loadSoundFiles(walkSoundFileNames, 'walk').then(as => {
-          walkSoundFiles = as;
-        }),
-        _loadSoundFiles(narutoRunSoundFileNames, 'narutoRun').then(as => {
-          narutoRunSoundFiles = as;
-        }),
-        _loadSoundFiles(runSoundFileNames, 'run').then(as => {
-          runSoundFiles = as;
-        }),
-        _loadSoundFiles(jumpSoundFileNames, 'jump').then(as => {
-          jumpSoundFiles = as;
-        }),
-        _loadSoundFiles(landSoundFileNames, 'land').then(as => {
-          landSoundFiles = as;
-        }),
-      ]);
     })(),
   ]);
 
@@ -1850,6 +1766,7 @@ class Avatar {
     const narutoRunTimeFactor = 2;
     const idleWalkFactor = Math.min(Math.max((currentSpeed - idleSpeed) / (walkSpeed - idleSpeed), 0), 1);
     const walkRunFactor = Math.min(Math.max((currentSpeed - walkSpeed) / (runSpeed - walkSpeed), 0), 1);
+    const crouchFactor = Math.min(Math.max(1 - (this.crouchTime / crouchMaxTime), 0), 1);
     // console.log('current speed', currentSpeed, idleWalkFactor, walkRunFactor);
 
     const _updatePosition = () => {
@@ -1891,16 +1808,6 @@ class Avatar {
             return 'walk';
           }
         }
-      };
-      const _getClosest2AnimationAngles = key => {
-        const animationAngleArray = animationsAngleArrays[key];
-        animationAngleArray.sort((a, b) => {
-          const aDistance = Math.abs(angleDifference(angle, a.angle));
-          const bDistance = Math.abs(angleDifference(angle, b.angle));
-          return aDistance - bDistance;
-        });
-        const closest2AnimationAngles = animationAngleArray.slice(0, 2);
-        return closest2AnimationAngles;
       };
       const _getMirrorAnimationAngles = (animationAngles, key) => {
         const animations = animationAngles.map(({animation}) => animation);
@@ -2089,11 +1996,11 @@ class Avatar {
       };
       
       // stand
-      const key = _getAnimationKey(false);
-      const keyWalkAnimationAngles = _getClosest2AnimationAngles('walk');
+      // const key = _getAnimationKey(false);
+      const keyWalkAnimationAngles = getClosest2AnimationAngles('walk', angle);
       const keyWalkAnimationAnglesMirror = _getMirrorAnimationAngles(keyWalkAnimationAngles, 'walk');
 
-      const keyRunAnimationAngles = _getClosest2AnimationAngles('run');
+      const keyRunAnimationAngles = getClosest2AnimationAngles('run', angle);
       const keyRunAnimationAnglesMirror = _getMirrorAnimationAngles(keyRunAnimationAngles, 'run');
       
       const idleAnimation = _getIdleAnimation('walk');
@@ -2141,14 +2048,13 @@ class Avatar {
       
       // crouch
       // const keyOther = _getAnimationKey(true);
-      const keyAnimationAnglesOther = _getClosest2AnimationAngles('crouch');
+      const keyAnimationAnglesOther = getClosest2AnimationAngles('crouch', angle);
       const keyAnimationAnglesOtherMirror = _getMirrorAnimationAngles(keyAnimationAnglesOther, 'crouch');
       const idleAnimationOther = _getIdleAnimation('crouch');
       
       const angleToClosestAnimation = Math.abs(angleDifference(angle, keyWalkAnimationAnglesMirror[0].angle));
       const angleBetweenAnimations = Math.abs(angleDifference(keyWalkAnimationAnglesMirror[0].angle, keyWalkAnimationAnglesMirror[1].angle));
       const angleFactor = (angleBetweenAnimations - angleToClosestAnimation) / angleBetweenAnimations;
-      const crouchFactor = Math.min(Math.max(1 - (this.crouchTime / crouchMaxTime), 0), 1);
       const isBackward = _getAngleToBackwardAnimation(keyWalkAnimationAnglesMirror) < Math.PI*0.4;
       if (isBackward !== this.lastIsBackward) {
         this.backwardAnimationSpec = {
@@ -2176,135 +2082,6 @@ class Avatar {
         mirrorFactor = isBackward ? 1 : 0;
       }
       this.lastBackwardFactor = mirrorFactor;
-
-      const _updateSound = () => {
-        // jump
-        {
-          if (this.jumpState && !this.lastJumpState) {
-            const candidateAudios = jumpSoundFiles;
-            const audio = candidateAudios[Math.floor(Math.random() * candidateAudios.length)];
-            audio.currentTime = 0;
-            audio.paused && audio.play();
-          } else if (this.lastJumpState && !this.jumpState) {
-            const candidateAudios = landSoundFiles;
-            const audio = candidateAudios[Math.floor(Math.random() * candidateAudios.length)];
-            audio.currentTime = 0;
-            audio.paused && audio.play();
-          }
-          this.lastJumpState = this.jumpState;
-        }
-
-        // step
-        if (idleWalkFactor > 0.7 && !this.jumpState && !this.flyState) {
-          const isRunning = walkRunFactor > 0.5;
-          const isCrouching = crouchFactor > 0.5;
-          const isNarutoRun = this.narutoRunState;
-          const walkRunAnimationName = (() => {
-            if (isNarutoRun) {
-              return 'naruto run.fbx';
-            } else {
-              const animationAngles = isCrouching ?
-                keyAnimationAnglesOther
-              :
-                (isRunning ? keyRunAnimationAngles : keyWalkAnimationAngles);
-              return animationAngles[0].name;
-            }
-          })();
-          const soundFiles = (() => {
-            if (isNarutoRun) {
-              return narutoRunSoundFiles;
-            } else if (isCrouching) {
-              return walkSoundFiles;
-            } else if (isRunning) {
-              return runSoundFiles;
-            } else {
-              return walkSoundFiles;
-            }
-          })();
-          const animation = animations.find(a => a.name === walkRunAnimationName);
-          const animationIndices = animationStepIndices.find(i => i.name === walkRunAnimationName);
-          const {leftStepIndices, rightStepIndices} = animationIndices;
-
-          // HACK: this is used to dynamically control the step offset for a particular animation
-          // it is useful during development to adjust sync between animations and sound
-          // the key listener part of this is in io-manager.js
-          /* if (typeof window.lol !== 'number') {
-            window.lol = 0.06;
-            window.lol2 = 0.18;
-          } */
-
-          const sneakingOffset = -0.14;
-          const walkingOffset = 0.13;
-          const walkingBackwardOffset = 0.18;
-          const runningBackwardOffset = 0.06;
-          const strafeWalkingOffset = 0.24;
-          const offsets = {
-            'Sneaking Forward.fbx': sneakingOffset, 
-            'walking.fbx': walkingOffset,
-            'walking backwards.fbx': walkingBackwardOffset,
-            'Fast Run.fbx': 0,
-            'left strafe walking.fbx': strafeWalkingOffset,
-            'right strafe walking.fbx': strafeWalkingOffset,
-            'running backwards.fbx': runningBackwardOffset,
-          };
-          const offset = offsets[walkRunAnimationName] ?? window.lol;
-          const _getStepIndex = timeSeconds => {
-            const f = walkRunAnimationName === 'naruto run.fbx' ? narutoRunTimeFactor : 1;
-            const t1 = (timeSeconds * f + offset) % animation.duration;
-            const walkFactor1 = t1 / animation.duration;
-            const stepIndex = Math.floor(mod(walkFactor1, 1) * leftStepIndices.length);
-            return stepIndex;
-          };
-          // const t1 = (timeSeconds + offset) % animationAngles[0].animation.duration;
-          // const walkFactor1 = t1 / animationAngles[0].animation.duration;
-          // const walkFactor2 = t2 / animationAngles[1].animation.duration;
-          // console.log('animation angles', {walkRunAnimationName, animationIndices, isCrouching, keyWalkAnimationAngles, keyAnimationAnglesOther});
-          // console.log('got animation name', walkRunAnimationName);
-
-          const startIndex = _getStepIndex(this.lastWalkTime);
-          const endIndex = _getStepIndex(timeSeconds);
-          for (let i = startIndex;; i++) {
-            i = i % leftStepIndices.length;
-            // console.log('check', i, startIndex, endIndex);
-            if (i !== endIndex) {
-              if (leftStepIndices[i] && !this.lastStepped[0]) {
-                const candidateAudios = soundFiles//.filter(a => a.paused);
-                if (candidateAudios.length > 0) {
-                  /* for (const a of candidateAudios) {
-                    !a.paused && a.pause();
-                  } */
-                  
-                  const audio = candidateAudios[Math.floor(Math.random() * candidateAudios.length)];
-                  audio.currentTime = 0;
-                  audio.paused && audio.play();
-                  // console.log('left');
-                }
-              }
-              this.lastStepped[0] = leftStepIndices[i];
-
-              if (rightStepIndices[i] && !this.lastStepped[1]) {
-                const candidateAudios = soundFiles// .filter(a => a.paused);
-                if (candidateAudios.length > 0) {
-                  /* for (const a of candidateAudios) {
-                    !a.paused && a.pause();
-                  } */
-
-                  const audio = candidateAudios[Math.floor(Math.random() * candidateAudios.length)];
-                  audio.currentTime = 0;
-                  audio.paused && audio.play();
-                  // console.log('right');
-                }
-              }
-              this.lastStepped[1] = rightStepIndices[i];
-            } else {
-              break;
-            }
-          }
-
-          this.lastWalkTime = timeSeconds;
-        }
-      };
-      _updateSound();
 
       const _getHorizontalBlend = (k, lerpFn, isPosition, target) => {
         _get7wayBlend(
@@ -3174,6 +2951,7 @@ class Avatar {
 }
 Avatar.waitForLoad = () => loadPromise;
 Avatar.getAnimations = () => animations;
+Avatar.getAnimationStepIndices = () => animationStepIndices;
 Avatar.getAnimationMappingConfig = () => animationMappingConfig;
 let avatarAudioContext = null;
 const getAudioContext = () => {
@@ -3186,4 +2964,5 @@ Avatar.getAudioContext = getAudioContext;
 Avatar.setAudioContext = newAvatarAudioContext => {
   avatarAudioContext = newAvatarAudioContext;
 };
+Avatar.getClosest2AnimationAngles = getClosest2AnimationAngles;
 export default Avatar;
