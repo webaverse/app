44d67acf689b08e8bc391d056c6f3217ac2e7f1f
Avaer Kazmer <a@modules.io>
2022-01-25 21:21:56 -0500

    Use voicer.js

--- character-hups.js
+++ character-hups.js
@@ -2,6 +2,7 @@
 it controls the animated dioramas that happen when players perform actions. */
 
 import * as THREE from 'three';
+import {Voicer} from './voicer.js';
 import Avatar from './avatars/avatars.js';
 import {loadAudio} from './util.js';
 
@@ -10,55 +11,6 @@ const syllableSoundFileNames = (() => {
   const files = Array(numFiles).fill(0).map((_, i) => `${i + 1}.wav`);
   return files;
 })();
-
-function weightedRandom(weights) {
-	var totalWeight = 0,
-		i, random;
-
-	for (i = 0; i < weights.length; i++) {
-		totalWeight += weights[i];
-	}
-
-	random = Math.random() * totalWeight;
-
-	for (i = 0; i < weights.length; i++) {
-		if (random < weights[i]) {
-			return i;
-		}
-
-		random -= weights[i];
-	}
-
-	return -1;
-}
-
-class Voicer {
-  constructor(audios) {
-    this.voices = audios.map(audio => {
-      return {
-        audio,
-        nonce: 0,
-      };
-    });
-    this.nonce = 0;
-  }
-  selectVoice() {
-    // the weight of each voice is proportional to the inverse of the number of times it has been used
-    const maxNonce = this.voices.reduce((max, voice) => Math.max(max, voice.nonce), 0);
-    const weights = this.voices.map(({nonce}) => {
-      return 1 - (nonce / (maxNonce + 1));
-    });
-    const selectionIndex = weightedRandom(weights);
-    const voiceSpec = this.voices[selectionIndex];
-    voiceSpec.nonce++;
-    while (this.voices.every(voice => voice.nonce > 0)) {
-      for (const voiceSpec of this.voices) {
-        voiceSpec.nonce--;
-      }
-    }
-    return voiceSpec.audio;
-  }
-}
 
 let syllableSoundFiles;
 const loadPromise = (async () => {
@@ -82,47 +34,11 @@ window.playVoice = async () => {
   await loadPromise;
 
   const voicer = new Voicer(syllableSoundFiles);
-  
+  voicer.start();
   const audioTime = 2000 + 5000 * Math.random();
-  const now = performance.now();
-  const endTime = now + audioTime;
-
-  // play a random audio file, wait for it to finish, then recurse to play another
-  const _recurse = async () => {
-    const now = performance.now();
-    if (now < endTime) {
-      const audio = voicer.selectVoice();
-      // const audio = syllableSoundFiles[Math.floor(Math.random() * syllableSoundFiles.length)];
-      if (audio.silencingInterval) {
-        clearInterval(audio.silencingInterval);
-        audio.silencingInterval = null;
-      }
-      audio.currentTime = 0;
-      audio.volume = 1;
-      if (audio.paused) {
-        await audio.play();
-      }
-      let audioTimeout = audio.duration * 1000;
-      audioTimeout *= 0.8 + 0.4 * Math.random();
-      setTimeout(async () => {
-        // await audio.pause();
-
-        audio.silencingInterval = setInterval(() => {
-          audio.volume = Math.max(audio.volume - 0.1, 0);
-          if (audio.volume === 0) {
-            clearInterval(audio.silencingInterval);
-            audio.silencingInterval = null;
-          }
-        }, 10);
-
-        _recurse();
-      }, audioTimeout);
-      /* audio.addEventListener('ended', async () => {
-        
-      }, {once: true}); */
-    }
-  };
-  _recurse();
+  setTimeout(() => {
+    voicer.stop();
+  }, audioTime);
 };
 
 let nextHupId = 0;
