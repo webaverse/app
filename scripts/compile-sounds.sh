b297a0a74c98c5ef7ca42cfd4f2b2530cb4a6b3d
Avaer Kazmer <a@modules.io>
2022-01-28 00:26:23 -0500

    Update compile-sounds.sh

--- scripts/compile-sounds.sh
+++ scripts/compile-sounds.sh
@@ -2,11 +2,12 @@
 # input: directory of wav files
 # output: mp3 data file and json metadata file
 
-ls *.wav | sort -n | awk '{print "file " $0}' >sound.txt
-ffmpeg -f concat -i sound.txt -b:a 320k -y sounds.mp3
+ls {walk,run,jump,land,narutoRun}/*.wav | sort -n >sounds.txt
+cat sounds.txt | awk '{print "file " $0}' >sounds-list.txt
+ffmpeg -f concat -i sounds-list.txt -b:a 320k -y sounds.mp3
 
 rm -f sound-files.txt
-for f in `ls *.wav | sort -n`; do
+cat sounds.txt | while read f; do
   a=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$f")
   echo "$a $f" | tee -a sound-files.txt
 done;
