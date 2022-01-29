c911617bda100a4aa334204e6fa4b31de958eab3
gonnavis <gonnavis@163.com>
2022-01-29 13:48:38 +0800

    add: physicsManager.collide() ( collidePhysics() ).

--- physics-manager.js
+++ physics-manager.js
@@ -210,6 +210,9 @@ physicsManager.setAngularVelocity = (physicsObject, velocity, autoWake) => {
 physicsManager.setTransform = (physicsObject, autoWake) => {
   physx.physxWorker.setTransformPhysics(physx.physics, physicsObject.physicsId, physicsObject.position, physicsObject.quaternion, physicsObject.scale, autoWake);
 };
+physicsManager.collide = (radius, halfHeight, p, q, maxIter) => {
+  return physx.physxWorker.collidePhysics(physx.physics, radius, halfHeight, p, q, maxIter);
+};
 physicsManager.createCharacterController = (radius, height, contactOffset, stepOffset, position, mat) => {
   const characterController = physx.physxWorker.createCharacterControllerPhysics(physx.physics, radius, height, contactOffset, stepOffset, position, mat);
   return characterController;
