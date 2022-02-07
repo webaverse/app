8680e2ffe85e4d2a066e13386f11e83d39f1e5ac
gonnavis <gonnavis@163.com>
2022-02-07 21:15:08 +0800

    no https.

--- index.mjs
+++ index.mjs
@@ -108,7 +108,8 @@ const _proxyUrl = (req, res, u) => {
     }
   });
 
-  const isHttps = !process.env.HTTP_ONLY && (!!certs.key && !!certs.cert);
+  // const isHttps = !process.env.HTTP_ONLY && (!!certs.key && !!certs.cert);
+  const isHttps = false;
   const port = parseInt(process.env.PORT, 10) || (isProduction ? 443 : 3000);
   const wsPort = port + 1;
 
