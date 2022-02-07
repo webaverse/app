04de0d3a48d4bb2a6e05df9628463626aab2a71c
Avaer Kazmer <a@modules.io>
2022-02-07 02:28:56 -0500

    Clean up old lore code

--- voice-endpoint-voicer.js
+++ voice-endpoint-voicer.js
@@ -22,61 +22,6 @@ class VoiceEndpointVoicer {
 
     this.player.avatar.setAudioEnabled(true);
 
-    /* _say = text => {
-      const voice = `1jLX0Py6j8uY93Fjf2l0HOZQYXiShfWUO`;
-      const a = new Audio();
-      a.src = `http://voice.webaverse.com:8080/tts?voice=${encodeURIComponent(voice)}&s=${encodeURIComponent(text)}`;
-      a.oncanplaythrough = () => {
-        a.play();
-      };
-      a.onerror = err => {
-        console.warn(err);
-      };
-      // document.body.appendChild(a);
-    };
-    genLore = (prompt, maxTokens = 25) => {
-      if (!prompt) {
-        throw new Error('prompt is required');
-      }
-    
-      prompt = `# Scene 1\n\nThe characters are having a conversation.\n\nCharacter 1: ${prompt}\nCharacter 2:`;
-      const end = `\n`;
-      const es = new EventSource(`https://ai.webaverse.com/lore?p=${encodeURIComponent(prompt)}&e=${encodeURIComponent(end)}&l=${maxTokens}`);
-      let fullS = '';
-      const _finish = s => {
-        // clean up non-latin characters
-        s = s.replace(/[^a-zA-Z0-9()*_\-!#$%^&*,."\'\][\s]+/g, '');
-        if (/[a-zA-Z0-9]/.test(s)) {
-          _say(s);
-        } else {
-          console.warn('retrying due to empty response');
-          prompt += ` ...\nCharacter 2:`;
-          genLore(prompt, maxTokens);
-        }
-        es.close();
-      };
-      es.addEventListener('message', e => {
-        const s = e.data;
-        console.log('got s', s);
-        if (s !== '[DONE]') {
-          const j = JSON.parse(s);
-          console.log(j.choices);
-          const {choices} = j;
-          const {text} = choices[0];
-          fullS += text;
-    
-          const endIndex = fullS.indexOf(end);
-          if (endIndex !== -1) {
-            _finish(fullS.substring(0, endIndex));
-          }
-        } else {
-          _finish(fullS);
-        }
-    
-        console.log(JSON.stringify(prompt + fullS));
-      });
-    }; */
-
     (async () => {
       const u = new URL(this.voiceEndpoint.url.toString());
       u.searchParams.set('s', text);
