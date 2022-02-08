391543a1b234a3341e348f24ca505bdb73302bc7
Avaer Kazmer <a@modules.io>
2022-02-08 01:48:09 -0500

    Clean up logging in lore-ai.js

--- lore-ai.js
+++ lore-ai.js
@@ -13,6 +13,7 @@ class LoreAI {
         if (typeof end !== 'undefined') {
           url.searchParams.set('e', end);
         }
+        // console.log('got url', url);
       
         // prompt = `# Scene 1\n\nThe characters are having a conversation.\n\nCharacter 1: ${prompt}\nCharacter 2:`;
         const es = new EventSource(url);
@@ -28,16 +29,23 @@ class LoreAI {
             fullS += text;
       
             const endIndex = fullS.indexOf(end);
+            // console.log('got end index', {fullS, end, endIndex});
             if (endIndex !== -1) {
               es.close();
               resolve(fullS.substring(0, endIndex));
             }
           } else {
+            // console.log('close');
             es.close();
             resolve(fullS);
           }
       
-          console.log(JSON.stringify(prompt + fullS));
+          // console.log(JSON.stringify(prompt + fullS));
+        });
+        es.addEventListener('error', err => {
+          console.log('lore event source error', err);
+          es.close();
+          reject(err);
         });
       } else {
         reject(new Error('prompt is required'));
