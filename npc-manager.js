1c59271f9dbda8c3f4a5b03fc9ca418bb78721cf
Avaer Kazmer <a@modules.io>
2022-02-08 09:57:50 -0500

    Add npc-manager.js

---
+++ npc-manager.js
@@ -0,0 +1,49 @@
+import {NpcPlayer} from './character-controller.js';
+
+class NpcManager extends EventTarget {
+  constructor() {
+    super();
+
+    this.npcs = [];
+  }
+  async createNpc({
+    name,
+    avatarApp,
+    position,
+    quaternion,
+    scale,
+  }) {
+    const npcPlayer = new NpcPlayer();
+    npcPlayer.name = name;
+
+    let matrixNeedsUpdate = false;
+    if (position) {
+      npcPlayer.position.copy(position);
+      matrixNeedsUpdate = true;
+    }
+    if (quaternion) {
+      npcPlayer.quaternion.copy(quaternion);
+      matrixNeedsUpdate = true;
+    }
+    if (scale) {
+      npcPlayer.scale.copy(scale);
+      matrixNeedsUpdate = true;
+    }
+    if (matrixNeedsUpdate) {
+      npcPlayer.updateMatrixWorld();
+    }
+
+    await npcPlayer.setAvatarAppAsync(avatarApp);
+    this.npcs.push(npcPlayer);
+    
+    return npcPlayer;
+  }
+  destroyNpc(npcPlayer) {
+    npcPlayer.destroy();
+
+    const removeIndex = this.npcs.indexOf(npcPlayer);
+    this.npcs.splice(removeIndex, 1);
+  }
+}
+const npcManager = new NpcManager();
+export default npcManager;
