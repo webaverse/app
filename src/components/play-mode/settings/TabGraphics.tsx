e7de7215a2a88d59bacf114ae91890dd6649d837
ohmed <bohdan.horpynchuk@gmail.com>
2022-02-10 13:59:10 +0100

    Settings: Added saving/loading graphics tab settings values to/from LocalStorage.

--- src/components/play-mode/settings/TabGraphics.tsx
+++ src/components/play-mode/settings/TabGraphics.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 
 import { Switch } from './switch';
@@ -8,29 +8,131 @@ import styles from './settings.module.css';
 
 //
 
+const DefaultSettings = {
+    resolution:         1,
+    antialias:          'NONE',
+    viewRange:          'HIGH',
+    shadowQuality:      'HIGH',
+    postprocessing: {
+        enabled:        'ON',
+        depthOfField:   'ON',
+        hdr:            'ON',
+        bloom:          'ON'
+    },
+    character: {
+        details:        'HIGH',
+        hairPhysics:    'ON'
+    }
+};
+
 export const TabGraphics = ({ active }) => {
+
+    const [ settingsLoaded, setSettingsLoaded ] = useState( false );
+    const [ resolution, setResolution ] = useState( 'HIGH' );
+    const [ antialias, setAntialias ] = useState( 'MSAA' );
+    const [ viewRange, setViewRange ] = useState( 'HIGH' );
+    const [ shadowQuality, setShadowQuality ] = useState( 'HIGH' );
+    const [ postprocessing, setPostprocessing ] = useState( 'ON' );
+    const [ depthOfField, setDepthOfField ] = useState( 'ON' );
+    const [ hdr, setHdr ] = useState( 'ON' );
+    const [ bloom, setBloom ] = useState( 'ON' );
+    const [ characterDetails, setCharacterDetails ] = useState( 'HIGH' );
+    const [ hairPhysics, setHairPhysics ] = useState( 'ON' );
+
+    function saveSettings () {
+
+        const settings = {
+            resolution:         resolution,
+            antialias:          antialias,
+            viewRange:          viewRange,
+            shadowQuality:      shadowQuality,
+            postprocessing: {
+                enabled:        postprocessing,
+                depthOfField:   depthOfField,
+                hdr:            hdr,
+                bloom:          bloom
+            },
+            character: {
+                details:        characterDetails,
+                hairPhysics:    hairPhysics
+            }
+        };
+
+        localStorage.setItem( 'GfxSettings', JSON.stringify( settings ) );
+
+    };
+
+    function loadSettings () {
+
+        const settingsString = localStorage.getItem( 'GfxSettings' );
+        let settings;
+
+        try {
+
+            settings = JSON.parse( settingsString );
+
+        } catch ( err ) {
+
+            settings = DefaultSettings;
+
+        }
+
+        settings = settings ?? DefaultSettings;
+
+        setResolution( settings.resolution ?? DefaultSettings.resolution );
+        setAntialias( settings.antialias ?? DefaultSettings.antialias );
+        setViewRange( settings.viewRange ?? DefaultSettings.viewRange );
+        setShadowQuality( settings.shadowQuality ?? DefaultSettings.shadowQuality );
+        setPostprocessing( settings.postprocessing.enabled ?? DefaultSettings.postprocessing.enabled );
+        setDepthOfField( settings.postprocessing.depthOfField ?? DefaultSettings.postprocessing.depthOfField );
+        setHdr( settings.postprocessing.hdr ?? DefaultSettings.postprocessing.hdr );
+        setBloom( settings.postprocessing.bloom ?? DefaultSettings.postprocessing.bloom );
+        setCharacterDetails( settings.character.details ?? DefaultSettings.character.details );
+        setHairPhysics( settings.character.hairPhysics ?? DefaultSettings.character.hairPhysics );
+
+        setSettingsLoaded( true );
+
+    };
+
+    //
+
+    useEffect( () => {
+
+        if ( ! settingsLoaded ) return;
+        saveSettings();
+        console.log( resolution );
+
+    }, [ settingsLoaded, resolution, antialias, viewRange, shadowQuality, postprocessing, depthOfField, hdr, bloom, characterDetails, hairPhysics ] );
+
+    useEffect( () => {
+
+        loadSettings();
+
+    }, [] );
+
+    //
 
     return (
         <div className={ classNames( styles.tabContent, active ? styles.active : null ) }>
             <div className={ styles.blockTitle }>Display</div>
             <div className={ styles.row }>
                 <div className={ styles.paramName }>Resolution</div>
-                <Switch className={ styles.switch } value="HIGH" values={ [ 'HIGH', 'MEDIUM', 'LOW' ] } />
+                <Switch className={ styles.switch } value={ resolution } setValue={ setResolution } values={ [ 'HIGH', 'MEDIUM', 'LOW' ] } />
                 <div className={ styles.clearfix } />
             </div>
             <div className={ styles.row }>
                 <div className={ styles.paramName }>Antialias</div>
-                <Switch className={ styles.switch } value="MSAA" values={ [ 'MSAA', 'FXAA', 'NONE' ] } />
+                <Switch className={ styles.switch } value={ antialias } setValue={ setAntialias } values={ [ 'MSAA', 'FXAA', 'NONE' ] } />
                 <div className={ styles.clearfix } />
             </div>
             <div className={ styles.row }>
                 <div className={ styles.paramName }>View range</div>
-                <Switch className={ styles.switch } value="HIGH" values={ [ 'HIGH', 'MEDIUM', 'LOW' ] } />
+                <Switch className={ styles.switch } value={ viewRange } setValue={ setViewRange } values={ [ 'HIGH', 'MEDIUM', 'LOW' ] } />
                 <div className={ styles.clearfix } />
             </div>
             <div className={ styles.row }>
                 <div className={ styles.paramName }>Shadows quality</div>
-                <Switch className={ styles.switch } value="HIGH" values={ [ 'HIGH', 'MEDIUM', 'LOW', 'OFF' ] } />
+                <Switch className={ styles.switch } value={ shadowQuality } setValue={ setShadowQuality } values={ [ 'HIGH', 'MEDIUM', 'LOW', 'OFF' ] } />
                 <div className={ styles.clearfix } />
             </div>
             <div className={ styles.row }>
@@ -39,22 +141,22 @@ export const TabGraphics = ({ active }) => {
             </div>
             <div className={ styles.row }>
                 <div className={ styles.paramName }>Enabled</div>
-                <Switch className={ styles.switch } value="ON" values={ [ 'ON', 'OFF' ] } />
+                <Switch className={ styles.switch } value={ postprocessing } setValue={ setPostprocessing } values={ [ 'ON', 'OFF' ] } />
                 <div className={ styles.clearfix } />
             </div>
             <div className={ styles.row }>
                 <div className={ styles.paramName }>Depth of field</div>
-                <Switch className={ styles.switch } value="ON" values={ [ 'ON', 'OFF' ] } />
+                <Switch className={ styles.switch } value={ depthOfField } setValue={ setDepthOfField } values={ [ 'ON', 'OFF' ] } />
                 <div className={ styles.clearfix } />
             </div>
             <div className={ styles.row }>
                 <div className={ styles.paramName }>HDR</div>
-                <Switch className={ styles.switch } value="ON" values={ [ 'ON', 'OFF' ] } />
+                <Switch className={ styles.switch } value={ hdr } setValue={ setHdr } values={ [ 'ON', 'OFF' ] } />
                 <div className={ styles.clearfix } />
             </div>
             <div className={ styles.row }>
                 <div className={ styles.paramName }>Bloom</div>
-                <Switch className={ styles.switch } value="ON" values={ [ 'ON', 'OFF' ] } />
+                <Switch className={ styles.switch } value={ bloom } setValue={ setBloom } values={ [ 'ON', 'OFF' ] } />
                 <div className={ styles.clearfix } />
             </div>
             <div className={ styles.row }>
@@ -63,12 +165,12 @@ export const TabGraphics = ({ active }) => {
             </div>
             <div className={ styles.row }>
                 <div className={ styles.paramName }>Character details</div>
-                <Switch className={ styles.switch } value="HIGH" values={ [ 'HIGH', 'MEDIUM', 'LOW' ] } />
+                <Switch className={ styles.switch } value={ characterDetails } setValue={ setCharacterDetails } values={ [ 'HIGH', 'MEDIUM', 'LOW' ] } />
                 <div className={ styles.clearfix } />
             </div>
             <div className={ styles.row }>
                 <div className={ styles.paramName }>Hair physics</div>
-                <Switch className={ styles.switch } value="ON" values={ [ 'ON', 'OFF' ] } />
+                <Switch className={ styles.switch } value={ hairPhysics } setValue={ setHairPhysics } values={ [ 'ON', 'OFF' ] } />
                 <div className={ styles.clearfix } />
             </div>
         </div>
--- src/components/play-mode/settings/switch/Switch.tsx
+++ src/components/play-mode/settings/switch/Switch.tsx
@@ -1,33 +1,33 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 
 import styles from './switch.module.css';
 
 //
 
-export const Switch = ({ value, values, className }) => {
-
-    const [ valueId, setValueId ] = useState( values.indexOf( value ) );
+export const Switch = ({ value, setValue, values, className }) => {
 
     const handleLeftArrowClick = () => {
 
-        const newValueId = ( valueId === 0 ? values.length - 1 : valueId - 1 );
-        setValueId( newValueId );
+        const oldValueId = values.indexOf( value );
+        const newValueId = ( oldValueId === 0 ? values.length - 1 : oldValueId - 1 );
+        setValue( values[ newValueId ] );
 
     };
 
     const handleRightArrowClick = () => {
 
-        const newValueId = ( valueId >= values.length - 1 ? 0 : valueId + 1 );
-        setValueId( newValueId );
+        const oldValueId = values.indexOf( value );
+        const newValueId = ( oldValueId >= values.length - 1 ? 0 : oldValueId + 1 );
+        setValue( values[ newValueId ] );
 
     };
 
     return (
         <div className={ classNames( styles.switch, className ) } >
             <div className={ styles.left } onClick={ handleLeftArrowClick }>&#8592;</div>
-            <div className={ styles.value } onClick={ handleRightArrowClick }>{ values[ valueId ] }</div>
+            <div className={ styles.value } onClick={ handleRightArrowClick }>{ value }</div>
             <div className={ styles.right } onClick={ handleRightArrowClick }>&#8594;</div>
         </div>
     );
