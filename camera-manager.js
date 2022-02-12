1c396b7e6e090df8cff1abeaf8ffbc637907b9d9
Avaer Kazmer <a@modules.io>
2022-02-12 10:51:21 -0500

    Add simplex noise to camera manager

--- camera-manager.js
+++ camera-manager.js
@@ -29,6 +29,62 @@ const lastCameraQuaternion = new THREE.Quaternion();
 let lastCameraZ = 0;
 let lastCameraValidZ = 0;
 
+function Simple1DNoise(seed = '') {
+    var MAX_VERTICES = 256;
+    var MAX_VERTICES_MASK = MAX_VERTICES -1;
+    var amplitude = 1;
+    var scale = 1;
+
+    const rng = alea(seed);
+    var r = [];
+    for ( var i = 0; i < MAX_VERTICES; ++i ) {
+        r.push(rng());
+    }
+
+    var getVal = function( x ){
+        var scaledX = x * scale;
+        var xFloor = Math.floor(scaledX);
+        var t = scaledX - xFloor;
+        var tRemapSmoothstep = t * t * ( 3 - 2 * t );
+
+        /// Modulo using &
+        var xMin = xFloor & MAX_VERTICES_MASK;
+        var xMax = ( xMin + 1 ) & MAX_VERTICES_MASK;
+
+        var y = lerp( r[ xMin ], r[ xMax ], tRemapSmoothstep );
+
+        return y * amplitude;
+    };
+
+    /**
+     * Linear interpolation function.
+     * @param a The lower integer value
+     * @param b The upper integer value
+     * @param t The value between the two
+     * @returns {number}
+     */
+    var lerp = function(a, b, t ) {
+        return a * ( 1 - t ) + b * t;
+    };
+
+    // return the API
+    /* return {
+        getVal: getVal,
+        setAmplitude: function(newAmplitude) {
+            amplitude = newAmplitude;
+        },
+        setScale: function(newScale) {
+            scale = newScale;
+        }
+    }; */
+    return getVal;
+}
+const seed = 'lol';
+const shakeNoise = new Simple1DNoise(seed);
+
+/* const seed = 'lol';
+const simplex = new Simplex(seed); */
+
 function lerpNum(value1, value2, amount) {
   amount = amount < 0 ? 0 : amount;
   amount = amount > 1 ? 1 : amount;
