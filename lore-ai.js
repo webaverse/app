b130ca0ed1dec889246051a5d1620092e0ae62f5
Avaer Kazmer <a@modules.io>
2022-02-13 17:46:34 -0500

    Initial lore AI scene api

--- lore-ai.js
+++ lore-ai.js
@@ -1,4 +1,79 @@
+import metaversefile from 'metaversefile';
+import {chatManager} from './chat-manager.js';
 import {loreAiEndpoint} from './constants.js';
+
+const characterLore = `\
+# Setting
+
+AI anime avatars in a virtual world. They have human-level intelligence, but they have interesting personalities and conversations. The script is throught provoking.
+`;
+const _makeChatPrompt = (srcCharacterName, dstCharacterName, srcBio, dstBio, messages) => `\
+${characterLore}
+# Characters
+
+${srcCharacterName}: ${srcBio}
+${dstCharacterName}:  ${dstBio}
+# Scene 1
+
+${
+  messages.map(m => {
+    return `${m.name}: ${m.message}`;
+  }).join('\n')
+}
+${((messages.length % 2) === 1) ?
+  `${dstCharacterName}:`
+:
+  `${srcCharacterName}:`
+}`;
+
+class AIScene {
+  constructor(localPlayer, {
+    setting = 'Some place.',
+ } = {}) {
+    this.setting = setting;
+    this.characters = [];
+    this.messages = [];
+
+    const messages = [];
+    localPlayer.characterHups.addEventListener('hupadd', e => {
+      const {hup} = e.data;
+      hup.addEventListener('voicestart', async e => {
+        const {message} = e.data;
+        if (messages.length > 0 || message.toLowerCase().includes(npcNameLowerCase)) { // continuation or start of conversation
+          messages.push({
+            name: localPlayerName,
+            message: message,
+          });
+
+          const prompt = _makeChatPrompt(localPlayerName, npcName, localPlayerBio, npcBio, messages);
+          let response = await loreAI.generate(prompt, {
+            end: '\n',
+            maxTokens: 100,
+            temperature: 1,
+            top_p: 0,
+          });
+          response = response.trimLeft();
+
+          console.log('got response', [prompt], [response]);
+
+          if (response) {
+            chatManager.addPlayerMessage(npcPlayer, response);
+            messages.push({
+              name: npcName,
+              message: response,
+            });
+          }
+        }
+      });
+    });
+  }
+  setSetting(setting) {
+    this.setting = setting;
+  }
+  addCharacter(characterSpec) {
+    this.characters.push(characterSpec);
+  }
+}
 
 class LoreAI {
   generate(prompt, {
@@ -59,6 +134,9 @@ class LoreAI {
         reject(new Error('prompt is required'));
       }
     });
+  }
+  createScene(localPlayer, opts) {
+    return new AIScene(localPlayer, opts);
   }
 };
 const loreAI = new LoreAI();
