b12ec16dcad504efd5982fd06316aca43210d5af
ohmed <bohdan.horpynchuk@gmail.com>
2022-02-16 16:16:41 +0200

    Fix for initing avatar cause of imports missing.

--- src/new-components/app/App.jsx
+++ src/new-components/app/App.jsx
@@ -1,21 +1,26 @@
 
+import * as THREE from 'three';
 import React, { useState, useEffect, useRef } from 'react';
+import classnames from 'classnames';
+import logo from '../../logo.svg';
+import MagicMenu from '../../MagicMenu.jsx';
+import {defaultAvatarUrl} from '../../../constants';
+import Header from '../../Header.jsx';
+import Footer from '../../Footer.jsx';
 
 import Webaverse from '../../../webaverse.js';
 import * as universe from '../../../universe.js';
-import metaversefileApi from '../../../metaversefile-api.js';
-import { defaultAvatarUrl } from '../../../constants';
+import metaversefileApi from '../../../metaversefile-api';
+const { useLocalPlayer } = metaversefileApi;
 
-import Header from '../../Header.jsx';
-import Footer from '../../Footer.jsx';
 import { Crosshair } from '../general/crosshair';
-
 import styles from './App.module.css';
 import { PlayMode } from '../play-mode/index.jsx';
+import dropManager from '../../../drop-manager.js';
 
 //
 
-const _startApp = async (weba, canvas) => {
+const _startApp = async ( weba, canvas ) => {
 
     weba.setContentLoaded();
 
@@ -53,7 +58,7 @@ export const App = () => {
         <div className={ styles.App } id="app">
             <Header app={ app } />
             <Crosshair />
-            <canvas className={ styles.canvas } ref={ canvasRef } />
+            <canvas id="canvas" className={ styles.canvas } ref={ canvasRef } />
             <PlayMode />
         </div>
     );
