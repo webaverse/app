9635890a6700a1ab32583462c93a7a79c044c2e2
ohmed <bohdan.horpynchuk@gmail.com>
2022-02-16 16:27:44 +0200

    Fixed component import path.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { App } from './new-components/app';
-import { ErrorPage } from './new-components/general/error-page';
+import { App } from './components/app';
+import { ErrorPage } from './components/general/error-page';
 
 //
 
