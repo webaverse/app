8dff92809d5beff697d33533e467f790fc765ec2
ohmed <bohdan.horpynchuk@gmail.com>
2022-02-18 01:01:55 +0200

    WIP: Working with 'World' panel [need to finish menu for one item selected].

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -12,6 +12,7 @@ import metaversefileApi from '../../../metaversefile-api';
 import { ActionMenu } from '../general/action-menu';
 import { Crosshair } from '../general/crosshair';
 import { Settings } from '../general/settings';
+import { WorldObjectsList } from '../general/world-objects-list';
 import { PlayMode } from '../play-mode';
 import Header from '../../Header.jsx';
 
@@ -38,10 +39,11 @@ const _startApp = async ( weba, canvas ) => {
 
 export const App = () => {
 
-    const canvasRef = useRef();
+    const canvasRef = useRef( null );
     const [ app, setApp ] = useState( () => new Webaverse() );
 
     const [ settingsOpened, setSettingsOpened ] = useState( false );
+    const [ worldObjectsListOpened, setWorldObjectsListOpened ] = useState( false );
 
     //
 
@@ -53,18 +55,19 @@ export const App = () => {
 
         }
 
-    }, [ canvasRef.current ] );
+    }, [ canvasRef ] );
 
     //
 
     return (
-        <div className={ styles.App } id="app">
+        <div className={ styles.App } id="app" >
             <Header app={ app } />
-            <ActionMenu app={ app } setSettingsOpened={ setSettingsOpened } />
+            <ActionMenu app={ app } setSettingsOpened={ setSettingsOpened } setWorldObjectsListOpened={ setWorldObjectsListOpened } />
             <Crosshair />
-            <canvas id="canvas" className={ styles.canvas } ref={ canvasRef } />
+            <canvas className={ styles.canvas } ref={ canvasRef } id="canvas" />
             <PlayMode />
             <Settings opened={ settingsOpened } setOpened={ setSettingsOpened } />
+            <WorldObjectsList opened={ worldObjectsListOpened } setOpened={ setWorldObjectsListOpened } />
         </div>
     );
 
