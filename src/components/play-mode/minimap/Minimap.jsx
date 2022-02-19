613d3750520c574e444514ada4887deab09221fd
Avaer Kazmer <a@modules.io>
2022-02-19 14:22:56 -0500

    Add minimap hook in react

--- src/components/play-mode/minimap/Minimap.jsx
+++ src/components/play-mode/minimap/Minimap.jsx
@@ -1,18 +1,34 @@
 
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
+import minimapManager from '../../../../minimap.js';
 
 import styles from './minimap.module.css';
 
 //
 
+let minimap = null;
+const minimapSize = 128;
+const minimapWorldSize = 50;
+
 export const Minimap = () => {
+    const canvasRef = useRef();
+
+    useEffect(() => {
+        if (canvasRef.current) {
+          const canvas = canvasRef.current;
+
+          if (!minimap) {
+            minimap = minimapManager.createMiniMap(minimapSize, minimapSize, minimapWorldSize, minimapWorldSize);
+          }
+          minimap.resetCanvases();
+          minimap.addCanvas(canvas);
+        }
+      }, [canvasRef.current]);
 
     return (
         <div className={ styles.locationMenu } >
 
-            <div className={ styles.map } >
-
-            </div>
+            <canvas width={minimapSize} height={minimapSize} className={ styles.map } ref={canvasRef} />
 
         </div>
     );
