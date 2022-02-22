17da6349e1316c4c439fa8f810a5270a45db3fd4
Avaer Kazmer <a@modules.io>
2022-02-22 18:02:37 -0500

    Add perlin noise implenentation

--- src/components/play-mode/hotbar/Hotbar.jsx
+++ src/components/play-mode/hotbar/Hotbar.jsx
@@ -21,6 +21,46 @@ const fullscreenFragmentShader = `\
   uniform float uSelected;
   uniform float uSelectFactor;
   varying vec2 vUv;
+
+  //---------------------------------------------------------------------------
+  //1D Perlin noise implementation 
+  //---------------------------------------------------------------------------
+  #define HASHSCALE 0.1031
+
+  float hash(float p)
+  {
+    vec3 p3  = fract(vec3(p) * HASHSCALE);
+      p3 += dot(p3, p3.yzx + 19.19);
+      return fract((p3.x + p3.y) * p3.z);
+  }
+
+  float fade(float t) { return t*t*t*(t*(6.*t-15.)+10.); }
+
+  float grad(float hash, float p)
+  {
+      int i = int(1e4*hash);
+    return (i & 1) == 0 ? p : -p;
+  }
+
+  float perlinNoise1D(float p)
+  {
+    float pi = floor(p), pf = p - pi, w = fade(pf);
+      return mix(grad(hash(pi), pf), grad(hash(pi + 1.0), pf - 1.0), w) * 2.0;
+  }
+
+  float fbm(float pos, int octaves, float persistence) 
+  {
+      float total = 0., frequency = 1., amplitude = 1., maxValue = 0.;
+      for(int i = 0; i < octaves; ++i) 
+      {
+          total += perlinNoise1D(pos * frequency) * amplitude;
+          maxValue += amplitude;
+          amplitude *= persistence;
+          frequency *= 2.;
+      }
+      return total / maxValue;
+  }
+
   
   struct Tri {
     vec2 a;
