8b7e0e57f6dc45f54a60e21235d37e786e122ffb
Avaer Kazmer <a@modules.io>
2022-02-23 01:15:49 -0500

    Hotbar animation time rate increase

--- src/components/play-mode/hotbar/Hotbar.jsx
+++ src/components/play-mode/hotbar/Hotbar.jsx
@@ -218,7 +218,7 @@ const fullscreenFragmentShader = `\
     vec4 s;
     if (uTexEnabled > 0.) {
       // XXX compute the frame offset using numFrames, frameTime, and uTime
-      float f = mod(uTime / 1000. * 0.1, 1.);
+      float f = mod(uTime / 1000. * 0.5, 1.);
       float frameIndex = floor(f * numFrames);
       float x = mod(frameIndex, numFramesPerRow);
       float y = floor(frameIndex / numFramesPerRow);
