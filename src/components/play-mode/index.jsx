d3fb2c8d74ca17dca0d5635cf0d7b9df1bf74072
ohmed <bohdan.horpynchuk@gmail.com>
2022-02-23 19:36:00 +0200

    Removed inventory.

--- src/components/play-mode/index.jsx
+++ src/components/play-mode/index.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 
 import { Minimap } from './minimap';
 import { Hotbar } from './hotbar';
-import { Inventory } from './inventory';
 
 import styles from './play-mode.module.css';
 
@@ -17,7 +16,6 @@ export const PlayMode = () => {
         <div className={ styles.playMode }>
             <Minimap />
             <Hotbar />
-            <Inventory />
         </div>
     );
 
