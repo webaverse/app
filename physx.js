e0c979a53f42790b20ba4894249ba279e2ddab62
gonnavis <gonnavis@163.com>
2022-02-24 00:07:16 +0800

    add back setIgnorePhysicsIds(). && clean.

--- physx.js
+++ physx.js
@@ -853,7 +853,6 @@ const physxWorker = (() => {
 
     const head = outputBufferOffset / Float32Array.BYTES_PER_ELEMENT;
     // let tail = head + 1;
-
     const numWaypointResult = moduleInstance.HEAPF32[head + 0];
     const waypointResultPositions = [];
     for (let i = 0; i < numWaypointResult; i++) {
@@ -863,31 +862,7 @@ const physxWorker = (() => {
       position.z = moduleInstance.HEAPF32[head + i * 3 + 3];
       waypointResultPositions.push(position);
     }
-    console.log(waypointResultPositions);
-
-    // // TEST:
-    // const outY = moduleInstance.HEAPF32[head];
-    // // const test = moduleInstance.HEAPF32[head + 1];
-    // console.log(
-    //   moduleInstance.HEAPF32[head + 0],
-    //   moduleInstance.HEAPF32[head + 1],
-    //   moduleInstance.HEAPF32[head + 2],
-    // );
-    // console.log(
-    //   moduleInstance.HEAPF32[head + 3],
-    //   moduleInstance.HEAPF32[head + 4],
-    //   moduleInstance.HEAPF32[head + 5],
-    // );
-    // console.log(
-    //   moduleInstance.HEAPF32[head + 6],
-    //   moduleInstance.HEAPF32[head + 7],
-    //   moduleInstance.HEAPF32[head + 8],
-    // );
-    // console.log(
-    //   moduleInstance.HEAPF32[head + 9],
-    //   moduleInstance.HEAPF32[head + 10],
-    //   moduleInstance.HEAPF32[head + 11],
-    // );
+    // console.log(waypointResultPositions);
 
     moduleInstance._doFree(outputBufferOffset);
 
