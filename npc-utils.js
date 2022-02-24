a152430b0a8c2eca6765aaa52152fb283ee42667
gonnavis <gonnavis@163.com>
2022-02-24 18:01:31 +0800

    add `debugRender`. && fix send wasm `voxelHeight` bug, use `hy` instead.

--- npc-utils.js
+++ npc-utils.js
@@ -1,12 +1,12 @@
 
 import * as THREE from 'three';
+import {
+  rootScene,
+} from './renderer.js';
 import physicsManager from './physics-manager.js';
 
-const identityPosition = new THREE.Vector3();
-const identityQuaternion = new THREE.Quaternion();
-
 class PathFinder {
-  constructor({voxelHeight = 1.5, heightTolerance = 0.6, detectStep = 0.1, maxIterdetect = 1000, maxIterStep = 1000, maxVoxelCacheLen = 10000, ignorePhysicsIds = []}) {
+  constructor({voxelHeight = 1.5, heightTolerance = 0.6, detectStep = 0.1, maxIterdetect = 1000, maxIterStep = 1000, maxVoxelCacheLen = 10000, ignorePhysicsIds = [], debugRender = false}) {
     /* args:
       voxelHeight: Voxel height ( Y axis ) for collide detection, usually equal to npc's physical capsule height. X/Z axes sizes are hard-coded 1 now.
       heightTolerance: Used to check whether currentVoxel can go above to neighbor voxels.
@@ -15,35 +15,61 @@ class PathFinder {
       maxIterStep: How many A* path-finding step can one getPath() iterate. One A* step can create up to 4 voxels, 0 ~ 4.
       maxVoxelCacheLen: How many detected voxels can be cached.
       ignorePhysicsIds: physicsIds that voxel detect() ignored, usually npc CharacterController's capsule.
+      debugRender: Whether show voxel boxes for debugging.
     */
 
     this.voxelHeight = voxelHeight;
-    this.voxelHeightHalf = this.voxelHeight / 2;
+    this.hy = this.voxelHeight / 2;
     this.heightTolerance = heightTolerance;
     this.detectStep = detectStep;
     this.maxIterDetect = maxIterdetect;
     this.maxIterStep = maxIterStep;
     this.maxVoxelCacheLen = maxVoxelCacheLen;
     this.ignorePhysicsIds = ignorePhysicsIds;
+    this.waypointResult = [];
+    this.debugRender = debugRender;
+    this.colorPathSimplified = new THREE.Color('rgb(69,0,98)');
+
+    if (this.debugRender) {
+      this.geometry = new THREE.BoxGeometry();
+      this.geometry.scale(0.5, this.voxelHeight, 0.5);
+      // this.geometry.scale(0.9, 0.1, 0.9);
+      this.material = new THREE.MeshLambertMaterial({color: this.colorPathSimplified, wireframe: true});
+      this.maxDebugCount = 100;
+      this.debugMesh = new THREE.InstancedMesh(this.geometry, this.material, this.maxDebugCount);
+      this.debugMesh.name = 'PathFinder debugMesh';
+      rootScene.add(this.debugMesh);
+    }
   }
 
   getPath(start, dest) {
-    const positions = physicsManager.getPath(start, dest, this.voxelHeight, this.heightTolerance, this.detectStep, this.maxIterdetect, this.maxIterStep, this.maxVoxelCacheLen, this.ignorePhysicsIds);
+    this.waypointResult = [];
+    const positions = physicsManager.getPath(start, dest, this.hy, this.heightTolerance, this.detectStep, this.maxIterdetect, this.maxIterStep, this.maxVoxelCacheLen, this.ignorePhysicsIds);
     const isFound = positions.length > 0;
-    const waypointResult = [];
     positions.forEach(position => {
       const result = new THREE.Object3D();
       result.position.copy(position);
-      waypointResult.push(result);
+      this.waypointResult.push(result);
     });
-    waypointResult.forEach((result, i) => {
-      const next = waypointResult[i + 1];
+    this.waypointResult.forEach((result, i) => {
+      const next = this.waypointResult[i + 1];
       if (next) {
         result._next = next;
       }
     });
 
-    return isFound ? waypointResult : null;
+    if (this.debugRender) {
+      this.debugMesh.count = this.waypointResult.length;
+      this.waypointResult.forEach((result, i) => {
+        result.updateMatrixWorld();
+        this.debugMesh.setMatrixAt(i, result.matrix);
+        // this.debugMesh.setColorAt(i, this.colorPathSimplified);
+      });
+      this.debugMesh.instanceMatrix.needsUpdate = true;
+      // this.debugMesh.instanceColor.needsUpdate = true;
+    }
+
+    return isFound ? this.waypointResult : null;
   }
 
   setIgnorePhysicsIds(ignorePhysicsIds) {
