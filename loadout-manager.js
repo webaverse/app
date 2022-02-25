51be419e69e39bd67efb9f3c30888357aea3d135
Avaer Kazmer <a@modules.io>
2022-02-24 23:33:31 -0500

    Loadout manager hook in infobox renderer

--- loadout-manager.js
+++ loadout-manager.js
@@ -1,32 +1,48 @@
-import {createHotbarRenderer} from './hotbar.js';
 import {localPlayer} from './players.js';
-import {hotbarSize} from './constants.js';
+import {createHotbarRenderer} from './hotbar.js';
+import {createInfoboxRenderer} from './infobox.js';
+import {createObjectSprite} from './object-spriter.js';
+import {hotbarSize, infoboxSize} from './constants.js';
 
 const numSlots = 8;
+
 class LoadoutManager extends EventTarget {
   constructor() {
     super();
 
+    this.apps = Array(numSlots).fill(null);
     this.hotbarRenderers = [];
+    this.infoboxRenderer = null;
     this.selectedIndex = -1;
   
     localPlayer.addEventListener('wearupdate', e => {
       const {app, wear} = e;
 
-      this.ensureHotbarRenderers();
+      this.ensureRenderers();
       if (wear) {
         const nextIndex = this.getNextFreeIndex();
         if (nextIndex !== -1) {
+          const spritesheet = createObjectSprite(app);
+
           const hotbarRenderer = this.hotbarRenderers[nextIndex];
-          hotbarRenderer.setApp(app);
+          hotbarRenderer.setSpritesheet(spritesheet);
+          this.infoboxRenderer.setSpritesheet(spritesheet);
+
+          this.apps[nextIndex] = app;
 
           this.setSelectedIndex(nextIndex);
         }
       } else {
-        for (let i = 0; i < this.hotbarRenderers.length; i++) {
-          const hotbarRenderer = this.hotbarRenderers[i];
-          if (hotbarRenderer.app === app) {
-            hotbarRenderer.setApp(null);
+        for (let i = 0; i < this.apps.length; i++) {
+          const a = this.apps[i];
+          if (a === app) {
+            const hotbarRenderer = this.hotbarRenderers[i];
+            hotbarRenderer.setSpritesheet(null);
+            if (i === this.selectedIndex) {
+              this.infoboxRenderer.setSpritesheet(null);
+            }
+
+            this.apps[i] = null;
 
             const nextIndex = this.getNextUsedIndex();
             this.setSelectedIndex(nextIndex);
@@ -36,7 +52,7 @@ class LoadoutManager extends EventTarget {
       }
     });
   }
-  ensureHotbarRenderers() {
+  ensureRenderers() {
     if (this.hotbarRenderers.length === 0) {
       /* const renderer = getRenderer();
       const size = hotbarSize * renderer.getPixelRatio(); */
@@ -48,28 +64,35 @@ class LoadoutManager extends EventTarget {
         this.hotbarRenderers.push(hotbarRenderer);
       }
     }
+    if (!this.infoboxRenderer) {
+      this.infoboxRenderer = createInfoboxRenderer(infoboxSize, infoboxSize);
+    }
   }
   getHotbarRenderer(index) {
-    this.ensureHotbarRenderers();
+    this.ensureRenderers();
     return this.hotbarRenderers[index];
   }
+  getInfoboxRenderer() {
+    this.ensureRenderers();
+    return this.infoboxRenderer;
+  }
   getSelectedApp() {
-    this.ensureHotbarRenderers();
+    this.ensureRenderers();
     
     if (this.selectedIndex !== -1) {
-      return this.hotbarRenderers[this.selectedIndex].app;
+      return this.apps[this.selectedIndex];
     } else {
       return null;
     }
   }
   setSelectedIndex(index) {
-    this.ensureHotbarRenderers();
+    this.ensureRenderers();
 
     if (index === this.selectedIndex) {
       index = -1;
     }
 
-    if (index === -1 || this.hotbarRenderers[index].app) {
+    if (index === -1 || this.apps[index]) {
       for (let i = 0; i < this.hotbarRenderers.length; i++) {
         this.hotbarRenderers[i].setSelected(i === index);
       }
@@ -77,18 +100,18 @@ class LoadoutManager extends EventTarget {
     }
   }
   getNextFreeIndex() {
-    this.ensureHotbarRenderers();
+    this.ensureRenderers();
     for (let i = 0; i < this.hotbarRenderers.length; i++) {
-      if (!this.hotbarRenderers[i].app) {
+      if (!this.apps[i]) {
         return i;
       }
     }
     return -1;
   }
   getNextUsedIndex() {
-    this.ensureHotbarRenderers();
+    this.ensureRenderers();
     for (let i = 0; i < this.hotbarRenderers.length; i++) {
-      if (this.hotbarRenderers[i].app) {
+      if (this.apps[i]) {
         return i;
       }
     }
@@ -97,6 +120,9 @@ class LoadoutManager extends EventTarget {
   update(timestamp, timeDiff) {
     for (let i = 0; i < this.hotbarRenderers.length; i++) {
       this.hotbarRenderers[i].update(timestamp, timeDiff, i);
+    }
+    if (this.infoboxRenderer !== null) {
+      this.infoboxRenderer.update(timestamp, timeDiff);
     }
   }
 }
