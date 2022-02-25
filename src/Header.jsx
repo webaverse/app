fa4fa5d4e5f37e8d714bd946245c8dba93f96b10
Avaer Kazmer <a@modules.io>
2022-02-24 20:00:11 -0500

    More cleanup

--- src/Header.jsx
+++ src/Header.jsx
@@ -21,8 +21,6 @@ import {Character} from './tabs/character';
 import {Claims} from './tabs/claims';
 import {Tokens} from './tabs/tokens';
 
-// const localEuler = new THREE.Euler();
-
 export default function Header({
   app,
 }) {
