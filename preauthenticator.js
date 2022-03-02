ba5a1c0c422c622464f85ecf0993d83862e937fa
Avaer Kazmer <a@modules.io>
2022-03-02 02:15:23 -0500

    Add missing preauthenticator.js

---
+++ preauthenticator.js
@@ -0,0 +1,38 @@
+import * as preauthenticator from 'https://preauthenticator.webaverse.online/preauthenticator.js';
+
+let port = null;
+const loadPromise = (async () => {
+  port = await preauthenticator.connect();
+})();
+
+const o = {
+  async callAuthenticatedApi() {
+    await loadPromise;
+
+    return port.callAuthenticatedApi.apply(port, arguments);
+  },
+  async setAuthenticatedApi() {
+    await loadPromise;
+
+    return port.setAuthenticatedApi.apply(port, arguments);
+  },
+  async getAuthenticatedApi() {
+    await loadPromise;
+
+    return port.getAuthenticatedApi.apply(port, arguments);
+  },
+  async hasAuthenticatedApi() {
+    await loadPromise;
+
+    return  port.hasAuthenticatedApi.apply(port, arguments);
+  },
+  async deleteAuthenticatedApi() {
+    await loadPromise;
+
+    return  port.deleteAuthenticatedApi.apply(port, arguments);
+  },
+  /* waitForLoad() {
+    return loadPromise;
+  }, */
+};
+export default o;
