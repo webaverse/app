b8a87f7e6c2b265e1340e78a647357a3a12d0b4f
Avaer Kazmer <a@modules.io>
2022-03-03 05:09:46 -0500

    Bind renderer canvas in pic-main.js

--- pic-main.js
+++ pic-main.js
@@ -1,8 +1,8 @@
+import {bindCanvas} from './renderer.js';
 import {genPic} from './pic.js';
 
-//
-
 const defaultUrl = `/avatars/scillia_drophunter_v15_vian.vrm`;
+const rendererSize = 2048;
 const width = 500;
 const height = 500;
 
@@ -10,12 +10,24 @@ const formEl = document.getElementById('form');
 const urlEl = document.getElementById('url');
 const canvasEl = document.getElementById('canvas');
 const videoEl = document.getElementById('video');
-// const outputsEl = document.getElementById('outputs');
-
 formEl.addEventListener('submit', async e => {
   e.preventDefault();
 
   const url = urlEl.value;
+
+  const _bindRendererCanvas = () => {
+    const canvas = document.createElement('canvas');
+    canvas.width = rendererSize;
+    canvas.height = rendererSize;
+    bindCanvas(canvas);
+  };
+  _bindRendererCanvas();
+
+  const _initLocalCanvas = () => {
+    canvasEl.width = width;
+    canvasEl.height = height;
+  };
+  _initLocalCanvas();
 
   await genPic({
     url,
