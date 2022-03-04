c4646923974a748f328d59a89a8f6fe083f12849
Avaer Kazmer <a@modules.io>
2022-03-03 20:59:44 -0500

    Major renderer resize refactoring

--- renderer.js
+++ renderer.js
@@ -33,11 +33,15 @@ function bindCanvas(c) {
     rendererExtensionFragDepth: true,
     logarithmicDepthBuffer: true,
   });
+  
+  const {
+    width,
+    height,
+    pixelRatio,
+  } = _getCanvasDimensions();
+  renderer.setSize(width, height);
+  renderer.setPixelRatio(pixelRatio);
 
-  // const rect = renderer.domElement.getBoundingClientRect();
-  // renderer.setSize(canvas.width, canvas.height);
-  // renderer.setPixelRatio(window.devicePixelRatio);
-  _setRendererSize();
   renderer.autoClear = false;
   renderer.sortObjects = false;
   renderer.physicallyCorrectLights = true;
@@ -45,30 +49,22 @@ function bindCanvas(c) {
   // renderer.gammaFactor = 2.2;
   renderer.shadowMap.enabled = true;
   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-  if (!canvas) {
-    canvas = renderer.domElement;
-  }
-  if (!context) {
-    context = renderer.getContext();
-  }
-  // context.enable(context.SAMPLE_ALPHA_TO_COVERAGE);
   renderer.xr.enabled = true;
 
   // initialize post-processing
-  {
-    const size = renderer.getSize(new THREE.Vector2());
-    const pixelRatio = renderer.getPixelRatio();
-    const encoding = THREE.sRGBEncoding;
-    const renderTarget = new THREE.WebGLMultisampleRenderTarget(size.x * pixelRatio, size.y * pixelRatio, {
-      minFilter: THREE.LinearFilter,
-      magFilter: THREE.LinearFilter,
-      format: THREE.RGBAFormat,
-      encoding,
-    });
-    renderTarget.samples = context.MAX_SAMPLES;
-    composer = new EffectComposer(renderer, renderTarget);
-  }
+  const renderTarget = new THREE.WebGLMultisampleRenderTarget(width * pixelRatio, height * pixelRatio, {
+    minFilter: THREE.LinearFilter,
+    magFilter: THREE.LinearFilter,
+    format: THREE.RGBAFormat,
+    encoding: THREE.sRGBEncoding,
+  });
+  renderTarget.samples = context.MAX_SAMPLES;
+  composer = new EffectComposer(renderer, renderTarget);
 
+  // initialize camera
+  _setCameraSize(width, height, pixelRatio);
+
+  // resolve promise
   waitPromise.accept();
 }
 
@@ -84,7 +80,6 @@ function getContainerElement() {
 function getComposer() {
   return composer;
 }
-
 
 const scene = new THREE.Object3D();
 scene.name = 'scene';
@@ -106,7 +101,7 @@ rootScene.add(sceneLowPriority);
 // const orthographicScene = new THREE.Scene();
 // const avatarScene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(minFov, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(minFov, 1, 0.1, 1000);
 camera.position.set(0, 1.6, 0);
 camera.rotation.order = 'YXZ';
 camera.name = 'sceneCamera';
@@ -125,49 +120,67 @@ scene.add(dolly);
 const orthographicCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.01, 100);
 // scene.add(orthographicCamera);
 
-const _setRendererSize = () => {
-  const renderer = getRenderer();
+const _getCanvasDimensions = () => {
+  let width, height, pixelRatio;
+  width = window.innerWidth;
+  height = window.innerHeight;
+  pixelRatio = window.devicePixelRatio;
+  
+  return {
+    width,
+    height,
+    pixelRatio,
+  };
+};
 
-  let width, height;
-  if (renderer.domElement.parentNode) {
-    const rect = renderer.domElement.getBoundingClientRect();
-    width = rect.width;
-    height = rect.height;
-  } else {
-    width = renderer.domElement.width;
-    height = renderer.domElement.height;
-  }
-  const pixelRatio = window.devicePixelRatio;
-  renderer.setSize(width, height);
-  renderer.setPixelRatio(pixelRatio);
-}
+const _setSizes = () => {
+  const {
+    width,
+    height,
+    pixelRatio,
+  } = _getCanvasDimensions();
+  _setRendererSize(width, height, pixelRatio);
+  _setComposerSize(width, height, pixelRatio);
+  _setCameraSize(width, height, pixelRatio);
+};
 
-window.addEventListener('resize', e => {
+const _setRendererSize = (width, height, pixelRatio) => {
+  // pause XR since it gets in the way of resize
   const renderer = getRenderer();
   if (renderer) {
     if (renderer.xr.getSession()) {
       renderer.xr.isPresenting = false;
     }
 
-    _setRendererSize();
+    const {
+      width,
+      height,
+      pixelRatio,
+    } = _getCanvasDimensions();
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(pixelRatio);
 
-    const aspect = width / height;
-    camera.aspect = aspect;
-    camera.updateProjectionMatrix();
-
-    // avatarCamera.aspect = aspect;
-    // avatarCamera.updateProjectionMatrix();
-    
+    // resume XR
     if (renderer.xr.getSession()) {
       renderer.xr.isPresenting = true;
     }
-
-    const composer = getComposer();
-    if (composer) {
-      composer.setSize(width, height);
-      composer.setPixelRatio(pixelRatio);
-    }
   }
+};
+const _setComposerSize = (width, height, pixelRatio) => {
+  const composer = getComposer();
+  if (composer) {
+    composer.setSize(width, height);
+    composer.setPixelRatio(pixelRatio);
+  }
+};
+const _setCameraSize = (width, height, pixelRatio) => {
+  const aspect = width / height;
+  camera.aspect = aspect;
+  camera.updateProjectionMatrix();
+};
+
+window.addEventListener('resize', e => {
+  _setSizes();
 });
 
 /* addDefaultLights(scene, {
