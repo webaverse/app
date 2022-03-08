1e4c1315d4b995704a997198fe115b72acf93b69
Avaer Kazmer <a@modules.io>
2022-03-08 16:05:04 -0500

    Add naruto run render case in pic.js

--- pic.js
+++ pic.js
@@ -5,7 +5,7 @@ import Avatar from './avatars/avatars.js';
 import * as audioManager from './audio-manager.js';
 import npcManager from './npc-manager.js';
 import dioramaManager from './diorama.js';
-import {getRenderer, scene} from './renderer.js';
+// import {getRenderer, scene, camera} from './renderer.js';
 
 // import GIF from './gif.js';
 import * as WebMWriter from 'webm-writer';
@@ -14,11 +14,23 @@ import * as WebMWriter from 'webm-writer';
 const FPS = 60;
 // const videoQuality = 0.99999;
 const videoQuality = 0.95;
+const narutoRunSpeed = 59;
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
-const localVector3 = new THREE.Vector3();
-const localMatrix = new THREE.Matrix4();
+// const localVector3 = new THREE.Vector3();
+const localQuaternion = new THREE.Quaternion();
+// const localEuler = new THREE.Euler();
+// const localMatrix = new THREE.Matrix4();
+
+/* function mod(a, n) {
+  return ((a % n) + n) % n;
+}
+function angleDifference(angle1, angle2) {
+  let a = angle2 - angle1;
+  a = mod(a + Math.PI, Math.PI*2) - Math.PI;
+  return a;
+} */
 
 // I can take all of you motherfuckers on at once!
 
@@ -38,7 +50,6 @@ const localMatrix = new THREE.Matrix4();
     context,
   });
   // renderer.setSize(width, height);
-  renderer.setClearColor(0xff0000, 1);
 
   const scene = new THREE.Scene();
   scene.autoUpdate = false;
@@ -95,6 +106,8 @@ export const genPic = async ({
   const animations = metaversefile.useAvatarAnimations();
   const idleAnimation = animations.find(a => a.name === 'idle.fbx');
   const idleAnimationDuration = idleAnimation.duration;
+  const narutoRunAnimation = animations.find(a => a.isNarutoRun);
+  const narutoRunAnimationDuration = narutoRunAnimation.duration;
 
   // load app
   const app = await metaversefile.createAppAsync({
@@ -131,20 +144,18 @@ export const genPic = async ({
       emotion: 'angry',
     });
   };
+  const _updateTarget = (timestamp, timeDiff) => {
+    target.matrixWorld.copy(player.avatar.modelBones.Head.matrixWorld)
+      .decompose(target.position, target.quaternion, target.scale);
+    target.position.set(player.position.x, target.position.y, player.position.z);
+    target.quaternion.copy(player.quaternion);
+    target.matrixWorld.compose(target.position, target.quaternion, target.scale);
+  };
   const _animate = (timestamp, timeDiff) => {
     // console.log('got position', player.position.y);
     player.updateAvatar(timestamp, timeDiff);
   };
-  const _lookAt = (camera, boundingBox) => {
-    /* boundingBox.getCenter(camera.position);
-    // const size = boundingBox.getSize(localVector);
-
-    camera.position.y = 0;
-    camera.position.z += 1;
-    camera.updateMatrixWorld();
-
-    fitCameraToBoundingBox(camera, boundingBox); */
-    
+  /* const _lookAt = (camera, boundingBox) => {
     camera.position.copy(player.position)
       .add(localVector.set(0.3, 0, -0.5).applyQuaternion(player.quaternion));
       camera.quaternion.setFromRotationMatrix(
@@ -155,24 +166,29 @@ export const genPic = async ({
       )
     );
     camera.updateMatrixWorld();
-  };
+  }; */
 
   // rendering
   const localLights = _makeLights();
   const objects = localLights.concat([
     player.avatar.model,
   ]);
+  // console.log('got model bones', player.avatar.modelBones);
+  // debugger;
+  const target = new THREE.Object3D();
   const diorama = dioramaManager.createPlayerDiorama({
     canvas,
-    target: player,
+    // target: player,
+    target,
     objects,
     lights: false,
     // label: true,
-    outline: true,
-    grassBackground: true,
+    // outline: true,
+    // grassBackground: true,
     // glyphBackground: true,
   });
   // diorama.enabled = false;
+  diorama.setClearColor(0xFFFFFF, 1);
 
   const videoWriter = new WebMWriter({
     quality: 1,
@@ -187,6 +203,91 @@ export const genPic = async ({
   writeCanvas.style.width = `${width/window.devicePixelRatio}px`;
   writeCanvas.style.height = `${height/window.devicePixelRatio}px`;
   const writeCtx = writeCanvas.getContext('2d');
+
+  const mode = 'idle';
+  // const mode = 'narutoRun';
+  let update;
+  let totalDuration;
+  switch (mode) {
+    case 'idle': {
+      update = () => {};
+      totalDuration = idleAnimationDuration*1000;
+      break;
+    }
+    case 'narutoRun': {
+      /* localEuler.setFromRotationMatrix(
+        localMatrix.lookAt(
+          localVector.set(0, 0, 0),
+          localVector2.set(0, 0, -1)
+            .applyQuaternion(player.quaternion),
+          localVector3.set(0, 1, 0)
+        ),
+        'YXZ'
+      );
+      const forwardY = localEuler.y; */
+      // const angle = forwardY;
+
+      const spriteSpec = {
+        name: 'naruto run',
+        duration: narutoRunAnimationDuration,
+        init({player}) {
+          let positionOffset = 0;
+          // let narutoRunTime = 0;
+          // const narutoRunIncrementSpeed = 1000 * 4;
+
+          player.addAction({
+            type: 'narutoRun',
+            // time: 0,
+          });
+
+          return {
+            update(timestamp, timeDiff) {
+              const timeDiffMs = timeDiff/1000;
+              const angle = -Math.PI/2;
+              positionOffset -= narutoRunSpeed/1000 * timeDiffMs;
+              
+              /* const euler = new THREE.Euler(0, angle, 0, 'YXZ');
+              camera.position.set(0, avatar.height*cameraHeightFactor, positionOffset)
+                .add(new THREE.Vector3(0, 0, -distance).applyEuler(euler));
+              camera.lookAt(new THREE.Vector3(0, avatar.height*cameraHeightFactor, positionOffset));
+              camera.updateMatrixWorld(); */
+
+              diorama.setCameraOffset(
+                localVector.set(0, 0, 2)
+                  .applyQuaternion(localQuaternion.setFromAxisAngle(localVector2.set(0, 1, 0), -angle))
+              );
+              // player.avatar.modelBones.Root.updateMatrixWorld();
+
+              // player.position.set(0, avatar.height, positionOffset);
+              // player.updateMatrixWorld();
+
+              // avatar.narutoRunState = true;
+              // avatar.narutoRunTime = narutoRunTime;
+
+              // narutoRunTime += timeDiffMs * narutoRunIncrementSpeed;
+
+              // avatar.update(timestamp, timeDiffMs);
+            },
+            /* cleanup() {
+              avatar.narutoRunState = false;
+            }, */            
+          };
+        },
+      };
+      const spriteSpecInstance = spriteSpec.init({
+        player,
+      });
+
+      update = (timestamp, timeDiff) => {
+        spriteSpecInstance.update(timestamp, timeDiff);
+      };
+      totalDuration = narutoRunAnimationDuration * 2 * 1000;
+      break;
+    }
+    default: {
+      throw new Error('unknown mode');
+    }
+  }
 
   const framePromises = [];
   const _pushFrame = async () => {
@@ -205,29 +306,23 @@ export const genPic = async ({
     framePromises.push(p);
   };
   const _render = async () => {
-    const boundingBox = new THREE.Box3().setFromObject(app);
-
     _initializeAnimation();
-    // _lookAt(camera, boundingBox);
 
     const _renderFrames = async () => {
       let now = 0;
       const timeDiff = 1000/FPS;
       for (let i = 0; i < FPS*2; i++) {
-        // _lookAt(camera, boundingBox);
         _animate(now, timeDiff);
-        // app.updateMatrixWorld();
       }
 
       let index = 0;
       const framesPerFrame = FPS;
-      while (now < idleAnimationDuration*1000) {
-        // _lookAt(camera, boundingBox);
+      while (now < totalDuration) {
+        update(now, timeDiff);
+        _updateTarget(now, timeDiff);
         _animate(now, timeDiff);
 
         diorama.update(now, timeDiff);
-        // renderer.render(scene, camera);
-        // renderer.getContext().flush();
 
         _pushFrame();
         now += timeDiff;
