90ab6e10600a61087060b527da51d45d18261faf
Avaer Kazmer <a@modules.io>
2022-03-14 01:29:12 -0400

    Major update regimen work

--- interpolants.js
+++ interpolants.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {mod} from './util.js';
+import {avatarInterpolationFrameRate, avatarInterpolationNumFrames} from './constants.js';
 
 export class ScalarInterpolant {
   constructor(fn, minValue, maxValue) {
@@ -24,7 +25,10 @@ export class BiActionInterpolant extends ScalarInterpolant {
   constructor(fn, minValue, maxValue) {
     super(fn, minValue, maxValue);
   }
-  update(timeDiff) {
+  update(timestamp, timeDiff) {
+    if (typeof timestamp !== 'number' || typeof timeDiff !== 'number') {
+      debugger;
+    }
     this.value += (this.fn() ? 1 : -1) * timeDiff;
     this.value = Math.min(Math.max(this.value, this.minValue), this.maxValue);
   }
@@ -35,7 +39,10 @@ export class UniActionInterpolant extends ScalarInterpolant {
   constructor(fn, minValue, maxValue) {
     super(fn, minValue, maxValue);
   }
-  update(timeDiff) {
+  update(timestamp, timeDiff) {
+    if (typeof timestamp !== 'number' || typeof timeDiff !== 'number') {
+      debugger;
+    }
     if (this.fn()) {
       this.value += timeDiff;
       this.value = Math.min(Math.max(this.value, this.minValue), this.maxValue);
@@ -50,7 +57,10 @@ export class InfiniteActionInterpolant extends ScalarInterpolant {
   constructor(fn, minValue) {
     super(fn, minValue, Infinity);
   }
-  update(timeDiff) {
+  update(timestamp, timeDiff) {
+    if (typeof timestamp !== 'number' || typeof timeDiff !== 'number') {
+      debugger;
+    }
     if (this.fn()) {
       this.value += timeDiff;
     } else {
@@ -65,7 +75,7 @@ const _makeSnapshots = (constructor, numFrames) => {
     result[i] = {
       startValue: constructor(),
       // endValue: constructor(),
-      startTime: 0,
+      // startTime: 0,
       endTime: 0,
     };
   }
@@ -81,45 +91,78 @@ export class SnapshotInterpolant {
     this.seekFn = seekFn;
     
     this.readTime = 0;
-    this.writeTime = 0;
+    // this.writeTime = 0;
 
     this.snapshots = _makeSnapshots(constructor, numFrames);
     this.snapshotWriteIndex = 0;
 
     this.value = constructor();
   }
-  update(timeDiff) {
+  update(timestamp, timeDiff) {
     this.readTime += timeDiff;
+    
+    let minEndTime = Infinity;
+    let maxEndTime = -Infinity;
+    for (let i = 0; i < this.numFrames; i++) {
+      const snapshot = this.snapshots[i];
+      if (snapshot.endTime < minEndTime) {
+        minEndTime = snapshot.endTime;
+      }
+      if (snapshot.endTime > maxEndTime) {
+        maxEndTime = snapshot.endTime;
+      }
+    }
+    // minEndTime -= 1000 / avatarInterpolationFrameRate * avatarInterpolationNumFrames;
 
-    const effectiveReadTime = this.readTime - this.timeDelay;
-    this.seekTo(effectiveReadTime);
+    if (maxEndTime > 0) { // if we had at least one snapshot
+      if (
+        (this.readTime - this.timeDelay) < minEndTime ||
+        (this.readTime - this.timeDelay) > maxEndTime
+      ) {
+        this.readTime = maxEndTime;
+      }
+
+      let effectiveReadTime = this.readTime - this.timeDelay;
+
+      this.seekTo(effectiveReadTime);
+    }
   }
   seekTo(t) {
     for (let i = -(this.numFrames - 1); i < 0; i++) {
       const index = this.snapshotWriteIndex + i;
       const snapshot = this.snapshots[mod(index, this.numFrames)];
-      if (snapshot.startTime >= t && t <= snapshot.endTime) {
-        const duration = snapshot.endTime - snapshot.startTime;
-        const f = (duration > 0 && duration < Infinity) ? ((t - snapshot.startTime) / duration) : 0;
-        const {startValue} = snapshot;
-        const nextSnapshot = this.snapshots[mod(index + 1, this.numFrames)];
-        const {startValue: endValue} = nextSnapshot;
-        this.value = this.seekFn(this.value, startValue, endValue, f);
-        return;
+      if (t <= snapshot.endTime) {
+        const prevSnapshot = this.snapshots[mod(index - 1, this.numFrames)];
+        const startTime = prevSnapshot.endTime;
+        if (t >= startTime) {
+          const duration = snapshot.endTime - startTime;
+          const f = (duration > 0 && duration < Infinity) ? ((t - startTime) / duration) : 0;
+          const {startValue} = snapshot;
+          const nextSnapshot = this.snapshots[mod(index + 1, this.numFrames)];
+          const {startValue: endValue} = nextSnapshot;
+          this.value = this.seekFn(this.value, startValue, endValue, f);
+          return;
+        }
       }
     }
     console.warn('could not seek to time', t, JSON.parse(JSON.stringify(this.snapshots)));
   }
-  snapshot(timeDiff) {
+  snapshot(timestamp, timeDiff) {
+    /* if (typeof timestamp !== 'number' || typeof timeDiff !== 'number') {
+      debugger;
+    } */
     const value = this.fn();
     // console.log('got value', value.join(','), timeDiff);
     const writeSnapshot = this.snapshots[this.snapshotWriteIndex];
+    
+    const lastWriteSnapshot = this.snapshots[mod(this.snapshotWriteIndex - 1, this.numFrames)];
+    const startTime = lastWriteSnapshot.endTime;
+    
     writeSnapshot.startValue = this.readFn(writeSnapshot.startValue, value);
-    writeSnapshot.startTime = this.writeTime;
-    writeSnapshot.endTime = this.writeTime + timeDiff;
+    // writeSnapshot.startTime = timestamp - timeDiff;
+    writeSnapshot.endTime = startTime + timeDiff;
     
     this.snapshotWriteIndex = mod(this.snapshotWriteIndex + 1, this.numFrames);
-    this.writeTime += timeDiff;
   }
   get() {
     return this.value;
@@ -145,15 +188,16 @@ export class PositionInterpolant extends SnapshotInterpolant {
   constructor(fn, timeDelay, numFrames) {
     super(fn, timeDelay, numFrames, () => new THREE.Vector3(), (target, value) => {
       target.fromArray(value);
-      if (isNaN(target.x) || isNaN(target.y) || isNaN(target.z)) {
+      /* if (isNaN(target.x) || isNaN(target.y) || isNaN(target.z)) {
         debugger;
-      }
+      } */
       return target;
     }, (target, src, dst, f) => {
       target.copy(src).lerp(dst, f);
-      if (isNaN(target.x) || isNaN(target.y) || isNaN(target.z)) {
+      // console.log('position lerp', target.toArray(), f);
+      /* if (isNaN(target.x) || isNaN(target.y) || isNaN(target.z)) {
         debugger;
-      }
+      } */
       return target;
     });
   }
@@ -165,7 +209,7 @@ export class QuaternionInterpolant extends SnapshotInterpolant {
   }
 }
 
-// allows ticking at a fixed rate regardless of frame rate
+/* // allows ticking at a fixed rate regardless of frame rate
 export class FixedTimeStep {
   constructor(fn, frameRate) {
     this.fn = fn;
@@ -179,4 +223,4 @@ export class FixedTimeStep {
       this.timeAcc -= this.maxTime;
     }
   }
-}
+} */
