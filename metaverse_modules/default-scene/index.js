86c6140558fee83d75338ad7ba95c2a6c643a00b
Avaer Kazmer <a@modules.io>
2022-03-16 21:34:04 -0400

    Default scene barrier collision set components handling

--- metaverse_modules/default-scene/index.js
+++ metaverse_modules/default-scene/index.js
@@ -6,11 +6,11 @@ const {useApp, useLocalPlayer, useProcGen, useFrame, useActivate, useLoaders, us
 // const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 
 const localVector = new THREE.Vector3();
-const localVector2 = new THREE.Vector3();
-const localVector2D = new THREE.Vector2();
+// const localVector2 = new THREE.Vector3();
+// const localVector2D = new THREE.Vector2();
 // const localBox = new THREE.Box3();
-const localLine = new THREE.Line3();
-const localMatrix = new THREE.Matrix4();
+// const localLine = new THREE.Line3();
+// const localMatrix = new THREE.Matrix4();
 
 export default e => {
   const app = useApp();
@@ -20,37 +20,49 @@ export default e => {
   // const {WebaverseShaderMaterial} = useMaterials();
   const {voxelWorldSize} = procGen;
 
-  const x = -5;
-  const y = -5;
-  const chunk = procGen.createMapChunk(undefined, x, y);
-  const {blocks, width, height} = chunk;
-  const worldWidth = width * voxelWorldSize;
-  const worldHeight = width * voxelWorldSize;
-  const exitBlocks = chunk.getExitBlocks();
-
+  const baseX = -5;
+  const baseY = -5;
+  let x = baseX;
+  let y = baseY;
   const physicsIds = [];
-
   let subApps = [];
 
-  e.waitUntil((async () => {
+  const makeComponents = (x, y) => {
+    const chunk = procGen.createMapChunk(undefined, x, y);
+    const {
+      blocks,
+      width,
+      height,
+    } = chunk;
+    const worldWidth = width * voxelWorldSize;
+    const worldHeight = width * voxelWorldSize;
+    const exitBlocks = chunk.getExitBlocks();
+    
     const coords = [x, y];
+    const dx = x - baseX;
+    const dy = y - baseY;
     const bounds = [
-      [-worldWidth/2, 0, -worldHeight/2],
-      [worldWidth/2, 16, worldHeight/2],
+      [dx*worldWidth - worldWidth/2, 0, dy*worldHeight - worldHeight/2],
+      [dx*worldWidth + worldWidth/2, 16, dy*worldHeight + worldHeight/2],
     ];
     const exits = exitBlocks.map(b => b.getLocalPosition(localVector).toArray());
+    const components = {
+      coords,
+      bounds,
+      exits,
+    };
+    return components;
+  };
 
+  e.waitUntil((async () => {
     // console.log('generate', exits);
 
+    const components = makeComponents(x, y);
     await Promise.all([
       (async () => {
         const filter = await metaversefile.createApp({
           start_url: './metaverse_modules/filter/',
-          components: {
-            coords,
-            bounds,
-            exits,
-          },
+          components,
         });
         app.add(filter);
         subApps.push(filter);
@@ -58,18 +70,30 @@ export default e => {
       (async () => {
         const barrier = await metaversefile.createApp({
           start_url: './metaverse_modules/barrier/',
-          components: {
-            coords,
-            bounds,
-            exits,
-          },
+          components,
         });
         app.add(barrier);
         subApps.push(barrier);
+
+        barrier.addEventListener('collision', e => {
+          const {direction} = e;
+          x += direction.x;
+          y += direction.z;
+          const components = makeComponents(x, y);
+          // console.log('new components', components);
+
+          for (const subApp of subApps) {
+            for (const k in components) {
+              const v = components[k];
+              subApp.setComponent(k, v);
+            }
+          }
+        });
       })(),
       (async () => {
         const infinistreet = await metaversefile.createApp({
           start_url: './metaverse_modules/infinistreet/',
+          components,
         });
         app.add(infinistreet);
         subApps.push(infinistreet);
@@ -77,11 +101,11 @@ export default e => {
     ]);
   })());
 
-  useFrame(() => {
-    /* for (const subApp of subApps) {
+  /* useFrame(() => {
+    for (const subApp of subApps) {
       
-    } */
-  });
+    }
+  }); */
   
   useCleanup(() => {
     for (const physicsId of physicsIds) {
