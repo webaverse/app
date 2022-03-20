6da2e49678d4ebe124f5ef60c97d4d93c9ab7f00
Avaer Kazmer <a@modules.io>
2022-03-20 02:59:59 -0400

    Hook in DragAndDrop.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -17,6 +17,7 @@ import { WorldObjectsList } from '../general/world-objects-list';
 import { IoHandler, registerIoEventHandler, unregisterIoEventHandler } from '../general/io-handler';
 import { ZoneTitleCard } from '../general/zone-title-card';
 import { MapGen } from '../general/map-gen/MapGen.jsx';
+import { DragAndDrop } from '../../DragAndDrop.jsx';
 import { Stats } from '../../Stats.jsx';
 import { PlayMode } from '../play-mode';
 import { EditorMode } from '../editor-mode';
@@ -155,6 +156,7 @@ export const App = () => {
                 <IoHandler />
                 <ZoneTitleCard />
                 <MapGen />
+                <DragAndDrop />
                 <Stats app={ app } />
             </AppContext.Provider>
         </div>
