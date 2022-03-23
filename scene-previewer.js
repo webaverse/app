391acf502bc4aaaefdf52bb0e875baa01324f0fc
Avaer Kazmer <a@modules.io>
2022-03-23 17:03:22 -0400

    Scene previewer use custom shader

--- scene-previewer.js
+++ scene-previewer.js
@@ -2,11 +2,61 @@ import * as THREE from 'three';
 // import metaversefile from 'metaversefile';
 // const {useApp, useFrame, useRenderer, useCamera, useMaterials, useCleanup} = metaversefile;
 import {getRenderer, camera} from './renderer.js';
+import {WebaverseShaderMaterial} from './materials.js';
 import metaversefile from 'metaversefile';
 
 const resolution = 2048;
 const worldSize = 10000;
 const near = 10;
+
+const vertexShader = `
+  varying vec3 vNormal;
+  varying vec3 vWorldPosition;
+  
+  void main() {
+    vec3 objectNormal = vec3(normal);
+    vec3 transformedNormal = objectNormal;
+    transformedNormal = normalMatrix * transformedNormal;
+    transformedNormal = -transformedNormal; // due to THREE.BackSide, FLIP_SIDED
+    vNormal = normalize(transformedNormal);
+
+    vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+		vWorldPosition = worldPosition.xyz;
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+`;
+const fragmentShader = `\
+  vec3 inverseTransformDirection( in vec3 dir, in mat4 matrix ) {
+    // dir can be either a direction vector or a normal vector
+    // upper-left 3x3 of matrix is assumed to be orthogonal
+    return normalize( ( vec4( dir, 0.0 ) * matrix ).xyz );
+  }
+
+  //
+
+  uniform samplerCube envMap;
+  varying vec3 vNormal;
+  varying vec3 vWorldPosition;
+  
+  void main() {
+    vec3 normal = normalize(vNormal);
+    const float flipEnvMap = 1.;
+    // const float refractionRatio = 0.98;
+    const float refractionRatio = 1.;
+
+    vec3 cameraToFrag;
+    cameraToFrag = normalize( vWorldPosition - cameraPosition );
+
+    // Transforming Normal Vectors with the Inverse Transformation
+    vec3 worldNormal = inverseTransformDirection( normal, viewMatrix );
+    
+    vec3 reflectVec = refract( cameraToFrag, worldNormal, refractionRatio );
+
+    vec4 envColor = textureCube( envMap, vec3( flipEnvMap * reflectVec.x, reflectVec.yz ) );
+    gl_FragColor = envColor;
+  }
+`;
 
 class ScenePreviewer {
   constructor() {
@@ -33,12 +83,25 @@ class ScenePreviewer {
     this.cubeCamera = cubeCamera;
 
     const _makeSkyboxMesh = () => {
-      // SphereGeometry( radius, widthSegments, heightSegments, phiStart, phiLength, thetaStart, thetaLength )
       const skyboxGeometry = new THREE.SphereGeometry(worldSize, 64, 32, 0, Math.PI);
-      const skyboxMaterial = new THREE.MeshBasicMaterial({
+      const skyboxMaterial = new WebaverseShaderMaterial({
+        uniforms: {
+          envMap: {
+            value: cubeRenderTarget.texture,
+            needsUpdate: true,
+          },
+        },
+        vertexShader,
+        fragmentShader,
         side: THREE.BackSide,
-        envMap: cubeRenderTarget.texture,
       });
+      /* const skyboxMaterial = new THREE.MeshBasicMaterial({
+        envMap: cubeRenderTarget.texture,
+        side: THREE.BackSide,
+      });
+      skyboxMaterial.onBeforeCompile = function() {
+        debugger;
+      }; */
       const skyboxMesh = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
       return skyboxMesh;
     };
