5f6062ad015c3de1445abd1adcdaeb8d256b5006
Avaer Kazmer <a@modules.io>
2022-03-23 00:16:51 -0400

    Use new scene previewer

--- metaverse_modules/overworld/index.js
+++ metaverse_modules/overworld/index.js
@@ -1,27 +1,9 @@
 import * as THREE from 'three';
-import {ShaderLib} from 'three/src/renderers/shaders/ShaderLib.js';
-import {UniformsUtils} from 'three/src/renderers/shaders/UniformsUtils.js';
-// import * as ThreeVrm from '@pixiv/three-vrm';
-// const {MToonMaterial} = ThreeVrm;
-// window.ThreeVrm = ThreeVrm;
-// import easing from './easing.js';
-// import {StreetGeometry} from './StreetGeometry.js';
 import metaversefile from 'metaversefile';
-const {useApp, useFrame, useRenderer, useCamera, useMaterials, useCleanup} = metaversefile;
-
-// const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
-
-// const localVector = new THREE.Vector3();
-// const localQuaternion = new THREE.Quaternion();
+const {useApp} = metaversefile;
 
 export default e => {
   const app = useApp();
-  // const physics = usePhysics();
-  // const procGen = useProcGen();
-  // const {alea, chunkWorldSize} = procGen;
-  // const renderer = useRenderer();
-  // const camera = useCamera();
-  // const {WebaverseShaderMaterial} = useMaterials();
 
   app.name = 'overworld';
 
@@ -44,6 +26,22 @@ export default e => {
           ]
         }
       ]
+    },
+    {
+      type: 'app',
+      position: [0, 0, -300],
+      quaternion: [0, 1, 0, 0],
+      start_url: "../metaverse_modules/scene-preview/",
+      components: [
+        {
+          key: "previewPosition",
+          value: [0, 0, -150]
+        },
+        {
+            key: "sceneUrl",
+            value: "./scenes/battalion.scn"
+        },
+      ],
     },
   ];
 
@@ -83,19 +81,6 @@ export default e => {
     });
     await Promise.all(promises);
   })());
-
-  //
-
-  /* const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(
-    resolution,
-    {
-      generateMipmaps: true,
-      minFilter: THREE.LinearMipmapLinearFilter,
-      // magFilter: THREE.LinearMipmapLinearFilter,
-    },
-  );
-  cubeRenderTarget.texture.mapping = THREE.CubeRefractionMapping;
-  const cubeCamera = new THREE.CubeCamera(near, camera.far, cubeRenderTarget); */
 
   return app;
 };
