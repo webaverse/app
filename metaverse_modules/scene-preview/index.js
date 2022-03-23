4cb268bce9160fbb57bbec50d87639372088b12a
Avaer Kazmer <a@modules.io>
2022-03-23 19:40:51 -0400

    Use high priority scene for scene preview skybox rendering

--- metaverse_modules/scene-preview/index.js
+++ metaverse_modules/scene-preview/index.js
@@ -1,16 +1,19 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useApp, useScenePreviewer} = metaversefile;
+const {useApp, useScenePreviewer, useInternals} = metaversefile;
 
 export default e => {
   const app = useApp();
   const {ScenePreviewer} = useScenePreviewer();
+  const {sceneHighPriority, sceneLowPriority} = useInternals();
 
   app.name = 'scene-preview';
 
   const sceneUrl = app.getComponent('sceneUrl') ?? '';
   const previewPositionArray = app.getComponent('previewPosition') ?? [0, 0, 0];
   const previewPosition = new THREE.Vector3().fromArray(previewPositionArray);
+  const previewPositionQuaternion = app.getComponent('previewQuaternion') ?? [0, 0, 0, 1];
+  const previewQuaternion = new THREE.Quaternion().fromArray(previewPositionQuaternion);
 
   const scenePreviewer = new ScenePreviewer();
   const {previewContainer, mesh} = scenePreviewer;
@@ -19,7 +22,9 @@ export default e => {
   previewContainer.matrixWorld.decompose(previewContainer.position, previewContainer.quaternion, previewContainer.scale);
 
   mesh.position.copy(previewPosition);
-  app.add(mesh);
+  mesh.quaternion.copy(previewQuaternion)
+    .premultiply(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI));
+  sceneHighPriority.add(mesh);
   mesh.updateMatrixWorld();
 
   e.waitUntil((async () => {
