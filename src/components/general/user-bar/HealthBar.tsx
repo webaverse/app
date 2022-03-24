8addf246557da0f5430858a1ed3d8ebed2a5d42c
ohmed <bohdan.horpynchuk@gmail.com>
2022-03-24 03:05:58 +0200

    UI: Updated design.

--- src/components/general/user-bar/HealthBar.tsx
+++ src/components/general/user-bar/HealthBar.tsx
@@ -7,16 +7,38 @@ import styles from './health-bar.module.css';
 
 export const HealthBar = () => {
 
-    const maxHealh = 100;
-    const currentHealth = 50;
+    const maxHealth = 100;
+    const currentHealth = 100;
+
+    const maxMana = 100;
+    const currentMana = 30;
+
+    const maxXP = 100;
+    const currentXP = 70;
+
+    //
 
     return (
         <div className={ styles.heathBarWrapper } >
+
             <div className={ styles.healthBarTitle } >HP</div>
+            {/* <div className={ styles.healthBarValue } >{ currentHealth } / { maxHealth }</div> */}
             <div className={ styles.healthProgressWrapper } >
-                <div className={ styles.progress } style={{ width: Math.floor( 100 * currentHealth / maxHealh ) + '%' }} />
+                <div className={ styles.progress } style={{ width: Math.floor( 100 * currentHealth / maxHealth ) + '%' }} />
             </div>
-            <div className={ styles.healthValueText } >{ currentHealth } / { maxHealh }</div>
+
+            <div className={ styles.manaBarTitle } >MN</div>
+            {/* <div className={ styles.manaBarValue } >{ currentMana } / { maxMana }</div> */}
+            <div className={ styles.manaProgressWrapper } >
+                <div className={ styles.progress } style={{ width: Math.floor( 100 * currentMana / maxMana ) + '%' }} />
+            </div>
+
+            <div className={ styles.xpBarTitle } >XP</div>
+            {/* <div className={ styles.xpBarValue } >{ currentXP } / { maxXP }</div> */}
+            <div className={ styles.xpProgressWrapper } >
+                <div className={ styles.progress } style={{ width: Math.floor( 100 * currentXP / maxXP ) + '%' }} />
+            </div>
+
         </div>
     );
 
