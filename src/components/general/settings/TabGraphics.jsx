8c3a7507bbc058219c12c5effd74a54ce3622e8b
0reo <oreo@halfbakedideals.com>
2022-03-24 15:04:29 -0400

    eslint

--- src/components/general/settings/TabGraphics.jsx
+++ src/components/general/settings/TabGraphics.jsx
@@ -1,85 +1,81 @@
 
-import React, { useEffect, useState } from 'react';
-import { getGPUTier } from 'detect-gpu';
+import React, {useEffect, useState} from 'react';
+import {getGPUTier} from 'detect-gpu';
 import classNames from 'classnames';
 
 import game from '../../../../game.js';
-import metaversefileApi from '../../../../metaversefile-api'
-import { Switch } from './switch';
+import metaversefileApi from '../../../../metaversefile-api';
+import {Switch} from './switch';
 
 import styles from './settings.module.css';
 
 // defaults get modified depending on the users detected GPU tier
 const DefaultSettings = {
-    resolution:         'HIGH',
-    antialias:          'NONE',
-    viewRange:          'HIGH',
-    shadowQuality:      'HIGH',
-    postprocessing: {
-        enabled:        'ON',
-        depthOfField:   'ON',
-        hdr:            'ON',
-        bloom:          'ON'
-    },
-    character: {
-        details:        'HIGH',
-        hairPhysics:    'ON'
-    }
+  resolution: 'HIGH',
+  antialias: 'NONE',
+  viewRange: 'HIGH',
+  shadowQuality: 'HIGH',
+  postprocessing: {
+    enabled: 'ON',
+    depthOfField: 'ON',
+    hdr: 'ON',
+    bloom: 'ON',
+  },
+  character: {
+    details: 'HIGH',
+    hairPhysics: 'ON',
+  },
 };
 
-export const TabGraphics = ({ active }) => {
+export const TabGraphics = ({active}) => {
+  const [appyingChanges, setAppyingChanges] = useState(false);
+  const [changesNotSaved, setChangesNotSaved] = useState(false);
+  const [settingsLoaded, setSettingsLoaded] = useState(false);
 
-    const [ appyingChanges, setAppyingChanges ] = useState( false );
-    const [ changesNotSaved, setChangesNotSaved ] = useState( false );
-    const [ settingsLoaded, setSettingsLoaded ] = useState( false );
+  const [resolution, setResolution] = useState(null);
+  const [antialias, setAntialias] = useState(null);
+  const [viewRange, setViewRange] = useState(null);
+  const [shadowQuality, setShadowQuality] = useState(null);
+  const [postprocessing, setPostprocessing] = useState(null);
+  const [depthOfField, setDepthOfField] = useState(null);
+  const [hdr, setHdr] = useState(null);
+  const [bloom, setBloom] = useState(null);
+  const [characterDetails, setCharacterDetails] = useState(null);
+  const [hairPhysics, setHairPhysics] = useState(null);
 
-    const [ resolution, setResolution ] = useState( null );
-    const [ antialias, setAntialias ] = useState( null );
-    const [ viewRange, setViewRange ] = useState( null );
-    const [ shadowQuality, setShadowQuality ] = useState( null );
-    const [ postprocessing, setPostprocessing ] = useState( null );
-    const [ depthOfField, setDepthOfField ] = useState( null );
-    const [ hdr, setHdr ] = useState( null );
-    const [ bloom, setBloom ] = useState( null );
-    const [ characterDetails, setCharacterDetails ] = useState( null );
-    const [ hairPhysics, setHairPhysics ] = useState( null );
+  //
 
-    //
+  const detailsMap = {
+    ULTRA: 4,
+    HIGH: 3,
+    MEDIUM: 2,
+    LOW: 1,
+  };
 
-    const detailsMap = {
-        ULTRA: 4,
-        HIGH: 3,
-        MEDIUM: 2,
-        LOW: 1,
+  function saveSettings() {
+    const settings = {
+      resolution: resolution,
+      antialias: antialias,
+      viewRange: viewRange,
+      shadowQuality: shadowQuality,
+      postprocessing: {
+        enabled: postprocessing,
+        depthOfField: depthOfField,
+        hdr: hdr,
+        bloom: bloom,
+      },
+      character: {
+        details: characterDetails,
+        hairPhysics: hairPhysics,
+      },
     };
 
-    function saveSettings () {
+    localStorage.setItem('GfxSettings', JSON.stringify(settings));
+  }
 
-        const settings = {
-            resolution:         resolution,
-            antialias:          antialias,
-            viewRange:          viewRange,
-            shadowQuality:      shadowQuality,
-            postprocessing: {
-                enabled:        postprocessing,
-                depthOfField:   depthOfField,
-                hdr:            hdr,
-                bloom:          bloom
-            },
-            character: {
-                details:        characterDetails,
-                hairPhysics:    hairPhysics
-            }
-        };
-
-        localStorage.setItem( 'GfxSettings', JSON.stringify( settings ) );
-
-    };
-
-    function loadSettings () {
-
-        const settingsString = localStorage.getItem( 'GfxSettings' );
-        let settings;
+  function loadSettings() {
+    const settingsString = localStorage.getItem('GfxSettings');
+    let settings;
 
     async function setDefault() {
       const gpuTier = await getGPUTier();
@@ -101,168 +97,141 @@ export const TabGraphics = ({ active }) => {
       console.log('set default character details to', DefaultSettings.character.details);
     }
     (async () => { await setDefault(); })();
-        
 
-        try {
+    try {
+      settings = JSON.parse(settingsString);
+    } catch (err) {
+      settings = DefaultSettings;
+    }
 
-            settings = JSON.parse( settingsString );
-            
-        } catch ( err ) {
-            
-            settings = DefaultSettings;
+    settings = settings ?? DefaultSettings;
 
-        }
+    setResolution(settings.resolution ?? DefaultSettings.resolution);
+    setAntialias(settings.antialias ?? DefaultSettings.antialias);
+    setViewRange(settings.viewRange ?? DefaultSettings.viewRange);
+    setShadowQuality(settings.shadowQuality ?? DefaultSettings.shadowQuality);
+    setPostprocessing(settings.postprocessing.enabled ?? DefaultSettings.postprocessing.enabled);
+    setDepthOfField(settings.postprocessing.depthOfField ?? DefaultSettings.postprocessing.depthOfField);
+    setHdr(settings.postprocessing.hdr ?? DefaultSettings.postprocessing.hdr);
+    setBloom(settings.postprocessing.bloom ?? DefaultSettings.postprocessing.bloom);
+    setCharacterDetails(settings.character.details ?? DefaultSettings.character.details);
+    setHairPhysics(settings.character.hairPhysics ?? DefaultSettings.character.hairPhysics);
+  }
 
-        settings = settings ?? DefaultSettings;
+  function applySettings() {
+    // set avatar style
 
-        setResolution( settings.resolution ?? DefaultSettings.resolution );
-        setAntialias( settings.antialias ?? DefaultSettings.antialias );
-        setViewRange( settings.viewRange ?? DefaultSettings.viewRange );
-        setShadowQuality( settings.shadowQuality ?? DefaultSettings.shadowQuality );
-        setPostprocessing( settings.postprocessing.enabled ?? DefaultSettings.postprocessing.enabled );
-        setDepthOfField( settings.postprocessing.depthOfField ?? DefaultSettings.postprocessing.depthOfField );
-        setHdr( settings.postprocessing.hdr ?? DefaultSettings.postprocessing.hdr );
-        setBloom( settings.postprocessing.bloom ?? DefaultSettings.postprocessing.bloom );
-        setCharacterDetails( settings.character.details ?? DefaultSettings.character.details );
-        setHairPhysics( settings.character.hairPhysics ?? DefaultSettings.character.hairPhysics );
+    const avatarStyle = detailsMap[characterDetails];
 
-    };
+    const localPlayer = metaversefileApi.useLocalPlayer();
 
-    function applySettings () {
+    function setAvatarQuality() {
+      game.setAvatarQuality(avatarStyle);
+      localPlayer.removeEventListener('avatarchange', setAvatarQuality);
+    }
 
-        // set avatar style
-
-        const avatarStyle = detailsMap[characterDetails];
-
-        const localPlayer = metaversefileApi.useLocalPlayer();
-
-        function setAvatarQuality () {
-
-            game.setAvatarQuality( avatarStyle );
-            localPlayer.removeEventListener( 'avatarchange', setAvatarQuality );
-
-        };
-
-        if ( ! localPlayer.avatar ) {
-
-            localPlayer.addEventListener( 'avatarchange', setAvatarQuality );
-
-        } else {
-
-            setAvatarQuality();
-
-        }
-
-        //
-
-        saveSettings();
-        setChangesNotSaved( false );
-        setTimeout( () => { setAppyingChanges( false ) }, 1000 );
-
-    };
-
-    function handleApplySettingsBtnClick () {
-
-        setAppyingChanges( true );
-        setTimeout( applySettings, 100 );
-
-    };
+    if (!localPlayer.avatar) {
+      localPlayer.addEventListener('avatarchange', setAvatarQuality);
+    } else {
+      setAvatarQuality();
+    }
 
     //
 
-    useEffect( () => {
+    saveSettings();
+    setChangesNotSaved(false);
+    setTimeout(() => { setAppyingChanges(false); }, 1000);
+  }
 
-        if ( resolution && antialias && viewRange && shadowQuality && postprocessing && depthOfField && hdr && bloom && characterDetails && hairPhysics ) {
+  function handleApplySettingsBtnClick() {
+    setAppyingChanges(true);
+    setTimeout(applySettings, 100);
+  }
 
-            if ( settingsLoaded ) {
+  //
 
-                setChangesNotSaved( true );
+  useEffect(() => {
+    if (resolution && antialias && viewRange && shadowQuality && postprocessing && depthOfField && hdr && bloom && characterDetails && hairPhysics) {
+      if (settingsLoaded) {
+        setChangesNotSaved(true);
+      } else {
+        setSettingsLoaded(true);
+        applySettings();
+      }
+    }
+  }, [resolution, antialias, viewRange, shadowQuality, postprocessing, depthOfField, hdr, bloom, characterDetails, hairPhysics]);
 
-            } else {
+  useEffect(() => {
+    loadSettings();
+  }, []);
 
-                setSettingsLoaded( true );
-                applySettings();
+  //
 
-            }
+  return (
+    <div className={ classNames(styles.tabContent, active ? styles.active : null) }>
+      <div className={ styles.blockTitle }>Display</div>
+      <div className={ styles.row }>
+        <div className={ styles.paramName }>Resolution</div>
+        <Switch className={ styles.switch } value={ resolution } setValue={ setResolution } values={ ['LOW', 'MEDIUM', 'HIGH'] } />
+        <div className={ styles.clearfix } />
+      </div>
+      <div className={ styles.row }>
+        <div className={ styles.paramName }>Antialias</div>
+        <Switch className={ styles.switch } value={ antialias } setValue={ setAntialias } values={ ['MSAA', 'FXAA', 'NONE'] } />
+        <div className={ styles.clearfix } />
+      </div>
+      <div className={ styles.row }>
+        <div className={ styles.paramName }>View range</div>
+        <Switch className={ styles.switch } value={ viewRange } setValue={ setViewRange } values={ ['LOW', 'MEDIUM', 'HIGH'] } />
+        <div className={ styles.clearfix } />
+      </div>
+      <div className={ styles.row }>
+        <div className={ styles.paramName }>Shadows quality</div>
+        <Switch className={ styles.switch } value={ shadowQuality } setValue={ setShadowQuality } values={ ['OFF', 'LOW', 'MEDIUM', 'HIGH'] } />
+        <div className={ styles.clearfix } />
+      </div>
+      <div className={ styles.row }>
+        <div className={ styles.blockTitle }>Postprocessing</div>
+        <div className={ styles.clearfix } />
+      </div>
+      <div className={ styles.row }>
+        <div className={ styles.paramName }>Enabled</div>
+        <Switch className={ styles.switch } value={ postprocessing } setValue={ setPostprocessing } values={ ['OFF', 'ON'] } />
+        <div className={ styles.clearfix } />
+      </div>
+      <div className={ styles.row }>
+        <div className={ styles.paramName }>Depth of field</div>
+        <Switch className={ styles.switch } value={ depthOfField } setValue={ setDepthOfField } values={ ['OFF', 'ON'] } />
+        <div className={ styles.clearfix } />
+      </div>
+      <div className={ styles.row }>
+        <div className={ styles.paramName }>HDR</div>
+        <Switch className={ styles.switch } value={ hdr } setValue={ setHdr } values={ ['OFF', 'ON'] } />
+        <div className={ styles.clearfix } />
+      </div>
+      <div className={ styles.row }>
+        <div className={ styles.paramName }>Bloom</div>
+        <Switch className={ styles.switch } value={ bloom } setValue={ setBloom } values={ ['OFF', 'ON'] } />
+        <div className={ styles.clearfix } />
+      </div>
+      <div className={ styles.row }>
+        <div className={ styles.blockTitle }>Character</div>
+        <div className={ styles.clearfix } />
+      </div>
+      <div className={ styles.row }>
+        <div className={ styles.paramName }>Character details</div>
+        <Switch className={ styles.switch } value={ characterDetails } setValue={ setCharacterDetails } values={ ['LOW', 'MEDIUM', 'HIGH', 'ULTRA'] } />
+        <div className={ styles.clearfix } />
+      </div>
+      <div className={ styles.row }>
+        <div className={ styles.paramName }>Hair physics</div>
+        <Switch className={ styles.switch } value={ hairPhysics } setValue={ setHairPhysics } values={ ['OFF', 'ON'] } />
+        <div className={ styles.clearfix } />
+      </div>
 
-        }
-
-    }, [ resolution, antialias, viewRange, shadowQuality, postprocessing, depthOfField, hdr, bloom, characterDetails, hairPhysics ] );
-
-    useEffect( () => {
-
-        loadSettings();
-
-    }, [] );
-
-    //
-
-    return (
-        <div className={ classNames( styles.tabContent, active ? styles.active : null ) }>
-            <div className={ styles.blockTitle }>Display</div>
-            <div className={ styles.row }>
-                <div className={ styles.paramName }>Resolution</div>
-                <Switch className={ styles.switch } value={ resolution } setValue={ setResolution } values={ [ 'LOW', 'MEDIUM', 'HIGH' ] } />
-                <div className={ styles.clearfix } />
-            </div>
-            <div className={ styles.row }>
-                <div className={ styles.paramName }>Antialias</div>
-                <Switch className={ styles.switch } value={ antialias } setValue={ setAntialias } values={ [ 'MSAA', 'FXAA', 'NONE' ] } />
-                <div className={ styles.clearfix } />
-            </div>
-            <div className={ styles.row }>
-                <div className={ styles.paramName }>View range</div>
-                <Switch className={ styles.switch } value={ viewRange } setValue={ setViewRange } values={ [ 'LOW', 'MEDIUM', 'HIGH' ] } />
-                <div className={ styles.clearfix } />
-            </div>
-            <div className={ styles.row }>
-                <div className={ styles.paramName }>Shadows quality</div>
-                <Switch className={ styles.switch } value={ shadowQuality } setValue={ setShadowQuality } values={ [ 'OFF', 'LOW', 'MEDIUM', 'HIGH' ] } />
-                <div className={ styles.clearfix } />
-            </div>
-            <div className={ styles.row }>
-                <div className={ styles.blockTitle }>Postprocessing</div>
-                <div className={ styles.clearfix } />
-            </div>
-            <div className={ styles.row }>
-                <div className={ styles.paramName }>Enabled</div>
-                <Switch className={ styles.switch } value={ postprocessing } setValue={ setPostprocessing } values={ [ 'OFF', 'ON' ] } />
-                <div className={ styles.clearfix } />
-            </div>
-            <div className={ styles.row }>
-                <div className={ styles.paramName }>Depth of field</div>
-                <Switch className={ styles.switch } value={ depthOfField } setValue={ setDepthOfField } values={ [ 'OFF', 'ON' ] } />
-                <div className={ styles.clearfix } />
-            </div>
-            <div className={ styles.row }>
-                <div className={ styles.paramName }>HDR</div>
-                <Switch className={ styles.switch } value={ hdr } setValue={ setHdr } values={ [ 'OFF', 'ON' ] } />
-                <div className={ styles.clearfix } />
-            </div>
-            <div className={ styles.row }>
-                <div className={ styles.paramName }>Bloom</div>
-                <Switch className={ styles.switch } value={ bloom } setValue={ setBloom } values={ [ 'OFF', 'ON' ] } />
-                <div className={ styles.clearfix } />
-            </div>
-            <div className={ styles.row }>
-                <div className={ styles.blockTitle }>Character</div>
-                <div className={ styles.clearfix } />
-            </div>
-            <div className={ styles.row }>
-                <div className={ styles.paramName }>Character details</div>
-                <Switch className={ styles.switch } value={ characterDetails } setValue={ setCharacterDetails } values={ [ 'LOW', 'MEDIUM', 'HIGH', 'ULTRA' ] } />
-                <div className={ styles.clearfix } />
-            </div>
-            <div className={ styles.row }>
-                <div className={ styles.paramName }>Hair physics</div>
-                <Switch className={ styles.switch } value={ hairPhysics } setValue={ setHairPhysics } values={ [ 'OFF', 'ON' ] } />
-                <div className={ styles.clearfix } />
-            </div>
-
-            <div className={ classNames( styles.applyBtn, changesNotSaved ? styles.active : null ) } onClick={ handleApplySettingsBtnClick } >
-                { appyingChanges ? 'APPLYING' : 'APPLY' }
-            </div>
-        </div>
-    );
-
+      <div className={ classNames(styles.applyBtn, changesNotSaved ? styles.active : null) } onClick={ handleApplySettingsBtnClick } >
+        { appyingChanges ? 'APPLYING' : 'APPLY' }
+      </div>
+    </div>
+  );
 };
