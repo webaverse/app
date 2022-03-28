f8f09c0e2c29aa2faa5a78998899a302e0154f00
Avaer Kazmer <a@modules.io>
2022-03-27 22:16:09 -0400

    Import HotBox from its own component

--- src/components/play-mode/hotbar/Hotbar.jsx
+++ src/components/play-mode/hotbar/Hotbar.jsx
@@ -1,59 +1,7 @@
 import React, {useState, useRef, useEffect} from 'react';
-import classnames from 'classnames';
 import styles from './hotbar.module.css';
-// import metaversefileApi from 'metaversefile';
-import loadoutManager from '../../../../loadout-manager.js';
+import {HotBox} from '../hotbox/HotBox.jsx';
 import {hotbarSize} from '../../../../constants.js';
-
-const HotbarItem = props => {
-    const canvasRef = useRef();
-    const [selected, setSelected] = useState(false);
-    
-    useEffect(() => {
-      if (canvasRef.current) {
-        const canvas = canvasRef.current;
-
-        const hotbarRenderer = loadoutManager.getHotbarRenderer(props.index);
-        hotbarRenderer.addCanvas(canvas);
-
-        return () => {
-          hotbarRenderer.removeCanvas(canvas);
-        };
-      }
-    }, [canvasRef]);
-    useEffect(() => {
-      function selectedchange(e) {
-        const {index, app} = e.data;
-        if (index === -1 || app) {
-          setSelected(index === props.index);
-        }
-      }
-
-      loadoutManager.addEventListener('selectedchange', selectedchange);
-
-      return () => {
-        loadoutManager.removeEventListener('selectedchange', selectedchange);
-      };
-    }, []);
-    
-    const pixelRatio = window.devicePixelRatio;
-
-    return (
-      <div className={ classnames(styles.item, selected ? styles.selected : null) } >
-        <div className={ styles.box } />
-        <div className={ styles.label }>
-          <div className={ styles.background } />
-          <div className={ styles.text }>{ props.index + 1 }</div>
-        </div>
-        <canvas
-          className={ styles.hotbox }
-          width={props.size * pixelRatio}
-          height={props.size * pixelRatio}
-          ref={canvasRef}
-        />
-      </div>
-    );
-};
 
 export const Hotbar = () => {
 
@@ -70,7 +18,7 @@ export const Hotbar = () => {
                     for ( let i = 0; i < itemsNum; i ++ ) {
 
                         items[ i ] = (
-                            <HotbarItem size={hotbarSize} index={i} key={i} />
+                            <HotBox size={hotbarSize} index={i} key={i} />
                         );
 
                     }
