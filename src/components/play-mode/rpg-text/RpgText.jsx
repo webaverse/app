65018d5073f0461bebdd4bba5e6d12f642472d7b
ohmed <bohdan.horpynchuk@gmail.com>
2022-03-30 02:07:03 +0300

    UI: Refactor part 3.

---
+++ src/components/play-mode/rpg-text/RpgText.jsx
@@ -0,0 +1,43 @@
+
+import React, { useState, useEffect } from 'react';
+import classnames from 'classnames';
+
+//
+
+export const RpgText = ({ className, styles, text, textSpeed = chatTextSpeed }) => {
+
+    const [ progressText, setProgressText ] = useState('');
+
+    //
+
+    useEffect( () => {
+
+        if ( text.length === 0 ) {
+
+            setProgressText('');
+
+        } else if ( progressText.length < text.length ) {
+
+            const timeout = setTimeout(() => {
+
+                // XXX this text slicing should be done with a mathematical factor in the hups code
+                const newProgressText = progressText + text.charAt( progressText.length );
+                setProgressText( newProgressText );
+
+            }, textSpeed );
+
+            return () => { clearTimeout( timeout ); };
+
+        }
+
+    }, [ text, progressText ] );
+
+    //
+
+    return (
+        <div className={ classnames( className, text.length > 0 ? styles.open : null ) } >
+            { progressText }
+        </div>
+    );
+
+};
