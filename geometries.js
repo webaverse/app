5e8a3540d38f06a5695c985b75e91924ccac5fd9
Avaer Kazmer <a@modules.io>
2022-03-30 12:17:54 -0400

    Expose street geometry

--- geometries.js
+++ geometries.js
@@ -1,9 +1,19 @@
 import {CapsuleGeometry} from './CapsuleGeometry.js';
 import {DoubleSidedPlaneGeometry} from './DoubleSidedPlaneGeometry.js';
 import {CameraGeometry} from './CameraGeometry.js';
+import {
+  StreetGeometry,
+  StreetFlatGeometry,
+  StreetOctagonGeometry,
+  StreetHalfpipeGeometry,
+} from './StreetGeometry.js';
 
 export {
   CapsuleGeometry,
   DoubleSidedPlaneGeometry,
   CameraGeometry,
+  StreetGeometry,
+  StreetFlatGeometry,
+  StreetOctagonGeometry,
+  StreetHalfpipeGeometry,
 };
