2c9842f6e8752a25bc4c611991209e9abf5cd225
Avaer Kazmer <a@modules.io>
2022-03-30 23:37:56 -0400

    Clean up scene screenshotter api

--- scene-screenshotter.js
+++ scene-screenshotter.js
@@ -1,21 +1,18 @@
 import * as THREE from 'three';
 import {getRenderer} from './renderer.js';
-import {minFov} from './constants.js';
+// import {minFov} from './constants.js';
 // import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 
-const localVector = new THREE.Vector3();
-const localVector2 = new THREE.Vector3();
+// const localVector = new THREE.Vector3();
+// const localVector2 = new THREE.Vector3();
 // const localVector3 = new THREE.Vector3();
 // const localVector4 = new THREE.Vector3();
 const localVector4D = new THREE.Vector4();
 // const localTriangle = new THREE.Triangle();
+const localColor = new THREE.Color();
 
-/* const cameraNear = 0;
-const cameraFar = 1000;
-const cameraHeight = 30; */
-
-export async function screenshotScene(scene, position, quaternion, worldResolution) {
-  const colorRenderTarget = new THREE.WebGLRenderTarget(
+export function screenshotScene(scene, position, quaternion, worldResolution, ctx) {
+  /* const colorRenderTarget = new THREE.WebGLRenderTarget(
     worldResolution,
     worldResolution,
     {
@@ -23,27 +20,44 @@ export async function screenshotScene(scene, position, quaternion, worldResoluti
       format: THREE.RGBAFormat,
       // encoding: THREE.sRGBEncoding,
     }
-  );
-  const aspect = 1;
-  const camera = new THREE.PerspectiveCamera(minFov, aspect, 0.1, 30000);
+  ); */
+  const camera = new THREE.PerspectiveCamera(90, 1, 0.1, 3000);
   camera.position.copy(position);
   camera.quaternion.copy(quaternion);
   camera.updateMatrixWorld();
+  // window.camera = camera;
+  // window.scene = scene;
   
   const renderer = getRenderer();
       
   // push old state
-  const oldRenderTarget = renderer.getRenderTarget();
+  // const oldRenderTarget = renderer.getRenderTarget();
   const oldViewport = renderer.getViewport(localVector4D);
+  const oldClearColor = renderer.getClearColor(localColor);
+  const oldClearAlpha = renderer.getClearAlpha();
 
   // render
-  renderer.setRenderTarget(colorRenderTarget);
+  // renderer.setRenderTarget(colorRenderTarget);
   renderer.setViewport(0, 0, worldResolution, worldResolution);
+  renderer.setClearColor(0x0000FF, 1);
+  renderer.clear();
   renderer.render(scene, camera);
+  // const p = createImageBitmap(renderer.domElement, 0, 0, worldResolution, worldResolution);
+
+  // for (const ctx of contexts) {
+    ctx.drawImage(
+      renderer.domElement,
+      0, renderer.domElement.height - worldResolution,
+      worldResolution, worldResolution,
+      0, 0,
+      worldResolution, worldResolution
+    );
+  // }
 
   // pop old state
-  renderer.setRenderTarget(oldRenderTarget);
+  // renderer.setRenderTarget(oldRenderTarget);
   renderer.setViewport(oldViewport);
+  renderer.setClearColor(oldClearColor, oldClearAlpha);
 
-  return renderTarget.texture;
+  // return p;
 }
