675aa4f3d2b2a44ab93c6094a1d00a38b567b4aa
Avaer Kazmer <a@modules.io>
2022-04-01 07:25:55 -0400

    Hook in mob metaverse component

--- metaverse-components.js
+++ metaverse-components.js
@@ -1,9 +1,11 @@
 import wear from './metaverse_components/wear.js';
 import drop from './metaverse_components/drop.js';
+import mob from './metaverse_components/mob.js';
 
 const componentTemplates = {
   wear,
   drop,
+  mob,
 };
 export {
   componentTemplates,
