fccdc3364ae4990edb565fa1aec03270b71cba26
Avaer Kazmer <a@modules.io>
2022-04-01 10:17:04 -0400

    Mob component dead code cleanup

--- metaverse_components/mob.js
+++ metaverse_components/mob.js
@@ -1,16 +1,4 @@
-// import * as THREE from 'three';
-// import metaversefile from 'metaversefile';
-// import Avatar from '../avatars/avatars.js';
-// import {world} from '../world.js';
-// import physicsManager from '../physics-manager.js';
-// import {glowMaterial} from '../shaders.js';
-// import easing from '../easing.js';
 import mobManager from '../mob-manager.js';
-// import {rarityColors} from '../constants.js';
-
-// const localVector = new THREE.Vector3();
-// const localVector2 = new THREE.Vector3();
-// const localQuaternion = new THREE.Quaternion();
 
 export default (app, component) => {
   mobManager.addMob(app);
