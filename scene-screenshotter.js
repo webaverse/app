951af478afbf04b7043e4e6c4b3e704909626137
Avaer Kazmer <a@modules.io>
2022-04-04 09:19:51 -0400

    Scene screenshotter rewrite to new API

--- scene-screenshotter.js
+++ scene-screenshotter.js
@@ -1,33 +1,10 @@
 import * as THREE from 'three';
 import {getRenderer} from './renderer.js';
-// import {minFov} from './constants.js';
-// import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 
-// const localVector = new THREE.Vector3();
-// const localVector2 = new THREE.Vector3();
-// const localVector3 = new THREE.Vector3();
-// const localVector4 = new THREE.Vector3();
 const localVector4D = new THREE.Vector4();
-// const localTriangle = new THREE.Triangle();
 const localColor = new THREE.Color();
 
-export function screenshotScene(scene, position, quaternion, worldResolution, ctx) {
-  /* const colorRenderTarget = new THREE.WebGLRenderTarget(
-    worldResolution,
-    worldResolution,
-    {
-      type: THREE.UnsignedByteType,
-      format: THREE.RGBAFormat,
-      // encoding: THREE.sRGBEncoding,
-    }
-  ); */
-  const camera = new THREE.PerspectiveCamera(90, 1, 0.1, 3000);
-  camera.position.copy(position);
-  camera.quaternion.copy(quaternion);
-  camera.updateMatrixWorld();
-  // window.camera = camera;
-  // window.scene = scene;
-  
+export function screenshotScene(scene, camera, width, height, ctx) {
   const renderer = getRenderer();
       
   // push old state
@@ -35,22 +12,27 @@ export function screenshotScene(scene, position, quaternion, worldResolution, ct
   const oldViewport = renderer.getViewport(localVector4D);
   const oldClearColor = renderer.getClearColor(localColor);
   const oldClearAlpha = renderer.getClearAlpha();
+  const pixelRatio = renderer.getPixelRatio();
 
   // render
   // renderer.setRenderTarget(colorRenderTarget);
-  renderer.setViewport(0, 0, worldResolution, worldResolution);
-  renderer.setClearColor(0x0000FF, 1);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+
+  renderer.setViewport(0, 0, width, height);
+  renderer.setClearColor(0x000000, 0);
   renderer.clear();
   renderer.render(scene, camera);
   // const p = createImageBitmap(renderer.domElement, 0, 0, worldResolution, worldResolution);
 
   // for (const ctx of contexts) {
+    ctx.clearRect(0, 0, width, height);
     ctx.drawImage(
       renderer.domElement,
-      0, renderer.domElement.height - worldResolution,
-      worldResolution, worldResolution,
+      0, renderer.domElement.height - height * pixelRatio,
+      width * pixelRatio, height * pixelRatio,
       0, 0,
-      worldResolution, worldResolution
+      width, height
     );
   // }
 
