1bd94130bb47ddeb45750de38e98b2244bbe94e7
gonnavis <gonnavis@163.com>
2022-04-06 21:08:32 +0800

    temp

--- avatars/avatars.js
+++ avatars/avatars.js
@@ -1514,6 +1514,7 @@ class Avatar {
       {
         actions: {
           entryCombo: () => {
+            this.fadeToAction('combo');
             game.startUse();
             this.actionTime = 0;
             setTimeout(() => {
