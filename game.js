9a5272850a96fcc5f9d85a13dc2fbd8fd5224ebc
Avaer Kazmer <a@modules.io>
2022-04-10 19:36:54 -0400

    Hook in renderCard hack to game.js

--- game.js
+++ game.js
@@ -28,6 +28,7 @@ import metaversefileApi from './metaversefile-api.js';
 import * as metaverseModules from './metaverse-modules.js';
 import loadoutManager from './loadout-manager.js';
 // import soundManager from './sound-manager.js';
+import {generateObjectUrlCard} from './card-generator.js';
 
 // const {contractNames} = metaversefileConstants;
 
@@ -1683,6 +1684,21 @@ class GameManager extends EventTarget {
   update = _gameUpdate;
   pushAppUpdates = _pushAppUpdates;
   pushPlayerUpdates = _pushPlayerUpdates;
+  async renderCard(object) { // HACK: this should be moved to a UI component
+    const start_url = object?.start_url;
+    if (start_url) {
+      console.log('render card 1', start_url);
+      const cardImg = await generateObjectUrlCard({
+        start_url,
+      });
+      console.log('render card 2', start_url, cardImg);
+      // const stats = procgen.generateStats();
+      /* console.log('render start url', {
+        start_url,
+        stats,
+      }); */
+    }
+  }
 }
 const gameManager = new GameManager();
 export default gameManager;
