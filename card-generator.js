6d046a13c9cb88cd3cf6977942cdb215236edebf
Avaer Kazmer <a@modules.io>
2022-04-14 01:13:33 -0400

    Card generator make gradient transparent

--- card-generator.js
+++ card-generator.js
@@ -227,8 +227,6 @@ export const generateCard = async ({
 
   const cardHeight = cardWidth / 2.5 * 3.5;
 
-  // console.log('card procgen', {name, description});
-
   const svg = document.createElement('svg');
   svg.setAttribute('xmlns', `http://www.w3.org/2000/svg`);
   svg.setAttribute('width', cardWidth);
@@ -342,7 +340,7 @@ export const generateCard = async ({
       const linearGradientName = 'linear-gradient-120';
       const stopEls = el.querySelectorAll(`#${linearGradientName} > stop`);
       // const c = `stop-color:${spec.art.colors[0]}`;
-      stopEls[1].style.cssText = `stop-color:${spec.art.colors[0]}`;
+      stopEls[1].style.cssText = `stop-color:${spec.art.colors[0]}80`;
       stopEls[3].style.cssText = `stop-color:${spec.art.colors[1]}`;
     }
   }
