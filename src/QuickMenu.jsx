e58b2fc6d76b316a2f2d723a53bef683ea6fcc01
Avaer Kazmer <a@modules.io>
2022-04-16 16:53:34 -0400

    More major quick menu work

--- src/QuickMenu.jsx
+++ src/QuickMenu.jsx
@@ -17,15 +17,18 @@ const localVector2D = new THREE.Vector2();
 //
 
 const options = [
-  'weapon-1',
-  'weapon-2',
-  'weapon-3',
-  'weapon-4',
-  'weapon-5',
-  'weapon-6',
+  'alert',
+  'angry',
+  'embarrassed',
+  'headNod',
+  'headShake',
+  'sad',
+  'surprise',
+  'victory',
 ];
+// const optionsSoft = options.map(o => o.name + 'Soft');
 
-const size = 300;
+const size = 400;
 const pixelRatio = window.devicePixelRatio;
 const pixelSize = size * pixelRatio;
 
@@ -38,9 +41,18 @@ const centerCoords = [
   size / 2 - 15,
 ];
 
+const outerRadius = pixelSize/2;
+const innerRadius = pixelSize/4;
+const outerRadiusSoft = pixelSize/4 - 10;
+const innerRadiusSoft = pixelSize/8;
+
+// const selectRadius = 30;
+
 export default function QuickMenu() {
   const [open, setOpen] = useState(false);
+  const [down, setDown] = useState(false);
   const [selectedSlice, setSelectedSlice] = useState(-1);
+  const [selectedDepth, setSelectedDepth] = useState(-1);
   const [coords, setCoords] = useState([0, 0]);
   const canvasRef = useRef();
 
@@ -49,6 +61,7 @@ export default function QuickMenu() {
       function keydown(e) {
         if (e.keyCode === 81) { // Q
           setOpen(true);
+          setDown(false);
           setCoords([0, 0]);
         }
       }
@@ -77,9 +90,24 @@ export default function QuickMenu() {
         return false;
       }
       registerIoEventHandler('mousemove', mousemove);
+      
+      function mousedown(e) {
+        setDown(true);
+        return false;
+      }
+      registerIoEventHandler('mousedown', mousedown);
+      
+      function mouseup(e) {
+        setDown(false);
+        return false;
+      }
+      registerIoEventHandler('mouseup', mouseup);
+      
       return () => {
         unregisterIoEventHandler('keyup', keyup);
         unregisterIoEventHandler('mousemove', mousemove);
+        unregisterIoEventHandler('mousedown', mousedown);
+        unregisterIoEventHandler('mouseup', mouseup);
       };
     }
   }, [open, coords]);
@@ -92,37 +120,58 @@ export default function QuickMenu() {
       ctx.clearRect(0, 0, pixelSize, pixelSize);
     
       for (let i = 0; i < numSlices; i++) {
-        ctx.fillStyle = i === selectedSlice ? '#4fc3f7' : '#111';
-        ctx.beginPath();
         const startAngle = i*sliceSize + interval - Math.PI/2;
         const endAngle = (i+1)*sliceSize - interval - Math.PI/2;
-        ctx.arc(pixelSize/2, pixelSize/2, pixelSize/2, startAngle, endAngle, false);
-        ctx.arc(pixelSize/2, pixelSize/2, pixelSize/4, endAngle, startAngle, true);
-        ctx.fill();
-
-        ctx.font = (pixelSize/20) + 'px Muli';
-        ctx.fillStyle = '#FFF';
-        ctx.textAlign = 'center';
-        ctx.textBaseline = 'middle';
-        const midAngle = (startAngle + endAngle)/2;
-        ctx.fillText(i + '', pixelSize/2 + Math.cos(midAngle)*(pixelSize/2+pixelSize/4)/2, pixelSize/2 + Math.sin(midAngle)*(pixelSize/2+pixelSize/4)/2);
-        ctx.font = (pixelSize/30) + 'px Muli';
-        ctx.fillText(options[i], pixelSize/2 + Math.cos(midAngle)*(pixelSize/2+pixelSize/4)/2, pixelSize/2 + Math.sin(midAngle)*(pixelSize/2+pixelSize/4)/2 + pixelSize/20);
+        
+        {
+          const selected = i === selectedSlice && selectedDepth === 0;
+          ctx.fillStyle = selected ? '#4fc3f7' : '#111';
+          ctx.beginPath();
+          ctx.arc(pixelSize/2, pixelSize/2, outerRadiusSoft, startAngle, endAngle, false);
+          ctx.arc(pixelSize/2, pixelSize/2, innerRadiusSoft, endAngle, startAngle, true);
+          ctx.fill();
+        }
+        {
+          const selected = i === selectedSlice && selectedDepth === 1;
+          ctx.fillStyle = selected ? '#4fc3f7' : '#111';
+          ctx.beginPath();
+          ctx.arc(pixelSize/2, pixelSize/2, outerRadius, startAngle, endAngle, false);
+          ctx.arc(pixelSize/2, pixelSize/2, innerRadius, endAngle, startAngle, true);
+          ctx.fill();
+        }
+        {
+          ctx.font = (pixelSize/20) + 'px Muli';
+          ctx.fillStyle = '#FFF';
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          const midAngle = (startAngle + endAngle)/2;
+          ctx.fillText(i + '', pixelSize/2 + Math.cos(midAngle)*(pixelSize/2+pixelSize/4)/2, pixelSize/2 + Math.sin(midAngle)*(pixelSize/2+pixelSize/4)/2);
+          ctx.font = (pixelSize/30) + 'px Muli';
+          ctx.fillText(options[i], pixelSize/2 + Math.cos(midAngle)*(pixelSize/2+pixelSize/4)/2, pixelSize/2 + Math.sin(midAngle)*(pixelSize/2+pixelSize/4)/2 + pixelSize/20);
+        }
       }
     }
   };
   useEffect(() => {
     _render();
-  }, [canvasRef.current, selectedSlice]);
+  }, [canvasRef.current, selectedSlice, selectedDepth]);
   
   useEffect(() => {
     localVector2D.fromArray(coords);
-    if (localVector2D.length() > 3) {
-      const angle = modPi2(Math.atan2(coords[1], coords[0]) + Math.PI/2);
-      const sliceIndex = Math.floor(angle/sliceSize);
+    const centerDistance = localVector2D.length() * pixelRatio;
+    const angle = modPi2(Math.atan2(coords[1], coords[0]) + Math.PI/2);
+    const sliceIndex = Math.floor(angle/sliceSize);
+
+    // console.log('got center', centerDistance, innerRadius, innerRadiusSoft);
+    if (centerDistance > innerRadius) {
       setSelectedSlice(sliceIndex);
+      setSelectedDepth(1);
+    } else if (centerDistance > innerRadiusSoft) {
+      setSelectedSlice(sliceIndex);
+      setSelectedDepth(0);
     } else {
       setSelectedSlice(-1);
+      setSelectedDepth(-1);
     }
   }, [coords]);
 
@@ -144,6 +193,7 @@ export default function QuickMenu() {
           /* enabled={!!arrowPosition}
           animate={!!selectCharacter} */
           enabled={open}
+          down={down}
           ax={coords[0] + centerCoords[0]}
           ay={coords[1] + centerCoords[1]}
         />
