8769de1fb15479dd23123ad20375d97d73e53fdc
ohmed <bohdan.horpynchuk@gmail.com>
2022-04-27 18:13:31 +0300

    Now using setGeometryScale.
    - When in WorldObject scale gets changed physical mesh scale gets updated via new setGeometryScale.
    - Update geometry wasm.

--- physx.js
+++ physx.js
@@ -1530,6 +1530,21 @@ const physxWorker = (() => {
     )
     allocator.freeAll()
   }
+  w.setGeometryScale = (
+    physics,
+    id,
+    scale,
+  ) => {
+    const allocator = new Allocator()
+    const s = allocator.alloc(Float32Array, 3)
+    scale.toArray(s)
+    moduleInstance._setGeometryScalePhysics(
+        physics,
+        id,
+        s.byteOffset
+    )
+    allocator.freeAll()
+  }
   w.setTransformPhysics = (
     physics,
     id,
