981a902565558d7160477a4a8f37fd933756d7b2
Avaer Kazmer <a@modules.io>
2022-04-27 01:57:32 -0400

    Revert "Merge pull request #2882 from gonnavis/AnimDelCrouchAfterUsePr"
    This reverts commit af49e543d9148f5c40997b4ca14f53cb8ca4f582, reversing
    changes made to 66e5dfd1bf8e495bfe493abfe37898e7ef5a7737.

--- game.js
+++ game.js
@@ -352,9 +352,6 @@ const _startUse = () => {
         };
         // console.log('new use action', newUseAction, useComponent, {animation, animationCombo, animationEnvelope});
         localPlayer.addAction(newUseAction);
-        if (newUseAction.ik !== 'pistol') {
-          localPlayer.removeAction('crouch');
-        }
 
         wearApp.use();
       }
