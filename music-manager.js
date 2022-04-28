7ddc7cc4781944f37ddea58d66fff3d352865e87
Avaer Kazmer <a@modules.io>
2022-04-28 10:00:39 -0400

    Describe music manager

--- music-manager.js
+++ music-manager.js
@@ -1,25 +1,38 @@
+/*
+this manager provides music preloading, selection, and playing.
+*/
+
 import WSRTC from 'wsrtc/wsrtc.js';
 
 class Music {
-  constructor({name, url}, audioContext) {
+  constructor({name, urls}, audioContext) {
     this.name = name;
-    this.url = url;
+    this.urls = urls;
     this.audioContext = audioContext;
-    this.audioBuffer = null;
+    this.audioBuffers = null;
+    this.audioBufferIndex = 0; // Math.floor(Math.random() * this.urls.length);
 
     this.loadPromise = (async () => {
-      const res = await fetch(url);
-      const arrayBuffer = await res.arrayBuffer();
-      this.audioBuffer = await this.audioContext.decodeAudioData(arrayBuffer);
+      this.audioBuffers = await Promise.all(this.urls.map(async url => {
+        const res = await fetch(url);
+        const arrayBuffer = await res.arrayBuffer();
+        return await this.audioContext.decodeAudioData(arrayBuffer);
+      }));
     })();
   }
-  play() {
+  play({
+    repeat = false,
+  } = {}) {
+    const audioBuffer = this.audioBuffers[this.audioBufferIndex];
+    this.audioBufferIndex = (this.audioBufferIndex + 1) % this.audioBuffers.length;
+    
     const source = this.audioContext.createBufferSource();
-    source.buffer = this.audioBuffer;
+    source.buffer = audioBuffer;
+    source.loop = repeat;
     source.start(0);
 
     const gain = this.audioContext.createGain();
-    gain.gain.value = 0.5;
+    gain.gain.value = 0.3;
 
     source.connect(gain);
     gain.connect(this.audioContext.gain);
@@ -36,7 +49,47 @@ class Music {
 const musicSpecs = [
   {
     name: 'battle',
-    url: './sounds/music/battle.mp3',
+    urls: [
+      './sounds/music/battle1.mp3',
+      './sounds/music/battle2.mp3',
+      './sounds/music/battle3.mp3',
+    ],
+  },
+  {
+    name: 'victory',
+    urls: [
+      './sounds/music/victory1.mp3',
+      './sounds/music/victory2.mp3',
+      './sounds/music/victory3.mp3',
+      './sounds/music/victory4.mp3',
+      './sounds/music/victory5.mp3',
+    ],
+  },
+  {
+    name: 'gameOver',
+    urls: [
+      './sounds/music/gameOver1.mp3',
+      './sounds/music/gameOver2.mp3',
+      './sounds/music/gameOver3.mp3',
+    ],
+  },
+  {
+    name: 'overworld',
+    urls: [
+      './sounds/music/overworld.mp3',
+    ],
+  },
+  {
+    name: 'dungeon',
+    urls: [
+      './sounds/music/dungeon.mp3',
+    ],
+  },
+  {
+    name: 'homespace',
+    urls: [
+      './sounds/music/homespace.mp3',
+    ],
   },
 ];
 class MusicManager {
@@ -54,12 +107,16 @@ class MusicManager {
     } */
     return music || null;
   }
-  playCurrentMusic(name) {
+  playCurrentMusic(name, {
+    repeat = false,
+  } = {}) {
     this.stopCurrentMusic();
 
-    const newMusic = this.musics.find(music => music.name);
+    const newMusic = this.musics.find(music => music.name === name);
     if (newMusic) {
-      this.currentMusic = newMusic.play();
+      this.currentMusic = newMusic.play({
+        repeat,
+      });
 
       const localCurrentMusic = this.currentMusic;
       this.currentMusic.source.addEventListener('ended', () => {
