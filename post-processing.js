467401e5a09a3674e83ec3098f29364445e3ef2d
Avaer Kazmer <a@modules.io>
2022-04-29 16:03:20 -0400

    Dead imports cleanup

--- post-processing.js
+++ post-processing.js
@@ -13,7 +13,7 @@ import {BokehPass} from './BokehPass.js';
 import {SSAOPass} from './SSAOPass.js';
 // import {RenderPass} from './RenderPass.js';
 import {DepthPass} from './DepthPass.js';
-import {SwirlPass} from './SwirlPass.js';
+// import {SwirlPass} from './SwirlPass.js';
 import {
   getRenderer,
   getComposer,
@@ -33,8 +33,8 @@ import {WebaverseRenderPass} from './webaverse-render-pass.js';
 import renderSettingsManager from './rendersettings-manager.js';
 import metaversefileApi from 'metaversefile';
 // import {parseQuery} from './util.js';
-import * as sounds from './sounds.js';
-import musicManager from './music-manager.js';
+// import * as sounds from './sounds.js';
+// import musicManager from './music-manager.js';
 
 // const hqDefault = parseQuery(window.location.search)['hq'] === '1';
 
