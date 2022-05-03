bc621da1c0988631a8c09d1c1b30cd2734b4481d
Avaer Kazmer <a@modules.io>
2022-05-03 19:05:11 -0400

    Hook in voices overrides support

--- voices.js
+++ voices.js
@@ -1,4 +1,7 @@
-import { voicePacksUrl, voiceEndpointsUrl } from './constants.js';
+import { voicePacksUrl, voiceEndpointsUrl, defaultVoiceEndpoint, defaultVoicePackName } from './constants.js';
+import overrides from './overrides.js';
+import {localPlayer} from './players.js';
+import * as voices from './voices.js';
 
 const voicePacks = [];
 const voiceEndpoints = [];
@@ -17,6 +20,40 @@ const loadPromise = (async () => {
     })(),
   ]);
 })();
+
+[
+  'overrideVoicePack',
+  'userVoicePack',
+].forEach(key => {
+  overrides[key].addEventListener('change', async e => {
+    const voicePackName = overrides.overrideVoicePack.get() ?? overrides.userVoicePack.get() ?? defaultVoicePackName;
+    const voicePack = voices.voicePacks.find(vp => vp.name === voicePackName);
+
+    const {
+      audioPath,
+      indexPath,
+    } = voicePack;
+    const voicePacksUrlBase = voicePacksUrl.replace(/\/+[^\/]+$/, '');
+    const audioUrl = voicePacksUrlBase + audioPath;
+    const indexUrl = voicePacksUrlBase + indexPath;
+
+    await localPlayer.loadVoicePack({
+      audioUrl,
+      indexUrl,
+    });
+  });
+});
+[
+  'overrideVoiceEndpoint',
+  'userVoiceEndpoint',
+].forEach(key => {
+  overrides[key].addEventListener('change', async e => {
+    const voiceEndpointName = overrides.overrideVoiceEndpoint.get() ?? overrides.userVoiceEndpoint.get() ?? defaultVoiceEndpoint;
+    const voiceEndpoint = voices.voiceEndpoints.find(ve => ve.name === voiceEndpointName);
+
+    localPlayer.setVoiceEndpoint(voiceEndpoint.drive_id);
+  });
+});
 
 const waitForLoad = () => {
   return loadPromise;
