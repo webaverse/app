36a68c722157f4b9d10a87e4dc4069bbd25ee845
gonnavis <gonnavis@163.com>
2022-05-04 16:03:46 +0800

    branch: zTargetingDev.

--- ai/lore/lore-model.js
+++ ai/lore/lore-model.js
@@ -1,1 +1,2 @@
-export * from 'https://webaverse.github.io/lore/lore-model.js';
+// export * from 'https://webaverse.github.io/lore/lore-model.js';
+export * from 'http://localhost/lib/webaverse_resource/lore/lore-model.js';
