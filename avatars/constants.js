316bbedd46e86d4b9e6a7d1b25d2465720aa9a12
gonnavis <gonnavis@163.com>
2022-05-05 17:06:34 +0800

    slow down narutoRun.

--- avatars/constants.js
+++ avatars/constants.js
@@ -1,4 +1,4 @@
 export const idleFactorSpeed = 0;
 export const walkFactorSpeed = 0.25;
 export const runFactorSpeed = 0.7;
-export const narutoRunTimeFactor = 2;
+export const narutoRunTimeFactor = 1.7;
