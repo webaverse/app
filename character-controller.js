6c49a37bf48afe9ecc09273d60538bd71760b8ef
gonnavis <gonnavis@163.com>
2022-05-06 17:30:28 +0800

    dev

--- character-controller.js
+++ character-controller.js
@@ -1282,6 +1282,9 @@ class StaticUninterpolatedPlayer extends PlayerBase {
 class NpcPlayer extends StaticUninterpolatedPlayer {
   constructor(opts) {
     super(opts);
+    
+    if (!window.npcPlayers) window.npcPlayers = []
+    window.npcPlayers.push(this);
   
     this.isNpcPlayer = true;
     this.avatarApp = null;
