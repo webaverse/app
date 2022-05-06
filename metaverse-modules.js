a466fda3937a1ee67d523c6ed85902c5987f0d0b
Avaer Kazmer <a@modules.io>
2022-05-06 02:01:00 -0400

    Add meshLodItem to metaverse modules

--- metaverse-modules.js
+++ metaverse-modules.js
@@ -19,6 +19,7 @@ const moduleUrls = {
   cameraPlaceholder: './metaverse_modules/camera-placeholder/',
   targetReticle: './metaverse_modules/target-reticle/',
   halo: './metaverse_modules/halo/',
+  meshLodItem: './metaverse_modules/mesh-lod-item/',
 };
 const modules = {};
 const loadPromise = (async () => {
