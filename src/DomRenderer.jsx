ebe82227c8b84513dc1acd4ad29dd01a0f79e222
Avaer Kazmer <a@modules.io>
2022-05-09 05:35:37 -0400

    Major DomRenderer refactoring

--- src/DomRenderer.jsx
+++ src/DomRenderer.jsx
@@ -2,6 +2,9 @@ import * as THREE from 'three';
 import React, {useState, useEffect, useRef} from 'react';
 import gameManager from '../game.js';
 import {scene, camera} from '../renderer.js';
+// import scenePreview from '../metaverse_modules/scene-preview/index.js';
+
+import {CharacterBanner} from './CharacterBanner.jsx';
 
 const floatFactor = 0.05;
 const floatTime = 3000;
@@ -13,6 +16,7 @@ const localMatrix = new THREE.Matrix4();
 const localMatrix2 = new THREE.Matrix4();
 
 const _getFov = () => camera.projectionMatrix.elements[ 5 ] * (window.innerHeight / 2);
+const _getScaleFactor = (width, height) => Math.min(1/width, 1/height);
 
 function epsilon(value) {
   return value;
@@ -77,7 +81,8 @@ class IFrameMesh extends THREE.Mesh {
     width,
     height,
   }) {
-    const geometry = new THREE.PlaneBufferGeometry(width, height);
+    const scaleFactor = _getScaleFactor(width, height);
+    const geometry = new THREE.PlaneBufferGeometry(width * scaleFactor, height * scaleFactor);
     const material = new THREE.MeshBasicMaterial({
       color: 0xFFFFFF,
       side: THREE.DoubleSide,
@@ -86,36 +91,38 @@ class IFrameMesh extends THREE.Mesh {
       blending: THREE.MultiplyBlending
     });
     super(geometry, material);
+
+    this.width = width;
+    this.height = height;
   }
 }
 
-class DomRenderEngine {
+class DomRenderEngine extends EventTarget {
   constructor() {
-    // this.iframeContainer = iframeContainer;
-  }
-  #bindChild(iframeContainer2) {
-    const basePosition = new THREE.Vector3(-9, 1, -1);
+    super();
 
+    this.doms = [];
+  }
+  #bindChild(object, iframeContainer2) {
     // gather constants
 
     const iframe = iframeContainer2.firstChild;
     const width = parseInt(iframe.getAttribute('width'), 10);
     const height = parseInt(iframe.getAttribute('height'), 10);
-    const scale = Math.min(1/width, 1/height);
+    const scaleFactor = _getScaleFactor(width, height);
 
     // attach scene punch-through object
 
-    const object = new THREE.Object3D();
-    object.position.copy(basePosition);
+    // const object = new THREE.Object3D();
+    // object.position.copy(basePosition);
     scene.add(object);
-    object.updateMatrixWorld();
+    // object.updateMatrixWorld();
 
     const object2 = new IFrameMesh({
-      // iframe,
-      width: width * scale,
-      height: height * scale,
+      width: width * scaleFactor,
+      height: height * scaleFactor,
     });
-    object2.frustumCulled = false;
+    // object2.frustumCulled = false;
 
     object2.onBeforeRender = (renderer) => {
       const context = renderer.getContext();
@@ -157,47 +164,174 @@ class DomRenderEngine {
       _updateCameraContainerMatrix();
     });
   }
-  setIframeContainer(iframeContainer) {
-    // this.iframeContainer = iframeContainer;
-
-    const _bindInitialChildren = () => {
-      const children = Array.from(iframeContainer.childNodes);
-      for (const child of children) {
-        this.#bindChild(child);
-      }
+  addDom({
+    position = new THREE.Vector3(),
+    quaternion = new THREE.Quaternion(),
+    scale = new THREE.Vector3(1, 1, 1),
+    width,
+    height,
+    render,
+  }) {
+    const dom = new IFrameMesh({
+      width: width,
+      height: height,
+    });
+    dom.onBeforeRender = renderer => {
+      const context = renderer.getContext();
+      context.disable(context.SAMPLE_ALPHA_TO_COVERAGE);
     };
-    _bindInitialChildren();
-
-    const _listenForChildren = () => {
-      const callback = function(mutationsList, observer) {
-        for (const mutation of mutationsList) {
-          if (mutation.type === 'childList') {
-            console.log('dom render childlist');
-          } else if (mutation.type === 'attributes') {
-            console.log('dom render attributes');
-          }
-        }
-      };
-      const observer = new MutationObserver(callback);
-      observer.observe(iframeContainer, {
-        childList: true,
-      });
+    dom.onAfterRender = renderer => {
+      const context = renderer.getContext();
+      context.enable(context.SAMPLE_ALPHA_TO_COVERAGE);
     };
-    _listenForChildren();
+    scene.add(dom);
+    dom.updateMatrixWorld();
+
+    dom.basePosition = position.clone();
+    dom.baseQuaternion = quaternion.clone();
+    dom.baseScale = scale.clone();
+    dom.render = render;
+    this.doms.push(dom);
+
+    this.dispatchEvent(new MessageEvent('update'));
   }
   destroy() {
     // XXX finish this
   }
 }
 const domRenderEngine = new DomRenderEngine();
+domRenderEngine.addDom({
+  position: new THREE.Vector3(-9, 1, -1),
+  width: 600,
+  height: 400,
+  render: () => (<CharacterBanner />),
+});
+
+const DomRendererChild = ({
+  dom,
+  innerWidth,
+  innerHeight,
+}) => {
+  const {width, height} = dom;
+  const scaleFactor = _getScaleFactor(width, height);
+  const iframeContainer2Ref = useRef();
+
+  useEffect(() => {
+    const iframeContainer2 = iframeContainer2Ref.current;
+
+    if (iframeContainer2) {
+      const render = e => {
+        const _animateMenuFloat = () => {
+          const now = performance.now();
+          dom.position.copy(dom.basePosition);
+          dom.position.y += Math.sin((now % floatTime)/floatTime * 2 * Math.PI) * floatFactor;
+          dom.position.y += Math.cos(((now / 2) % floatTime)/floatTime * 2 * Math.PI) * floatFactor/2;
+          dom.position.y += Math.sin(((now / 4) % floatTime)/floatTime * 2 * Math.PI) * floatFactor/4;
+          dom.updateMatrixWorld();
+        };
+        _animateMenuFloat();
+        
+        const _updateCameraContainerMatrix = () => {
+          const fov = _getFov();
+          const cameraCSSMatrix = getCameraCSSMatrix(
+            localMatrix.copy(camera.matrixWorldInverse)
+              .premultiply(
+                localMatrix2.makeTranslation(0, 0, fov)
+              )
+              .multiply(
+                dom.matrixWorld
+              )
+          );
+          iframeContainer2.style.transform = cameraCSSMatrix;
+        };
+        _updateCameraContainerMatrix();
+      };
+      gameManager.addEventListener('render', render);
+
+      return () => {
+        gameManager.removeEventListener('render', render);
+      };
+    }
+  }, [iframeContainer2Ref]);
+
+  return (
+    <div
+      className={'iframe-container-2'}
+      style={{
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}
+      ref={iframeContainer2Ref}
+    >
+      <div
+        className={'iframe'}
+        width={width}
+        height={height}
+        style={{
+          position: 'relative',
+          width: width + 'px',
+          height: height + 'px',
+          border: '0',
+          overflow: 'hidden',
+
+          transform: 'translate(' + (innerWidth/2 - width/2) + 'px,' + (innerHeight/2 - height/2) + 'px) ' +
+            getObjectCSSMatrix(
+              localMatrix.compose(
+                localVector.set(0, 0, 0),
+                localQuaternion.set(0, 0, 0, 1),
+                localVector2.setScalar(scaleFactor)
+              )
+            ),
+          pointerEvents: 'auto',
+        }}
+      >
+        {dom.render()}
+      </div>
+    </div>
+  );
+}
+
+const DomRendererChildren = ({
+  domRenderEngine,
+  innerWidth,
+  innerHeight,
+  fov,
+}) => {
+  const [epoch, setEpoch] = useState(0);
+
+  useEffect(() => {
+    const update = () => {
+      setEpoch(epoch + 1);
+    };
+    domRenderEngine.addEventListener('update', update);
+    return () => {
+      domRenderEngine.removeEventListener('update', update);
+    };
+  }, [epoch]);
+
+  return <>
+    {domRenderEngine.doms.map((dom, i) => {
+      return (
+        <DomRendererChild
+          dom={dom}
+          innerWidth={innerWidth}
+          innerHeight={innerHeight}
+          fov={fov}
+          key={i}
+        />
+      );
+    })}
+  </>
+};
 
 const DomRenderer = props => {
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
   const [innerHeight, setInnerHeight] = useState(window.innerHeight);
   const [fov, setFov] = useState(_getFov());
   const iframeContainerRef = useRef();
-  // const [domRenderEngine, setDomRenderEngine] = useState(null);
-  const children = React.Children.toArray(props.children);
 
   useEffect(() => {
     const resize = e => {
@@ -211,19 +345,10 @@ const DomRenderer = props => {
     };
   }, []);
 
-  useEffect(() => {
+  /* useEffect(() => {
     const iframeContainer = iframeContainerRef.current;
-    if (iframeContainer) {
-      // const domRenderEngine = new DomRenderEngine(iframeContainer, scene);
-      // setDomRenderEngine(domRenderEngine);
-
-      domRenderEngine.setIframeContainer(iframeContainer);
-
-      /* return () => {
-        domRenderEngine.destroy();
-      }; */
-    }
-  }, [iframeContainerRef]);
+    domRenderEngine.setIframeContainer(iframeContainer);
+  }, [iframeContainerRef]); */
 
   return (
     <div
@@ -241,51 +366,12 @@ const DomRenderer = props => {
       }}
       ref={iframeContainerRef}
     >
-      {
-        children.map((child, i) => {
-          const width = child.props.width;
-          const height = child.props.height;
-          const scale = Math.min(1/width, 1/height);
-
-          return (
-            <div
-              className={'iframe-container-2'}
-              style={{
-                position: 'absolute',
-                top: 0,
-                bottom: 0,
-                left: 0,
-                right: 0,
-              }}
-              key={i}
-            >
-              <div
-                className={'iframe'}
-                width={width}
-                height={height}
-                style={{
-                  position: 'relative',
-                  width: width + 'px',
-                  height: height + 'px',
-                  border: '0',
-                  overflow: 'hidden',
-
-                  transform: 'translate(' + (innerWidth/2 - width/2) + 'px,' + (innerHeight/2 - height/2) + 'px) ' + getObjectCSSMatrix(
-                    localMatrix.compose(
-                      localVector.set(0, 0, 0),
-                      localQuaternion.set(0, 0, 0, 1),
-                      localVector2.setScalar(scale)
-                    )
-                  ),
-                  pointerEvents: 'auto',
-                }}
-              >
-                {child}
-              </div>
-            </div>
-          );
-        })
-      }
+      <DomRendererChildren
+        domRenderEngine={domRenderEngine}
+        innerWidth={innerWidth}
+        innerHeight={innerHeight}
+        fov={fov}
+      />
     </div>
   );
 };
