f22284dbc57bc5159851fd3a878dd8b09def33ab
Avaer Kazmer <a@modules.io>
2022-05-09 02:08:12 -0400

    Add loot drop to dead mesh lod item

--- metaverse_modules/mesh-lod-item/index.js
+++ metaverse_modules/mesh-lod-item/index.js
@@ -1,9 +1,13 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useApp, useFrame, useScene, usePhysics, useWear, useMeshLodder} = metaversefile;
+const {useApp, useFrame, usePhysics, useWear, useMeshLodder, useCleanup, useDefaultModules, addTrackedApp} = metaversefile;
 
-// const zeroVector = new THREE.Vector3(0, 0, 0);
+const localVector = new THREE.Vector3();
+const localVector2 = new THREE.Vector3();
+const localQuaternion = new THREE.Quaternion();
 const localMatrix = new THREE.Matrix4();
+
+const r = () => (-0.5+Math.random())*2;
 
 const _copyTransform = (dst, src) => {
   dst.position.copy(src.position);
@@ -15,13 +19,15 @@ const _copyTransform = (dst, src) => {
 
 export default () => {
   const app = useApp();
+  const physicsManager = usePhysics();
   const meshLodManager = useMeshLodder();
+  const {moduleUrls} = useDefaultModules();
 
   app.name = 'mesh-lod-item';
 
   const meshLodderId = app.getComponent('meshLodderId');
   const physicsId = app.getComponent('physicsId');
-
+  
   const meshLodder = meshLodManager.getMeshLodder(meshLodderId);
   const item = meshLodder.getItemByPhysicsId(physicsId);
 
@@ -76,6 +82,42 @@ export default () => {
   let wearing = false;
   useWear(e => {
     wearing = e.wear;
+  });
+
+  app.addEventListener('die', async () => {
+    const numSilks = Math.floor(1 + Math.random() * 6);
+    const promises = [];
+    for (let i = 0; i < numSilks; i++) {
+      const components = [
+        {
+          key: 'drop',
+          value: {
+            velocity: new THREE.Vector3(r(), 1+Math.random(), r())
+              .normalize()
+              .multiplyScalar(5)
+              .toArray(),
+            angularVelocity: new THREE.Vector3(0, 0.001, 0)
+              .toArray(),
+          },
+        },
+      ];
+
+      const p = addTrackedApp(
+        moduleUrls.silk,
+        localVector.setFromMatrixPosition(itemMesh.matrixWorld),
+        localQuaternion.identity(),
+        localVector2.set(1, 1, 1),
+        components
+      );
+      promises.push(p);
+    }
+    await Promise.all(promises);
+  });
+
+  useCleanup(() => {
+    for (const physicsObject of physicsObjects) {
+      physicsManager.removeGeometry(physicsObject);
+    }
   });
 
   app.getPhysicsObjects = () => physicsObjects;
