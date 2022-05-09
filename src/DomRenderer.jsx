1e77d233d80c73ec8643977dfbacedc6d9592ff3
Avaer Kazmer <a@modules.io>
2022-05-09 02:28:51 -0400

    Dom renderer add float

--- src/DomRenderer.jsx
+++ src/DomRenderer.jsx
@@ -15,7 +15,8 @@ import {scene, camera} from '../renderer.js';
 // const {useLocalPlayer} = metaversefile;
 // import {chatTextSpeed} from '../constants.js';
 
-const width = 400;
+const floatFactor = 0.05;
+const floatTime = 3000;
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
@@ -111,7 +112,15 @@ class IFrameMesh extends THREE.Mesh {
   } */
 }
 
-class DomRenderFrame {
+
+
+
+
+
+
+
+
+/* class DomRenderFrame {
   constructor(iframeContainer2) {
     this.iframeContainer2 = iframeContainer2;
   }
@@ -123,21 +132,14 @@ class DomRenderFrame {
       object2.updateMatrixWorld();
       
       const cameraCSSMatrix =
-        // 'translateZ(' + fov + 'px) ' +
         getCameraCSSMatrix(
           localMatrix.copy(camera.matrixWorldInverse)
-            // .invert()
             .premultiply(
               localMatrix2.makeTranslation(0, 0, fov)
             )
             .multiply(
               object.matrixWorld
             )
-            /* .premultiply(
-              localMatrix2.makeScale(1/window.innerWidth, -1/window.innerHeight, 1)
-                .invert()
-            ) */
-            // .invert()
         );
       this.iframeContainer2.style.transform = cameraCSSMatrix;
       this.iframeContainer2.style.visibility = null;
@@ -145,15 +147,22 @@ class DomRenderFrame {
       this.iframeContainer2.style.visibility = 'hidden';
     }
   }
-}
+} */
+
+
+
+
+
 
 class DomRenderEngine {
   constructor(iframeContainer, scene) {
     this.iframeContainer = iframeContainer;
     this.scene = scene;
 
+    const basePosition = new THREE.Vector3(-9, 1, -1);
+
     const object = new THREE.Object3D();
-    object.position.set(-9, 1, -1);
+    object.position.copy(basePosition);
     scene.add(object);
     object.updateMatrixWorld();
     
@@ -214,6 +223,13 @@ class DomRenderEngine {
         object2.scale.copy(object.scale);
         object2.updateMatrixWorld(); */
 
+        const now = performance.now();
+        object.position.copy(basePosition);
+        object.position.y += Math.sin((now % floatTime)/floatTime * 2 * Math.PI) * floatFactor;
+        object.position.y += Math.cos(((now / 2) % floatTime)/floatTime * 2 * Math.PI) * floatFactor/2;
+        object.position.y += Math.sin(((now / 4) % floatTime)/floatTime * 2 * Math.PI) * floatFactor/4;
+        object.updateMatrixWorld();
+
         // camera.updateMatrixWorld();
   
         const fov = _getFov();
@@ -272,161 +288,6 @@ class DomRenderEngine {
 
 
 
-
-
-
-
-
-
-
-
-
-const TotumHtmlTypeTemplate = e => {
-  const app = useApp();
-  const physics = usePhysics();
-  
-  const object = app;
-  const {
-    sceneHighPriority,
-    camera,
-    iframeContainer,
-  } = useInternals();
-  const res = app.getComponent('resolution');
-  const width = res[0];
-  const height = res[1];
-  const scale = Math.min(1/width, 1/height);
-
-  const _makeIframe = () => {
-    const iframe = document.createElement('iframe');
-    iframe.setAttribute('width', width); 
-    iframe.setAttribute('height', height); 
-    iframe.style.width = width + 'px';
-    iframe.style.height = height + 'px';
-    // iframe.style.opacity = 0.75;
-    iframe.style.background = 'white';
-    iframe.style.border = '0';
-    iframe.src = href;
-    // window.iframe = iframe;
-    iframe.style.width = width + 'px';
-    iframe.style.height = height + 'px';
-    iframe.style.visibility = 'hidden';
-    return iframe;
-  };
-  let iframe = _makeIframe();
-  document.body.appendChild(iframe);
-  
-  const iframeContainer2 = document.createElement('div');
-  iframeContainer2.style.cssText = 'position: absolute; left: 0; top: 0; bottom: 0; right: 0;';
-  iframeContainer.appendChild(iframeContainer2);
-  
-  let fov = 0;
-  const _updateSize = () => {
-    fov = iframeContainer.getFov();
-    
-    iframe.style.transform = 'translate(' + (window.innerWidth/2 - width/2) + 'px,' + (window.innerHeight/2 - height/2) + 'px) ' + getObjectCSSMatrix(
-      localMatrix.compose(
-        localVector.set(0, 0, 0),
-        localQuaternion.set(0, 0, 0, 1),
-        localVector2.setScalar(scale)
-      )
-    );
-    iframe.style.pointerEvents = 'auto';
-  };
-  _updateSize();
-
-  const object2 = new IFrameMesh({
-    // iframe,
-    width: width * scale,
-    height: height * scale,
-  });
-  object2.frustumCulled = false;
-
-  object2.onBeforeRender = (renderer) => {
-    const context = renderer.getContext();
-    context.disable(context.SAMPLE_ALPHA_TO_COVERAGE);
-  };
-  object2.onAfterRender = (renderer) => {
-    const context = renderer.getContext();
-    context.enable(context.SAMPLE_ALPHA_TO_COVERAGE);
-  };
-  // const object = new THREE.Mesh();
-  // object.contentId = contentId;
-  // object.frustumCulled = false;
-  // object.isHtml = true;
-  let physicsIds = [];
-  let staticPhysicsIds = [];
-  {
-    const physicsId = physics.addBoxGeometry(
-      new THREE.Vector3(),
-      new THREE.Quaternion(),
-      new THREE.Vector3(width * scale * app.scale.x / (app.scale.x * 2), height * scale * app.scale.y / (app.scale.y * 2), 0.001),
-      false
-    );
-    physicsIds.push(physicsId);
-    staticPhysicsIds.push(physicsId);
-    
-    iframe.addEventListener('load', e => {
-      iframe.style.visibility = null;
-      iframeContainer2.appendChild(iframe);
-    }, {once: true});
-    sceneHighPriority.add(object2);
-  }
-  useCleanup(() => {
-    for (const physicsId of physicsIds) {
-      physics.removeGeometry(physicsId);
-    }
-    physicsIds.length = 0;
-    staticPhysicsIds.length = 0;
-    
-    iframeContainer2.parentElement.removeChild(iframeContainer2);
-    sceneHighPriority.remove(object2);
-  });
-  /* object.getPhysicsIds = () => physicsIds;
-  object.getStaticPhysicsIds = () => staticPhysicsIds;
-  object.hit = () => {
-    console.log('hit', object); // XXX
-    return {
-      hit: false,
-      died: false,
-    };
-  }; */
-  
-  useFrame(e => {
-    if (app.parent) {
-      object2.position.copy(object.position);
-      object2.quaternion.copy(object.quaternion);
-      object2.scale.copy(object.scale);
-      // object2.matrix.copy(object.matrix);
-      // object2.matrixWorld.copy(object.matrixWorld);
-      object2.updateMatrixWorld();
-
-      const cameraCSSMatrix =
-        // 'translateZ(' + fov + 'px) ' +
-        getCameraCSSMatrix(
-          localMatrix.copy(camera.matrixWorldInverse)
-            // .invert()
-            .premultiply(
-              localMatrix2.makeTranslation(0, 0, fov)
-            )
-            .multiply(
-              object.matrixWorld
-            )
-            /* .premultiply(
-              localMatrix2.makeScale(1/window.innerWidth, -1/window.innerHeight, 1)
-                .invert()
-            ) */
-            // .invert()
-        );
-      iframeContainer2.style.transform = cameraCSSMatrix;
-      iframeContainer2.style.visibility = null;
-    } else {
-      iframeContainer2.style.visibility = 'hidden';
-    }
-  });
-  useResize(_updateSize);
-  
-  return object;
-};
 
 
 
@@ -524,11 +385,8 @@ const DomRenderer = props => {
                   position: 'relative',
                   width: width + 'px',
                   height: height + 'px',
-                  // background: 'red',
                   border: '0',
                   overflow: 'hidden',
-
-                  // visibility: 'hidden',
 
                   transform: 'translate(' + (innerWidth/2 - width/2) + 'px,' + (innerHeight/2 - height/2) + 'px) ' + getObjectCSSMatrix(
                     localMatrix.compose(
