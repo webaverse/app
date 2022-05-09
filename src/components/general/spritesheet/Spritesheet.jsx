520d2ffd898d2278f19095f1f341e7d65f76b6bb
ohmed <bohdan.horpynchuk@gmail.com>
2022-05-09 15:18:06 +0300

    Code clean up.

--- src/components/general/spritesheet/Spritesheet.jsx
+++ src/components/general/spritesheet/Spritesheet.jsx
@@ -11,41 +11,50 @@ export const Spritesheet = ({
     startUrl,
     enabled,
     size,
-    numFrames,
-    animated = true,
-    background
+    numFrames
 }) => {
 
-    const [ spritesheet, setSpritesheet ] = useState(null);
+    const [ spritesheet, setSpritesheet ] = useState (null );
     const canvasRef = useRef();
 
-    const numFramesPow2 = Math.pow(2, Math.ceil(Math.log2(numFrames)));
-    const numFramesPerRow = Math.ceil(Math.sqrt(numFramesPow2));
+    const numFramesPow2 = Math.pow( 2, Math.ceil( Math.log2( numFrames ) ) );
+    const numFramesPerRow = Math.ceil( Math.sqrt( numFramesPow2 ) );
     const frameSize = size / numFramesPerRow;
     const frameLoopTime = 2000;
     const frameTime = frameLoopTime / numFrames;
 
     //
 
-    useEffect(() => {
-        if (startUrl) {
-            let live = true;
-            (async () => {
-                const spritesheet = await spritesheetManager.getSpriteSheetForAppUrlAsync(startUrl, {
-                    size,
-                    numFrames,
-                });
+    useEffect( () => {
 
-                if (!live) {
-                    return;
-                }
-                setSpritesheet(spritesheet);
-            })();
-            return () => {
-              live = false;
-            };
-        }
-    }, [startUrl]);
+        if ( ! startUrl ) return;
+
+        let live = true;
+
+        ( async () => {
+
+            const spritesheet = await spritesheetManager.getSpriteSheetForAppUrlAsync(startUrl, {
+                size,
+                numFrames,
+            });
+
+            if ( ! live ) {
+
+                return;
+
+            }
+
+            setSpritesheet( spritesheet );
+
+        })();
+
+        return () => {
+
+            live = false;
+
+        };
+
+    }, [ startUrl ] );
 
     useEffect( () => {
 
@@ -60,58 +69,37 @@ export const Spritesheet = ({
 
             const _recurse = () => {
 
-                const x = (frameIndex % numFramesPerRow) * frameSize;
-                const y = size - frameSize - Math.floor(frameIndex / numFramesPerRow) * frameSize;
-                frameIndex = (frameIndex + 1) % numFrames;
-
-                if ( background ) {
-
-                    ctx.rect( 0, 0, canvas.width, canvas.height );
-                    ctx.fillStyle = background;
-                    ctx.fill();
-
-                } else {
-
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-                }
-
-                ctx.drawImage(imageBitmap, x, y, frameSize, frameSize, 0, 0, canvas.width, canvas.height);
+                const x = ( frameIndex % numFramesPerRow ) * frameSize;
+                const y = size - frameSize - Math.floor( frameIndex / numFramesPerRow ) * frameSize;
+                frameIndex = ( frameIndex + 1 ) % numFrames;
+                ctx.clearRect( 0, 0, canvas.width, canvas.height );
+                ctx.drawImage( imageBitmap, x, y, frameSize, frameSize, 0, 0, canvas.width, canvas.height );
 
             };
 
-            if ( animated ) {
-
-                setInterval(_recurse, frameTime);
-
-            }
-
+            setInterval( _recurse, frameTime );
             _recurse();
 
             //
 
             return () => {
 
-                if ( animated ) {
-
-                    clearInterval(interval);
-
-                }
+                clearInterval( interval );
 
             };
 
         }
 
-    }, [ canvasRef, spritesheet, enabled, animated ] );
+    }, [ canvasRef, spritesheet, enabled ] );
 
     //
 
     return (
         <canvas
-            className={classnames(className, styles.canvas)}
-            width={frameSize}
-            height={frameSize}
-            ref={canvasRef}
+            className={ classnames( className, styles.canvas ) }
+            width={ frameSize }
+            height={ frameSize }
+            ref={ canvasRef }
         />
     );
 
