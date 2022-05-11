bd8637e9e74a8ee96dc76041b205564eab47ae05
Avaer Kazmer <a@modules.io>
2022-05-10 23:54:53 -0400

    Revert "Merge pull request #2785 from webaverse/2126-app-world-editor-uses-rotation-scn-uses-quaternions"
    This reverts commit 76b58cf8af7252ab791cff976b72b2969f04b786, reversing
    changes made to 5dc846a1e03a80465f2b3111c9a835537089993d.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -11,7 +11,6 @@ import universe from '../../../universe.js';
 import metaversefileApi from '../../../metaversefile-api';
 import cameraManager from '../../../camera-manager';
 import { world } from '../../../world';
-import { handleStoryKeyControls } from '../../../story';
 
 import { ActionMenu } from '../general/action-menu';
 import { Crosshair } from '../general/crosshair';
@@ -78,21 +77,13 @@ const _getCurrentRoom = () => {
 export const AppContext = createContext();
 
 const useWebaverseApp = (() => {
-
-    let webaverse = null;
-
-    return () => {
-
+  let webaverse = null;
+  return () => {
         if ( webaverse === null ) {
-
             webaverse = new Webaverse();
-
         }
-
         return webaverse;
-
-    };
-
+  };
 })();
 
 export const App = () => {
@@ -133,25 +124,6 @@ export const App = () => {
         }
 
     }, [ state.openedPanel ] );
-
-    useEffect( () => {
-
-        const handleStoryKeyUp = ( event ) => {
-
-            if ( game.inputFocused() ) return;
-            handleStoryKeyControls( event );
-
-        };
-
-        registerIoEventHandler( 'keyup', handleStoryKeyUp );
-
-        return () => {
-
-            unregisterIoEventHandler( 'keyup', handleStoryKeyUp );
-
-        };
-
-    }, [] );
 
     useEffect( () => {
 
@@ -261,7 +233,10 @@ export const App = () => {
                 <Crosshair />
                 <ActionMenu />
                 <Settings />
-                <WorldObjectsList />
+                <WorldObjectsList
+                    setSelectedApp={ setSelectedApp }
+                    selectedApp={ selectedApp }
+                />
                 <PlayMode />
                 <EditorMode
                     selectedScene={ selectedScene }
