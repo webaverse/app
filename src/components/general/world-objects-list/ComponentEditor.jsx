78adc54348e4cc0f259d0b4a3a673aadeb35939e
ohmed <bohdan.horpynchuk@gmail.com>
2022-05-16 15:16:04 +0300

    Populated new Component management features.
    - Can add new components.
    - Can remove old components.
    - 'ContentId' & 'InstanceId' now locked.
    - Now can edit components 'key' value.

--- src/components/general/world-objects-list/ComponentEditor.jsx
+++ src/components/general/world-objects-list/ComponentEditor.jsx
@@ -1,4 +1,5 @@
 
+import classNames from 'classnames';
 import React, { useContext, useEffect, useState } from 'react';
 
 import { AppContext } from '../../app';
@@ -10,7 +11,9 @@ import styles from './component-editor.module.css';
 export const ComponentEditor = () => {
 
     const { selectedApp } = useContext( AppContext );
-    const [ components, setComponents ] = useState([]);
+    const [ components, setComponents ] = useState( [] );
+    const [ editComponentKey, setEditComponentKey ] = useState( null );
+    const [ editComponentKeyNewValue, setEditComponentKeyNewValue ] = useState( null );
 
     //
 
@@ -25,6 +28,36 @@ export const ComponentEditor = () => {
         });
 
         setComponents( newComponents );
+
+    };
+
+    const handleAddNewBtnClick = () => {
+
+        selectedApp.components.push({ key: 'New item', value: '' });
+        update();
+
+    };
+
+    const handleRemoveItemBtnClick = ( key ) => {
+
+        const newList = [];
+
+        for ( let i = 0; i < selectedApp.components.length; i ++ ) {
+
+            if ( selectedApp.components[ i ].key === key ) continue;
+            newList.push( selectedApp.components[ i ] );
+
+        }
+
+        selectedApp.components = newList;
+        update();
+
+    };
+
+    const handleEditItemBtnClick = ( key ) => {
+
+        setEditComponentKey( key );
+        setEditComponentKeyNewValue( key );
 
     };
 
@@ -44,6 +77,45 @@ export const ComponentEditor = () => {
 
     };
 
+    const handleKeyInputKeyUp = ( key, event ) => {
+
+        if ( event.key === 'Enter' ) {
+
+            event.preventDefault();
+            event.stopPropagation();
+            handleApplyItemKeyBtnClick( key );
+            event.target.blur();
+
+        }
+
+    };
+
+    const handleKeyInputChange = ( event ) => {
+
+        setEditComponentKeyNewValue( event.target.value );
+
+    };
+
+    const handleApplyItemKeyBtnClick = ( key, event ) => {
+
+        for ( let i = 0; i < selectedApp.components.length; i ++ ) {
+
+            if ( editComponentKey === selectedApp.components[ i ].key ) {
+
+                selectedApp.components[ i ].key = editComponentKeyNewValue;
+                break;
+
+            }
+
+        }
+
+        setEditComponentKey( null );
+        update();
+
+    };
+
+    //
+
     useEffect( () => {
 
         update();
@@ -56,14 +128,35 @@ export const ComponentEditor = () => {
         <div className={ styles.componentsEditor }>
             <div className={ styles.title }>Components ({ selectedApp.components.length })</div>
             {
-                components.map( component => (
-                    <div className={ styles.item } key={ component.key } >
-                        <div className={ styles.itemTitle } >{ component.key }</div>
-                        <input className={ styles.itemValue } type="text" value={ component.value } onChange={ handleValueInputChange.bind( this, component.key ) } />
-                        <div className={ styles.clearfix } />
-                    </div>
-                ))
+                components.map( component => {
+
+                    const isEditable = ( component.key !== 'instanceId' && component.key !== 'contentId' );
+
+                    return (
+                        <div className={ classNames( styles.item, ( ! isEditable ? styles.disabled : null ) ) } key={ component.key } >
+                            <img src="./images/ui/lock.svg" className={ styles.lock } />
+                            <div className={ styles.itemRemove } onClick={ isEditable ? handleRemoveItemBtnClick.bind( this, component.key ) : null } >x</div>
+                            {
+                                editComponentKey === component.key ? (
+                                    <>
+                                        <img src="./images/ui/check.svg" className={ styles.itemApply } onClick={ handleApplyItemKeyBtnClick.bind( this, component.key ) } />
+                                        <input className={ styles.itemKey } value={ editComponentKeyNewValue } onChange={ handleKeyInputChange } type="text" onKeyUp={ handleKeyInputKeyUp.bind( this, component.key ) } />
+                                    </>
+                                ) : (
+                                    <>
+                                        <img src="./images/ui/edit.svg" className={ styles.itemEdit } onClick={ handleEditItemBtnClick.bind( this, component.key ) } />
+                                        <div className={ styles.itemTitle } >{ component.key }</div>
+                                    </>
+                                )
+                            }
+                            <input className={ styles.itemValue } disabled={ ! isEditable } type="text" value={ component.value } onChange={ handleValueInputChange.bind( this, component.key ) } />
+                            <div className={ styles.clearfix } />
+                        </div>
+                    );
+
+                })
             }
+            <div className={ styles.addNewItem } onClick={ handleAddNewBtnClick } >Add new</div>
         </div>
     );
 
