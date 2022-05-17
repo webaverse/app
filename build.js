d0e47db12c6ee904fed49cab4e272ffccbfea063
Matthew Willox <matthew@cybermancers.ca>
2022-05-17 14:33:52 -0600

    remove extra esbuild

--- build.js
+++ build.js
@@ -1,42 +1,55 @@
+const fs = require('fs');
 const esbuild = require('esbuild');
 const cssModulesPlugin = require('esbuild-css-modules-plugin');
 
-esbuild
-  .build({
-    format: 'esm',
-    entryPoints: ['src/main.jsx'],
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    platform: 'browser',
-    resolveExtensions: ['.js', '.jsx'],
-    define: {
-      this: 'window',
-    },
-    external: ['require', 'fs', 'path', 'module', '/public/*', '/public/bin/*'],
-    target: ['chrome80', 'firefox72', 'safari13', 'edge80'],
-    outfile: 'dist/out.js',
-    watch: false,
-    loader: {
-      '.png': 'dataurl',
-      '.svg': 'text',
-      '.ttf': 'file',
-      '.woff': 'file',
-      '.woff2': 'file',
-      '.gltf': 'file',
-      '.glb': 'file',
-      '.jpg': 'file',
-    },
-    plugins: [
-      cssModulesPlugin({
-        inject: false,
-        localsConvention: 'camelCaseOnly',
-        generateScopedName: (name, filename, css) => string,
-        cssModulesOption: {},
-        v2: true,
-      }),
-    ],
-  })
-  .then(result => {
-    // console.log('watching...');
-  });
+(async () => {
+  const result = await esbuild
+    .build({
+      format: 'esm',
+      entryPoints: ['src/main.jsx', 'webaverse.js'],
+      bundle: true,
+      minify: true,
+      sourcemap: true,
+      splitting: true,
+      platform: 'browser',
+      resolveExtensions: ['.js', '.jsx'],
+      define: {
+        this: 'window',
+      },
+      external: ['require', 'fs', 'path', 'module', '/public/*', '/public/bin/*'],
+      target: ['chrome80', 'firefox72', 'safari13', 'edge80'],
+      outdir: 'dist',
+      watch: false,
+      loader: {
+        '.png': 'dataurl',
+        '.svg': 'text',
+        '.ttf': 'file',
+        '.woff': 'file',
+        '.woff2': 'file',
+        '.gltf': 'file',
+        '.glb': 'file',
+        '.jpg': 'file',
+        '.json': 'file',
+        '.mp3': 'file',
+        '.mp4': 'file',
+        '.ogg': 'file',
+        '.wav': 'file',
+        '.m4a': 'file',
+        '.mov': 'file',
+        '.scn': 'file',
+        '.metaversefile': 'file',
+      },
+      plugins: [
+        cssModulesPlugin({
+          inject: false,
+          localsConvention: 'camelCaseOnly',
+          generateScopedName: (name, filename, css) => string,
+          cssModulesOption: {},
+          v2: true,
+        }),
+      ],
+    });
+
+  const text = await esbuild.analyzeMetafile(result.metafile);
+  console.log(text);
+})();
