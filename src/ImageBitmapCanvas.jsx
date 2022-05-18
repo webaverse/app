813c5b84b9be1e7d30d823fd609d22c69d6ef46a
Avaer Kazmer <a@modules.io>
2022-05-18 11:08:56 -0400

    Add card back rendering

--- src/ImageBitmapCanvas.jsx
+++ src/ImageBitmapCanvas.jsx
@@ -1,15 +1,23 @@
 import React, {useRef, useEffect} from 'react';
-// import classnames from 'classnames';
+import classnames from 'classnames';
+
+import styles from './ImageBitmapCanvas.module.css';
 
 export const ImageBitmapCanvas = ({
   imageBitmap = null,
+  backImageBitmap = null,
   className,
-  style,
+  canvasClassName,
+  // style,
+  flip = false,
+  rotateX = 0,
+  rotateY = 0,
 }) => {
-  const canvasRef = useRef();
+  const frontCanvasRef = useRef();
+  const backCanvasRef = useRef();
 
   useEffect(() => {
-    const canvas = canvasRef.current;
+    const canvas = frontCanvasRef.current;
     if (canvas) {
       const ctx = canvas.getContext('2d');
       if (imageBitmap) {
@@ -18,15 +26,48 @@ export const ImageBitmapCanvas = ({
         ctx.clearRect(0, 0, canvas.width, canvas.height);
       }
     } 
-  }, [canvasRef, imageBitmap]);
+  }, [frontCanvasRef, imageBitmap]);
+
+  useEffect(() => {
+    const canvas = backCanvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext('2d');
+      if (backImageBitmap) {
+        ctx.drawImage(backImageBitmap, 0, 0);
+      } else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      }
+    } 
+  }, [backCanvasRef, backImageBitmap]);
   
-  return imageBitmap ? (
-    <canvas
-      width={imageBitmap.width}
-      height={imageBitmap.height}
-      className={className}
-      style={style}
-      ref={canvasRef}
-    />
-  ) : null;
+  const _getStyle = front => {
+    return {
+      transform: `rotateY(${(rotateY + ((+flip ^ +front) ? 0 : Math.PI)).toFixed(8)}rad) rotateX(${rotateX.toFixed(8)}rad)`,
+    };
+  };
+
+  return (
+    <div
+      className={classnames(styles.imageBitmapCanvas, className)}
+    >
+      {backImageBitmap ? (
+        <canvas
+          className={classnames(canvasClassName, styles.canvas, styles.backCanvas)}
+          style={_getStyle(false)}
+          width={backImageBitmap.width}
+          height={backImageBitmap.height}
+          ref={backCanvasRef}
+        />
+      ) : null}
+      {imageBitmap ? (
+        <canvas
+          className={classnames(canvasClassName, styles.canvas, styles.frontCanvas)}
+          style={_getStyle(true)}
+          width={imageBitmap.width}
+          height={imageBitmap.height}
+          ref={frontCanvasRef}
+        />
+      ) : null}
+    </div>
+  );
 };
