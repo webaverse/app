b79597598f95574f12a517292b1e32be1f703f3a
Matthew Willox <matthew@cybermancers.ca>
2022-05-18 17:01:46 -0600

    only move the specified folders

--- build-system/build.js
+++ build-system/build.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-var path = require('path');
 const esbuild = require('esbuild');
 const cssModulesPlugin = require('esbuild-css-modules-plugin');
 
@@ -10,7 +9,12 @@ const scn = require('./plugins/scn.js');
 const transferFolders = require('./copyDir.json');
 const transferFiles = require('./copyFiles.json');
 
-const {copyFileSync, copyFolderRecursiveSync} = require('./file-management.js');
+const {
+  copyFileSync,
+  copyFolderRecursiveSync,
+} = require('./file-management.js');
+
+const start = Date.now();
 
 if (!fs.existsSync('./dist')) {
   fs.mkdirSync('./dist');
@@ -21,71 +25,70 @@ if (!fs.existsSync('./dist/public')) {
 }
 
 (async () => {
-  const result = await esbuild
-    .build({
-      format: 'esm',
-      entryPoints: [
-        'src/main.jsx',
-        // './webaverse.js'
-      ],
-      bundle: true,
-      minify: true,
-      sourcemap: true,
-      splitting: false,
-      platform: 'browser',
-      // browser: {
-      //   geometry: './public/bin/geometry.js',
-      // },
-      treeShaking: true,
-      resolveExtensions: ['.js', '.jsx'],
-      define: {
-        this: 'window',
-      },
-      external: [
-        // 'require',
-        // 'fs',
-        'path',
-        'module',
-        '/public/*',
-        '/public/bin/*',
-      ],
-      drop: ['debugger'],
-      target: ['chrome80', 'firefox72', 'safari13', 'edge80'],
-      outdir: 'dist',
-      watch: false,
-      assetNames: 'assets/[name]-[hash]',
-      loader: {
-        // '.json': 'json',
-        '.png': 'file',
-        '.svg': 'text',
-        '.ttf': 'file',
-        '.woff': 'file',
-        '.woff2': 'file',
-        '.gltf': 'json',
-        '.glb': 'file',
-        '.jpg': 'file',
-        '.mp3': 'file',
-        '.mp4': 'file',
-        '.ogg': 'file',
-        '.wav': 'file',
-        '.m4a': 'file',
-        '.mov': 'file',
-        '.scn': 'json',
-        '.metaversefile': 'json',
-      },
-      plugins: [
-        cssModulesPlugin({
-          inject: false,
-          localsConvention: 'camelCaseOnly',
-          generateScopedName: (name, filename, css) => string,
-          cssModulesOption: {},
-          v2: true,
-        }),
-        // metaverseFile,
-        // wasmPlugin,
-        scn,
-      ],
-    });
+  const result = await esbuild.build({
+    format: 'esm',
+    entryPoints: [
+      'src/main.jsx',
+      // './webaverse.js'
+    ],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    splitting: false,
+    platform: 'browser',
+    // browser: {
+    //   geometry: './public/bin/geometry.js',
+    // },
+    treeShaking: true,
+    resolveExtensions: ['.js', '.jsx'],
+    define: {
+      this: 'window',
+    },
+    external: [
+      // 'require',
+      // 'fs',
+      'path',
+      'module',
+      '/public/*',
+      '/public/bin/*',
+    ],
+    drop: ['debugger'],
+    target: ['chrome80', 'firefox72', 'safari13', 'edge80'],
+    outdir: 'dist',
+    watch: false,
+    assetNames: 'assets/[name]-[hash]',
+    loader: {
+      // '.json': 'json',
+      '.png': 'file',
+      '.svg': 'text',
+      '.ttf': 'file',
+      '.woff': 'file',
+      '.woff2': 'file',
+      '.gltf': 'json',
+      '.glb': 'file',
+      '.jpg': 'file',
+      '.mp3': 'file',
+      '.mp4': 'file',
+      '.ogg': 'file',
+      '.wav': 'file',
+      '.m4a': 'file',
+      '.mov': 'file',
+      '.scn': 'json',
+      '.metaversefile': 'json',
+    },
+    plugins: [
+      cssModulesPlugin({
+        inject: false,
+        localsConvention: 'camelCaseOnly',
+        generateScopedName: (name, filename, css) => string,
+        cssModulesOption: {},
+        v2: true,
+      }),
+      // metaverseFile,
+      // wasmPlugin,
+      scn,
+    ],
+  });
 
   const report = await esbuild.analyzeMetafile(result.metafile);
 
@@ -97,22 +100,32 @@ if (!fs.existsSync('./dist/public')) {
     result = result.replace('<% APP_JS %>', '/main.js');
     result = result.replace('<% APP_CSS %>', '/main.css');
 
-    fs.writeFile('./dist/index.html', result, {
-      encoding: 'utf8',
-    }, function(err) {});
+    fs.writeFile(
+      './dist/index.html',
+      result,
+      {
+        encoding: 'utf8',
+      },
+      function(err) {},
+    );
   });
 
-  fs.writeFile('./build-system/report.txt', report, {
-    encoding: 'utf8',
-  }, function(err) {});
+  fs.writeFile(
+    './build-system/report.txt',
+    report,
+    {
+      encoding: 'utf8',
+    },
+    function(err) {},
+  );
 
-  transferFiles.forEach((file) => {
+  transferFiles.forEach(file => {
     copyFileSync(file, `./dist/${file.replace('./', '')}`);
   });
 
-  transferFiles.forEach((folder) => {
+  transferFolders.forEach(folder => {
     copyFolderRecursiveSync(folder, './dist');
   });
-
-  copyFolderRecursiveSync('./public', './dist');
+  
+  console.log((Date.now() - start) / 1000);
 })();
