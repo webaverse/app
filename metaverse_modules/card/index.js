ee1981985c923479f107c3f89e3d979515425f24
Avaer Kazmer <a@modules.io>
2022-05-26 15:41:30 -0400

    Card app dead code cleanup

--- metaverse_modules/card/index.js
+++ metaverse_modules/card/index.js
@@ -7,13 +7,13 @@ const {useApp, useFrame, useLocalPlayer, useMaterials} = metaversefile;
 
 // const cardPreviewHost = `https://card-preview.exokit.org`;
 const dropItemSize = 0.2;
-const pickUpDistance = 1;
+// const pickUpDistance = 1;
 
-const localVector = new THREE.Vector3();
-const localEuler = new THREE.Euler();
+// const localVector = new THREE.Vector3();
+// const localEuler = new THREE.Euler();
 
-const zeroVector = new THREE.Vector3(0, 0, 0);
-const gravity = new THREE.Vector3(0, -9.8, 0);
+// const zeroVector = new THREE.Vector3(0, 0, 0);
+// const gravity = new THREE.Vector3(0, -9.8, 0);
 const cardWidth = 0.063 * 3;
 const cardHeight = cardWidth / 2.5 * 3.5;
 
@@ -95,144 +95,15 @@ const _makeCardMesh = () => {
   // itemletMesh.velocity = new THREE.Vector3(0, 3, 0);
   itemletMesh.frustumCulled = false;
   // itemletMesh.updateMatrixWorld();
-
-  let pickedUp = false;
-  let rotY = 0;
-  itemletMesh.update = (timestamp, timeDiff) => {
-    const timeDiffS = timeDiff / 1000;
-    const localPlayer = useLocalPlayer();
-
-    if (!pickedUp) {
-      if (!itemletMesh.velocity.equals(zeroVector)) {
-        itemletMesh.position.add(localVector.copy(itemletMesh.velocity).multiplyScalar(timeDiffS));
-        itemletMesh.velocity.add(localVector.copy(gravity).multiplyScalar(timeDiffS));
-        if (itemletMesh.position.y < 0) {
-          itemletMesh.position.y = 0;
-          itemletMesh.velocity.set(0, 0, 0);
-        }
-      } else {
-        const localPosition = localVector.copy(localPlayer.position);
-        localPosition.y -= localPlayer.avatar.height;
-
-        if (localPosition.distanceTo(itemletMesh.position) < pickUpDistance) {
-          console.log('trigger drop pickUp');
-          
-          const localPlayer = useLocalPlayer();
-          localPlayer.addAction({
-            type: 'pickUp',
-          });
-
-          pickedUp = true;
-        }
-      }
-
-      // rotation
-      rotY += 0.3 * Math.PI * 2 * timeDiffS;
-      rotY = rotY % (Math.PI * 2);
-      localEuler.set(0, rotY, 0, 'YXZ');
-      itemletMesh.quaternion.setFromEuler(localEuler);
-      
-      // console.log('got rotation', localEuler.y);
-    } /* else {
-      // console.log('animate');
-    } */
-
-    itemletMesh.updateMatrixWorld();
-  };
   return itemletMesh;
 };
 
-
-
-
-
-
-
-
-
-/* const _loadImage = u => new Promise((accept, reject) => {
-  const img = new Image();
-  img.crossOrigin = 'Anonymous';
-  
-  img.src = u;
-  img.onload = () => {
-    accept(img);
-  };
-  img.onerror = reject;
-});
-const cardFrontGeometry = new THREE.PlaneBufferGeometry(cardWidth, cardHeight);
-const cardBackGeometry = cardFrontGeometry.clone()
-  .applyMatrix4(
-    new THREE.Matrix4().compose(
-      new THREE.Vector3(0, 0, -0.001),
-      new THREE.Quaternion()
-        .setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI),
-      new THREE.Vector3(1, 1, 1)
-    )
-  );
-const _makeCardBackMaterial = () => {
-  const material = new THREE.MeshBasicMaterial({
-    color: 0xFFFFFF,
-    map: new THREE.Texture(),
-    transparent: true,
-  });
-  (async () => {
-    const img = await _loadImage(`${import.meta.url.replace(/(\/)[^\/]*$/, '$1')}cardback.png`);
-    material.map.image = img;
-    material.map.minFilter = THREE.LinearMipmapLinearFilter;
-    material.map.magFilter = THREE.LinearFilter;
-    material.map.encoding = THREE.sRGBEncoding;
-    material.map.anisotropy = 16;
-    material.map.needsUpdate = true;
-  })();
-  return material;
-};
-const _makeCardMesh = img => {
-  const geometry = cardFrontGeometry;
-  const material = new THREE.MeshBasicMaterial({
-    color: 0xFFFFFF,
-    map: new THREE.Texture(img),
-    side: THREE.DoubleSide,
-    transparent: true,
-  });
-  material.map.minFilter = THREE.LinearMipmapLinearFilter;
-  material.map.magFilter = THREE.LinearFilter;
-  material.map.encoding = THREE.sRGBEncoding;
-  material.map.anisotropy = 16;
-  material.map.needsUpdate = true;
-  const mesh = new THREE.Mesh(geometry, material);
-  
-  {
-    const geometry = cardBackGeometry;
-    const material = _makeCardBackMaterial();
-    const back = new THREE.Mesh(geometry, material);
-    mesh.add(back);
-    mesh.back = back;
-  }
-  
-  return mesh;
-}; */
 export default () => {
   const app = useApp();
 
   const cardMesh = _makeCardMesh();
   app.add(cardMesh);
   cardMesh.updateMatrixWorld();
-  /* let cardMesh = null;
-  (async() => {
-    const id = 1;
-    const w = 1024;
-    const ext = 'png';
-    const u = `${cardPreviewHost}/?t=${id}&w=${w}&ext=${ext}`;
-    const img = await _loadImage(u);
-    cardMesh = _makeCardMesh(img);
-    cardMesh.rotation.order = 'YXZ';
-    app.add(cardMesh);
-  })(); */
-  
-  /* useFrame(({timestamp, timeDiff}) => {
-    cardMesh.update(timestamp, timeDiff);
-  }); */
   
   return app;
 };
