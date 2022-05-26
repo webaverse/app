569e9f6692f0812da15eae0e11f3020f66614295
Avaer Kazmer <a@modules.io>
2022-05-26 15:23:36 -0400

    Rewrite card metaverse module

--- metaverse_modules/card/index.js
+++ metaverse_modules/card/index.js
@@ -1,14 +1,156 @@
 import * as THREE from 'three';
 // import easing from './easing.js';
 import metaversefile from 'metaversefile';
-const {useApp, useFrame} = metaversefile;
+const {useApp, useFrame, useLocalPlayer, useMaterials} = metaversefile;
 
-const cardPreviewHost = `https://card-preview.exokit.org`;
-// const cubicBezier = easing(0, 1, 0, 1);
+// const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 
+// const cardPreviewHost = `https://card-preview.exokit.org`;
+const dropItemSize = 0.2;
+const pickUpDistance = 1;
+
+const localVector = new THREE.Vector3();
+const localEuler = new THREE.Euler();
+
+const zeroVector = new THREE.Vector3(0, 0, 0);
+const gravity = new THREE.Vector3(0, -9.8, 0);
 const cardWidth = 0.063 * 3;
 const cardHeight = cardWidth / 2.5 * 3.5;
-const _loadImage = u => new Promise((accept, reject) => {
+
+function getCardBackTexture() {
+  const img = new Image();
+  const texture = new THREE.Texture(img);
+
+  img.crossOrigin = 'Anonymous';
+  img.onload = () => {
+    texture.needsUpdate = true;
+  };
+  img.onerror = err => {
+    console.warn(err);
+  };
+  img.src = `images/cardback-01.svg`;
+
+  return texture;
+}
+const _makeCardMesh = () => {
+  const {WebaverseShaderMaterial} = useMaterials();
+
+  // let w = 520;
+  // let h = 728;
+  let w = cardWidth;
+  let h = cardHeight;
+
+  h /= w;
+  w /= w;
+
+  w *= dropItemSize;
+  h *= dropItemSize;
+
+  const geometry = new THREE.PlaneBufferGeometry(w, h)
+    // .translate(0, 0.5, 0);
+  const texture = getCardBackTexture();
+  
+  const material = new WebaverseShaderMaterial({
+    uniforms: {
+      uTex: {
+        value: texture,
+        needsUpdate: true,
+      },
+    },
+    vertexShader: `\
+      // uniform vec4 cameraBillboardQuaternion;
+      varying vec2 vUv;
+
+      /* vec3 rotate_vertex_position(vec3 position, vec4 q) {
+        return position + 2.0 * cross(q.xyz, cross(q.xyz, position) + q.w * position);
+      } */
+
+      void main() {
+        vec3 pos = position;
+
+        // pos = rotate_vertex_position(pos, cameraBillboardQuaternion);
+
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+        vUv = uv;
+      }
+    `,
+    fragmentShader: `\
+      uniform sampler2D uTex;
+      // uniform vec3 color1;
+      // uniform vec3 color2;
+      varying vec2 vUv;
+
+      void main() {
+        vec4 diffuseColor = texture2D(uTex, vUv);
+        gl_FragColor = diffuseColor;
+        if (gl_FragColor.a < 0.1) {
+          discard;
+        }
+      }
+    `,
+    side: THREE.DoubleSide,
+    transparent: true,
+  });
+  const itemletMesh = new THREE.Mesh(geometry, material);
+  // itemletMesh.velocity = new THREE.Vector3(0, 3, 0);
+  itemletMesh.frustumCulled = false;
+  // itemletMesh.updateMatrixWorld();
+
+  let pickedUp = false;
+  let rotY = 0;
+  itemletMesh.update = (timestamp, timeDiff) => {
+    const timeDiffS = timeDiff / 1000;
+    const localPlayer = useLocalPlayer();
+
+    if (!pickedUp) {
+      if (!itemletMesh.velocity.equals(zeroVector)) {
+        itemletMesh.position.add(localVector.copy(itemletMesh.velocity).multiplyScalar(timeDiffS));
+        itemletMesh.velocity.add(localVector.copy(gravity).multiplyScalar(timeDiffS));
+        if (itemletMesh.position.y < 0) {
+          itemletMesh.position.y = 0;
+          itemletMesh.velocity.set(0, 0, 0);
+        }
+      } else {
+        const localPosition = localVector.copy(localPlayer.position);
+        localPosition.y -= localPlayer.avatar.height;
+
+        if (localPosition.distanceTo(itemletMesh.position) < pickUpDistance) {
+          console.log('trigger drop pickUp');
+          
+          const localPlayer = useLocalPlayer();
+          localPlayer.addAction({
+            type: 'pickUp',
+          });
+
+          pickedUp = true;
+        }
+      }
+
+      // rotation
+      rotY += 0.3 * Math.PI * 2 * timeDiffS;
+      rotY = rotY % (Math.PI * 2);
+      localEuler.set(0, rotY, 0, 'YXZ');
+      itemletMesh.quaternion.setFromEuler(localEuler);
+      
+      // console.log('got rotation', localEuler.y);
+    } /* else {
+      // console.log('animate');
+    } */
+
+    itemletMesh.updateMatrixWorld();
+  };
+  return itemletMesh;
+};
+
+
+
+
+
+
+
+
+
+/* const _loadImage = u => new Promise((accept, reject) => {
   const img = new Image();
   img.crossOrigin = 'Anonymous';
   
@@ -69,11 +211,14 @@ const _makeCardMesh = img => {
   }
   
   return mesh;
-};
+}; */
 export default () => {
   const app = useApp();
 
-  let cardMesh = null;
+  const cardMesh = _makeCardMesh();
+  app.add(cardMesh);
+  cardMesh.updateMatrixWorld();
+  /* let cardMesh = null;
   (async() => {
     const id = 1;
     const w = 1024;
@@ -83,15 +228,11 @@ export default () => {
     cardMesh = _makeCardMesh(img);
     cardMesh.rotation.order = 'YXZ';
     app.add(cardMesh);
-  })();
+  })(); */
   
-  useFrame(() => {
-    if (cardMesh) {
-      const maxTime = 2000;
-      const f = (Date.now() % maxTime) / maxTime;
-      cardMesh.rotation.y = f * (2*Math.PI);
-    }
-  });
+  /* useFrame(({timestamp, timeDiff}) => {
+    cardMesh.update(timestamp, timeDiff);
+  }); */
   
   return app;
 };
