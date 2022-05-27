cfebe7348c5e4ddf088646e35030ec1d77dea119
Matthew Willox <matthew@cybermancers.ca>
2022-05-26 18:39:19 -0600

    better organization of the chains

--- src/hooks/web3-constants.js
+++ src/hooks/web3-constants.js
@@ -1,12 +1,12 @@
-export const isLocal = window.location.host.includes('localhost');
-
 import _CONTRACTS from 'https://contracts.webaverse.com/config/addresses.js';
 import _CONTRACT_ABIS from 'https://contracts.webaverse.com/config/abi.js';
+
+export const isLocal = window.location.host.includes('localhost');
 
 export const CONTRACTS = _CONTRACTS;
 export const CONTRACT_ABIS = _CONTRACT_ABIS;
 
-export const CHAIN = {
+export const CHAINS = {
   LOCALHOST: {
     name: 'Localhost',
     chainId: '0x539',
@@ -18,6 +18,8 @@ export const CHAIN = {
     blockExplorerUrls: ['https://rinkeby.etherscan.io'],
     symbol: 'ETH',
     rpcUrls: ['https://mainnet.infura.io/v3/'],
+    contract_name: 'testnet',
+    previewLink: 'https://rinkeby.etherscan.io/address/',
   },
   ETHEREUM_MAIN: {
     chainName: 'Ethereum Mainnet',
@@ -26,16 +28,18 @@ export const CHAIN = {
     chainId: 0x1,
     symbol: 'ETH',
     rpcUrls: ['https://mainnet.infura.io/v3/'],
+    contract_name: 'mainnet',
+    previewLink: 'https://etherscan.io/address/',
   },
-  AVALANCHE_MAIN: {
-    chainName: 'Avalanche Network',
-    name: 'Avalanche',
-    blockExplorerUrls: ['https://snowtrace.io/'],
-    chainId: '0xa86a',
-    symbol: 'AVAX',
-    decimals: 18,
-    rpcUrls: ['https://api.avax.network/ext/bc/C/rpc'],
-  },
+  // AVALANCHE_MAIN: {
+  //   chainName: 'Avalanche Network',
+  //   name: 'Avalanche',
+  //   blockExplorerUrls: ['https://snowtrace.io/'],
+  //   chainId: '0xa86a',
+  //   symbol: 'AVAX',
+  //   decimals: 18,
+  //   rpcUrls: ['https://api.avax.network/ext/bc/C/rpc'],
+  // },
   POLYGON_MAIN: {
     chainName: 'Polygon Mainnet',
     name: 'Polygon',
@@ -43,9 +47,20 @@ export const CHAIN = {
     chainId: '0x137',
     symbol: 'MATIC',
     rpcUrls: ['https://polygon-rpc.com'],
+    contract_name: 'polygon',
+    previewLink: 'https://polygonscan.com/address/',
+  },
+  POLYGON_TEST: {
+    chainName: 'Polygon Test',
+    name: 'Polygon',
+    blockExplorerUrls: ['https://polygonscan.com/'],
+    chainId: '0x137',
+    symbol: 'MATIC',
+    rpcUrls: ['https://polygon-rpc.com'],
+    contract_name: 'testnetpolygon',
+    previewLink: 'https://polygonscan.com/address/',
   },
 };
 
-export const CHAINS = Object.keys(CHAIN);
-export const DEFAULT_CHAIN = CHAIN.ETHEREUM_MAIN;
-export const CHAIN_IN_USE = CHAIN[DEFAULT_CHAIN];
+export const NETWORK_KEYS = Object.keys(CHAINS);
+export const DEFAULT_CHAIN = CHAINS.RINKBY;
