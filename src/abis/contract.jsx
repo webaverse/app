8abb7f30576ebf383bf95374d2bbe3a6dd105687
Super <supernftier@gmail.com>
2022-05-28 14:33:41 +0300

    complete all

--- src/abis/contract.jsx
+++ src/abis/contract.jsx
@@ -4,12 +4,12 @@ import FT from "./WebaverseERC20.json"
 const NFTABI = NFT.abi;
 const FTABI = FT.abi;
 
-const AccountcontractAddress = "0x68599c0859382Fbc1c8aF50eB534143CD3994D2F";
-const FTcontractAddress = "0x42c30Debe0c8A6EBe2a07ED4C495d1D119442Cc9";
-const FTProxycontractAddress = "0x1e8C40C2b5F3f276D918EdE1aCB904Dea8dd06E9";
-const NFTcontractAddress = "0xf0157Ad2Aed7B1bA14aBaa2287c87b05aA75b6fF";
-const NFTProxycontractAddress = "0xCc71915d4c9d7bb8938D727335ddf931561fF2C7";
-const LANDcontractAddress = "0x96efb818A529d69c30A3979a7C4c8208Ca25Bc94";
-const LANDProxy= "0x90e44221CF31e8DA6677557DeC694e2fbe31A91f";
+const AccountcontractAddress = "0x3077E2a272fDF73Df414d0017B3280F143587094";
+const FTcontractAddress = "0x3243338940ECdD49448AD66baFbBf2E169f3D3c6";
+const FTProxycontractAddress = "0x08E7f3d5F8836d9E4Da3f28aFCB1Be6Af1c90476";
+const NFTcontractAddress = "0xc043479003b5d338A8a632E05C5868540837BA44";
+const NFTProxycontractAddress = "0x6d345d4cB6e93FFb10260a321978088968363257";
+const LANDcontractAddress = "0x0755af05B2B378FD682b53d8eb05F6039C5EA56C";
+const LANDProxy= "0xE3C83C5669584D7A0F0179A99FAb18d27c96B009";
 
 export { NFTABI, FTABI, AccountcontractAddress, FTcontractAddress, FTProxycontractAddress, NFTcontractAddress, NFTProxycontractAddress, LANDcontractAddress, LANDProxy }
