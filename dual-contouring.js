ddb098754900fb37a4e14e894030feada3a33c5e
aryasalehi <64514807+aryasalehi@users.noreply.github.com>
2022-05-30 21:39:05 +0430

    making the lod system faster

--- dual-contouring.js
+++ dual-contouring.js
@@ -27,14 +27,6 @@ w.setChunkSize = (chunkSize) => {
   Module._setChunkSize(chunkSize)
 };
 
-w.generateChunkDataDualContouring = (x, y, z) => {
-  Module._generateChunkDataDualContouring(x, y, z)
-}
-
-w.setChunkLodDualContouring = (x, y, z, lod) => {
-  Module._setChunkLodDualContouring(x, y, z, lod)
-}
-
 w.clearTemporaryChunkDataDualContouring = () => {
   Module._clearTemporaryChunkDataDualContouring()
 }
@@ -43,8 +35,8 @@ w.clearChunkRootDualContouring = (x, y, z) => {
   Module._clearChunkRootDualContouring(x, y, z)
 }
 
-w.createChunkMeshDualContouring = (x, y, z) => {
-  const outputBufferOffset = Module._createChunkMeshDualContouring(x, y, z);
+w.createChunkMeshDualContouring = (x, y, z, lod) => {
+  const outputBufferOffset = Module._createChunkMeshDualContouring(x, y, z, lod);
   // console.log('create xyz', x, y, z, outputBufferOffset);
 
   if (outputBufferOffset) {
