4315ecc034476dda2ff21f63d5f3e0a537dc9a1e
Avaer Kazmer <a@modules.io>
2022-06-03 22:44:00 -0400

    Add build-megatexture-atlas.sh

---
+++ scripts/build-megatexture-atlas.sh
@@ -0,0 +1,5 @@
+~/app/bin/basisu Base_Color.png -ktx2 -mipmap;
+~/app/bin/basisu Height.png -ktx2 -mipmap;
+~/app/bin/basisu Normal.png -ktx2 -mipmap -normal_map;
+~/app/bin/basisu Roughness.png -ktx2 -mipmap;
+~/app/bin/basisu Ambient_Occlusion.png -ktx2 -mipmap;
