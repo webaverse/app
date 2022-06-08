e52a644fe395120e862341900dbf78aec477fb3e
Matthew Willox <matthew@cybermancers.ca>
2022-06-08 11:08:40 -0600

    move isSupported to hook

--- src/components/web3/chains.jsx
+++ src/components/web3/chains.jsx
@@ -3,14 +3,14 @@ import {ChainContext} from '../../hooks/chainProvider';
 import { CONTRACTS } from '../../hooks/web3-constants';
 
 export default function Chains() {
-  const {chains, selectedChain, setSelectedChain} = useContext(ChainContext);
+  const {chains, selectedChain, selectChain, isSupported} = useContext(ChainContext);
   return (<div style={{position: 'absolute', zIndex: 10, background: 'grey'}}>
     <h1>{selectedChain.name}</h1>
 
     <ul id="CHAINS" >
       {Object.keys(chains)
         .map(key => chains[key])
-        .filter(chain => CONTRACTS[chain.contract_name])
+        .filter(isSupported)
         .map(c => (
           <li key={c.contract_name} onClick={() => setSelectedChain(c)}>{c.name}</li>
         ))}
