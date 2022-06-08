bc1b9aca3349d6a1e5228abac1fc5465d5285911
Matthew Willox <matthew@cybermancers.ca>
2022-06-08 15:12:22 -0600

    style and UX for chains

--- src/components/web3/chains.jsx
+++ src/components/web3/chains.jsx
@@ -1,20 +1,27 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {ChainContext} from '../../hooks/chainProvider';
-import { CONTRACTS } from '../../hooks/web3-constants';
+
+import styles from './chains.module.css';
 
 export default function Chains() {
   const {chains, selectedChain, selectChain, isSupported} = useContext(ChainContext);
-  return (<div style={{position: 'absolute', zIndex: 10, background: 'grey'}}>
-    <h1>{selectedChain.name}</h1>
 
-    <ul id="CHAINS" >
-      {Object.keys(chains)
-        .map(key => chains[key])
-        .filter(isSupported)
-        .map(c => (
-          <li key={c.contract_name} onClick={() => selectChain(c)}>{c.name}</li>
-        ))}
-    </ul>
+  const [open, setOpen] = useState(false);
+  return (<div style={{background: selectedChain.brandColor || 'black'}} className={styles.chainSelector}>
+    <div className={styles.selectedChain} onClick={() => setOpen(!open)}>{selectedChain.name}:</div>
+    <div className={[styles.chainDropDown, open ? styles.showChainSelector : styles.hideChainSelector].join(' ')}>
+      <ul id="chains" className={styles.chains}>
+        {Object.keys(chains)
+          .map(key => chains[key])
+          .filter(isSupported)
+          .map(c => (
+            <li key={c.contract_name} className={selectedChain.chainId === c.chainId ? styles.selected : undefined}><button onClick={() => {
+              setOpen(false);
+              selectChain(c);
+            }}>{c.name}</button></li>
+          ))}
+      </ul>
+    </div>
   </div>
   );
 }
