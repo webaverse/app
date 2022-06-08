4ca6e4373398f8e68b0ecc11319fd14cbe0464e5
Matthew Willox <matthew@cybermancers.ca>
2022-06-08 10:03:17 -0600

    hook RPC into chain switch

--- src/hooks/useChain.js
+++ src/hooks/useChain.js
@@ -1,8 +1,13 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {switchChain} from './rpcHelpers';
 import {DEFAULT_CHAIN, CHAINS} from './web3-constants';
 
 export default function useChain(network = DEFAULT_CHAIN) {
   const [selectedChain, setSelectedChain] = useState(network);
+
+  useEffect(() => {
+    switchChain(selectedChain.chainId);
+  }, [selectedChain]);
 
   return {
     selectedChain,
