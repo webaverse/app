26f7214b16e8057cc46fe715df2074f5fc4a7b1b
Matthew Willox <matthew@cybermancers.ca>
2022-06-08 11:08:40 -0600

    move isSupported to hook

--- src/hooks/useChain.js
+++ src/hooks/useChain.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {switchChain} from './rpcHelpers';
-import {DEFAULT_CHAIN, CHAINS} from './web3-constants';
+import {DEFAULT_CHAIN, CHAINS, CONTRACTS} from './web3-constants';
 
 export default function useChain(network = DEFAULT_CHAIN) {
   const [selectedChain, setSelectedChain] = useState(network);
@@ -8,8 +8,12 @@ export default function useChain(network = DEFAULT_CHAIN) {
   useEffect(() => {
     switchChain(selectedChain.chainId);
   }, [selectedChain]);
+  function isSupported(chain) {
+    return CONTRACTS[chain.contract_name] !== undefined;
+  }
 
   return {
+    isSupported,
     selectedChain,
     setSelectedChain,
     chains: CHAINS,
