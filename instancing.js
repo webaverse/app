d8d0e9761aeba24443f33e801c59c61a9bb6391b
Avaer Kazmer <a@modules.io>
2022-06-10 09:03:04 -0400

    Add instancing.js

--- instancing.js
+++ instancing.js
@@ -446,3 +446,15 @@ export class InstancedGeometryAllocator {
     } */
   }
 }
+
+export class BatchedMesh extends THREE.InstancedMesh {
+  constructor(geometry, material, allocator) {
+    super(geometry, material);
+    
+    this.isBatchedMesh = true;
+    this.allocator = allocator;
+  }
+	getDrawSpec(multiDrawStarts, multiDrawCounts, multiDrawInstanceCounts) {
+    this.allocator.getDrawSpec(multiDrawStarts, multiDrawCounts, multiDrawInstanceCounts);
+  }
+}
