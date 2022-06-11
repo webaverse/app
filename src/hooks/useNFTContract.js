986494a5fe904cc5c26f4007396825b88f458645
Matthew Willox <matthew@cybermancers.ca>
2022-06-11 14:35:20 -0600

    really watch for chain changes in the NFTcontract hook

--- src/hooks/useNFTContract.js
+++ src/hooks/useNFTContract.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useContext, useEffect, useState} from 'react';
 
 import {ethers, BigNumber} from 'ethers';
 
@@ -8,6 +8,7 @@ import {
   CONTRACT_ABIS,
 } from './web3-constants.js';
 import {FTABI, NFTABI} from '../abis/contract.jsx';
+import { ChainContext } from './chainProvider.jsx';
 
 const FILE_ADDRESS = 'https://ipfs.webaverse.com/';
 
@@ -22,20 +23,21 @@ const CONTRACT_EVENTS = {
   SINGLE_COLLABORATOR_REMOVED: 'SingleCollaboratorRemoved',
 };
 
-export default function useNFTContract(currentAccount, chain = DEFAULT_CHAIN) {
+export default function useNFTContract(currentAccount) {
+  const {selectedChain} = useContext(ChainContext);
   const [NFTcontractAddress, setNFTcontractAddress] = useState(null);
   const [FTcontractAddress, setFTcontractAddress] = useState(null);
 
   useEffect(() => {
     try {
-      const NFTcontractAddress = CONTRACTS[chain.contract_name].NFT;
-      const FTcontractAddress = CONTRACTS[chain.contract_name].FT;
+      const NFTcontractAddress = CONTRACTS[selectedChain.contract_name].NFT;
+      const FTcontractAddress = CONTRACTS[selectedChain.contract_name].FT;
       setNFTcontractAddress(NFTcontractAddress);
       setFTcontractAddress(FTcontractAddress);
     } catch (error) {
       console.log(error);
     }
-  }, [chain]);
+  }, [selectedChain]);
 
   const [minting, setMinting] = useState(false);
   const [showWallet, setShowWallet] = useState(false);
@@ -47,7 +49,7 @@ export default function useNFTContract(currentAccount, chain = DEFAULT_CHAIN) {
   }
 
   const getContract = async () => {
-    const simpleRpcProvider = new ethers.providers.StaticJsonRpcProvider(chain.rpcUrls[0]);
+    const simpleRpcProvider = new ethers.providers.StaticJsonRpcProvider(selectedChain.rpcUrls[0]);
     const contract = new ethers.Contract(NFTcontractAddress, NFTABI, simpleRpcProvider);
     return contract;
   };
@@ -73,25 +75,26 @@ export default function useNFTContract(currentAccount, chain = DEFAULT_CHAIN) {
         if (FTapproveres.transactionHash) {
           try {
             const NFTmintres = await NFTcontract.mint(currentAccount, hash, name, ext, description, 1);
-            // after mint transaction, refresh the website
-            onMint(NFTmintres);
-            } catch (err) {
-            setError(err.message);
+            callback(NFTmintres);
+          } catch (err) {
+            setError('Mint Failed');
           }
+          setMinting(false);
+        } else {
+          setError('transaction not approved');
           setMinting(false);
         }
       } else { // mintfee = 0 for Polygon not webaverse sidechain
         try {
           const NFTmintres = await NFTcontract.mint(currentAccount, hash, name, ext, description, 1);
-          onMint(NFTmintres);
-          // after mint transaction, refresh the website
+          callback(NFTmintres);
         } catch (err) {
-          setError(error.message);
+          setError('Mint Failed');
           setMinting(false);
         }
       }
     } catch (error) {
-      setError(error.message);
+      setError('Mint Failed');
       setMinting(false);
     }
   }
@@ -119,7 +122,7 @@ export default function useNFTContract(currentAccount, chain = DEFAULT_CHAIN) {
 
   async function getTokens() {
     const tokenIdsOf = await getTokenIdsOf();
-    return await Promise.all(tokenIdsOf.map(async (tokenId) => {
+    return await Promise.all(tokenIdsOf.map(async tokenId => {
       const token = await getToken(tokenId);
       return token;
     }));
