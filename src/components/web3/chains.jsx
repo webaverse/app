697a96655da2ba12477973ee19336a7088d5309b
Matthew Willox <matthew@cybermancers.ca>
2022-06-11 14:43:00 -0600

    display all the different chain types and support

--- src/components/web3/chains.jsx
+++ src/components/web3/chains.jsx
@@ -1,5 +1,7 @@
 import React, {useContext, useState} from 'react';
 import {ChainContext} from '../../hooks/chainProvider';
+import { isChainSupported } from '../../hooks/useChain';
+import { CHAIN_TYPE } from '../../hooks/web3-constants';
 
 import styles from './chains.module.css';
 
@@ -15,22 +17,27 @@ function DownArrow() {
   </svg>;
 }
 
+function Supported({chain}) {
+  return <>{!isChainSupported(chain) && '❌' } {chain.type === CHAIN_TYPE.TEST && '🧪'}</>
+}
+
 export default function Chains() {
-  const {chains, selectedChain, selectChain, isChainSupported} = useContext(ChainContext);
+  const {chains, selectedChain, selectChain} = useContext(ChainContext);
+  const {brandColor} = selectedChain;
 
   const [open, setOpen] = useState(false);
-  return (<div style={{background: selectedChain.brandColor || 'black'}} className={styles.chainSelector}>
-    <div className={styles.selectedChain} onClick={() => setOpen(!open)}>{open ? <UpArrow /> : <DownArrow />} {selectedChain.name}</div>
+  return (<div style={{background: brandColor || 'black'}} className={styles.chainSelector}>
+    <div className={styles.selectedChain} onClick={() => setOpen(!open)}>{open ? <UpArrow /> : <DownArrow />} <Supported chain={selectedChain} /> {selectedChain.name}</div>
     <div className={[styles.chainDropDown, open ? styles.showChainSelector : styles.hideChainSelector].join(' ')}>
       <ul id="chains" className={styles.chains}>
         {Object.keys(chains)
           .map(key => chains[key])
-          .filter(isChainSupported)
           .map(c => (
             <li key={c.contract_name} className={selectedChain.chainId === c.chainId ? styles.selected : undefined}><button onClick={() => {
               selectChain(c);
               setOpen(false);
-            }}>{c.name}</button></li>
+            }}>
+              <Supported chain={c} /> {c.name}</button></li>
           ))}
       </ul>
     </div>
