d416a26c5ec9bac59f725612213371882de1b96f
gonnavis <gonnavis@163.com>
2022-06-14 04:23:28 +0800

    AnimationNodeOverwrite: add arg: filters, instead of hardcoded.

--- avatars/WebaverseAnimationMixer.js
+++ avatars/WebaverseAnimationMixer.js
@@ -36,8 +36,8 @@ class WebaverseAnimationMixer extends EventDispatcher {
     return motion;
   }
 
-  createNode(NodeClass, name) {
-    const node = new NodeClass(name, this);
+  createNode(NodeClass, name, argo) {
+    const node = new NodeClass(name, this, argo);
     this.nodes.push(node);
     return node;
   }
