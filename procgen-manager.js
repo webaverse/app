102908a869e9a545180c4c5240e13b89d467b070
Avaer Kazmer <a@modules.io>
2022-06-19 04:50:14 -0400

    Procgen manager support seeding

--- procgen-manager.js
+++ procgen-manager.js
@@ -1,21 +1,13 @@
-// import * as THREE from 'three';
-// import {makeId} from './util.js';
+import {murmurhash3} from './procgen/procgen.js';
 import {defaultChunkSize} from './constants.js';
-// import metaversefile from 'metaversefile';
-// import { terrainVertex, terrainFragment } from './shaders/terrainShader.js';
-// import physics from './physics-manager.js';
 import {DcWorkerManager} from './dc-worker-manager.js';
-// import {GeometryAllocator} from './instancing.js';
-
-// const localVector = new THREE.Vector3();
-
-const numWorkers = 4;
 
 class ProcGenInstance {
-  constructor(instance) {
+  constructor(instance) { 
+    const seed = typeof instance === 'string' ? murmurhash3(instance) : Math.floor(Math.random() * 0xFFFFFF);
     this.dcWorkerManager = new DcWorkerManager({
       chunkSize: defaultChunkSize,
-      seed: Math.floor(Math.random() * 0xFFFFFF),
+      seed,
       instance,
     });
   }
