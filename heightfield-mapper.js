07e720d4f93902a278a26c3a08229d8957963f1d
Avaer Kazmer <a@modules.io>
2022-06-20 04:39:10 -0400

    Heightfield mapper track coords

--- heightfield-mapper.js
+++ heightfield-mapper.js
@@ -1,6 +1,10 @@
 import * as THREE from 'three';
 // import {getRenderer} from './renderer.js';
 
+const localVector2D = new THREE.Vector2();
+const localVector2D2 = new THREE.Vector2();
+const localVector2D3 = new THREE.Vector2();
+
 const maxAnisotropy = 16;
 
 const _makeHeightfieldRenderTarget = (w, h) => new THREE.WebGLRenderTarget(w, h, {
@@ -31,5 +35,16 @@ export class HeightfieldMapper {
     this.heightfieldFourTapRenderTarget = _makeHeightfieldRenderTarget(terrainSize, terrainSize);
 
     this.heightfieldMinPosition = new THREE.Vector2();
+  }
+  updateCoord(min2xCoord, target) {
+    const oldHeightfieldPosition = localVector2D.copy(this.heightfieldMinPosition);
+    const newHeightfieldPosition = localVector2D2.set(min2xCoord.x, min2xCoord.z)
+      .multiplyScalar(this.chunkSize);
+    const delta = target.copy(newHeightfieldPosition)
+      .sub(oldHeightfieldPosition);
+    
+    this.heightfieldMinPosition.copy(newHeightfieldPosition);
+
+    return delta;
   }
 }
