349ddb61d6a80c7344281a31e57e4c96324b7f4b
Kevin <klattkev@gmail.com>
2022-06-21 18:23:52 +0800

    vr2

--- ai/lore/constants.js
+++
@@ -1,4 +0,0 @@
-export const defaultPlayerName = 'Anon';
-export const defaultPlayerBio = 'A new player. Not much is known about them.';
-export const defaultObjectName = 'Thing';
-export const defaultObjectDescription = 'A thing. Not much is known about it.';
