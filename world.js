23848c066fe7a9ebd808f53a4ea17480c0620180
shawticus <shawmakesmagic@gmail.com>
2022-06-25 05:49:54 -0700

    Remove debug event

--- world.js
+++ world.js
@@ -78,16 +78,6 @@ export class World {
 
     this.appManager.bindState(appsArray);
 
-    // Handy debug function to see the state
-    // Delete this eventually. For now press escape to see world state
-    document.addEventListener('keydown', event => {
-      if (event.key === 'Escape') {
-        console.log(state.getArray('world'));
-        console.log(state.getArray('players'));
-        console.log(scene);
-      }
-    });
-
     const localPlayer = getLocalPlayer();
     localPlayer.bindState(state.getArray(playersMapName));
   }
@@ -109,16 +99,6 @@ export class World {
     this.wsrtc = new WSRTC(u, {
       localPlayer,
       crdtState: state,
-    });
-
-    // Handy debug function to see the state
-    // Delete this eventually. For now press escape to see world state
-    document.addEventListener('keydown', event => {
-      if (event.key === 'Escape') {
-        console.log(state.getArray('world'));
-        console.log(state.getArray('players'));
-        console.log(scene);
-      }
     });
 
     // This is called when the websocket connection opens, i.e. server is connectedw
