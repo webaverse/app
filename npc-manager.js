44e1a481b69e668924b998475fd67ce989e07d88
gonnavis <gonnavis@163.com>
2022-07-15 02:26:22 +0800

    dev

--- npc-manager.js
+++ npc-manager.js
@@ -36,6 +36,9 @@ class NpcManager extends EventTarget {
     });
     npcPlayer.name = name;
 
+    if (!window.npcPlayers) window.npcPlayers = []
+    window.npcPlayers.push(npcPlayer);
+
     let matrixNeedsUpdate = false;
     if (position) {
       npcPlayer.position.copy(position);
