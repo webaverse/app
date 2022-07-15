49caf544c08f6c339f1bb6e39323d08429d30200
zepvalue <zepvalue@gmail.com>
2022-07-15 02:18:18 +0200

    clearRemotePlayers handled by the universe

--- players-manager.js
+++ players-manager.js
@@ -14,27 +14,28 @@ class PlayersManager {
     
     this.unbindStateFn = null;
   }
-  getPlayersState() {
-    return this.playersArray;
-  }
-  unbindState() {
+  clearRemotePlayers() {
     const lastPlayers = this.playersArray;
     if (lastPlayers) {
       const playerSpecs = lastPlayers.toJSON();
       const nonLocalPlayerSpecs = playerSpecs.filter(p => {
         return p.playerId !== getLocalPlayer().playerId;
       });
-      // Destroy all remote players if they still exist
       for (const nonLocalPlayer of nonLocalPlayerSpecs) {
         const remotePlayer = this.remotePlayers.get(nonLocalPlayer.playerId);
         remotePlayer.destroy();
         this.remotePlayers.delete(nonLocalPlayer.playerId);
       }
+    }
+  }
+  getPlayersState() {
+    return this.playersArray;
+  }
+  unbindState() {
       this.unbindStateFn();
       this.playersArray = null;
       this.unbindStateFn = null;
     }
-  }
   bindState(nextPlayersArray) {
     this.unbindState();
     
