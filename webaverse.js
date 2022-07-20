cb3919c5674378fdecf72124660f1384388cd00c
gonnavis <gonnavis@163.com>
2022-07-20 08:05:46 +0800

    dev: Add domInfo.

--- webaverse.js
+++ webaverse.js
@@ -296,6 +296,8 @@ export default class Webaverse extends EventTarget {
     const animate = (timestamp, frame) => {
       performanceTracker.startFrame();
 
+      window.domInfo.innerHTML = '';
+
       const _frame = () => {
         timestamp = timestamp ?? performance.now();
         const timeDiff = timestamp - lastTimestamp;
