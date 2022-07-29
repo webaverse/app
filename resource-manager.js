f89275b89c7bb437a9d6c89036a32b235b1194fd
Avaer Kazmer <a@modules.io>
2022-07-29 16:51:06 -0400

    Update resource-manager.js

--- resource-manager.js
+++ resource-manager.js
@@ -3,9 +3,47 @@ import {makePromise} from './util.js';
 class ResourceManager {
   constructor() {
     this.loadPromise = makePromise();
+
+    this.pendingLoads = 0;
+    this.postLoads = [];
+  }
+  add(promise) {
+    (async () => {
+      try {
+        await promise;
+      } catch(err) {
+        console.warn('resource load error', err);
+        // debugger;
+        this.loadPromise.reject(err);
+      }
+      
+      if (--this.pendingLoads === 0) {
+        this.loadPromise.accept();
+        for (const fn of this.postLoads) {
+          fn();
+        }
+
+        this.loadPromise = makePromise();
+        this.postLoads.length = 0;
+      }
+    })();
+    this.pendingLoads++;
+  }
+  addAll(promises) {
+    for (const promise of promises) {
+      this.add(promise);
+    }
+  }
+  addPost(promise) {
+    this.postLoads.push(promise);
+  }
+  addPostAll(promises) {
+    for (const promise of promises) {
+      this.addPost(promise);
+    }
   }
   waitForLoad() {
-
+    return this.loadPromise;
   }
 }
 const resourceManager = new ResourceManager();
