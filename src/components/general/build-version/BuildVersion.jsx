8070a618b428c18adf92e1d27dd2fc8d5f686b6d
Avaer Kazmer <a@modules.io>
2022-08-04 22:04:19 -0400

    Add new build code and styles

--- src/components/general/build-version/BuildVersion.jsx
+++ src/components/general/build-version/BuildVersion.jsx
@@ -1,16 +1,60 @@
 // import * as THREE from 'three';
 import React from 'react';
+import classnames from 'classnames';
 import styles from './build-version.module.css';
 
 //
 
-export const BuildVersion = () => {
+export const BuildVersionLayer = ({
+  color,
+  backgroundImage,
+  clear = false,
+  animate = false,
+}) => {
+  /* console.log('got classname', {
+    classnames: classnames(styles.buildVersion, clear ? styles.clear : null),
+    backgroundImage,
+  }); */
+
+  /* console.log('got colors', {
+    color,
+    backgroundImage,
+    // we need to special case dash prefixed properties in React
+    // '-webkit-text-fill-color',
+    WebkitTextFillColor: color,
+  }); */
+
   return (
-    <div className={styles.buildVersion}>
-    <div className={styles.wrap}>
+    <div
+      className={classnames(
+        styles.buildVersion,
+        clear ? styles.clear : null,
+        animate ? styles.animate : null,
+      )}
+      style={{
+        color,
+        // backgroundImage,
+        // background: `url(${backgroundImage})`,
+        backgroundImage: backgroundImage ? `url(${backgroundImage})` : null,
+        // we need to special case dash prefixed properties in React
+        // '-webkit-text-fill-color',
+        // WebkitTextFillColor: color,
+      }}
+    >
+      <div className={styles.wrap}>
         <div className={styles.text}>Dev Alpha</div>
         <div className={styles.subtext}>Season 0.51</div>
       </div>
+    </div>
+  );
+};
+
+export const BuildVersion = () => {
+  return (
+    <div className={styles.buildVersions}>
+      <BuildVersionLayer color="#000000" />
+      {/* <BuildVersionLayer color="#FFFFFF" backgroundImage="images/ui/line-highlight-white.svg" clear animate /> */}
+      <BuildVersionLayer color="#FFFFFF" backgroundImage="images/ui/line-slash.svg" clear animate />
     </div>
   );
 };
