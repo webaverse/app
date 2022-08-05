8790654a4baa27735e8801d1bc3cf807424b5116
tcm390 <tcm390@nyu.edu>
2022-08-04 23:57:11 -0400

    stick diorama camera around player

--- diorama.js
+++ diorama.js
@@ -702,6 +702,10 @@ const createPlayerDiorama = ({
             localVector2.set(0, cameraOffset.y, 0)
               .applyQuaternion(targetQuaternion)
           );
+          if(this.player){
+            const headPosition = localVector3.setFromMatrixPosition(this.player.avatar.modelBones.Head.matrixWorld);
+            sideCamera.position.y -= this.player.position.y - headPosition.y;
+          }
           
           sideCamera.updateMatrixWorld();
         }
