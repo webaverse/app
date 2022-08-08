aed1045e9f0389109396d47c80eeca855a417085
tcm390 <tcm390@nyu.edu>
2022-08-08 16:16:20 -0400

    set character-sfx after isLocalPlayer has been set

--- character-controller.js
+++ character-controller.js
@@ -157,7 +157,6 @@ class PlayerBase extends THREE.Object3D {
     this.name = defaultPlayerName;
     this.bio = defaultPlayerBio;
     this.characterHups = new CharacterHups(this);
-    // this.characterSfx = new CharacterSfx(this);
     this.characterFx = new CharacterFx(this);
     this.characterHitter = new CharacterHitter(this);
     this.characterBehavior = new CharacterBehavior(this);
