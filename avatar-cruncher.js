38cf4781281325022f33d6159d3a42ed80fe216e
Avaer Kazmer <a@modules.io>
2022-08-10 23:49:16 -0400

    Avatar cruncher rewrite

--- avatar-cruncher.js
+++ avatar-cruncher.js
@@ -1,67 +1,124 @@
 import * as THREE from 'three';
-import {getMergeableObjects, mergeGeometryTextureAtlas} from './geometry-texture-atlas.js';
+import {getMergeableObjects, getBones, mergeGeometryTextureAtlas} from './avatar-optimizer.js';
 import exporters from './exporters.js';
 
 const defaultTextureSize = 4096;
+const textureTypes = [
+  'map',
+  'emissiveMap',
+  'normalMap',
+];
+const getObjectKey = () => '';
 
 export const crunchAvatarModel = async (model, options = {}) => {
   const textureSize = options.textureSize ?? defaultTextureSize;
 
-  const textureTypes = [
-    'map',
-    'emissiveMap',
-    'normalMap',
-  ];
-
-  const getObjectKey = () => '';
   const mergeables = getMergeableObjects(model, getObjectKey);
-  const mergeable = mergeables[0];
-  const {
-    skeletons,
-    morphTargetDictionaryArray,
-    morphTargetInfluencesArray,
-  } = mergeable;
-  const {
-    // atlas,
-    // atlasImages,
-    // attributeLayouts,
-    // morphAttributeLayouts,
-    geometry,
-    atlasTextures,
-  } = mergeGeometryTextureAtlas(mergeable, textureSize);
 
-  // create material
-  const material = new THREE.MeshBasicMaterial();
-  for (const k of textureTypes) {
-    const t = atlasTextures[k];
-    material[k] = t;
+  const _mergeMesh = (mergeable, mergeableIndex) => {
+    const {
+      type,
+      material,
+      // geometries,
+      // maps,
+      // emissiveMaps,
+      // normalMaps,
+      skeletons,
+      morphTargetDictionaryArray,
+      morphTargetInfluencesArray,
+    } = mergeable;
+    const {
+      // atlas,
+      // atlasImages,
+      // attributeLayouts,
+      // morphAttributeLayouts,
+      geometry,
+      atlasTextures,
+    } = mergeGeometryTextureAtlas(mergeable, textureSize);
+
+    // const m = material;
+    const m = new THREE.MeshStandardMaterial();
+    console.log('swap materials crunch', {material, m});
+    m.roughness = 1;
+    // m.alphaTest = 0.1;
+    // m.side = THREE.BackSide;
+    // m.side = THREE.DoubleSide;
+    // m.alphaToCoverage = true;
+    // m.alphaTest = 1;
+    // m.transparent = false;
+    m.transparent = true;
+    const _updateMaterial = () => {
+      if (atlasTextures) {
+        for (const textureType of textureTypes) {
+          const atlasTexture = atlasTextures[textureType];
+
+          if (atlasTexture) {
+            m[textureType] = atlasTexture;
+            if (m.uniforms) {
+              m.uniforms[textureType].value = atlasTexture;
+              m.uniforms[textureType].needsUpdate = true;
+            }
+          }
+        }
+      }
+      m.needsUpdate = true;
+    };
+    _updateMaterial();
+
+    const _makeMesh = () => {
+      if (type === 'mesh') {
+        const mesh = new THREE.Mesh(geometry, m);
+        return mesh;
+      } else if (type === 'skinnedMesh') {
+        const skinnedMesh = new THREE.SkinnedMesh(geometry, m);
+        skinnedMesh.skeleton = skeletons[0];
+        skinnedMesh.morphTargetDictionary = morphTargetDictionaryArray[0];
+        skinnedMesh.morphTargetInfluences = morphTargetInfluencesArray[0];
+        // skinnedMesh.updateMorphTargets();
+        return skinnedMesh;
+      } else {
+        throw new Error(`unknown type ${type}`);
+      }
+    };
+    const mesh = _makeMesh();
+    // console.log('got mesh', mesh);
+
+    return mesh;
+  };
+  const mergedMeshes = mergeables.map((mergeable, i) => _mergeMesh(mergeable, i));
+
+  // construct a new object with the merged meshes
+  const object = new THREE.Object3D();
+  for (const mesh of mergedMeshes) {
+    object.add(mesh);
   }
-  material.roughness = 1;
-  material.alphaTest = 0.1;
-  material.transparent = true;
+  // also need to keep the bones
+  const bones = getBones(model);
+  for (const bone of bones) {
+    object.add(bone);
+  }
 
-  // create mesh
-  const crunchedModel = new THREE.SkinnedMesh(geometry, material);
-  crunchedModel.skeleton = skeletons[0];
-  crunchedModel.morphTargetDictionary = morphTargetDictionaryArray[0];
-  crunchedModel.morphTargetInfluences = morphTargetInfluencesArray[0];
-  crunchedModel.frustumCulled = false;
+  // XXX this should anti-index flattened index ranges for the multi-materials case
+
+  // return object;
 
   const glbData = await new Promise((accept, reject) => {
     const {gltfExporter} = exporters;
-    gltfExporter.parse(crunchedModel, function onCompleted(arrayBuffer) {
-      accept(arrayBuffer);
-    }, function onError(error) {
-      reject(error);
-    },
-    {
-      binary: true,
-      // onlyVisible: false,
-      // forceIndices: true,
-      // truncateDrawRange: false,
-      includeCustomExtensions: true,
-    },
-  );
+    gltfExporter.parse(
+      object,
+      function onCompleted(arrayBuffer) {
+        accept(arrayBuffer);
+      }, function onError(error) {
+        reject(error);
+      },
+      {
+        binary: true,
+        // onlyVisible: false,
+        // forceIndices: true,
+        // truncateDrawRange: false,
+        includeCustomExtensions: true,
+      },
+    );
   });
   return glbData;
 };
