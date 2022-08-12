19323591e0c3b763c99eabc40541acfcd3cd3e59
Avaer Kazmer <a@modules.io>
2022-08-12 01:20:16 -0400

    Offscreen engine proxy/direct cleanup

--- offscreen-engine-manager.js
+++ offscreen-engine-manager.js
@@ -1,4 +1,9 @@
-import {OffscreenEngine} from './offscreen-engine.js';
+import {
+  OffscreenEngineProxy,
+  // OffscreenEngineDirect,
+} from './offscreen-engine.js';
 
-const offscreenEngineManager = new OffscreenEngine();
+// const isOffscreenEngine = location.pathname === '/engine.html';
+// const offscreenEngineManager = isOffscreenEngine ? new OffscreenEngineDirect() : new OffscreenEngineProxy();
+const offscreenEngineManager = new OffscreenEngineProxy();
 export default offscreenEngineManager;
