4eb12ba6227413d26a2766a5a07101f3169d151a
Avaer Kazmer <a@modules.io>
2022-08-25 03:12:21 -0400

    Add camera panel code

---
+++ src/components/editor-mode/ai-menu/panels/camera-ai-panel.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useRef, useContext } from 'react';
+// import { AppContext } from '../../../app';
+
+import styles from './camera-ai-panel.module.css';
+
+export function CameraAiPanel() {
+    // const audioRef = useRef();
+  
+    //
+  
+    const _capture = () => {
+      // XXX
+    };
+  
+    //
+    
+    return (
+          <div className={styles.panel}>
+              <div className={styles.buttons}>
+                  <button className={styles.button} onClick={_capture}>Capture</button>
+              </div>
+          </div>
+      );
+  }
