eb29a05d5b1a35de15535d92feb47622d72933f0
Avaer Kazmer <a@modules.io>
2022-08-25 03:11:54 -0400

    Add audio panel code

---
+++ src/components/editor-mode/ai-menu/panels/audio-ai-panel.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, useRef, useContext } from 'react';
+// import { AppContext } from '../../../app';
+
+import styles from './audio-ai-panel.module.css';
+
+export function AudioAiPanel() {
+  /* const { state, setState } = useContext( AppContext );
+  const [page, setPage] = useState('input');
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+  const [compiling, setCompiling] = useState(false);
+  const [ai, setAi] = useState(null);
+  const inputTextarea = useRef();
+  const outputTextarea = useRef(); */
+  const audioRef = useRef();
+
+  //
+
+  const _generate = () => {
+    // XXX
+  };
+
+  //
+  
+  return (
+        <div className={styles.panel}>
+            <textarea className={styles.textarea} placeholder="insects chirp in a windy field"></textarea>
+            <audio className={styles.audio} ref={audioRef} />
+            <div className={styles.buttons}>
+                <button className={styles.button} onClick={_generate}>Generate</button>
+            </div>
+        </div>
+    );
+}
