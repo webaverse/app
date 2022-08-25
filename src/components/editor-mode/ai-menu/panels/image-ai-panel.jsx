a71dfafdc59ecdb25f1d6b6a77c3e6defd167c7b
Avaer Kazmer <a@modules.io>
2022-08-25 08:16:37 -0400

    Image gen ui

--- src/components/editor-mode/ai-menu/panels/image-ai-panel.jsx
+++ src/components/editor-mode/ai-menu/panels/image-ai-panel.jsx
@@ -1,12 +1,20 @@
-import React, { useState, useEffect, useRef, useContext } from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import classnames from 'classnames';
-// import { AppContext } from '../../../app';
-
+import imageAI from '../../../../../ai/image/image-ai';
+import materialColors from '../../../../../material-colors';
 import styles from './image-ai-panel.module.css';
 
 const size = 512;
+const presetNames = Object.keys(imageAI.generator);
+const baseColors = Object.keys(materialColors)
+  .map(k => materialColors[k][400].slice(1))
+  .concat([
+    'FFFFFF',
+    '000000',
+  ]);
 
 export function ImageAiPanel() {
+    const [selectedColor, setSelectedColor] = useState(baseColors[0]);
     const canvasRef = useRef();
 
     //
@@ -39,9 +47,40 @@ export function ImageAiPanel() {
     return (
         <div className={classnames(styles.panel, styles.imageAiPanel)}>
             <textarea className={styles.textarea} placeholder="mysterious forest"></textarea>
-            <canvas width={size} height={size} className={styles.canvas} ref={canvasRef} />
-            <div className={styles.buttons}>
-                <button className={styles.button} onClick={_generate}>Generate image</button>
+            <div className={styles.wrap}>
+                <div className={classnames(styles.items, styles.leftPanel)}>
+                    <div className={styles.item}>Draw</div>
+                    <div className={styles.colors}>
+                        {baseColors.map((color, i) => {
+                            return (
+                                <div
+                                    className={classnames(styles.color, selectedColor === color ? styles.selected : null)}
+                                    style={{
+                                        backgroundColor: `#${color}`,
+                                    }}
+                                    onClick={e => {
+                                        setSelectedColor(color);
+                                    }}
+                                    key={i}
+                                />
+                            );
+                        })}
+                    </div>
+                    <div className={styles.item}>Erase</div>
+                    <div className={styles.item}>Move</div>
+                    <div className={styles.item}>Cut</div>
+                </div>
+                <div className={classnames(styles.items, styles.rightPanel)}>
+                    {presetNames.map((preset, i) => {
+                        return (
+                            <div className={styles.item} key={i}>{preset}</div>
+                        );
+                    })}
+                </div>
+                <canvas width={size} height={size} className={styles.canvas} ref={canvasRef} />
+                <div className={styles.buttons}>
+                    <button className={styles.button} onClick={_generate}>Generate image</button>
+                </div>
             </div>
         </div>
     );
