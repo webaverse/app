d8ea040af89d87f8b0bc7564e2697459019b3e9b
Avaer Kazmer <a@modules.io>
2022-08-25 06:40:00 -0400

    Update AI UI

--- src/components/editor-mode/ai-menu/AiMenu.jsx
+++ src/components/editor-mode/ai-menu/AiMenu.jsx
@@ -84,42 +84,54 @@ export function AiMenu () {
                         setPanel('image');
                     }}>
                         <div className={styles.block}>
-                            <img src={'/images/ui/paintbrush.svg'} className={styles.icon} />
+                            <div className={styles.inner}>
+                                <img src={'/images/ui/paintbrush.svg'} className={styles.icon} />
+                            </div>
                         </div>
                     </div>
                     <div className={classnames(styles.panelButton, panel === 'audio' ? styles.selected : null)} onClick={() => {
                         setPanel('audio');
                     }}>
                         <div className={styles.block}>
-                            <img src={'/images/ui/audio-speaker.svg'} className={styles.icon} />
+                            <div className={styles.inner}>
+                                <img src={'/images/ui/audio-speaker.svg'} className={styles.icon} />
+                            </div>
                         </div>
                     </div>
                     <div className={classnames(styles.panelButton, panel === 'model' ? styles.selected : null)} onClick={() => {
                         setPanel('model');
                     }}>
                         <div className={styles.block}>
-                            <img src={'/images/ui/sword.svg'} className={styles.icon} />
+                            <div className={styles.inner}>
+                                <img src={'/images/ui/sword.svg'} className={styles.icon} />
+                            </div>
                         </div>
                     </div>
                     <div className={classnames(styles.panelButton, panel === 'camera' ? styles.selected : null)} onClick={() => {
                         setPanel('camera');
                     }}>
                         <div className={styles.block}>
-                            <img src={'/images/ui/camera.svg'} className={styles.icon} />
+                            <div className={styles.inner}>
+                                <img src={'/images/ui/camera.svg'} className={styles.icon} />
+                            </div>
                         </div>
                     </div>
                     <div className={classnames(styles.panelButton, panel === 'lore' ? styles.selected : null)} onClick={() => {
                         setPanel('lore');
                     }}>
                         <div className={styles.block}>
-                            <img src={'/images/ui/brain.svg'} className={styles.icon} />
+                            <div className={styles.inner}>
+                                <img src={'/images/ui/brain.svg'} className={styles.icon} />
+                            </div>
                         </div>
                     </div>
                     <div className={classnames(styles.panelButton, panel === 'code' ? styles.selected : null)} onClick={() => {
                         setPanel('code');
                     }}>
                         <div className={styles.block}>
-                            <img src={'/images/ui/magic-scroll.svg'} className={styles.icon} />
+                            <div className={styles.inner}>
+                                <img src={'/images/ui/magic-scroll.svg'} className={styles.icon} />
+                            </div>
                         </div>
                     </div>
                 </div>
