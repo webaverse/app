86eed0ea8cf85cc8ececc861b9d6ce5fb4911494
Avaer Kazmer <a@modules.io>
2022-08-25 20:40:44 -0400

    Audio AI design cleanup

--- src/components/editor-mode/ai-menu/panels/audio-ai-panel.jsx
+++ src/components/editor-mode/ai-menu/panels/audio-ai-panel.jsx
@@ -1,29 +1,30 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
+import classnames from 'classnames';
 // import { AppContext } from '../../../app';
 
 import styles from './audio-ai-panel.module.css';
 
 export function AudioAiPanel() {
-  /* const { state, setState } = useContext( AppContext );
-  const [page, setPage] = useState('input');
-  const [input, setInput] = useState('');
-  const [output, setOutput] = useState('');
-  const [compiling, setCompiling] = useState(false);
-  const [ai, setAi] = useState(null);
-  const inputTextarea = useRef();
-  const outputTextarea = useRef(); */
-  const audioRef = useRef();
+    /* const { state, setState } = useContext( AppContext );
+    const [page, setPage] = useState('input');
+    const [input, setInput] = useState('');
+    const [output, setOutput] = useState('');
+    const [compiling, setCompiling] = useState(false);
+    const [ai, setAi] = useState(null);
+    const inputTextarea = useRef();
+    const outputTextarea = useRef(); */
+    const audioRef = useRef();
 
-  //
+    //
 
-  const _generate = () => {
-    // XXX
-  };
+    const _generate = () => {
+        // XXX
+    };
 
-  //
+    //
   
-  return (
-        <div className={styles.panel}>
+    return (
+        <div className={classnames(styles.panel, styles.audioAiPanel)}>
             <textarea className={styles.textarea} placeholder="insects chirp in a windy field"></textarea>
             <audio className={styles.audio} ref={audioRef} />
             <div className={styles.buttons}>
