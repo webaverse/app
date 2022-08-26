a0bb67ba15aaf7f0ae3b0c1a9e4bc8de68e51cde
Avaer Kazmer <a@modules.io>
2022-08-25 21:23:40 -0400

    Audio AI integration work

--- src/components/editor-mode/ai-menu/panels/audio-ai-panel.jsx
+++ src/components/editor-mode/ai-menu/panels/audio-ai-panel.jsx
@@ -1,34 +1,65 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
 import classnames from 'classnames';
-// import { AppContext } from '../../../app';
 
 import styles from './audio-ai-panel.module.css';
 
+import audioAI from '../../../../../ai/audio/audio-ai';
+
+const defaultPrompt = `insects chirp in a windy field`;
+
 export function AudioAiPanel() {
-    /* const { state, setState } = useContext( AppContext );
-    const [page, setPage] = useState('input');
-    const [input, setInput] = useState('');
-    const [output, setOutput] = useState('');
-    const [compiling, setCompiling] = useState(false);
-    const [ai, setAi] = useState(null);
-    const inputTextarea = useRef();
-    const outputTextarea = useRef(); */
-    const audioRef = useRef();
+    const [prompt, setPrompt] = useState('');
+    const [generating, setGenerating] = useState(false);
+    const [audioBlobUrl, setAudioBlobUrl] = useState('');
 
     //
 
-    const _generate = () => {
-        // XXX
+    const _generate = async () => {
+        if (!generating) {
+            setGenerating(true);
+
+            try {
+                const localPrompt = prompt || defaultPrompt;
+                const audioBlob = await audioAI.txt2sound(localPrompt);
+                
+                _clear();
+                
+                const newAudioBlobUrl = URL.createObjectURL(audioBlob);
+                setAudioBlobUrl(newAudioBlobUrl);
+            } finally {
+                setGenerating(false);
+            }
+        }
     };
+    const _clear = () => {
+        if (audioBlobUrl) {
+            URL.revokeObjectURL(audioBlobUrl);
+            setAudioBlobUrl('');
+        }
+    }
 
     //
   
     return (
         <div className={classnames(styles.panel, styles.audioAiPanel)}>
-            <textarea className={styles.textarea} placeholder="insects chirp in a windy field"></textarea>
-            <audio className={styles.audio} ref={audioRef} />
+            <textarea
+                className={styles.textarea}
+                value={prompt}
+                onChange={e => {
+                    setPrompt(e.target.value);
+                }}
+                placeholder={defaultPrompt}
+            ></textarea>
+            {
+                audioBlobUrl ? (
+                    <audio className={styles.audio} src={audioBlobUrl} controls loop />
+                ) : (
+                    <div className={styles.audioPlaceholder} />
+                )
+            }
             <div className={styles.buttons}>
-                <button className={styles.button} onClick={_generate}>Generate sound</button>
+                <button className={styles.button} onClick={_generate} disabled={generating}>Generate sound</button>
+                <button className={styles.button} onClick={_clear}>clear</button>
             </div>
         </div>
     );
