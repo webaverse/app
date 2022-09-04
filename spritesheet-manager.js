4112ce45910937a9e6ecb02d340a5d7369dae08a
NPatel10 <nayanpatel.np@yahoo.com>
2022-09-04 15:25:53 +0530

    Created Asset Load Manager to only load fixed number of assets simultaniously

--- spritesheet-manager.js
+++ spritesheet-manager.js
@@ -1,12 +1,71 @@
 import {createObjectSprite} from './object-spriter.js';
 import offscreenEngineManager from './offscreen-engine-manager.js';
 
-class SpritesheetManager {
-  maxConcurrentLoading = 5;
-  currentlyLoading = 0;
-  waitQueue = [];
+class AssetLoader {
+  isIdle = true;
+
+  async load(promiseFunc) {
+    this.isIdle = false;
+    await promiseFunc();
+    this.isIdle = true;
+  }
+}
+
+class AssetLoadManager {
+  pushQueue = [];
+  pushQueueLock = Promise.resolve();
+  getQueue = [];
+  getIndex = -1;
+
+  maxLoaderCount = 5;
+  loaderPool = [];
+  intervalID = 0;
+
+  schedulerInterval = 10;
 
   constructor() {
+    for (let i = 0; i < this.maxLoaderCount; i++) {
+      this.loaderPool.push(new AssetLoader())
+    }
+  }
+
+  poll() {
+    this.intervalID = setInterval(() => {
+      if (this.getQueue.length > 0) {
+        for (let i = 0; i < this.loaderPool.length; i++) {
+          if (this.loaderPool[i].isIdle && this.getIndex < this.getQueue.length - 1) {
+            this.loaderPool[i].load(this.getQueue[++this.getIndex]);
+          }
+        }
+      } else if (this.pushQueue.length <= 0) {
+        clearInterval(this.intervalID);
+        this.intervalID = 0;
+      }
+
+      if (this.getIndex > -1) {
+        this.getQueue.splice(0, this.getIndex + 1);
+        this.getIndex = -1;
+      }
+
+      const t = this.pushQueue
+      this.pushQueue = this.getQueue;
+      this.getQueue = t;
+    }, this.schedulerInterval);
+  }
+
+  putInQueue(promise) {
+    this.pushQueueLock = this.pushQueueLock.then(() => {
+      this.pushQueue.push(promise);
+      if (this.intervalID === 0) this.poll();
+    });
+
+    return this.pushQueueLock;
+  }
+}
+
+class SpritesheetManager {
+  constructor() {
+    this.loadManager = new AssetLoadManager();
     this.spritesheetCache = new Map();
     this.getSpriteSheetForAppUrlInternal = offscreenEngineManager.createFunction([
       `\
@@ -35,33 +94,17 @@ class SpritesheetManager {
   }
 
   async getSpriteSheetForAppUrlAsync(appUrl, opts) {
-    const spritesheet = this.spritesheetCache.get(appUrl);
+    let spritesheet = this.spritesheetCache.get(appUrl);
 
-    if (spritesheet) return spritesheet;
-
-    return new Promise((resolve) => {
-      this.waitQueue.push({ url: appUrl, opts, resolve });
-      this.loadSpriteSheetFromQueue();
-    });
-  }
-
-  loadSpriteSheetFromQueue() {
-    if (this.currentlyLoading >= this.maxConcurrentLoading || this.waitQueue.length <= 0) return;
-
-    const loopCount = Math.min(this.maxConcurrentLoading, this.waitQueue.length);
-    for (let i = 0; i < loopCount; i++) {
-      this.currentlyLoading++;
-      const data = this.waitQueue.splice(0, 1)[0];
-
-      this.getSpriteSheetForAppUrlInternal([ data.url, data.opts ]).then((spritesheet) => {
-        this.currentlyLoading--;
-
-        this.spritesheetCache.set(data.url, spritesheet);
-        data.resolve(spritesheet);
-
-        this.loadSpriteSheetFromQueue();
+    if (!spritesheet) {
+      spritesheet = await new Promise(async (resolve) => {
+        this.loadManager.putInQueue(() => this.getSpriteSheetForAppUrlInternal([ appUrl, opts ]).then(resolve))
       });
+
+      this.spritesheetCache.set(appUrl, spritesheet);
     }
+
+    return spritesheet;
   }
 }
 const spritesheetManager = new SpritesheetManager();
