0178ccb94aa910eb354e80c0f64a0457361cd29e
M̵̞̗̝̼̅̏̎͝Ȯ̴̝̻̊̃̋̀Õ̷̼͋N̸̩̿͜ ̶̜̠̹̼̩͒ <shawmakesmagic@gmail.com>
2022-09-10 13:52:24 -0700

    Update app-manager.js

--- app-manager.js
+++ app-manager.js
@@ -304,9 +304,9 @@ class AppManager extends EventTarget {
       const {instanceId, app} = e.data;
       
       this.unbindTrackedApp(instanceId);
-      if(app){
-      this.removeApp(app);
-      app.destroy();
+      if (app){
+        this.removeApp(app);
+        app.destroy();
       }
     });
     this.addEventListener('trackedappimport', async e => {
