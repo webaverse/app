2088f0f60bf9259d97376a86a5032fe1e1693090
Avaer Kazmer <a@modules.io>
2022-09-18 19:11:10 -0400

    Update object spriter to new spriting infrastructure

--- object-spriter.js
+++ object-spriter.js
@@ -10,7 +10,8 @@ const localColor = new THREE.Color();
 const localMatrix = new THREE.Matrix4();
 
 const defaultSize = 2048;
-const defaultNumFrames = 128;
+const defaultNumAnimationFrames = 128;
+const defaultNumSpritesheetFrames = 8;
 
 const _addPreviewLights = scene => {
   const ambientLight = new THREE.AmbientLight(0xffffff, 2);
@@ -37,7 +38,7 @@ const _makeSpritesheetRenderTarget = (w, h) => new THREE.WebGLRenderTarget(w, h,
 const createObjectSpriteAnimation = (app, {
   // canvas,
   size = defaultSize,
-  numFrames = defaultNumFrames,
+  numFrames = defaultNumAnimationFrames,
 } = {}, {
   type = 'texture',
 } = {}) => {
@@ -98,19 +99,19 @@ const createObjectSpriteAnimation = (app, {
         const physicsObject = physicsObjects[0];
         const {physicsMesh} = physicsObject;
         sideCamera.position.copy(physicsMesh.geometry.boundingBox.getCenter(localVector3))
-        .add(
-          localVector.set(Math.cos(angle), 0, Math.sin(angle))
-            .applyQuaternion(app.quaternion)
-            .multiplyScalar(2)
-        );
+          .add(
+            localVector.set(Math.cos(angle), 0, Math.sin(angle))
+              .applyQuaternion(app.quaternion)
+              .multiplyScalar(2)
+          );
         fitCameraToBoundingBox(sideCamera, physicsMesh.geometry.boundingBox, 1.2);
       } else {
         sideCamera.position.copy(app.position)
-        .add(
-          localVector.set(Math.cos(angle), 0, Math.sin(angle))
-            .applyQuaternion(app.quaternion)
-            .multiplyScalar(2)
-        );
+          .add(
+            localVector.set(Math.cos(angle), 0, Math.sin(angle))
+              .applyQuaternion(app.quaternion)
+              .multiplyScalar(2)
+          );
         sideCamera.quaternion.setFromRotationMatrix(
           localMatrix.lookAt(
             sideCamera.position,
@@ -164,7 +165,114 @@ const createObjectSpriteAnimation = (app, {
     throw new Error('Unknown type');
   }
 };
+const createObjectSpriteSheet = async (app, {
+  size = defaultSize,
+  numFrames = defaultNumSpritesheetFrames,
+} = {}) => {
+  const renderer = getRenderer();
+  const pixelRatio = renderer.getPixelRatio();
+
+  const numFramesPow2 = Math.pow(2, Math.ceil(Math.log2(numFrames)));
+  const numFramesPerRow = Math.ceil(Math.sqrt(numFramesPow2));
+  const frameSize = size / numFramesPerRow;
+
+  // create render target
+  const requiredWidth = frameSize * numFramesPerRow * pixelRatio;
+  const requiredHeight = frameSize * numFramesPerRow * pixelRatio;
+  if (requiredWidth > renderer.domElement.width || requiredHeight > renderer.domElement.height) {
+    renderer.setSize(requiredWidth / pixelRatio, requiredHeight / pixelRatio);
+    renderer.setPixelRatio(pixelRatio);
+  }
+
+  // push old state
+  const oldViewport = renderer.getViewport(localVector4D);
+  const oldClearColor = renderer.getClearColor(localColor);
+  const oldClearAlpha = renderer.getClearAlpha();
+  
+  const originalParent = app.parent;
+  sideScene.add(app);
+  sideScene.updateMatrixWorld();
+
+  // clear
+  renderer.setClearColor(0xffffff, 0);
+  renderer.clear();
+
+  // get size
+  const physicsObjects = app.getPhysicsObjects();
+  let worldWidth, worldHeight;
+  if (physicsObjects.length > 0) {
+    const physicsObject = physicsObjects[0];
+    const {physicsMesh} = physicsObject;
+    const size = physicsMesh.geometry.boundingBox.getCenter(localVector);
+    worldWidth = Math.max(size.x, size.z);
+    worldHeight = size.y;
+  } else {
+    worldWidth = 1;
+    worldHeight = 1;
+  }
+
+  // render
+  for (let i = 0; i < numFrames; i++) {
+    const y = Math.floor(i / numFramesPerRow);
+    const x = i - y * numFramesPerRow;
+  
+    // set up side camera
+    const angle = (i / numFrames) * Math.PI * 2;
+    if (physicsObjects.length > 0) {
+      const physicsObject = physicsObjects[0];
+      const {physicsMesh} = physicsObject;
+      sideCamera.position.copy(physicsMesh.geometry.boundingBox.getCenter(localVector3))
+        .add(
+          localVector.set(Math.cos(angle), 0, Math.sin(angle))
+            .applyQuaternion(app.quaternion)
+            .multiplyScalar(2)
+        );
+      fitCameraToBoundingBox(sideCamera, physicsMesh.geometry.boundingBox, 1.2);
+    } else {
+      sideCamera.position.copy(app.position)
+        .add(
+          localVector.set(Math.cos(angle), 0, Math.sin(angle))
+            .applyQuaternion(app.quaternion)
+            .multiplyScalar(2)
+        );
+      sideCamera.quaternion.setFromRotationMatrix(
+        localMatrix.lookAt(
+          sideCamera.position,
+          app.position,
+          localVector2.set(0, 1, 0)
+        )
+      );
+    }
+    sideCamera.updateMatrixWorld();
+    
+    // render side scene
+    renderer.setViewport(x*frameSize, y*frameSize, frameSize, frameSize);
+    renderer.render(sideScene, sideCamera);
+  }
+
+  // pop old state
+  if (originalParent) {
+    originalParent.add(app);
+  } else {
+    sideScene.remove(app);
+  }
+  renderer.setViewport(oldViewport);
+  renderer.setClearColor(oldClearColor, oldClearAlpha);
+
+  const imageBitmap = await createImageBitmap(renderer.domElement, 0, 0, size, size, {
+    // imageOrientation: 'flipY',
+  });
+  return {
+    result: imageBitmap,
+    numFrames,
+    frameSize,
+    numFramesPerRow,
+    worldWidth,
+    worldHeight,
+  };
+};
 
 export {
   createObjectSpriteAnimation,
+  createObjectSpriteSheet,
 };
