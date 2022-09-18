7667c91147eeb4bebf320b7439eb17f56e27c5b4
Avaer Kazmer <a@modules.io>
2022-09-18 19:10:47 -0400

    Add new spriting.js

---
+++ spriting.js
@@ -0,0 +1,35 @@
+import offscreenEngineManager from './offscreen-engine-manager.js';
+
+let getSpriteSheetForAppUrlInternal = null;
+const createAppUrlSpriteSheet = async (appUrl, opts) => {
+  if (!getSpriteSheetForAppUrlInternal) {
+    getSpriteSheetForAppUrlInternal = offscreenEngineManager.createFunction([
+      `\
+      import {createObjectSpriteSheet} from './object-spriter.js';
+      import metaversefile from './metaversefile-api.js';
+      import physx from './physx.js';
+      `,
+      async function(appUrl, opts) {
+        await physx.waitForLoad();
+        
+        const app = await metaversefile.createAppAsync({
+          start_url: appUrl,
+          components: [
+            {
+              key: 'physics',
+              value: true,
+            },
+          ],
+        });
+        const spritesheet = await createObjectSpriteSheet(app, opts);
+        return spritesheet;
+      }
+    ]);
+  }
+  const result = await getSpriteSheetForAppUrlInternal([appUrl, opts]);
+  return result;
+};
+
+export {
+  createAppUrlSpriteSheet,
+};
