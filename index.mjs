f165a7bf2b8d758093bc339f4e69dfe139622c1b
shawticus <shawmakesmagic@gmail.com>
2022-09-25 19:15:35 -0700

    build is building, but issues running

--- index.mjs
+++ index.mjs
@@ -1,19 +1,24 @@
+/* eslint-disable node/no-deprecated-api */
+/* eslint-disable camelcase */
+/* eslint-disable promise/param-names */
 import http from 'http';
 import https from 'https';
 import url from 'url';
 import path from 'path';
 import fs from 'fs';
 import express from 'express';
-import {createServer} from 'vite';
+import { createServer } from 'vite';
 import wsrtc from 'wsrtc/wsrtc-server.mjs';
-
-const SERVER_ADDR = '0.0.0.0';
-const SERVER_NAME = 'local.webaverse.com';
+import metaversefile from 'metaversefile/plugins/rollup.js';
+import glob from 'glob';
 
 Error.stackTraceLimit = 300;
 const cwd = process.cwd();
 
 const isProduction = process.argv[2] === '-p';
+process.env.MODULE_URL = process.env.MODULE_URL || 'https://local.webaverse.com/';
+process.env.NODE_ENV = isProduction ? 'production' : process.env.NODE_ENV;
+const totum = metaversefile();
 
 const _isMediaType = p => /\.(?:png|jpe?g|gif|svg|glb|mp3|wav|webm|mp4|mov)$/.test(p);
 
@@ -39,7 +44,36 @@ function makeId(length) {
   return result;
 }
 
-/* const _proxyUrl = (req, res, u) => {
+async function dynamicImporter(o, req, res, next) {
+  try {
+    const loadUrl = o.pathname.slice(o.pathname.lastIndexOf('/@import'), o.pathname.length).replace('/@import', '');
+    const fullUrl = req.protocol + '://' + req.get('host') + loadUrl;
+    const reqURL = new URL(fullUrl);
+
+    /** Check intiator */
+    if (req.headers['sec-fetch-dest'] === 'script') {
+      let id = await totum.resolveId(loadUrl, reqURL.href);
+      if (!id) {
+        res.status(500);
+        return res.end('Failed to load');
+      }
+
+      id = id.replace('/@proxy/', '');
+      const { code, map } = await totum.load(id);
+      res.writeHead(200, { 'Content-Type': 'application/javascript' });
+      res.end(code);
+    } else {
+      req.originalUrl = loadUrl;
+      return /^\/(?:@proxy)\//.test(req.originalUrl) ? proxyReq(loadUrl, res) : res.redirect(req.originalUrl);
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).end(e.stack);
+  }
+}
+
+function proxyReq(u, res) {
+  u = u.replace(/^\/@proxy\//, '');
   const proxyReq = /https/.test(u) ? https.request(u) : http.request(u);
   proxyReq.on('response', proxyRes => {
     for (const header in proxyRes.headers) {
@@ -54,7 +88,24 @@ function makeId(length) {
     res.end();
   });
   proxyReq.end();
-}; */
+}
+
+const _proxyUrl = (req, res, u) => {
+  const proxyReq = /https/.test(u) ? https.request(u) : http.request(u);
+  proxyReq.on('response', proxyRes => {
+    for (const header in proxyRes.headers) {
+      res.setHeader(header, proxyRes.headers[header]);
+    }
+    res.statusCode = proxyRes.statusCode;
+    proxyRes.pipe(res);
+  });
+  proxyReq.on('error', err => {
+    console.error(err);
+    res.statusCode = 500;
+    res.end();
+  });
+  proxyReq.end();
+};
 
 (async () => {
   const app = express();
@@ -63,32 +114,21 @@ function makeId(length) {
     res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
     res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
 
-    // eslint-disable-next-line n/no-deprecated-api
     const o = url.parse(req.originalUrl, true);
-    // url.parse is deprecated. rewrite the line above using the new syntax
+
+    /** Replace any double / caused due to both proxy & import */
+    o.pathname = o.pathname.replace(/(?<!(http:|https:))\/\//g, '/');
+
     if (/^\/(?:@proxy|public)\//.test(o.pathname) && o.query.import === undefined) {
       const u = o.pathname
         .replace(/^\/@proxy\//, '')
         .replace(/^\/public/, '')
         .replace(/^(https?:\/(?!\/))/, '$1/');
-      if (_isMediaType(o.pathname)) {
-        const proxyReq = /https/.test(u) ? https.request(u) : http.request(u);
-        proxyReq.on('response', proxyRes => {
-          for (const header in proxyRes.headers) {
-            res.setHeader(header, proxyRes.headers[header]);
-          }
-          res.statusCode = proxyRes.statusCode;
-          proxyRes.pipe(res);
-        });
-        proxyReq.on('error', err => {
-          console.error(err);
-          res.statusCode = 500;
-          res.end();
-        });
-        proxyReq.end();
+      if (_isMediaType(o.pathname) && !/^\/(?:@proxy)\//.test(o.pathname)) {
+        proxyReq(u, res);
       } else {
         req.originalUrl = u;
-        next();
+        isProduction ? dynamicImporter(o, req, res, next) : next();
       }
     } else if (o.query.noimport !== undefined) {
       const p = path.join(cwd, path.resolve(o.pathname));
@@ -109,7 +149,8 @@ function makeId(length) {
       req.originalUrl = req.originalUrl.replace(/^\/(login)/, '/');
       return res.redirect(req.originalUrl);
     } else {
-      next();
+      isProduction && /^\/@import/.test(o.pathname)
+        ? dynamicImporter(o, req, res, next) : next();
     }
   });
 
@@ -119,26 +160,52 @@ function makeId(length) {
 
   const _makeHttpServer = () => isHttps ? https.createServer(certs, app) : http.createServer(app);
   const httpServer = _makeHttpServer();
-  const viteServer = await createServer({
-    server: {
-      middlewareMode: true,
-      force: true,
-      hmr: {
-        server: httpServer,
-        port,
-        overlay: false,
+
+  if (!isProduction) {
+    const viteServer = await createServer({
+      server: {
+        middlewareMode: true,
+        force: true,
+        hmr: {
+          server: httpServer,
+          port,
+          overlay: false,
+        },
       },
-    },
-  });
-  app.use(viteServer.middlewares);
+    });
+    app.use(viteServer.middlewares);
+  } else if (isProduction) {
+    /** Setup static assets */
+    if (isProduction) {
+      app.use(express.static('dist'));
+      app.use(express.static('dist/assets'));
+
+      app.use('*', (req, res) => {
+        const o = url.parse(req.originalUrl, true);
+
+        const fileName = path.parse(o.pathname).base;
+
+        glob(`dist/**/${fileName}`, (err, files) => {
+          const _404 = err || files.length === 0;
+          if (files.length > 0) {
+            return res.sendFile(path.resolve('.', files[0]));
+          } else if (_404) {
+            res.status(404).end();
+          }
+        });
+      });
+
+      app.enable('view cache');
+    }
+  }
 
   await new Promise((accept, reject) => {
-    httpServer.listen(port, SERVER_ADDR, () => {
+    httpServer.listen(port, '0.0.0.0', () => {
       accept();
     });
     httpServer.on('error', reject);
   });
-  console.log(`  > Local: http${isHttps ? 's' : ''}://${SERVER_NAME}:${port}/`);
+  console.log(`  > Local: http${isHttps ? 's' : ''}://localhost:${port}/`);
 
   const wsServer = (() => {
     if (isHttps) {
@@ -148,18 +215,16 @@ function makeId(length) {
     }
   })();
   const initialRoomState = (() => {
-    const s = fs.readFileSync('./scenes/prototype.scn', 'utf8');
+    const s = fs.readFileSync(`./${isProduction ? 'dist/' : ''}scenes/gunroom.scn`, 'utf8');
     const j = JSON.parse(s);
-    const {objects} = j;
+    const { objects } = j;
 
     const appsMapName = 'apps';
     const result = {
       [appsMapName]: [],
     };
     for (const object of objects) {
-      let {start_url, type, content, position = [0, 0, 0], quaternion = [0, 0, 0, 1], scale = [1, 1, 1]} = object;
-
-      const transform = Float32Array.from([...position, ...quaternion, ...scale]);
+      let { start_url, type, content, position = [0, 0, 0], quaternion = [0, 0, 0, 1], scale = [1, 1, 1] } = object;
       const instanceId = makeId(5);
       if (!start_url && type && content) {
         start_url = `data:${type},${encodeURI(JSON.stringify(content))}`;
@@ -167,23 +232,27 @@ function makeId(length) {
       const appObject = {
         instanceId,
         contentId: start_url,
-        transform,
+        position,
+        quaternion,
+        scale,
         components: JSON.stringify([]),
       };
       result[appsMapName].push(appObject);
     }
     return result;
   })();
-  const initialRoomNames = [];
+  const initialRoomNames = [
+    'Erithor',
+  ];
   wsrtc.bindServer(wsServer, {
     initialRoomState,
     initialRoomNames,
   });
   await new Promise((accept, reject) => {
-    wsServer.listen(wsPort, SERVER_ADDR, () => {
+    wsServer.listen(wsPort, '0.0.0.0', () => {
       accept();
     });
     wsServer.on('error', reject);
   });
-  console.log(`  > World: ws${isHttps ? 's' : ''}://${SERVER_NAME}:${wsPort}/`);
+  console.log(`  > World: ws${isHttps ? 's' : ''}://localhost:${wsPort}/`);
 })();
